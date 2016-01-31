3b635f46c5273a6bac20515e609e34e038a77373
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-01-31 15:30:07 -0800

    Forgot to add in install file.

---
+++ devtools/travis-ci/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+wget http://repo.continuum.io/archive/Anaconda2-2.4.1-Linux-x86_64.sh -O anaconda.sh;
+bash anaconda.sh -b -p $HOME/anaconda
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda install -c omnia rdkit
+conda install -c omnia openbabel
+conda install joblib
+conda install -c omnia theano
+conda install -c omnia keras
+python setup.py install
