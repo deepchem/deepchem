2387eafb1a81c1a6a1bf6c280816acb4938f0077
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-01-31 15:34:18 -0800

    Trying to debug import failures.

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -3,8 +3,10 @@ wget http://repo.continuum.io/archive/Anaconda2-2.4.1-Linux-x86_64.sh -O anacond
 bash anaconda.sh -b -p $HOME/anaconda
 hash -r
 conda config --set always_yes yes --set changeps1 no
+conda config --add channels http://conda.binstar.org/omnia
 conda update -q conda
 conda info -a
+conda install pandas
 conda install -c omnia rdkit
 conda install -c omnia openbabel
 conda install joblib
