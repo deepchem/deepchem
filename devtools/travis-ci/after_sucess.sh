7caaa098eb6b24d7b8d367aeabcd38f170e9dc0e
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-01-31 18:36:13 -0800

    uncommenting PR check

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -1,11 +1,9 @@
 echo '$TRAVIS_PULL_REQUEST $TRAVIS_BRANCH'
 echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
 
-## TODO(rbharath): DO NOT MERGE WITH THIS COMMENTED! Only commented to
-## facilitate debugging of AWS docs push.
-#if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-#    echo "This is a pull request. No deployment will be done."; exit 0
-#fi
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    echo "This is a pull request. No deployment will be done."; exit 0
+fi
 
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
