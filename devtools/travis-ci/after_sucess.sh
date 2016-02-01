0366465662610a47f16020daf98c1ddf623dbb20
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-01-31 17:26:47 -0800

    Adding s3cmd install back in.

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -1,5 +1,5 @@
-echo '$TRAVIS_PULL_REQUEST $TRAVIS_BRANCH $python'
-echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH $python
+echo '$TRAVIS_PULL_REQUEST $TRAVIS_BRANCH'
+echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
 
 ## TODO(rbharath): DO NOT MERGE WITH THIS COMMENTED! Only commented to
 ## facilitate debugging of AWS docs push.
@@ -11,11 +11,11 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
 fi
 
-
 # Create the docs and push them to S3
 # -----------------------------------
-echo "pwd"
-pwd
+echo "About to install numpydoc, s3cmd"
+pip install numpydoc s3cmd
+conda list -e
 mkdir -p docs/_build
 echo "About to build docs"
 sphinx build -b html docs docs/_build
