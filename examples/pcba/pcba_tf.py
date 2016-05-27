ab98fe22cb1580916c2cf78ae6bab513caba89d3
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-05-27 11:45:47 -0700

    PCBA TF Example

--- examples/pcba/pcba_tf.py
+++ examples/pcba/pcba_tf.py
@@ -1,5 +1,5 @@
 """
-Script that trains TF multitask models on MUV dataset.
+Script that trains Tensorflow multitask models on PCBA dataset.
 """
 from __future__ import print_function
 from __future__ import division
@@ -8,14 +8,13 @@ from __future__ import unicode_literals
 import os
 import numpy as np
 import shutil
-from sklearn.linear_model import LogisticRegression
 from deepchem.utils.save import load_from_disk
 from deepchem.datasets import Dataset
 from deepchem import metrics
 from deepchem.metrics import Metric
 from deepchem.metrics import to_one_hot
 from deepchem.utils.evaluate import Evaluator
-from deepchem.datasets.muv_datasets import load_muv
+from deepchem.datasets.pcba_datasets import load_pcba
 from deepchem.models.tensorflow_models.fcnet import TensorflowMultiTaskClassifier
 from deepchem.models.tensorflow_models import TensorflowModel
 
@@ -26,16 +25,15 @@ np.random.seed(123)
 
 reload = True
 verbosity = "high"
-model = "logistic"
 
-base_data_dir = "/scratch/users/rbharath/muv"
+base_data_dir = "/scratch/users/rbharath/pcba"
 
-muv_tasks, featurized_samples, dataset, transformers = load_muv(
+pcba_tasks, featurized_samples, dataset, transformers = load_pcba(
     base_data_dir, reload=reload)
 print("len(featurized_samples), len(dataset)")
 print(len(featurized_samples), len(dataset))
 
-base_dir = "/scratch/users/rbharath/muv_analysis"
+base_dir = "/scratch/users/rbharath/pcba_analysis"
 if os.path.exists(base_dir):
   shutil.rmtree(base_dir)
 if not os.path.exists(base_dir):
@@ -48,10 +46,10 @@ model_dir = os.path.join(base_dir, "model")
 print("About to perform train/valid/test split.")
 num_train = .8 * len(featurized_samples)
 X, y, w, ids = dataset.to_numpy()
-num_tasks = 17
-muv_tasks = muv_tasks[:num_tasks]
+num_tasks = 120
+pcba_tasks = pcba_tasks[:num_tasks]
 print("Using following tasks")
-print(muv_tasks)
+print(pcba_tasks)
 X_train, X_valid = X[:num_train], X[num_train:]
 y_train, y_valid = y[:num_train, :num_tasks], y[num_train:, :num_tasks]
 w_train, w_valid = w[:num_train, :num_tasks], w[num_train:, :num_tasks]
@@ -60,15 +58,15 @@ ids_train, ids_valid = ids[:num_train], ids[num_train:]
 if os.path.exists(train_dir):
   shutil.rmtree(train_dir)
 train_dataset = Dataset.from_numpy(train_dir, X_train, y_train,
-                                   w_train, ids_train, muv_tasks)
+                                   w_train, ids_train, pcba_tasks)
 
 if os.path.exists(valid_dir):
   shutil.rmtree(valid_dir)
 valid_dataset = Dataset.from_numpy(valid_dir, X_valid, y_valid,
-                                   w_valid, ids_valid, muv_tasks)
+                                   w_valid, ids_valid, pcba_tasks)
 
 # Fit Logistic Regression models
-muv_task_types = {task: "classification" for task in muv_tasks}
+pcba_task_types = {task: "classification" for task in pcba_tasks}
 
 
 classification_metric = Metric(metrics.roc_auc_score, np.mean,
@@ -82,7 +80,7 @@ params_dict = {
     "weight_init_stddevs": [.1],
     "bias_init_consts": [1.],
     "dropouts": [.25],
-    "num_classification_tasks": len(muv_tasks),
+    "num_classification_tasks": len(pcba_tasks),
     "num_classes": 2,
     "penalty": .0,
     "optimizer": "momentum",
@@ -90,11 +88,11 @@ params_dict = {
     "momentum": .9,
 }   
 
+
 if os.path.exists(model_dir):
   shutil.rmtree(model_dir)
 os.makedirs(model_dir)
-
-model = TensorflowModel(muv_tasks, muv_task_types, params_dict, model_dir,
+model = TensorflowModel(pcba_tasks, pcba_task_types, params_dict, model_dir,
                         tf_class=TensorflowMultiTaskClassifier,
                         verbosity=verbosity)
 
