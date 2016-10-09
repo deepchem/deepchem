035d8d4efe832f05cd9caa070f69807582517091
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2016-10-08 17:57:24 -0700

    Attempting to move docs generation onto travis

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -12,10 +12,18 @@ fi
 # Create the docs and push them to S3
 # -----------------------------------
 echo "About to install numpydoc, s3cmd"
-pip install numpydoc s3cmd msmb_theme
+pip install numpydoc s3cmd msmb_theme sphinx_rtd_theme
 conda list -e
 mkdir -p docs/_build
 echo "About to build docs"
-sphinx-build -b html docs docs/_build
+sphinx-apidoc -f -o docs/source deepchem
+sphinx-build -b html docs/source docs/_build
+#sphinx-build -b html docs docs/_build
 echo "About to push docs to s3"
+#source ~/.deeprc
+#which python
+#which sphinx-build
+#~/anaconda/envs/deepchem/bin/python devtools/travis-ci/push-docs-to-s3.py
+#bash ../../deepchem-docs-sh
+#s3cmd -M -H --config ~/.s3cfg sync docs/_build/ s3://deepchem.io
 python devtools/travis-ci/push-docs-to-s3.py
