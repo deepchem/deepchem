e4f22568665140474c99b2771c1ba683b6ca3ca5
Aneesh Pappu <apappu97@gmail.com>
2016-11-01 02:55:59 -0700

    sider keras code

--- examples/sider/sider_datasets.py
+++ examples/sider/sider_datasets.py
@@ -1,22 +1,23 @@
 """
-Sider dataset loader.
-@Author Bharath Ramsundar, Aneesh Pappu
+SIDER dataset loader.
 """
 from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
 import os
+import numpy as np
 import shutil
 from deepchem.utils.save import load_from_disk
-from deepchem.datasets import Dataset
+from deepchem.datasets import DiskDataset
 from deepchem.featurizers.featurize import DataLoader
 from deepchem.featurizers.fingerprints import CircularFingerprint
 from deepchem.transformers import BalancingTransformer
 
-def load_sider(base_dir, reload=True):
-  """Load Sider datasets. Does not do train/test split"""
+def load_sider(base_dir, reload=True, frac_train=.8):
+  """Load SIDER datasets. Does not do train/test split"""
   # Set some global variables up top
+  reload = True
   verbosity = "high"
   model = "logistic"
   regen = False
@@ -29,39 +30,56 @@ def load_sider(base_dir, reload=True):
   if not os.path.exists(base_dir):
     os.makedirs(base_dir)
   current_dir = os.path.dirname(os.path.realpath(__file__))
-  # Make directories to store the raw and featurized datasets.
+  #Make directories to store the raw and featurized datasets.
   data_dir = os.path.join(base_dir, "dataset")
+  train_dir = os.path.join(base_dir, "train_dataset")
+  valid_dir = os.path.join(base_dir, "valid_dataset")
 
-  # Load sider dataset
-  print("About to load sider dataset.")
+  # Load SIDER dataset
+  print("About to load SIDER dataset.")
   dataset_file = os.path.join(
-    current_dir, "./sider.csv.gz")
+      current_dir, "./sider.csv.gz")
   dataset = load_from_disk(dataset_file)
   print("Columns of dataset: %s" % str(dataset.columns.values))
   print("Number of examples in dataset: %s" % str(dataset.shape[0]))
 
-  # Featurize sider dataset
-  print("About to featurize siderdataset.")
-
+  # Featurize SIDER dataset
+  print("About to featurize SIDER dataset.")
   featurizer = CircularFingerprint(size=1024)
-  all_sider_tasks = dataset.columns.values[1:].tolist()
+  SIDER_tasks = dataset.columns.values[1:].tolist()
 
+  loader = DataLoader(tasks=SIDER_tasks,
+                      smiles_field="smiles",
+                      featurizer=featurizer,
+                      verbosity=verbosity)
   if not reload or not os.path.exists(data_dir):
-    loader = DataLoader(tasks=all_sider_tasks,
-                        smiles_field="smiles",
-                        featurizer=featurizer,
-                        verbosity=verbosity)
-    dataset = loader.featurize(
-      dataset_file, data_dir, shard_size=8192)
+    dataset = loader.featurize(dataset_file, data_dir)
+    regen = True
   else:
-    dataset = Dataset(data_dir, all_sider_tasks, reload=True)
+    dataset = DiskDataset(data_dir, reload=True)
 
   # Initialize transformers
   transformers = [
-    BalancingTransformer(transform_w=True, dataset=dataset)]
+      BalancingTransformer(transform_w=True, dataset=dataset)]
   if regen:
     print("About to transform data")
     for transformer in transformers:
-      transformer.transform(dataset)
+        dataset = transformer.transform(dataset)
 
-  return all_sider_tasks, dataset, transformers
+  X, y, w, ids = (dataset.X, dataset.y, dataset.w, dataset.ids)
+  num_tasks = 17
+  num_train = frac_train * len(dataset)
+  SIDER_tasks = SIDER_tasks[:num_tasks]
+  print("Using following tasks")
+  print(SIDER_tasks)
+  X_train, X_valid = X[:num_train], X[num_train:]
+  y_train, y_valid = y[:num_train, :num_tasks], y[num_train:, :num_tasks]
+  w_train, w_valid = w[:num_train, :num_tasks], w[num_train:, :num_tasks]
+  ids_train, ids_valid = ids[:num_train], ids[num_train:]
+
+  train_dataset = DiskDataset.from_numpy(train_dir, X_train, y_train,
+                                     w_train, ids_train, SIDER_tasks)
+  valid_dataset = DiskDataset.from_numpy(valid_dir, X_valid, y_valid,
+                                     w_valid, ids_valid, SIDER_tasks)
+
+  return SIDER_tasks, (train_dataset, valid_dataset), transformers
