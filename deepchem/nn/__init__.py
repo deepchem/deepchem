836709e23ce8db23bc456f10d39d8169ff43d376
Han <han.altaetran@gmail.com>
2016-11-05 17:16:02 -0700

    some final tweaks for merging

--- deepchem/nn/__init__.py
+++ deepchem/nn/__init__.py
@@ -23,5 +23,6 @@ from deepchem.models.tensorflow_models.model_ops import multitask_logits
 from deepchem.models.tensorflow_models.model_ops import softmax_N 
 
 from deepchem.models.tf_keras_models.keras_layers import AttnLSTMEmbedding
+from deepchem.models.tf_keras_models.keras_layers import MNAttnLSTMEmbedding
 from deepchem.models.tf_keras_models.keras_layers import ResiLSTMEmbedding
 from deepchem.models.tf_keras_models.keras_layers import DualAttnLSTMEmbedding
