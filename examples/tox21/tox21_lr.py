9ec8ff3de2891ef8a4ddcccd46e0ca76957bbbc9
ZHENQIN WU <zqwu@sherlock-ln02.stanford.edu>
2016-11-09 23:08:27 -0800

    merged with new master

---
+++ examples/tox21/tox21_lr.py
@@ -0,0 +1,43 @@
+"""
+Script that trains multitask models on Tox21 dataset.
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+import os
+import shutil
+import numpy as np
+import deepchem as dc
+from tox21_datasets import load_tox21
+
+# Only for debug!
+np.random.seed(123)
+
+# Load Tox21 dataset
+n_features = 1024
+tox21_tasks, tox21_datasets, transformers = load_tox21()
+train_dataset, valid_dataset, test_dataset = tox21_datasets
+
+# Fit models
+metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean,
+                           mode="classification")
+
+tensorflow_model = dc.models.TensorflowLogisticRegression(
+    len(tox21_tasks), n_features, learning_rate=0.006, penalty = 0.05,
+    weight_init_stddevs=[0.002],batch_size=32, verbosity="high")
+model = dc.models.TensorflowModel(tensorflow_model)
+
+# Fit trained model
+model.fit(train_dataset,nb_epoch = 50)
+model.save()
+
+print("Evaluating model")
+train_scores = model.evaluate(train_dataset, [metric], transformers)
+valid_scores = model.evaluate(valid_dataset, [metric], transformers)
+
+print("Train scores")
+print(train_scores)
+
+print("Validation scores")
+print(valid_scores)
