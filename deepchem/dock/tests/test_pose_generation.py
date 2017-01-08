ec672cafb63b22a0c9402a52e52653ee5699f6ef
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2017-01-07 16:53:59 -0800

    Turn down exhuastiveness and add on py3 guards

--- deepchem/dock/tests/test_pose_generation.py
+++ deepchem/dock/tests/test_pose_generation.py
@@ -9,9 +9,10 @@ __author__ = "Bharath Ramsundar"
 __copyright__ = "Copyright 2016, Stanford University"
 __license__ = "GPL"
 
+import os
+import sys
 import unittest
 import tempfile
-import os
 import shutil
 import numpy as np
 import deepchem as dc
@@ -24,7 +25,16 @@ class TestPoseGeneration(unittest.TestCase):
   def test_vina_initialization(self):
     """Test that VinaPoseGenerator can be initialized."""
     # Note this may download autodock Vina...
-    vpg = dc.dock.VinaPoseGenerator()
+    vpg = dc.dock.VinaPoseGenerator(
+        detect_pockets=False, exhaustiveness=1)
+
+  def test_pocket_vina_initialization(self):
+    """Test that VinaPoseGenerator can be initialized."""
+    # Note this may download autodock Vina...
+    if sys.version_info >= (3,0):
+      return
+    vpg = dc.dock.VinaPoseGenerator(
+        detect_pockets=True, exhaustiveness=1)
 
   def test_vina_poses(self):
     """Test that VinaPoseGenerator creates pose files."""
@@ -33,7 +43,26 @@ class TestPoseGeneration(unittest.TestCase):
     ligand_file = os.path.join(current_dir, "1jld_ligand.sdf")
   
     # Note this may download autodock Vina...
-    vpg = dc.dock.VinaPoseGenerator()
+    vpg = dc.dock.VinaPoseGenerator(
+        detect_pockets=False, exhaustiveness=1)
+    protein_pose_file, ligand_pose_file = vpg.generate_poses(
+        protein_file, ligand_file, out_dir="/tmp")
+
+    # Check returned files exist
+    assert os.path.exists(protein_pose_file)
+    assert os.path.exists(ligand_pose_file)
+
+  def test_pocket_vina_poses(self):
+    """Test that VinaPoseGenerator creates pose files."""
+    if sys.version_info >= (3,0):
+      return
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    protein_file = os.path.join(current_dir, "1jld_protein.pdb")
+    ligand_file = os.path.join(current_dir, "1jld_ligand.sdf")
+  
+    # Note this may download autodock Vina...
+    vpg = dc.dock.VinaPoseGenerator(
+        detect_pockets=True, exhaustiveness=1)
     protein_pose_file, ligand_pose_file = vpg.generate_poses(
         protein_file, ligand_file, out_dir="/tmp")
 
