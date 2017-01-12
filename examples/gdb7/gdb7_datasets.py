ccd934f12b7f866215b1e566dbb15d178e5182b0
miaecle <zqwu@stanford.edu>
2017-01-11 16:53:18 -0800

    little change

--- examples/gdb7/gdb7_datasets.py
+++ examples/gdb7/gdb7_datasets.py
@@ -10,7 +10,7 @@ import numpy as np
 import shutil
 import deepchem as dc
 
-def load_gdb7(featurizer=None, split='index'):
+def load_gdb7(featurizer=None, split='random'):
   """Load gdb7 datasets."""
   # Featurize gdb7 dataset
   print("About to featurize gdb7 dataset.")
@@ -25,7 +25,7 @@ def load_gdb7(featurizer=None, split='index'):
   loader = dc.data.SDFLoader(tasks=gdb7_tasks, smiles_field="smiles", 
                              mol_field="mol", featurizer=featurizer)
   dataset = loader.featurize(dataset_file)
-
+ 
   # Initialize transformers 
   transformers = [
       dc.trans.NormalizationTransformer(transform_X=True, dataset=dataset),
@@ -33,10 +33,11 @@ def load_gdb7(featurizer=None, split='index'):
 
   print("About to transform data")
   for transformer in transformers:
-      dataset = transformer.transform(dataset)
-
+    dataset = transformer.transform(dataset)
+  
   splitters = {'index': dc.splits.IndexSplitter(),
                'random': dc.splits.RandomSplitter()}
   splitter = splitters[split]
   train, valid, test = splitter.train_valid_test_split(dataset)
+ 
   return gdb7_tasks, (train, valid, test), transformers
