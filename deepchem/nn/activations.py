47057e8eab93eeb3d996d9e4b1c63fd4aaec48f5
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2017-01-13 12:01:16 -0800

    Starting on digesting keras code

--- deepchem/nn/activations.py
+++ deepchem/nn/activations.py
@@ -1,4 +1,5 @@
 #from __future__ import absolute_import
+import six
 from keras import backend as K
 
 def get_from_module(identifier, module_params, module_name,
@@ -24,26 +25,25 @@ def get_from_module(identifier, module_params, module_name,
     # Raises
         ValueError: if the identifier cannot be found.
     """
-    #if isinstance(identifier, str) or isinstance(identifier, unicode):
-    assert not isinstance(identifier, dict)
-    res = module_params.get(identifier)
-    if not res:
-        raise ValueError('Invalid ' + str(module_name) + ': ' +
-                         str(identifier))
-    if instantiate and not kwargs:
-        return res()
-    elif instantiate and kwargs:
-        return res(**kwargs)
-    else:
-        return res
-    #elif isinstance(identifier, dict):
-    #    name = identifier.pop('name')
-    #    res = module_params.get(name)
-    #    if res:
-    #        return res(**identifier)
-    #    else:
-    #        raise ValueError('Invalid ' + str(module_name) + ': ' +
-    #                         str(identifier))
+    if isinstance(identifier, six.string_types):
+      res = module_params.get(identifier)
+      if not res:
+          raise ValueError('Invalid ' + str(module_name) + ': ' +
+                           str(identifier))
+      if instantiate and not kwargs:
+          return res()
+      elif instantiate and kwargs:
+          return res(**kwargs)
+      else:
+          return res
+    elif isinstance(identifier, dict):
+        name = identifier.pop('name')
+        res = module_params.get(name)
+        if res:
+            return res(**identifier)
+        else:
+            raise ValueError('Invalid ' + str(module_name) + ': ' +
+                             str(identifier))
     return identifier
 
 def softmax(x):
