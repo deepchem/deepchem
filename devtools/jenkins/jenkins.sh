ca8af9f959b1c134c3ff8cd407e9d11f8c1aeccc
leswing <lilleswing@gmail.com>
2017-02-15 10:17:56 -0800

    Freeze Tensorflow

--- devtools/jenkins/jenkins.sh
+++ devtools/jenkins/jenkins.sh
@@ -11,7 +11,8 @@ conda install scikit-learn
 conda install setuptools
 conda install keras
 conda install -c conda-forge protobuf=3.1.0
-pip install tensorflow-gpu
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
+pip install --upgrade $TF_BINARY_URL
 pip install nose
 python setup.py install
 
