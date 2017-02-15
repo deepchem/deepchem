b554d41bca786f0b7ca0810bd6d80494b72f26ba
leswing <lilleswing@gmail.com>
2017-02-15 10:21:58 -0800

    GOGO

--- devtools/jenkins/jenkins.sh
+++ devtools/jenkins/jenkins.sh
@@ -12,7 +12,7 @@ conda install setuptools
 conda install keras
 conda install -c conda-forge protobuf=3.1.0
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip install $TF_BINARY_URL
 pip install nose
 python setup.py install
 
