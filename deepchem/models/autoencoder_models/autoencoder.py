451a37e136373ebeb162c0a73d8462621baa0ccb
Karl Leswing <lilleswing@gmail.com>
2017-02-21 08:36:51 -0800

    WIP

--- deepchem/models/autoencoder_models/autoencoder.py
+++ deepchem/models/autoencoder_models/autoencoder.py
@@ -8,46 +8,49 @@ from deepchem.models.autoencoder_models.model import MoleculeVAE
 import os
 
 
-
 class TensorflowMoleculeEncoder(Model):
 
-    def __init__(self, model_dir=None, verbose=True, charset=None, latent_rep_size=292):
-        """
+  def __init__(self,
+               model_dir=None,
+               verbose=True,
+               charset=None,
+               latent_rep_size=292):
+    """
         TODO(LESWING) Convert Dataset.[X,y,w] from the h5.py format.
         TODO(LESWING) default to a charset constructed via Zinc -- it should be a superset
         of other charsets
         :param model_dir:
         :param verbose:
-        :param charset:
+        :param charset: list of chars
         :param latent_rep_size:
         """
-        super(TensorflowMoleculeEncoder, self).__init__(model_dir=model_dir, verbose=verbose)
-        weights_file = os.path.join(model_dir, "model.h5")
-        if os.path.isfile(weights_file):
-            m = MoleculeVAE()
-            self.model = m.load(charset, weights_file, latent_rep_size=latent_rep_size)
-        else:
-            # TODO (LESWING) Lazy Load
-            raise ValueError("Model file %s doesn't exist" % weights_file)
+    super(TensorflowMoleculeEncoder, self).__init__(
+        model_dir=model_dir, verbose=verbose)
+    weights_file = os.path.join(model_dir, "model.h5")
+    if os.path.isfile(weights_file):
+      m = MoleculeVAE()
+      self.model = m.load(charset, weights_file,
+                          latent_rep_size=latent_rep_size)
+    else:
+      # TODO (LESWING) Lazy Load
+      raise ValueError("Model file %s doesn't exist" % weights_file)
 
-
-    def fit(self, dataset, nb_epoch=10, batch_size=50, **kwargs):
-        """
+  def fit(self, dataset, nb_epoch=10, batch_size=50, **kwargs):
+    """
         TODO(LESWING) Test
         """
-        self.model.autoencoder.fit(
-            dataset.X,
-            dataset.X,
-            shuffle=True,
-            nb_epoch=10,
-            batch_size=50,
-            callbacks=[],
-            validation_data=(dataset.y, dataset.y)
-        )
+    self.model.autoencoder.fit(
+        dataset.X,
+        dataset.X,
+        shuffle=True,
+        nb_epoch=10,
+        batch_size=50,
+        callbacks=[],
+        validation_data=(dataset.y, dataset.y))
 
-    def predict_on_batch(self, X):
-        """
+  def predict_on_batch(self, X):
+    """
         TODO(LESWING) Test
         """
-        x_latent = self.model.encoder.predict(X)
-        return x_latent
+    x_latent = self.model.encoder.predict(X)
+    return x_latent
--- deepchem/models/autoencoder_models/utils.py
+++ deepchem/models/autoencoder_models/utils.py
@@ -3,31 +3,36 @@ import pandas
 import h5py
 import numpy as np
 
+
 def one_hot_array(i, n):
-    return map(int, [ix == i for ix in xrange(n)])
+  return map(int, [ix == i for ix in xrange(n)])
+
 
 def one_hot_index(vec, charset):
-    return map(charset.index, vec)
+  return map(charset.index, vec)
+
 
 def from_one_hot_array(vec):
-    oh = np.where(vec == 1)
-    if oh[0].shape == (0, ):
-        return None
-    return int(oh[0][0])
+  oh = np.where(vec == 1)
+  if oh[0].shape == (0,):
+    return None
+  return int(oh[0][0])
+
 
 def decode_smiles_from_indexes(vec, charset):
-    return "".join(map(lambda x: charset[x], vec)).strip()
+  return "".join(map(lambda x: charset[x], vec)).strip()
 
-def load_dataset(filename, split = True):
-    h5f = h5py.File(filename, 'r')
-    if split:
-        data_train = h5f['data_train'][:]
-    else:
-        data_train = None
-    data_test = h5f['data_test'][:]
-    charset =  h5f['charset'][:]
-    h5f.close()
-    if split:
-        return (data_train, data_test, charset)
-    else:
-        return (data_test, charset)
+
+def load_dataset(filename, split=True):
+  h5f = h5py.File(filename, 'r')
+  if split:
+    data_train = h5f['data_train'][:]
+  else:
+    data_train = None
+  data_test = h5f['data_test'][:]
+  charset = h5f['charset'][:]
+  h5f.close()
+  if split:
+    return (data_train, data_test, charset)
+  else:
+    return (data_test, charset)
