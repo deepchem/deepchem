eda15246cd04ac357a37bb8ca926ff2305c6e256
leswing <Karl>
2017-03-23 18:23:36 -0700

    Raise Learning Rate for IRV

--- deepchem/models/tests/test_overfit.py
+++ deepchem/models/tests/test_overfit.py
@@ -439,7 +439,7 @@ class TestOverfit(test_util.TensorFlowTestCase):
     classification_metric = dc.metrics.Metric(
         dc.metrics.accuracy_score, task_averager=np.mean)
     model = dc.models.TensorflowMultiTaskIRVClassifier(
-        n_tasks, K=5, learning_rate=0.001, batch_size=n_samples)
+        n_tasks, K=5, learning_rate=0.01, batch_size=n_samples)
 
     # Fit trained model
     model.fit(dataset_trans)
