91489e4605f4a7e48a69dfd7575d5d5c31b67c48
miaecle <zqwu@stanford.edu>
2017-04-10 00:39:36 -0700

    recover change

--- examples/delaney/delaney_weave.py
+++ examples/delaney/delaney_weave.py
@@ -19,17 +19,22 @@ train_dataset, valid_dataset, test_dataset = delaney_datasets
 # Fit models
 metric = dc.metrics.Metric(dc.metrics.pearson_r2_score, np.mean)
 
+max_atoms_train = max([mol.get_num_atoms() for mol in train_dataset.X])
+max_atoms_valid = max([mol.get_num_atoms() for mol in valid_dataset.X])
+max_atoms_test = max([mol.get_num_atoms() for mol in test_dataset.X])
+max_atoms = max([max_atoms_train, max_atoms_valid, max_atoms_test])
+
 n_atom_feat = 75
 n_pair_feat = 14
 # Batch size of models
 batch_size = 64
 n_feat = 128
-graph = dc.nn.SequentialWeaveGraph(batch_size, 
-    n_atom_feat=n_atom_feat, n_pair_feat=n_pair_feat)
+graph = dc.nn.SequentialWeaveGraph(
+    max_atoms=max_atoms, n_atom_feat=n_atom_feat, n_pair_feat=n_pair_feat)
 
-graph.add(dc.nn.WeaveLayer(75, 14))
-#graph.add(dc.nn.WeaveLayer(50, 50))
-graph.add(dc.nn.Dense(n_feat, 50, activation='tanh'))
+graph.add(dc.nn.WeaveLayer(max_atoms, 75, 14))
+#graph.add(dc.nn.WeaveLayer(max_atoms, 50, 50))
+graph.add(dc.nn.WeaveConcat(batch_size, n_output=n_feat))
 graph.add(dc.nn.BatchNormalization(epsilon=1e-5, mode=1))
 graph.add(dc.nn.WeaveGather(batch_size, n_input=n_feat, gaussian_expand=True))
 
@@ -45,7 +50,7 @@ model = dc.models.MultitaskGraphRegressor(
     beta2=.999)
 
 # Fit trained model
-model.fit(train_dataset, nb_epoch=50, log_every_N_batches=5)
+model.fit(train_dataset, nb_epoch=50, log_every_N_batches=50)
 
 print("Evaluating model")
 train_scores = model.evaluate(train_dataset, [metric], transformers)
