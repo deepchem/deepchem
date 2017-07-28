c3cefe3b9fd430fedd648e1f2d6dd251b517ac23
galenxing <xing.galen@gmail.com>
2017-07-28 10:54:40 -0400

    yapf

--- deepchem/utils/evaluate.py
+++ deepchem/utils/evaluate.py
@@ -235,11 +235,11 @@ class GeneratorEvaluator(object):
     for metric in metrics:
       if per_task_metrics:
         multitask_scores[metric.name], computed_metrics = metric.compute_metric(
-            y, y_pred, w, per_task_metrics=True, n_classes = self.n_classes)
+            y, y_pred, w, per_task_metrics=True, n_classes=self.n_classes)
         all_task_scores[metric.name] = computed_metrics
       else:
         multitask_scores[metric.name] = metric.compute_metric(
-            y, y_pred, w, per_task_metrics=False, n_classes = self.n_classes)
+            y, y_pred, w, per_task_metrics=False, n_classes=self.n_classes)
 
     if not per_task_metrics:
       return multitask_scores
