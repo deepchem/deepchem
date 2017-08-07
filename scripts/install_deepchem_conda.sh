4f4a91d726f724cef6ce3095971d0d34b485a929
leswing <Karl>
2017-08-07 11:13:16 -0400

    less

--- scripts/install_deepchem_conda.sh
+++ scripts/install_deepchem_conda.sh
@@ -17,13 +17,6 @@ then
     export python_version=3.5
 fi
 
-if [ "$python_version" == "3.5" ]
-then
-    export protobuf_url=https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.1.0-cp35-none-linux_x86_64.whl
-else
-    export protobuf_url=https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.1.0-cp27-none-linux_x86_64.whl
-fi
-
 export envname=$1
 conda create -y --name $envname python=$python_version
 source activate $envname
@@ -35,8 +28,6 @@ conda install -y -c omnia mdtraj
 conda install -y scikit-learn=0.18.1
 conda install -y setuptools
 conda install -y -c conda-forge keras=1.2.2
-yes | pip install --upgrade $protobuf_url
-yes | pip install --upgrade $protobuf_url
 conda install -y -c anaconda networkx=1.11
 conda install -y -c conda-forge xgboost=0.6a2
 conda install -y -c anaconda pillow=4.2.1
