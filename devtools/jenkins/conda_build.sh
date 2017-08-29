3d5f7297e796385d90875bfc763118b317b5f056
leswing <Karl>
2017-08-29 13:38:17 -0400

    Fatfinger

--- devtools/jenkins/conda_build.sh
+++ devtools/jenkins/conda_build.sh
@@ -4,22 +4,22 @@ source activate root
 conda upgrade -y -q conda
 conda upgrade -y -q conda-build
 conda build purge
-#export package_name=deepchem
-#export python_version=2.7
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
-#export python_version=3.5
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
-#export python_version=3.6
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
-#
-#export package_name=deepchem-gpu
-#export tensorflow_enabled=tensorflow-gpu
-#export python_version=2.7
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
-#export python_version=3.5
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
-#export python_version=3.6
-#conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+export package_name=deepchem
+export python_version=2.7
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+export python_version=3.5
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+export python_version=3.6
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+
+export package_name=deepchem-gpu
+export tensorflow_enabled=tensorflow-gpu
+export python_version=2.7
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+export python_version=3.5
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
+export python_version=3.6
+conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
 
 export package_name=deepchem-deps
 export tensorflow_enabled=tensorflow
