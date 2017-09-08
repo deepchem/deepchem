5dde90896820026191f3f5bb2ca0227384d12f53
kmckiern <kmckiern@stanford.edu>
2017-09-07 19:17:08 -0700

    dependencies for dataset_preparation tutorial

--- devtools/jenkins/test_notebooks.sh
+++ devtools/jenkins/test_notebooks.sh
@@ -15,6 +15,9 @@ conda install -y ipywidgets
 conda install -y zlib
 conda install -y cmake
 yes | pip install gym[atari]
+conda install -y -c auto pubchempy
+conda install xlrd
+conda install seaborn
 
 cd examples/notebooks
 nosetests --with-timer tests.py --with-xunit --xunit-file=notebook_tests.xml|| true
