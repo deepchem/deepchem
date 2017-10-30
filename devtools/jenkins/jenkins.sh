b5fa4fbf38f97a6fb62266eff53db3110d8767cb
miaecle <zqwu@stanford.edu>
2017-10-30 13:30:18 -0700

    fix errors

--- devtools/jenkins/jenkins.sh
+++ devtools/jenkins/jenkins.sh
@@ -9,14 +9,14 @@ rm examples/results.csv || true
 export DEEPCHEM_DATA_DIR='/tmp/molnet_'$envname
 mkdir $DEEPCHEM_DATA_DIR
 cd examples
-#python benchmark.py -d tox21 -m weave -m graphconv -m tf_robust -m tf -m irv -m xgb -m logreg --seed 123
-#export retval_tox21=$?
-#python benchmark.py -d hiv -m graphconv -m tf -m irv -m logreg --seed 123
-#export retval_hiv=$?
+python benchmark.py -d tox21 -m weave -m graphconv -m tf_robust -m tf -m irv -m xgb -m logreg --seed 123
+export retval_tox21=$?
+python benchmark.py -d hiv -m graphconv -m tf -m irv -m logreg --seed 123
+export retval_hiv=$?
 python benchmark.py -d delaney -m weave_regression -m graphconvreg -m tf_regression -m dag_regression -m mpnn --seed 123
 export retval_delaney=$?
-#python benchmark.py -d qm7 -m dtnn -m graphconvreg -m tf_regression_ft --seed 123
-#export retval_qm7=$?
+python benchmark.py -d qm7 -m dtnn -m graphconvreg -m tf_regression_ft --seed 123
+export retval_qm7=$?
 
 cd ..
 nosetests -v devtools/jenkins/compare_results.py --with-xunit || true
