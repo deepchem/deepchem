c93c234209c0820f9528ea908c24f7a6d71564e6
Bharath Ramsundar <rbharath@stanford.edu>
2017-11-05 17:49:54 -0800

    Fixing some issues

--- deepchem/models/tensorgraph/tests/test_sequential.py
+++ deepchem/models/tensorgraph/tests/test_sequential.py
@@ -23,3 +23,16 @@ class TestSequential(unittest.TestCase):
     model.fit(dataset, loss="binary_crossentropy", nb_epoch=1000)
     prediction = np.squeeze(model.predict_on_batch(X))
     assert_true(np.all(np.isclose(prediction, y, atol=0.4)))
+
+  def test_fit_twice(self):
+    n_data_points = 20
+    n_features = 2
+    X = np.random.rand(n_data_points, n_features)
+    y = [[0, 1] for x in range(n_data_points)]
+    dataset = dc.data.NumpyDataset(X, y)
+    model = dc.models.Sequential(learning_rate=0.01)
+    model.add(Dense(out_channels=2))
+    model.add(SoftMax())
+    # Should be able to call fit twice without failure.
+    model.fit(dataset, loss="binary_crossentropy", nb_epoch=1000)
+    model.fit(dataset, loss="binary_crossentropy", nb_epoch=1000)
