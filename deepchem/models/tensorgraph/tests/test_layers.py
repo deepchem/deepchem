ec2c93c177d2197efe4340ae5c62895316523a00
peastman <peastman@stanford.edu>
2017-12-13 12:50:23 -0800

    Created LSTM layer

--- deepchem/models/tensorgraph/tests/test_layers.py
+++ deepchem/models/tensorgraph/tests/test_layers.py
@@ -30,6 +30,7 @@ from deepchem.models.tensorgraph.layers import InputFifoQueue
 from deepchem.models.tensorgraph.layers import InteratomicL2Distances
 from deepchem.models.tensorgraph.layers import IterRefLSTMEmbedding
 from deepchem.models.tensorgraph.layers import L2Loss
+from deepchem.models.tensorgraph.layers import LSTM
 from deepchem.models.tensorgraph.layers import LSTMStep
 from deepchem.models.tensorgraph.layers import Log
 from deepchem.models.tensorgraph.layers import Multiply
@@ -168,6 +169,21 @@ class TestLayers(test_util.TensorFlowTestCase):
     with self.test_session() as sess:
       in_tensor = tf.convert_to_tensor(in_tensor, dtype=tf.float32)
       out_tensor = GRU(n_hidden, batch_size)(in_tensor)
+      sess.run(tf.global_variables_initializer())
+      out_tensor = out_tensor.eval()
+      assert out_tensor.shape == (batch_size, n_steps, n_hidden)
+
+  def test_lstm(self):
+    """Test that LSTM can be invoked."""
+    batch_size = 10
+    n_hidden = 7
+    in_channels = 4
+    n_repeat = 2
+    n_steps = 6
+    in_tensor = np.random.rand(batch_size, n_steps, in_channels)
+    with self.test_session() as sess:
+      in_tensor = tf.convert_to_tensor(in_tensor, dtype=tf.float32)
+      out_tensor = LSTM(n_hidden, batch_size)(in_tensor)
       sess.run(tf.global_variables_initializer())
       out_tensor = out_tensor.eval()
       assert out_tensor.shape == (batch_size, n_steps, n_hidden)
