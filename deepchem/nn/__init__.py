9069db05fc9067a526974180b3a5b633f8b18b6d
Bharath Ramsundar <rbharath@stanford.edu>
2017-12-20 17:36:19 -0800

    Cleanup

--- deepchem/nn/__init__.py
+++ deepchem/nn/__init__.py
@@ -5,18 +5,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
-from deepchem.nn.copy import Input
-from deepchem.nn.copy import Dense
-from deepchem.nn.copy import Dropout
-from deepchem.nn.copy import BatchNormalization
-from deepchem.nn.layers import AttnLSTMEmbedding
-
-from deepchem.nn.weave_layers import WeaveLayer
-from deepchem.nn.weave_layers import AlternateWeaveLayer
-from deepchem.nn.weave_layers import WeaveConcat
-from deepchem.nn.weave_layers import WeaveGather
-from deepchem.nn.weave_layers import AlternateWeaveGather
-
 from deepchem.nn.model_ops import weight_decay
 from deepchem.nn.model_ops import optimizer
 from deepchem.nn.model_ops import add_bias
