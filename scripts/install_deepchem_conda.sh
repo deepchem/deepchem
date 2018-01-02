4ff60b4c9df060db7163de4eb6c6c89d3bd0d788
leswing <lilleswing@gmail.com>
2018-01-02 11:59:53 -0500

    update mdtraj and xgboost

--- scripts/install_deepchem_conda.sh
+++ scripts/install_deepchem_conda.sh
@@ -23,12 +23,11 @@ source activate $envname
 conda install -y -q -c omnia pdbfixer=1.4
 conda install -y -q -c conda-forge joblib=0.11
 conda install -y -q -c conda-forge six=1.10.0
-conda install -y -q -c conda-forge mdtraj=1.9.0
+conda install -y -q -c conda-forge mdtraj=1.9.1
 conda install -y -q -c conda-forge scikit-learn=0.19.1
 conda install -y -q -c conda-forge setuptools=36.2.2
 conda install -y -q -c conda-forge keras=1.2.2
 conda install -y -q -c conda-forge networkx=1.11
-conda install -y -q -c conda-forge xgboost=0.6a2
 conda install -y -q -c conda-forge pillow=4.3.0
 conda install -y -q -c conda-forge pandas=0.19.2
 conda install -y -q -c conda-forge $tensorflow=1.4.0
@@ -39,3 +38,4 @@ conda install -y -q -c conda-forge zlib=1.2.11
 conda install -y -q -c conda-forge requests=2.18.4
 conda install -y -q -c conda-forge numpy=1.13.3
 conda install -y -q -c rdkit rdkit=2017.09.1
+yes | pip install xgboost==0.7.post3
