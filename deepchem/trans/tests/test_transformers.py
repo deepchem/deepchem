6729f20d5a2da9264f6894b87fbe55214b8e0bd5
leswing <lilleswing@gmail.com>
2018-01-03 14:37:19 -0500

    imports and yapf

--- deepchem/trans/tests/test_transformers.py
+++ deepchem/trans/tests/test_transformers.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
-from molnet import load_delaney
+from deepchem.molnet import load_delaney
 from trans.transformers import FeaturizationTransformer
 
 __author__ = "Bharath Ramsundar"
