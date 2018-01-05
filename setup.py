654a99934a0e475adec2714cff581d27d2ed098b
Bharath Ramsundar <rbharath@stanford.edu>
2018-01-04 16:23:19 -0800

    bugfix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 config = {
   'install_requires': ['simdna==0.3'],
   'dependency_links': ["https://github.com/kundajelab/simdna/tarball/0.3#egg=simdna-0.3"],
-  'setup_requires'=['pbr'],
+  'setup_requires': ['pbr'],
   'pbr': True
 }
 setup(**config)
