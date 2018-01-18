225f27d430c16275a900593a38f71ab3a55c76c6
leswing <lilleswing@gmail.com>
2018-01-18 11:31:34 -0500

    Move simdna to conda forge

--- scripts/install_deepchem_conda.sh
+++ scripts/install_deepchem_conda.sh
@@ -43,5 +43,5 @@ conda install -y -q -c conda-forge flaky=3.3.0
 conda install -y -q -c conda-forge zlib=1.2.11
 conda install -y -q -c conda-forge requests=2.18.4
 conda install -y -q -c conda-forge xgboost=0.6a2
+conda install -y -q -c conda-forge simdna=0.4.2
 conda install -y -q -c rdkit rdkit=2017.09.1
-yes | pip install simdna
