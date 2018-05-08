6a5993d322eace3f4e572a467e079d144f22d518
leswing <lilleswing@gmail.com>
2018-05-08 14:22:46 -0400

    Try This

--- devtools/jenkins/test_notebooks.sh
+++ devtools/jenkins/test_notebooks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 envname=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
 sed -i -- 's/tensorflow$/tensorflow-gpu/g' scripts/install_deepchem_conda.sh
-export python_version=2.7
 bash scripts/install_deepchem_conda.sh $envname
 source activate $envname
 python setup.py install
@@ -18,9 +17,10 @@ yes | pip install gym[atari]
 conda install -y -c auto pubchempy
 conda install -y xlrd
 conda install -y seaborn
+conda install -y lime
 
 cd examples/notebooks
 nosetests --with-timer tests.py --with-xunit --xunit-file=notebook_tests.xml|| true
 
 source deactivate
-# conda remove --name $envname --all
+conda remove --name $envname --all
