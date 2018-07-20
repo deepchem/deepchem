6b71d4b83665e25fce28353f766a7a4ee303e8eb
Bharath Ramsundar <rbharath@stanford.edu>
2018-07-19 22:49:23 -0700

    Remove another

--- deepchem/utils/genomics.py
+++
@@ -1,9 +0,0 @@
-"""
-Genomic data handling utilities.
-"""
-import simdna
-from simdna.synthetic import LoadedEncodeMotifs
-
-loaded_motifs = LoadedEncodeMotifs(simdna.ENCODE_MOTIFS_PATH,
-                                   pseudocountProb=0.001)
-
