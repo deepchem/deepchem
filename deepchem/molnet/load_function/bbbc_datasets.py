7ac215505f4f785d83adf8096af0773f2111e467
Bharath Ramsundar <rbharath@stanford.edu>
2018-07-25 02:43:39 -0700

    bbbc2

--- deepchem/molnet/load_function/bbbc_datasets.py
+++ deepchem/molnet/load_function/bbbc_datasets.py
@@ -73,3 +73,67 @@ def load_bbbc001(split='index', reload=True):
     deepchem.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
                                              transformers)
   return bbbc001_tasks, all_dataset, transformers
+
+def load_bbbc002(split='index', reload=True):
+  """Load BBBC002 dataset
+  
+  This dataset contains data corresponding to 5 samples of Drosophilia Kc167
+  cells. There are 10 fields of view for each sample, each an image of size
+  512x512. Ground truth labels contain cell counts for this dataset. Full
+  details about this dataset are present at
+  https://data.broadinstitute.org/bbbc/BBBC002/.
+  """
+  # Featurize BBBC002 dataset
+  bbbc002_tasks = ["cell-count"]
+  data_dir = deepchem.utils.get_data_dir()
+  if reload:
+    save_dir = os.path.join(data_dir, "bbbc002/" + str(split))
+    loaded, all_dataset, transformers = deepchem.utils.save.load_dataset_from_disk(
+        save_dir)
+    if loaded:
+      return bbbc002_tasks, all_dataset, transformers
+  dataset_file = os.path.join(data_dir, "BBBC002_v1_images.zip")
+  labels_file = os.path.join(data_dir, "BBBC002_v1_counts.txt")
+
+  if not os.path.exists(dataset_file):
+    deepchem.utils.download_url(
+        'https://data.broadinstitute.org/bbbc/BBBC002/BBBC002_v1_images.zip'
+    )
+  if not os.path.exists(labels_file):
+    deepchem.utils.download_url(
+        'https://data.broadinstitute.org/bbbc/BBBC002/BBBC002_v1_counts.txt')
+  # Featurize Images into NumpyArrays
+  loader = deepchem.data.ImageLoader()
+  dataset = loader.featurize(dataset_file, in_memory=False)
+
+  # Load text file with labels
+  with open(labels_file) as f:
+    content = f.readlines()
+  # Strip the first line which holds field labels
+  lines = [x.strip() for x in content][1:]
+  # Format is: Image_name count1 count2
+  lines = [x.split("\t") for x in lines]
+  counts = [(float(x[1]) + float(x[2])) / 2.0 for x in lines]
+  y = np.reshape(np.array(counts), (len(counts),1))
+  ids = [x[0] for x in lines]
+
+  # This is kludgy way to add y to dataset. Can be done better?
+  dataset = deepchem.data.DiskDataset.from_numpy(dataset.X, y, ids=ids)
+
+  if split == None:
+    return bbbc002_tasks, (dataset, None, None), transformers
+
+  splitters = {
+      'index': deepchem.splits.IndexSplitter(),
+      'random': deepchem.splits.RandomSplitter(),
+  }
+  if split not in splitters:
+    raise ValueError("Only index and random splits supported.")
+  splitter = splitters[split]
+
+  train, valid, test = splitter.train_valid_test_split(dataset)
+  all_dataset = (train, valid, test)
+  if reload:
+    deepchem.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
+                                             transformers)
+  return bbbc002_tasks, all_dataset, transformers
