55043af14726a84a43e4ab31369ec302d5a0214a
leswing <lilleswing@gmail.com>
2018-07-28 09:36:23 -0400

    Install test conda-verify

--- devtools/jenkins/conda_build.sh
+++ devtools/jenkins/conda_build.sh
@@ -2,8 +2,8 @@
 cd devtools/conda-recipe
 source activate base
 conda upgrade conda -y
-conda install conda-build anaconda-client -y
-conda upgrade conda-build anaconda-client -y
+conda install conda-build anaconda-client conda-verify -y
+conda upgrade conda-build anaconda-client conda-verify -y
 conda build purge
 export python_version=2.7
 conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge
