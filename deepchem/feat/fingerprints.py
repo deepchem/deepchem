ef72bca4bd131aeacdd185c88576457dfd566032
K Yasaswi Sri Chandra Gandhi <yasaswisrichandragandhi@gmail.com>
2019-03-10 19:24:07 +0530

    Fixed indentation
    Fixed indentation using yapf as suggested

--- deepchem/feat/fingerprints.py
+++ deepchem/feat/fingerprints.py
@@ -16,7 +16,7 @@ from deepchem.feat import Featurizer
 
 
 class CircularFingerprint(Featurizer):
-  """
+    """
   Circular (Morgan) fingerprints.
 
   Parameters
@@ -41,21 +41,28 @@ class CircularFingerprint(Featurizer):
       Whether to calculate SMILES strings for fragment IDs (only applicable
       when calculating sparse fingerprints).
   """
-  name = 'circular'
+    name = 'circular'
 
-  def __init__(self, radius=2, size=2048, chiral=False, bonds=True,
-             features=False, sparse=False, smiles=False, counts = False):
-    self.radius = radius
-    self.size = size
-    self.chiral = chiral
-    self.counts = counts
-    self.bonds = bonds
-    self.features = features
-    self.sparse = sparse
-    self.smiles = smiles
+    def __init__(self,
+                 radius=2,
+                 size=2048,
+                 chiral=False,
+                 bonds=True,
+                 features=False,
+                 sparse=False,
+                 smiles=False,
+                 counts=False):
+        self.radius = radius
+        self.size = size
+        self.chiral = chiral
+        self.counts = counts
+        self.bonds = bonds
+        self.features = features
+        self.sparse = sparse
+        self.smiles = smiles
 
-  def _featurize(self, mol):
-    """
+    def _featurize(self, mol):
+        """
     Calculate circular fingerprint.
 
     Parameters
@@ -63,50 +70,62 @@ class CircularFingerprint(Featurizer):
     mol : RDKit Mol
         Molecule.
     """
-    if self.sparse:
-      info = {}
-      fp = rdMolDescriptors.GetMorganFingerprint(
-          mol, self.radius, useChirality=self.chiral,
-          useBondTypes=self.bonds, useFeatures=self.features,
-          bitInfo=info)
-      fp = fp.GetNonzeroElements()  # convert to a dict
+        if self.sparse:
+            info = {}
+            fp = rdMolDescriptors.GetMorganFingerprint(
+                mol,
+                self.radius,
+                useChirality=self.chiral,
+                useBondTypes=self.bonds,
+                useFeatures=self.features,
+                bitInfo=info)
+            fp = fp.GetNonzeroElements()  # convert to a dict
 
-      # generate SMILES for fragments
-      if self.smiles:
-        fp_smiles = {}
-        for fragment_id, count in fp.items():
-          root, radius = info[fragment_id][0]
-          env = Chem.FindAtomEnvironmentOfRadiusN(mol, radius, root)
-          frag = Chem.PathToSubmol(mol, env)
-          smiles = Chem.MolToSmiles(frag)
-          fp_smiles[fragment_id] = {'smiles': smiles, 'count': count}
-        fp = fp_smiles
-    elif self.counts:
-        info = {}
-        fpbv = rdMolDescriptors.GetMorganFingerprintAsBitVect(
-          mol, self.radius, nBits=self.size, useChirality=self.chiral,
-          useBondTypes=self.bonds, useFeatures=self.features, bitInfo=info)
-        fp = np.zeros((1,), dtype=int)
-        DataStructs.ConvertToNumpyArray(fpbv,fp)
-        for b,c in info.items():
-            fp[b] = len(c)
-    else:
-      fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(
-          mol, self.radius, nBits=self.size, useChirality=self.chiral,
-          useBondTypes=self.bonds, useFeatures=self.features)
-    return fp
+            # generate SMILES for fragments
+            if self.smiles:
+                fp_smiles = {}
+                for fragment_id, count in fp.items():
+                    root, radius = info[fragment_id][0]
+                    env = Chem.FindAtomEnvironmentOfRadiusN(mol, radius, root)
+                    frag = Chem.PathToSubmol(mol, env)
+                    smiles = Chem.MolToSmiles(frag)
+                    fp_smiles[fragment_id] = {'smiles': smiles, 'count': count}
+                fp = fp_smiles
+        elif self.counts:
+            info = {}
+            fpbv = rdMolDescriptors.GetMorganFingerprintAsBitVect(
+                mol,
+                self.radius,
+                nBits=self.size,
+                useChirality=self.chiral,
+                useBondTypes=self.bonds,
+                useFeatures=self.features,
+                bitInfo=info)
+            fp = np.zeros((1, ), dtype=int)
+            DataStructs.ConvertToNumpyArray(fpbv, fp)
+            for b, c in info.items():
+                fp[b] = len(c)
+        else:
+            fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(
+                mol,
+                self.radius,
+                nBits=self.size,
+                useChirality=self.chiral,
+                useBondTypes=self.bonds,
+                useFeatures=self.features)
+        return fp
 
-  def __hash__(self):
-    return hash((self.radius, self.size, self.chiral, self.bonds, self.features,
-                 self.sparse, self.smiles))
+    def __hash__(self):
+        return hash((self.radius, self.size, self.chiral, self.bonds,
+                     self.features, self.sparse, self.smiles))
 
-  def __eq__(self, other):
-    if not isinstance(self, other.__class__):
-      return False
-    return self.radius == other.radius and \
-           self.size == other.size and \
-           self.chiral == other.chiral and \
-           self.bonds == other.bonds and \
-           self.features == other.features and \
-           self.sparse == other.sparse and \
-           self.smiles == other.smiles
+    def __eq__(self, other):
+        if not isinstance(self, other.__class__):
+            return False
+        return self.radius == other.radius and \
+               self.size == other.size and \
+               self.chiral == other.chiral and \
+               self.bonds == other.bonds and \
+               self.features == other.features and \
+               self.sparse == other.sparse and \
+               self.smiles == other.smiles
