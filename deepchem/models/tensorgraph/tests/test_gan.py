c4568e49baaff3ea4ca196745a72c14d055712cd
Peter Eastman <peter.eastman@gmail.com>
2019-03-15 17:50:25 -0700

    Marked another test as flaky

--- deepchem/models/tensorgraph/tests/test_gan.py
+++ deepchem/models/tensorgraph/tests/test_gan.py
@@ -61,6 +61,7 @@ class TestGAN(unittest.TestCase):
     assert abs(np.mean(deltas)) < 1.0
     assert np.std(deltas) > 1.0
 
+  @flaky
   def test_mix_gan(self):
     """Test a GAN with multiple generators and discriminators."""
 
