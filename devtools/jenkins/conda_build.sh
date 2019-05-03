012bac5991d8647885e6e92a3127f8127a632ff2
leswing <lilleswing@gmail.com>
2019-05-03 13:58:44 -0400

    GOGO

--- devtools/jenkins/conda_build.sh
+++ devtools/jenkins/conda_build.sh
@@ -5,7 +5,7 @@ conda upgrade conda -y
 conda install conda-build anaconda-client conda-verify -y
 conda upgrade conda-build anaconda-client conda-verify -y
 conda build purge
-conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge >> log.txt
+conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
 
 export package_name=deepchem-gpu
-conda build deepchem -c defaults -c rdkit -c omnia -c conda-forge >> log.txt
+conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
