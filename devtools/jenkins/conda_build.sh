bab4bc6ec3ab8027043d4fa0c0ed6dc3d4238b5e
leswing <lilleswing@gmail.com>
2019-05-03 14:24:29 -0400

    proper gpu build with tf gpu

--- devtools/jenkins/conda_build.sh
+++ devtools/jenkins/conda_build.sh
@@ -5,11 +5,13 @@ conda upgrade conda -y
 conda install conda-build anaconda-client conda-verify -y
 conda upgrade conda-build anaconda-client conda-verify -y
 conda build purge
+
 conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
 
 unamestr=`uname`
 if [[ "$unamestr" != 'Darwin' ]];
 then
    export package_name=deepchem-gpu
+   export tensorflow_enabled=tensorflow-gpu
    conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
 fi
