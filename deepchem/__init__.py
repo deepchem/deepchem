3b108365f765c68064fd9ad650f3efb39452a7b5
leswing <lilleswing@gmail.com>
2019-05-03 15:08:32 -0400

    Set version string correctly

--- deepchem/__init__.py
+++ deepchem/__init__.py
@@ -4,7 +4,8 @@ Imports all submodules
 from __future__ import division
 from __future__ import unicode_literals
 
-__version__ = '2.1.1'
+__version__ = '2.2.0'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 import deepchem.data
 import deepchem.feat
