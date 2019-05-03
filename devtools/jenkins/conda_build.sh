12c8c0b90e1078439d20bfdb6236ada1e041be6f
leswing <lilleswing@gmail.com>
2019-05-03 14:02:04 -0400

    Do not build gpu on osx

--- devtools/jenkins/conda_build.sh
+++ devtools/jenkins/conda_build.sh
@@ -7,5 +7,9 @@ conda upgrade conda-build anaconda-client conda-verify -y
 conda build purge
 conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
 
-export package_name=deepchem-gpu
-conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
+unamestr=`uname`
+if [[ "$unamestr" != 'Darwin' ]];
+then
+   export package_name=deepchem-gpu
+   conda build deepchem -c defaults -c rdkit -c anaconda -c omnia -c conda-forge >> log.txt
+fi
