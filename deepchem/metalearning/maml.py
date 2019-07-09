1330ea3102315bd79c9c6efdbd8818c8e2a3cb8f
peastman <peastman@stanford.edu>
2019-07-09 14:45:17 -0700

    Created new MAML API

--- deepchem/metalearning/maml.py
+++ deepchem/metalearning/maml.py
@@ -1,7 +1,7 @@
 """Model-Agnostic Meta-Learning (MAML) algorithm for low data learning."""
 
-from deepchem.models.tensorgraph.layers import Layer
 from deepchem.models.tensorgraph.optimizers import Adam, GradientDescent
+import numpy as np
 import os
 import shutil
 import tempfile
@@ -17,23 +17,32 @@ class MetaLearner(object):
   data for training it on a large (possibly infinite) set of different tasks.
   """
 
-  @property
-  def loss(self):
-    """Get the model's loss function, represented as a Layer or Tensor."""
+  def compute_model(self, inputs, variables, training):
+    """Compute the model for a set of inputs and variables.
+
+    Parameters
+    ----------
+    inputs: list of tensors
+      the inputs to the model
+    variables: list of tensors
+      the values to use for the model's variables.  This might be the actual
+      variables (as returned by the MetaLearner's variables property), or
+      alternatively it might be the values of those variables after one or more
+      steps of gradient descent for the current task.
+    training: bool
+      indicates whether the model is being invoked for training or prediction
+
+    Returns
+    -------
+    (loss, outputs) where loss is the value of the model's loss function, and
+    outputs is a list of the model's outputs
+    """
     raise NotImplemented("Subclasses must implement this")
 
   @property
   def variables(self):
-    """Get the list of Tensorflow variables to train.
-
-    The default implementation returns all trainable variables in the graph.  This is usually
-    what you want, but subclasses can customize it if needed.
-    """
-    loss = self.loss
-    if isinstance(loss, Layer):
-      loss = loss.out_tensor
-    with loss.graph.as_default():
-      return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    """Get the list of Tensorflow variables to train."""
+    raise NotImplemented("Subclasses must implement this")
 
   def select_task(self):
     """Select a new task to train on.
@@ -47,8 +56,8 @@ class MetaLearner(object):
   def get_batch(self):
     """Get a batch of data for training.
 
-    This should return the data in the form of a Tensorflow feed dict, that is, a dict
-    mapping tensors to values.  This will usually be called twice for each task, and should
+    This should return the data as a list of arrays, one for each of the model's
+    inputs.  This will usually be called twice for each task, and should
     return a different batch on each call.
     """
     raise NotImplemented("Subclasses must implement this")
@@ -85,9 +94,9 @@ class MAML(object):
     ----------
     learner: MetaLearner
       defines the meta-learning problem
-    learning_rate: float, Layer, or Tensor
+    learning_rate: float or Tensor
       the learning rate to use for optimizing each task (not to be confused with the one used
-      for meta-learning).  This can optionally be made a variable (represented as a Layer or
+      for meta-learning).  This can optionally be made a variable (represented as a
       Tensor), in which case the learning rate will itself be learnable.
     optimization_steps: int
       the number of steps of gradient descent to perform for each task
@@ -101,21 +110,10 @@ class MAML(object):
     """
     # Record inputs.
 
-    raise Exception(
-        'MAML does not currently work correctly.  It needs to be rewritten to be compatible with modern TensorFlow'
-    )
     self.learner = learner
-    if isinstance(learner.loss, Layer):
-      self._loss = learner.loss.out_tensor
-    else:
-      self._loss = learner.loss
-    if isinstance(learning_rate, Layer):
-      self._learning_rate = learning_rate.out_tensor
-    else:
-      self._learning_rate = learning_rate
+    self._learning_rate = learning_rate
     self.meta_batch_size = meta_batch_size
     self.optimizer = optimizer
-    self._graph = self._loss.graph
 
     # Create the output directory if necessary.
 
@@ -128,70 +126,74 @@ class MAML(object):
       self._model_dir_is_temp = True
     self.model_dir = model_dir
     self.save_file = "%s/%s" % (self.model_dir, "model")
-    with self._graph.as_default():
-      # Create duplicate placeholders for meta-optimization.
 
-      learner.select_task()
-      self._meta_placeholders = {}
-      for p in learner.get_batch().keys():
-        name = 'meta/' + p.name.split(':')[0]
-        self._meta_placeholders[p] = tf.placeholder(p.dtype, p.shape, name)
+    learner.select_task()
+    example_inputs = learner.get_batch()
+    self._input_shapes = [(None,) + i.shape[1:] for i in example_inputs]
+    self._input_dtypes = [x.dtype for x in example_inputs]
+    self._input_placeholders = [
+        tf.placeholder(dtype=tf.as_dtype(t), shape=s)
+        for s, t in zip(self._input_shapes, self._input_dtypes)
+    ]
+    self._meta_placeholders = [
+        tf.placeholder(dtype=tf.as_dtype(t), shape=s)
+        for s, t in zip(self._input_shapes, self._input_dtypes)
+    ]
+    variables = learner.variables
+    self._loss, _ = learner.compute_model(self._input_placeholders, variables,
+                                          False)
+    loss, _ = learner.compute_model(self._input_placeholders, variables, True)
 
-      # Create the loss function for meta-optimization.
+    # Build the meta-learning model.
 
-      updated_loss = self._loss
-      updated_variables = learner.variables
-      for i in range(optimization_steps):
-        gradients = tf.gradients(updated_loss, updated_variables)
-        updated_variables = [
-            v if g is None else v - self._learning_rate * g
-            for v, g in zip(updated_variables, gradients)
-        ]
-        replacements = dict(
-            (tf.convert_to_tensor(v1), v2)
-            for v1, v2 in zip(learner.variables, updated_variables))
-        if i == optimization_steps - 1:
-          # In the final loss, use different placeholders for all inputs so the loss will be
-          # computed from a different batch.
-
-          for p in self._meta_placeholders:
-            replacements[p] = self._meta_placeholders[p]
-        updated_loss = tf.contrib.graph_editor.graph_replace(
-            self._loss, replacements)
-      self._meta_loss = updated_loss
-
-      # Create variables for accumulating the gradients.
-
-      variables = list(learner.variables)
-      gradients = tf.gradients(self._meta_loss, variables)
-      for i in reversed(range(len(variables))):
-        if gradients[i] is None:
-          del variables[i]
-          del gradients[i]
-      zero_gradients = [tf.zeros(g.shape, g.dtype) for g in gradients]
-      summed_gradients = [
-          tf.Variable(z, trainable=False) for z in zero_gradients
+    updated_variables = variables
+    for i in range(optimization_steps):
+      gradients = tf.gradients(loss, updated_variables)
+      updated_variables = [
+          v if g is None else v - self._learning_rate * g
+          for v, g in zip(updated_variables, gradients)
       ]
-      self._clear_gradients = tf.group(
-          *[s.assign(z) for s, z in zip(summed_gradients, zero_gradients)])
-      self._add_gradients = tf.group(
-          *[s.assign_add(g) for s, g in zip(summed_gradients, gradients)])
+      if i == optimization_steps - 1:
+        # In the final loss, use different placeholders for all inputs so the loss will be
+        # computed from a different batch.
 
-      # Create the optimizers for meta-optimization and task optimization.
+        inputs = self._meta_placeholders
+      else:
+        inputs = self._input_placeholders
+      loss, outputs = learner.compute_model(inputs, updated_variables, True)
+    self._meta_loss = loss
 
-      self._global_step = tf.placeholder(tf.int32, [])
-      grads_and_vars = list(zip(summed_gradients, variables))
-      self._meta_train_op = optimizer._create_optimizer(
-          self._global_step).apply_gradients(grads_and_vars)
-      task_optimizer = GradientDescent(learning_rate=self._learning_rate)
-      self._task_train_op = task_optimizer._create_optimizer(
-          self._global_step).minimize(self._loss)
-      self._session = tf.Session()
+    # Create variables for accumulating the gradients.
 
-      # Create a Checkpoint for saving.
+    variables = list(learner.variables)
+    gradients = tf.gradients(self._meta_loss, variables)
+    for i in reversed(range(len(variables))):
+      if gradients[i] is None:
+        del variables[i]
+        del gradients[i]
+    zero_gradients = [tf.zeros(g.shape, g.dtype) for g in gradients]
+    summed_gradients = [tf.Variable(z, trainable=False) for z in zero_gradients]
+    self._clear_gradients = tf.group(
+        *[s.assign(z) for s, z in zip(summed_gradients, zero_gradients)])
+    self._add_gradients = tf.group(
+        *[s.assign_add(g) for s, g in zip(summed_gradients, gradients)])
 
-      self._checkpoint = tf.train.Checkpoint()
-      self._checkpoint.listed = learner.variables
+    # Create the optimizers for meta-optimization and task optimization.
+
+    self._global_step = tf.placeholder(tf.int32, [])
+    grads_and_vars = list(zip(summed_gradients, variables))
+    self._meta_train_op = optimizer._create_optimizer(
+        self._global_step).apply_gradients(grads_and_vars)
+    task_optimizer = GradientDescent(learning_rate=self._learning_rate)
+    self._task_train_op = task_optimizer._create_optimizer(
+        self._global_step).minimize(self._loss)
+    self._session = tf.Session()
+    self._session.run(tf.global_variables_initializer())
+
+    # Create a Checkpoint for saving.
+
+    self._checkpoint = tf.train.Checkpoint()
+    self._checkpoint.listed = learner.variables
 
   def __del__(self):
     if '_model_dir_is_temp' in dir(self) and self._model_dir_is_temp:
@@ -214,45 +216,43 @@ class MAML(object):
     checkpoint_interval: float
       the time interval at which to save checkpoints, measured in seconds
     restore: bool
-      if True, restore the model from the most recent checkpoint and continue training
-      from there.  If False, retrain the model from scratch.
+      if True, restore the model from the most recent checkpoint before training
+      it further
     """
-    with self._graph.as_default():
-      self._session.run(tf.global_variables_initializer())
-      if restore:
-        self.restore()
-      manager = tf.train.CheckpointManager(self._checkpoint, self.model_dir,
-                                           max_checkpoints_to_keep)
-      checkpoint_time = time.time()
+    if restore:
+      self.restore()
+    manager = tf.train.CheckpointManager(self._checkpoint, self.model_dir,
+                                         max_checkpoints_to_keep)
+    checkpoint_time = time.time()
 
-      # Main optimization loop.
+    # Main optimization loop.
 
-      for i in range(steps):
-        self._session.run(self._clear_gradients)
-        for j in range(self.meta_batch_size):
-          self.learner.select_task()
-          feed_dict = self.learner.get_batch()
-          feed_dict[self._global_step] = i
-          for key, value in self.learner.get_batch().items():
-            feed_dict[self._meta_placeholders[key]] = value
-          self._session.run(self._add_gradients, feed_dict=feed_dict)
-        self._session.run(self._meta_train_op)
+    for i in range(steps):
+      self._session.run(self._clear_gradients)
+      for j in range(self.meta_batch_size):
+        self.learner.select_task()
+        inputs = self.learner.get_batch()
+        feed_dict = {}
+        feed_dict[self._global_step] = i
+        for k in range(len(inputs)):
+          feed_dict[self._input_placeholders[k]] = inputs[k]
+          feed_dict[self._meta_placeholders[k]] = inputs[k]
+        self._session.run(self._add_gradients, feed_dict=feed_dict)
+      self._session.run(self._meta_train_op)
 
-        # Do checkpointing.
+      # Do checkpointing.
 
-        if i == steps - 1 or time.time(
-        ) >= checkpoint_time + checkpoint_interval:
-          with self._session.as_default():
-            manager.save()
-          checkpoint_time = time.time()
+      if i == steps - 1 or time.time() >= checkpoint_time + checkpoint_interval:
+        with self._session.as_default():
+          manager.save()
+        checkpoint_time = time.time()
 
   def restore(self):
     """Reload the model parameters from the most recent checkpoint file."""
     last_checkpoint = tf.train.latest_checkpoint(self.model_dir)
     if last_checkpoint is None:
       raise ValueError('No checkpoint found')
-    with self._graph.as_default():
-      self._checkpoint.restore(last_checkpoint).run_restore_ops(self._session)
+    self._checkpoint.restore(last_checkpoint).run_restore_ops(self._session)
 
   def train_on_current_task(self, optimization_steps=1, restore=True):
     """Perform a few steps of gradient descent to fine tune the model on the current task.
@@ -266,7 +266,9 @@ class MAML(object):
     """
     if restore:
       self.restore()
-    with self._graph.as_default():
-      feed_dict = self.learner.get_batch()
-      for i in range(optimization_steps):
-        self._session.run(self._task_train_op, feed_dict=feed_dict)
+    inputs = self.learner.get_batch()
+    feed_dict = {}
+    for p, v in zip(self._input_placeholders, inputs):
+      feed_dict[p] = v
+    for i in range(optimization_steps):
+      self._session.run(self._task_train_op, feed_dict=feed_dict)
