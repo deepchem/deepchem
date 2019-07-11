b2f0404f255091468a1653d9829e339d0cc0b886
peastman <peastman@stanford.edu>
2019-07-11 12:44:41 -0700

    More dealing with flaky test case

--- deepchem/metalearning/tests/test_maml.py
+++ deepchem/metalearning/tests/test_maml.py
@@ -54,7 +54,7 @@ class TestMAML(unittest.TestCase):
     learner = SineLearner()
     optimizer = dc.models.tensorgraph.optimizers.Adam(learning_rate=5e-3)
     maml = dc.metalearning.MAML(learner, meta_batch_size=4, optimizer=optimizer)
-    maml.fit(7000)
+    maml.fit(9000)
 
     # Test it out on some new tasks and see how it works.
 
