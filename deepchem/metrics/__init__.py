1463b2cbea216948bfe229185b89148f8a42e252
Jon Sorenson <jon@atomwise.com>
2019-11-26 16:56:44 -0800

    allow bedroc_score to not require mode

--- deepchem/metrics/__init__.py
+++ deepchem/metrics/__init__.py
@@ -237,7 +237,7 @@ class Metric(object):
       if self.metric.__name__ in [
           "roc_auc_score", "matthews_corrcoef", "recall_score",
           "accuracy_score", "kappa_score", "precision_score",
-          "balanced_accuracy_score", "prc_auc_score"
+          "balanced_accuracy_score", "prc_auc_score", "bedroc_score",
       ]:
         mode = "classification"
       elif self.metric.__name__ in [
