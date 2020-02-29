563656238406082a30b06e6283307ee3b6cf4a2a
KacperKubara <kjk1u17@soton.ac.uk>
2020-02-29 12:46:25 +0000

    Added tests for the new scaffoldsplitter

--- deepchem/splits/splitters.py
+++ deepchem/splits/splitters.py
@@ -847,40 +847,25 @@ class ButinaSplitter(Splitter):
 
 class ScaffoldSplitter(Splitter):
   """
-    Class for doing data splits based on the scaffold of small molecules.
-    """
+  Class for doing data splits based on the scaffold of small molecules.
+  """
 
   def split(self,
             dataset,
-            seed=None,
             frac_train=.8,
             frac_valid=.1,
             frac_test=.1,
             log_every_n=1000):
     """
-        Splits internal compounds into train/validation/test by scaffold.
-        """
+    Splits internal compounds into train/validation/test by scaffold.
+    """
     np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.)
-    scaffolds = {}
-    log("About to generate scaffolds", self.verbose)
-    data_len = len(dataset)
-    for ind, smiles in enumerate(dataset.ids):
-      if ind % log_every_n == 0:
-        log("Generating scaffold %d/%d" % (ind, data_len), self.verbose)
-      scaffold = generate_scaffold(smiles)
-      if scaffold not in scaffolds:
-        scaffolds[scaffold] = [ind]
-      else:
-        scaffolds[scaffold].append(ind)
-    # Sort from largest to smallest scaffold sets
-    scaffolds = {key: sorted(value) for key, value in scaffolds.items()}
-    scaffold_sets = [
-        scaffold_set for (scaffold, scaffold_set) in sorted(
-            scaffolds.items(), key=lambda x: (len(x[1]), x[1][0]), reverse=True)
-    ]
+    scaffold_sets = self.generate_scaffolds(dataset)
+
     train_cutoff = frac_train * len(dataset)
     valid_cutoff = (frac_train + frac_valid) * len(dataset)
     train_inds, valid_inds, test_inds = [], [], []
+
     log("About to sort in scaffold sets", self.verbose)
     for scaffold_set in scaffold_sets:
       if len(train_inds) + len(scaffold_set) > train_cutoff:
@@ -891,6 +876,31 @@ class ScaffoldSplitter(Splitter):
       else:
         train_inds += scaffold_set
     return train_inds, valid_inds, test_inds
+
+  def generate_scaffolds(self, dataset, log_every_n=1000):
+    """
+    Returns all scaffolds from the dataset
+    """
+    scaffolds = {}
+    data_len = len(dataset)
+
+    log("About to generate scaffolds", self.verbose)
+    for ind, smiles in enumerate(dataset.ids):
+      if ind % log_every_n == 0:
+        log(f"Generating scaffold {ind} {data_len}", self.verbose)
+      scaffold = generate_scaffold(smiles)
+      if scaffold not in scaffolds:
+        scaffolds[scaffold] = [ind]
+      else:
+        scaffolds[scaffold].append(ind)
+
+    # Sort from largest to smallest scaffold sets
+    scaffolds = {key: sorted(value) for key, value in scaffolds.items()}
+    scaffold_sets = [
+        scaffold_set for (scaffold, scaffold_set) in sorted(
+            scaffolds.items(), key=lambda x: (len(x[1]), x[1][0]), reverse=True)
+    ]
+    return scaffold_sets
 
 
 class FingerprintSplitter(Splitter):
