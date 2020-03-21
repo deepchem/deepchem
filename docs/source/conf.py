8d14fdd40ff8852e73f558bbe506076e08007d47
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-03-20 18:19:37 -0700

    Changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
