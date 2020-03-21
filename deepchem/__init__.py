39457c351aea375f7d6f8d031d12b991ec2242e5
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-03-21 13:32:40 -0700

    Changes

--- deepchem/__init__.py
+++ deepchem/__init__.py
@@ -3,15 +3,9 @@ Imports all submodules
 """
 __version__ = '2.3.0'
 
-import deepchem.data
-import deepchem.feat
 import deepchem.hyper
 import deepchem.metalearning
 import deepchem.metrics
 import deepchem.models
-import deepchem.splits
-import deepchem.trans
 import deepchem.utils
-import deepchem.dock
-import deepchem.molnet
 import deepchem.rl
