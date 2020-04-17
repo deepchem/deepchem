39d93bb9a7c420febe7a53cc56ce8a4f2e5747e3
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-04-16 18:14:00 -0700

    Changes

--- examples/pdbbind/pdbbind_fcnet.py
+++ examples/pdbbind/pdbbind_fcnet.py
@@ -1,14 +1,6 @@
 """
 Script that trains Tensorflow models on PDBbind dataset.
 """
-from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-
-__author__ = "Bharath Ramsundar"
-__copyright__ = "Copyright 2016, Stanford University"
-__license__ = "MIT"
-
 import os
 import numpy as np
 import tensorflow as tf
