9802c504940f6f602d52f1d8e80e6f9ea5c78892
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-04-29 18:16:38 -0700

    Doctest fail

--- deepchem/utils/geometry_utils.py
+++ deepchem/utils/geometry_utils.py
@@ -13,9 +13,9 @@ def unit_vector(vector):
 def angle_between(vector_i, vector_j):
   """Returns the angle in radians between vectors "vector_i" and "vector_j"::
 
-  >>> print("%0.06f" % _angle_between((1, 0, 0), (0, 1, 0)))
+  >>> print("%0.06f" % angle_between((1, 0, 0), (0, 1, 0)))
   1.570796
-  >>> print("%0.06f" % _angle_between((1, 0, 0), (1, 0, 0)))
+  >>> print("%0.06f" % angle_between((1, 0, 0), (1, 0, 0)))
   0.000000
   >>> print("%0.06f" % angle_between((1, 0, 0), (-1, 0, 0)))
   3.141593
