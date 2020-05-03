e247a63f16adc6b2070bfb0f73d025d54a876144
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-05-02 23:16:34 -0700

    fix docstring

--- deepchem/feat/coulomb_matrices.py
+++ deepchem/feat/coulomb_matrices.py
@@ -60,8 +60,7 @@ class CoulombMatrix(Featurizer):
   >>> featurizers = dc.feat.CoulombMatrix(max_atoms=23)
   >>> input_file = 'deepchem/feat/tests/data/water.sdf' # really backed by water.sdf.csv
   >>> tasks = ["atomization_energy"]
-  >>> featurizer = dc.data.SDFLoader(tasks, smiles_field="smiles", mol_field="mol",
-  ...                                    featurizer=featurizers, verbose=False)
+  >>> featurizer = dc.data.SDFLoader(tasks, featurizer=featurizers)
   >>> dataset = featurizer.featurize(input_file)
   Reading structures from deepchem/feat/tests/data/water.sdf.
   Featurizing sample 0
