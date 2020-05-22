2f51189950c39d8d295b30f73b3a274e7604ff59
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-05-21 20:54:23 -0700

    Cleanup

--- deepchem/utils/test/test_fragment_util.py
+++ deepchem/utils/test/test_fragment_util.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from deepchem.utils import rdkit_util
-from deepchem.utils.fragment_util import MolecularFragment
+from deepchem.utils.fragment_util import get_contact_atom_indices
 
 
 class TestFragmentUtil(unittest.TestCase):
@@ -14,8 +14,7 @@ class TestFragmentUtil(unittest.TestCase):
     self.ligand_file = os.path.join(current_dir,
                                     '../../feat/tests/3ws9_ligand.sdf')
 
-  def test_create_molecular_fragment(self):
-    mol_xyz, mol_rdk = rdkit_util.load_molecule(self.ligand_file)
-    fragment = MolecularFragment(mol_rdk.GetAtoms(), mol_xyz)
-    assert len(mol_rdk.GetAtoms()) == len(fragment.GetAtoms())
-    assert (fragment.GetCoords() == mol_xyz).all()
+  def test_get_contact_atom_indices(self):
+    complexes = rdkit_util.load_complex([self.protein_file, self.ligand_file])
+    contact_indices = get_contact_atom_indices(complexes)
+    assert len(contact_indices) == 2
