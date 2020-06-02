41fa37e8b4bae6f4d46751c69e20e846753f0ab9
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-01 18:23:19 -0700

    Changes

--- examples/hyperparam_opt/gaussian_hyperparam_opt.py
+++ examples/hyperparam_opt/gaussian_hyperparam_opt.py
@@ -6,18 +6,13 @@ import deepchem as dc
 
 # Load delaney dataset
 delaney_tasks, delaney_datasets, transformers = dc.molnet.load_delaney()
-train, valid, test= delaney_datasets
+train, valid, test = delaney_datasets
 
 # Fit models
 regression_metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
 
-
 # TODO(rbharath): I don't like this awkward string/class divide. Maybe clean up?
 optimizer = dc.hyper.GaussianProcessHyperparamOpt('tf_regression')
 best_hyper_params, best_performance = optimizer.hyperparam_search(
-  dc.molnet.preset_hyper_parameters.hps['tf_regression'],
-  train,
-  valid,
-  transformers,
-  [regression_metric]
-)
+    dc.molnet.preset_hyper_parameters.hps['tf_regression'], train, valid,
+    transformers, [regression_metric])
