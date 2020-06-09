175f1edf1ed5bd5781e4e295c76b9cd056035408
nd-02110114 <nd.12021218@gmail.com>
2020-06-09 22:46:43 +0900

    :recycle: refactor pypi configuration

--- setup.py
+++ setup.py
@@ -1,4 +1,41 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-config = {'setup_requires': ['pbr'], 'pbr': True}
-setup(**config)
+VERSION = '2.4.0'
+
+setup(
+    name='deepchem',
+    version=VERSION,
+    url='https://github.com/deepchem/deepchem',
+    maintainer='DeepChem contributors',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='MIT',
+    description='Deep learning models for drug discovery, \
+        quantum chemistry, and the life sciences.',
+    keywords=[
+        'deepchem',
+        'life-science',
+        'drug-discovery',
+    ],
+    packages=find_packages('deepchem'),
+    project_urls={
+        'Documentation': 'https://deepchem.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/deepchem/deepchem',
+    },
+    install_requires=[
+        'joblib'
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'scipy',
+    ],
+    python_requires='>=3.5')
