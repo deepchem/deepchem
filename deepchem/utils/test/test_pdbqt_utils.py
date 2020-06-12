41654ab3defccad3cb6b32c85029e748789bcc39
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-12 16:22:27 -0700

    Pytest fixes

--- deepchem/utils/test/test_pdbqt_utils.py
+++ deepchem/utils/test/test_pdbqt_utils.py
@@ -1,7 +1,6 @@
 import unittest
 import os
 import tempfile
-from nose.tools import assert_equal
 from deepchem.utils import rdkit_util
 from deepchem.utils import pdbqt_utils
 
@@ -33,11 +32,11 @@ class TestPDBQTUtils(unittest.TestCase):
       xyz, pdbqt_mol = rdkit_util.load_molecule(
           out_pdbqt, add_hydrogens=False, calc_charges=False)
 
-    assert_equal(pdb_mol.GetNumAtoms(), pdbqt_mol.GetNumAtoms())
+    assert pdb_mol.GetNumAtoms() == pdbqt_mol.GetNumAtoms()
     for atom_idx in range(pdb_mol.GetNumAtoms()):
       atom1 = pdb_mol.GetAtoms()[atom_idx]
       atom2 = pdbqt_mol.GetAtoms()[atom_idx]
-      assert_equal(atom1.GetAtomicNum(), atom2.GetAtomicNum())
+      assert atom1.GetAtomicNum() == atom2.GetAtomicNum()
 
   def test_convert_mol_to_pdbqt(self):
     """Test that a ligand molecule can be coverted to PDBQT."""
@@ -52,11 +51,11 @@ class TestPDBQTUtils(unittest.TestCase):
       pdbqt_utils.convert_mol_to_pdbqt(mol, outfile)
       pdbqt_xyz, pdbqt_mol = rdkit_util.load_molecule(
           outfile, add_hydrogens=False, calc_charges=False)
-    assert_equal(pdbqt_mol.GetNumAtoms(), pdbqt_mol.GetNumAtoms())
+    assert pdbqt_mol.GetNumAtoms() == pdbqt_mol.GetNumAtoms()
     for atom_idx in range(pdbqt_mol.GetNumAtoms()):
       atom1 = pdbqt_mol.GetAtoms()[atom_idx]
       atom2 = pdbqt_mol.GetAtoms()[atom_idx]
-      assert_equal(atom1.GetAtomicNum(), atom2.GetAtomicNum())
+      assert atom1.GetAtomicNum() == atom2.GetAtomicNum()
 
   def test_convert_protein_to_pdbqt(self):
     """Test a protein in a PDB can be converted to PDBQT."""
@@ -71,8 +70,8 @@ class TestPDBQTUtils(unittest.TestCase):
       pdbqt_utils.convert_protein_to_pdbqt(mol, outfile)
       pdbqt_xyz, pdbqt_mol = rdkit_util.load_molecule(
           outfile, add_hydrogens=False, calc_charges=False)
-    assert_equal(pdbqt_mol.GetNumAtoms(), pdbqt_mol.GetNumAtoms())
+    assert pdbqt_mol.GetNumAtoms() == pdbqt_mol.GetNumAtoms()
     for atom_idx in range(pdbqt_mol.GetNumAtoms()):
       atom1 = pdbqt_mol.GetAtoms()[atom_idx]
       atom2 = pdbqt_mol.GetAtoms()[atom_idx]
-      assert_equal(atom1.GetAtomicNum(), atom2.GetAtomicNum())
+      assert atom1.GetAtomicNum() == atom2.GetAtomicNum()
