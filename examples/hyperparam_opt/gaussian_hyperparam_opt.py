75167dfc27f5fda860340f1d6b041e37868e8e73
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-23 21:15:16 -0700

    Fix examples

--- examples/hyperparam_opt/gaussian_hyperparam_opt.py
+++ examples/hyperparam_opt/gaussian_hyperparam_opt.py
@@ -3,24 +3,24 @@ np.random.seed(123)
 import tensorflow as tf
 tf.random.set_seed(123)
 import deepchem as dc
+import sklearn
 
 # Load delaney dataset
 delaney_tasks, delaney_datasets, transformers = dc.molnet.load_delaney()
 train, valid, test = delaney_datasets
 
 # Fit models
-regression_metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+
 
 def rf_model_builder(**model_params):
-  rf_params = {k:v for (k,v) in model_params.items() if k != 'model_dir'}
+  rf_params = {k: v for (k, v) in model_params.items() if k != 'model_dir'}
   model_dir = model_params['model_dir']
   sklearn_model = sklearn.ensemble.RandomForestRegressor(**rf_params)
   return dc.models.SklearnModel(sklearn_model, model_dir)
 
+
+params_dict = {"n_estimators": 30}
 optimizer = dc.hyper.GaussianProcessHyperparamOpt(rf_model_builder)
-best_hyper_params, best_performance = optimizer.hyperparam_search(
-    params_dict,
-    train_dataset,
-    valid_dataset,
-    transformers,
-    metric)
+best_model, best_params, all_results = optimizer.hyperparam_search(
+    params_dict, train, valid, transformers, metric)
---
+++ examples/hyperparam_opt/grid_hyperparam_opt.py
@@ -0,0 +1,26 @@
+import numpy as np
+np.random.seed(123)
+import tensorflow as tf
+tf.random.set_seed(123)
+import deepchem as dc
+import sklearn
+
+# Load delaney dataset
+delaney_tasks, delaney_datasets, transformers = dc.molnet.load_delaney()
+train, valid, test = delaney_datasets
+
+# Fit models
+metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+
+
+def rf_model_builder(**model_params):
+  rf_params = {k: v for (k, v) in model_params.items() if k != 'model_dir'}
+  model_dir = model_params['model_dir']
+  sklearn_model = sklearn.ensemble.RandomForestRegressor(**rf_params)
+  return dc.models.SklearnModel(sklearn_model, model_dir)
+
+
+params_dict = {"n_estimators": [10, 30, 50, 100]}
+optimizer = dc.hyper.GridHyperparamOpt(rf_model_builder)
+best_model, best_params, all_results = optimizer.hyperparam_search(
+    params_dict, train, valid, transformers, metric)
