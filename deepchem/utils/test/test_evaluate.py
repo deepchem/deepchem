40233ec686ca92558a506d0a0066f7f7ab4d0b3d
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-25 19:53:36 -0700

    Debugging

--- deepchem/utils/test/test_evaluate.py
+++ deepchem/utils/test/test_evaluate.py
@@ -21,21 +21,21 @@ class TestEvaluator(unittest.TestCase):
     assert len(multitask_scores) == 1
     assert multitask_scores['mae_score'] > 0
 
-#  def test_generator_evaluator_dc_metric_multitask(self):
-#    """Test generator evaluator on a dataset."""
-#    X = np.random.rand(10, 5)
-#    y = np.random.rand(10, 3)
-#    dataset = dc.data.NumpyDataset(X, y)
-#    model = dc.models.MultitaskRegressor(1, 5)
-#    generator = model.default_generator(dataset, pad_batches=False)
-#    transformers = []
-#    evaluator = GeneratorEvaluator(model, generator, transformers)
-#    metric = dc.metrics.Metric(dc.metrics.mae_score)
-#    multitask_scores = evaluator.compute_model_performance([metric])
-#    assert isinstance(multitask_scores, dict)
-#    assert len(multitask_scores) == 1
-#    assert multitask_scores['mae_score'] > 0
-#
+  def test_generator_evaluator_dc_metric_multitask(self):
+    """Test generator evaluator on a dataset."""
+    X = np.random.rand(10, 5)
+    y = np.random.rand(10, 3)
+    dataset = dc.data.NumpyDataset(X, y)
+    model = dc.models.MultitaskRegressor(1, 5)
+    generator = model.default_generator(dataset, pad_batches=False)
+    transformers = []
+    evaluator = GeneratorEvaluator(model, generator, transformers)
+    metric = dc.metrics.Metric(dc.metrics.mae_score)
+    multitask_scores = evaluator.compute_model_performance([metric])
+    assert isinstance(multitask_scores, dict)
+    assert len(multitask_scores) == 1
+    assert multitask_scores['mae_score'] > 0
+
 #  def test_generator_evaluator_dc_metric_multitask_single_point(self):
 #    """Test generator evaluator on a dataset."""
 #    X = np.random.rand(1, 5)
