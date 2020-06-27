b3463f05941b7a08f1e9564ceb1402f6f17f0ae2
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-26 18:02:59 -0700

    Fixes

--- deepchem/utils/test/test_evaluate.py
+++ deepchem/utils/test/test_evaluate.py
@@ -2,6 +2,7 @@
 import deepchem as dc
 import numpy as np
 import unittest
+import sklearn
 from deepchem.utils.evaluate import Evaluator
 from deepchem.utils.evaluate import GeneratorEvaluator
 
@@ -12,40 +13,72 @@ class TestEvaluator(unittest.TestCase):
     X = np.random.rand(10, 5)
     y = np.random.rand(10, 1)
     self.dataset = dc.data.NumpyDataset(X, y)
+    self.model = dc.models.MultitaskRegressor(1, 5)
 
   def test_evaluator_dc_metric(self):
     """Test an evaluator on a dataset."""
-    model = dc.models.MultitaskRegressor(1, 5)
-    transformers = []
-    evaluator = Evaluator(model, self.dataset, transformers)
+    evaluator = Evaluator(self.model, self.dataset, [])
     metric = dc.metrics.Metric(dc.metrics.mae_score)
-    multitask_scores = evaluator.compute_model_performance([metric])
+    multitask_scores = evaluator.compute_model_performance(metric)
     assert isinstance(multitask_scores, dict)
     assert len(multitask_scores) == 1
     assert multitask_scores['mae_score'] > 0
 
+  def test_evaluator_dc_multi_metric(self):
+    """Test an evaluator on a dataset."""
+    evaluator = Evaluator(self.model, self.dataset, [])
+    metric1 = dc.metrics.Metric(dc.metrics.mae_score)
+    metric2 = dc.metrics.Metric(dc.metrics.r2_score)
+    multitask_scores = evaluator.compute_model_performance(
+      [metric1, metric2])
+    assert isinstance(multitask_scores, dict)
+    assert len(multitask_scores) == 2
+    assert multitask_scores['mae_score'] > 0
+    assert "r2_score" in multitask_scores
+    
+    
+  def test_evaluator_sklearn_metric(self):
+    """Test an evaluator on a dataset."""
+    evaluator = Evaluator(self.model, self.dataset, [])
+    multitask_scores = evaluator.compute_model_performance(
+      sklearn.metrics.mean_absolute_error)
+    assert isinstance(multitask_scores, dict)
+    assert len(multitask_scores) == 1
+    # Note that since no name as provided, metrics are index by order
+    # given.
+    assert multitask_scores['metric-1'] > 0
+
+  def test_evaluator_sklearn_multi_metric(self):
+    """Test an evaluator on a dataset."""
+    evaluator = Evaluator(self.model, self.dataset, [])
+    multitask_scores = evaluator.compute_model_performance(
+      [sklearn.metrics.mean_absolute_error,
+       sklearn.metrics.r2_score])
+    assert isinstance(multitask_scores, dict)
+    assert len(multitask_scores.keys()) == 2
+    # Note that since no name as provided, metrics are index by order
+    # given.
+    assert multitask_scores['metric-1'] > 0
+    assert "metric-2" in multitask_scores
+
   def test_generator_evaluator_dc_metric_multitask(self):
     """Test generator evaluator on a generator."""
-    model = dc.models.MultitaskRegressor(1, 5)
-    generator = model.default_generator(
+    generator = self.model.default_generator(
       self.dataset, pad_batches=False)
-    transformers = []
-    evaluator = GeneratorEvaluator(model, generator, transformers)
+    evaluator = GeneratorEvaluator(self.model, generator, [])
     metric = dc.metrics.Metric(dc.metrics.mae_score)
-    multitask_scores = evaluator.compute_model_performance([metric])
+    multitask_scores = evaluator.compute_model_performance(metric)
     assert isinstance(multitask_scores, dict)
     assert len(multitask_scores) == 1
     assert multitask_scores['mae_score'] > 0
 
   def test_generator_evaluator_dc_metric_multitask_single_point(self):
     """Test generator evaluator on a generator."""
-    model = dc.models.MultitaskRegressor(1, 5)
-    generator = model.default_generator(
+    generator = self.model.default_generator(
       self.dataset, pad_batches=False)
-    transformers = []
-    evaluator = GeneratorEvaluator(model, generator, transformers)
+    evaluator = GeneratorEvaluator(self.model, generator, [])
     metric = dc.metrics.Metric(dc.metrics.mae_score)
-    multitask_scores = evaluator.compute_model_performance([metric])
+    multitask_scores = evaluator.compute_model_performance(metric)
     assert isinstance(multitask_scores, dict)
     assert len(multitask_scores) == 1
     print("multitask_scores")
@@ -54,11 +87,26 @@ class TestEvaluator(unittest.TestCase):
 
   def test_evaluator_dc_metric_singletask(self):
     """Test an evaluator on a dataset."""
-    model = dc.models.MultitaskRegressor(1, 5)
-    transformers = []
-    evaluator = Evaluator(model, self.dataset, transformers)
+    evaluator = Evaluator(self.model, self.dataset, [])
     metric = dc.metrics.Metric(dc.metrics.mae_score)
-    multitask_scores = evaluator.compute_model_performance([metric])
+    multitask_scores = evaluator.compute_model_performance(metric)
     assert isinstance(multitask_scores, dict)
     assert len(multitask_scores) == 1
     assert multitask_scores['mae_score'] > 0
+
+  def test_multiclass_classification_singletask(self):
+    """Test multiclass classification evaluation."""
+    X = np.random.rand(100, 5)
+    y = np.random.randint(5, size=(100,))
+    dataset = dc.data.NumpyDataset(X, y)
+    model = dc.models.MultitaskClassifier(1, 5, n_classes=5)
+    evaluator = Evaluator(model, dataset, [])
+    multitask_scores = evaluator.compute_model_performance(
+      sklearn.metrics.accuracy_score, n_classes=5)
+    assert len(multitask_scores) == 1
+    assert multitask_scores["metric-1"] >= 0
+
+# TODO: Add a multtiask metrics example
+# TODO: Add a multitask per-task metric example
+# TODO: Add metrics for images here as a test
+
