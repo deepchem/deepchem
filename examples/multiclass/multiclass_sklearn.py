7a5f663f168f779af5723e49e54b50a3de8e672f
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-06-28 17:57:57 -0700

    changes

--- examples/multiclass/multiclass_sklearn.py
+++ examples/multiclass/multiclass_sklearn.py
@@ -1,21 +1,18 @@
 import deepchem as dc
 import numpy as np
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 
-N = 10
+N = 100
 n_feat = 5
 n_classes = 3
-n_tasks = 1
 X = np.random.rand(N, n_feat)
-y = np.random.randint(3, size=(N, n_tasks))
+y = np.random.randint(3, size=(N,))
 dataset = dc.data.NumpyDataset(X, y)
 
 sklearn_model = RandomForestClassifier(
     class_weight="balanced", n_estimators=50)
 model = dc.models.SklearnModel(sklearn_model)
-
-# Fit models
-metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean)
 
 # Fit trained model
 print("About to fit model")
@@ -23,7 +20,8 @@ model.fit(dataset)
 model.save()
 
 print("About to evaluate model")
-train_scores = model.evaluate(dataset, [metric], [])
+train_scores = model.evaluate(dataset,
+    sklearn.metrics.roc_auc_score, [])
 
 print("Train scores")
 print(train_scores)
