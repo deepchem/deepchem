58095f0f3eac07976729db870f780a43963e5b96
peastman <peastman@stanford.edu>
2020-06-29 09:32:13 -0700

    Fixes to fit_on_batch() and iterbatches()

--- deepchem/models/keras_model.py
+++ deepchem/models/keras_model.py
@@ -415,7 +415,15 @@ class KerasModel(Model):
 
     return apply_gradient_for_batch
 
-  def fit_on_batch(self, X, y, w, variables=None, loss=None, callbacks=[]):
+  def fit_on_batch(self,
+                   X,
+                   y,
+                   w,
+                   variables=None,
+                   loss=None,
+                   callbacks=[],
+                   checkpoint=True,
+                   max_checkpoints_to_keep=5):
     """Perform a single step of training.
 
     Parameters
@@ -436,13 +444,18 @@ class KerasModel(Model):
     callbacks: function or list of functions
       one or more functions of the form f(model, step) that will be invoked after
       every step.  This can be used to perform validation, logging, etc.
-   """
-    if not self.built:
-      self.build()
+    checkpoint: bool
+      if true, save a checkpoint after performing the training step
+    max_checkpoints_to_keep: int
+      the maximum number of checkpoints to keep.  Older checkpoints are discarded.
+    """
+    self._ensure_built()
     dataset = NumpyDataset(X, y, w)
     return self.fit(
         dataset,
         nb_epoch=1,
+        max_checkpoints_to_keep=max_checkpoints_to_keep,
+        checkpoint_interval=self._global_step.numpy() + 2 if checkpoint else 0,
         variables=variables,
         loss=loss,
         callbacks=callbacks)
@@ -1061,8 +1074,8 @@ class KerasModel(Model):
         if True, copies the weights and bias associated with the final dense
         layer. Used only when assignment map is None
     inputs: List, input tensors for model
-        if not None, then the weights are built for both the source and self. 
-        This option is useful only for models that are built by 
+        if not None, then the weights are built for both the source and self.
+        This option is useful only for models that are built by
         subclassing tf.keras.Model, and not using the functional API by tf.keras
     """
     if inputs is not None:
