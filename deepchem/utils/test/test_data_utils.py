b268cdd8b1e273c8ebbe6d8e4a9d846e69c1f3be
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-07-08 18:58:14 -0700

    changes

---
+++ deepchem/utils/test/test_data_utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import deepchem as dc
+from deepchem.utils.data import datasetify
+
+
+def test_datasetify():
+  l = ["C", "CC"]
+  d = datasetify(l)
+  assert isinstance(d, dc.data.Dataset)
+  assert (d.ids == np.array(l)).all()
+
+  X = np.random.rand(5, 5)
+  d = datasetify(X)
+  assert (d.X == X).all()
+
+  dataset = dc.data.NumpyDataset(np.random.rand(5, 5))
+  d = datasetify(dataset)
+  assert (d.X == dataset.X).all()
