5a17bb15dffab029566d946c328d379dc96e58d5
Bharath Ramsundar <bharath@Bharaths-MBP.zyxel.com>
2020-07-15 16:55:22 -0700

    improving logging

--- deepchem/data/datasets.py
+++ deepchem/data/datasets.py
@@ -1485,7 +1485,7 @@ class DiskDataset(Dataset):
 
     def generator():
       for ind, dataset in enumerate(datasets):
-        loger.info("Merging in dataset %d" % ind)
+        loger.info("Merging in dataset %d/%d" % (ind, len(datasets)))
         X, y, w, ids = (dataset.X, dataset.y, dataset.w, dataset.ids)
         yield (X, y, w, ids)
 
