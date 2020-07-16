e08d2f714c0cdfae902366ac62dc5bec18567ed6
nd-02110114 <nd.12021218@gmail.com>
2020-07-16 14:25:17 +0900

    :art: just format

--- devtools/run_doctest.sh
+++ devtools/run_doctest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ $TRAVIS_PYTHON_VERSION == '3.7' ]; then
-    find ./deepchem -name "*.py" ! -name '*load_dataset_template.py' | xargs python -m doctest -v;
+  find ./deepchem -name "*.py" ! -name '*load_dataset_template.py' | xargs python -m doctest -v;
 fi
