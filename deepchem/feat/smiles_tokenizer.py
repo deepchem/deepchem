2e39ffbe147b063cf69d0f98379c99a8c62dbc1c
seyonechithrananda <seyonec@gmail.com>
2020-08-21 20:01:46 -0400

    add load_vocab method

--- deepchem/feat/smiles_tokenizer.py
+++ deepchem/feat/smiles_tokenizer.py
@@ -166,3 +166,14 @@ class BasicSmilesTokenizer(object):
         """
         tokens = [token for token in self.regex.findall(text)]
         return tokens
+
+
+def load_vocab(vocab_file):
+    """Loads a vocabulary file into a dictionary."""
+    vocab = collections.OrderedDict()
+    with open(vocab_file, "r", encoding="utf-8") as reader:
+        tokens = reader.readlines()
+    for index, token in enumerate(tokens):
+        token = token.rstrip("\n")
+        vocab[token] = index
+    return vocab
