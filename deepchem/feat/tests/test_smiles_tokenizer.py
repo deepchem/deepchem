2df051c6813cd947deb7ea747db51a6a603bf2ed
seyonechithrananda <seyonec@gmail.com>
2020-08-21 20:25:07 -0400

    update vocab file path

--- deepchem/feat/tests/test_smiles_tokenizer.py
+++ deepchem/feat/tests/test_smiles_tokenizer.py
@@ -13,6 +13,6 @@ class TestSmilesTokenizer(TestCase):
       model = RobertaForMaskedLM.from_pretrained('seyonec/SMILES_tokenized_PubChem_shard00_50k')
       model.num_parameters()
 
-      tokenizer = SmilesTokenizer('tokenizers/vocab.txt', max_len=model.config.max_position_embeddings)
+      tokenizer = SmilesTokenizer('deepchem/feat/tests/data/vocab.txt', max_len=model.config.max_position_embeddings)
       print(tokenizer.encode("CCC(CC)COC(=O)[C@H](C)N[P@](=O)(OC[C@H]1O[C@](C#N)([C@H](O)[C@@H]1O)C1=CC=C2N1N=CN=C2N)OC1=CC=CC=C1"))
 
