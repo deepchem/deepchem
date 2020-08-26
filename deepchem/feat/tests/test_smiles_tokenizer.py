845fefb51f36c64d0ceef4ba371641dd48005495
seyonechithrananda <seyonec@gmail.com>
2020-08-26 01:17:29 -0400

    add assertion

--- deepchem/feat/tests/test_smiles_tokenizer.py
+++ deepchem/feat/tests/test_smiles_tokenizer.py
@@ -12,6 +12,7 @@ class TestSmilesTokenizer(TestCase):
   def test_tokenize(self):
       current_dir = os.path.dirname(os.path.realpath(__file__))
       vocab_path = os.path.join(current_dir, 'data', 'vocab.txt')
+      tokenized_smiles = [12, 16, 16, 16, 17, 16, 16, 18, 16, 19, 16, 17, 22, 19, 18, 33, 17, 16, 18, 23, 181, 17, 22, 19, 18, 17, 19, 16, 33, 20, 19, 55, 17, 16, 38, 23, 18, 17, 33, 17, 19, 18, 35, 20, 19, 18, 16, 20, 22, 16, 16, 22, 16, 21, 23, 20, 23, 22, 16, 23, 22, 16, 21, 23, 18, 19, 16, 20, 22, 16, 16, 22, 16, 16, 22, 16, 20, 13]
 
       model = RobertaForMaskedLM.from_pretrained('seyonec/SMILES_tokenized_PubChem_shard00_50k')
       model.num_parameters()
@@ -19,3 +20,4 @@ class TestSmilesTokenizer(TestCase):
       tokenizer = SmilesTokenizer(vocab_path, max_len=model.config.max_position_embeddings)
       print(tokenizer.encode("CCC(CC)COC(=O)[C@H](C)N[P@](=O)(OC[C@H]1O[C@](C#N)([C@H](O)[C@@H]1O)C1=CC=C2N1N=CN=C2N)OC1=CC=CC=C1"))
 
+      assert tokenized_smiles == tokenizer.encode("CCC(CC)COC(=O)[C@H](C)N[P@](=O)(OC[C@H]1O[C@](C#N)([C@H](O)[C@@H]1O)C1=CC=C2N1N=CN=C2N)OC1=CC=CC=C1")
