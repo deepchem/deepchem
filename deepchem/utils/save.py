c7e0e48c5c7ed1641a2348eb37043f068aef929a
nd-02110114 <nd.12021218@gmail.com>
2020-09-16 21:19:42 +0900

    :green_heart: fix ci

--- deepchem/utils/save.py
+++ deepchem/utils/save.py
@@ -5,8 +5,8 @@
 # flake8: noqa
 import logging
 logger = logging.getLogger(__name__)
-logger.warn("deepchem.utils.save has been deprecated.\n"
-            "The utilities in save.py are moved to deepchem.utils.data_utils"
-            " or deepchem.utils.genomics_utils.")
+logger.warning("deepchem.utils.save has been deprecated.\n"
+               "The utilities in save.py are moved to deepchem.utils.data_utils"
+               " or deepchem.utils.genomics_utils.")
 from deepchem.utils.data_utils import *
 from deepchem.utils.genomics_utils import *
