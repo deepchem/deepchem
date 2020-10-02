e9dd3bb105bf05a52d5b24557de02c9ddb6a22e0
peastman <peastman@stanford.edu>
2020-10-02 13:52:57 -0700

    Changes to ButinaSplitter

--- deepchem/splits/splitters.py
+++ deepchem/splits/splitters.py
@@ -1015,14 +1015,26 @@ class ButinaSplitter(Splitter):
   This class requires RDKit to be installed.
   """
 
-  def split(self,
-            dataset: Dataset,
-            frac_train: float = 0.8,
-            frac_valid: float = 0.1,
-            frac_test: float = 0.1,
-            seed: Optional[int] = None,
-            log_every_n: Optional[int] = None,
-            cutoff: float = 0.18) -> Tuple[List[int], List[int], List]:
+  def __init__(self, cutoff: float = 0.6):
+    """Create a ButinaSplitter.
+
+    Parameters
+    ----------
+    cutoff: float (default 0.6)
+      The cutoff value for tanimoto similarity.  Molecules that are more similar
+      than this will tend to be put in the same dataset.
+    """
+    super(ButinaSplitter, self).__init__()
+    self.cutoff = cutoff
+
+  def split(
+      self,
+      dataset: Dataset,
+      frac_train: float = 0.8,
+      frac_valid: float = 0.1,
+      frac_test: float = 0.1,
+      seed: Optional[int] = None,
+      log_every_n: Optional[int] = None) -> Tuple[List[int], List[int], List]:
     """
     Splits internal compounds into train and validation based on the butina
     clustering algorithm. This splitting algorithm has an O(N^2) run time, where N
@@ -1047,19 +1059,12 @@ class ButinaSplitter(Splitter):
       Random seed to use.
     log_every_n: int, optional (default None)
       Log every n examples (not currently used).
-    cutoff: float, optional (default 0.18)
-      The cutoff value for similarity.
 
     Returns
     -------
     Tuple[List[int], List[int], List[int]]
       A tuple of train indices, valid indices, and test indices.
       Each indices is a list of integers and test indices is always an empty list.
-
-    Notes
-    -----
-    This function entirely disregards the ratios for frac_train, frac_valid,
-    and frac_test. Furthermore, it does not generate a test set, only a train and valid set.
     """
     try:
       from rdkit import Chem, DataStructs
@@ -1068,7 +1073,7 @@ class ButinaSplitter(Splitter):
     except ModuleNotFoundError:
       raise ValueError("This function requires RDKit to be installed.")
 
-    logger.info("Performing butina clustering with cutoff of", cutoff)
+    logger.info("Performing butina clustering with cutoff of", self.cutoff)
     mols = []
     for ind, smiles in enumerate(dataset.ids):
       mols.append(Chem.MolFromSmiles(smiles))
@@ -1081,26 +1086,26 @@ class ButinaSplitter(Splitter):
     for i in range(1, nfps):
       sims = DataStructs.BulkTanimotoSimilarity(fps[i], fps[:i])
       dists.extend([1 - x for x in sims])
-    scaffold_sets = Butina.ClusterData(dists, nfps, cutoff, isDistData=True)
+    scaffold_sets = Butina.ClusterData(
+        dists, nfps, self.cutoff, isDistData=True)
     scaffold_sets = sorted(scaffold_sets, key=lambda x: -len(x))
 
-    ys = dataset.y
-    valid_inds = []
-    for c_idx, cluster in enumerate(scaffold_sets):
-      # for m_idx in cluster:
-      valid_inds.extend(cluster)
-      # continue until we find an active in all the tasks, otherwise we can't
-      # compute a meaningful AUC
-      # TODO (ytz): really, we want at least one active and inactive in both scenarios.
-      # TODO (Ytz): for regression tasks we'd stop after only one cluster.
-      active_populations = np.sum(ys[valid_inds], axis=0)
-      if np.all(active_populations):
-        logger.info("# of actives per task in valid:", active_populations)
-        logger.info("Total # of validation points:", len(valid_inds))
-        break
+    train_cutoff = frac_train * len(dataset)
+    valid_cutoff = (frac_train + frac_valid) * len(dataset)
+    train_inds: List[int] = []
+    valid_inds: List[int] = []
+    test_inds: List[int] = []
 
-    train_inds = list(itertools.chain.from_iterable(scaffold_sets[c_idx + 1:]))
-    return train_inds, valid_inds, []
+    logger.info("About to sort in scaffold sets")
+    for scaffold_set in scaffold_sets:
+      if len(train_inds) + len(scaffold_set) > train_cutoff:
+        if len(train_inds) + len(valid_inds) + len(scaffold_set) > valid_cutoff:
+          test_inds += scaffold_set
+        else:
+          valid_inds += scaffold_set
+      else:
+        train_inds += scaffold_set
+    return train_inds, valid_inds, test_inds
 
 
 def _generate_scaffold(smiles: str, include_chirality: bool = False) -> str:
