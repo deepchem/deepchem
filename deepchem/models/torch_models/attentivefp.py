ee33411e99d6871f68e8fe83ba321a7d95261b6e
Ubuntu <ubuntu@ip-172-31-30-148.us-west-2.compute.internal>
2020-11-04 19:18:08 +0000

    Update

--- deepchem/models/torch_models/attentivefp.py
+++ deepchem/models/torch_models/attentivefp.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from deepchem.models.losses import Loss, L2Loss, SparseSoftmaxCrossEntropy
 from deepchem.models.torch_models.torch_model import TorchModel
 
+
 class AttentiveFP(nn.Module):
   """Model for Graph Property Prediction.
 
@@ -123,13 +124,14 @@ class AttentiveFP(nn.Module):
 
     from dgllife.model import AttentiveFPPredictor as DGLAttentiveFPPredictor
 
-    self.model = DGLAttentiveFPPredictor(node_feat_size=number_atom_features,
-                                         edge_feat_size=number_bond_features,
-                                         num_layers=num_layers,
-                                         num_timesteps=num_timesteps,
-                                         graph_feat_size=graph_feat_size,
-                                         n_tasks=out_size,
-                                         dropout=dropout)
+    self.model = DGLAttentiveFPPredictor(
+        node_feat_size=number_atom_features,
+        edge_feat_size=number_bond_features,
+        num_layers=num_layers,
+        num_timesteps=num_timesteps,
+        graph_feat_size=graph_feat_size,
+        n_tasks=out_size,
+        dropout=dropout)
 
   def forward(self, g):
     """Predict graph labels
@@ -174,7 +176,7 @@ class AttentiveFP(nn.Module):
 
 
 class AttentiveFPModel(TorchModel):
-    """Model for Graph Property Prediction.
+  """Model for Graph Property Prediction.
 
       This model proceeds as follows:
 
@@ -212,21 +214,21 @@ class AttentiveFPModel(TorchModel):
       (https://github.com/awslabs/dgl-lifesci) to be installed.
       """
 
-    def __init__(self,
-                 n_tasks: int,
-                 num_layers: int = 2,
-                 num_timesteps: int = 2,
-                 graph_feat_size: int = 200,
-                 dropout: float = 0.,
-                 mode: str = 'regression',
-                 number_atom_features: int = 30,
-                 number_bond_features: int = 11,
-                 n_classes: int = 2,
-                 nfeat_name: str = 'x',
-                 efeat_name: str = 'edge_attr',
-                 self_loop: bool = True,
-                 **kwargs):
-        """
+  def __init__(self,
+               n_tasks: int,
+               num_layers: int = 2,
+               num_timesteps: int = 2,
+               graph_feat_size: int = 200,
+               dropout: float = 0.,
+               mode: str = 'regression',
+               number_atom_features: int = 30,
+               number_bond_features: int = 11,
+               n_classes: int = 2,
+               nfeat_name: str = 'x',
+               efeat_name: str = 'edge_attr',
+               self_loop: bool = True,
+               **kwargs):
+    """
             Parameters
             ----------
             n_tasks: int
@@ -263,31 +265,31 @@ class AttentiveFPModel(TorchModel):
             kwargs
                 This can include any keyword argument of TorchModel.
             """
-        model = AttentiveFP(
-            n_tasks=n_tasks,
-            num_layers=num_layers,
-            num_timesteps=num_timesteps,
-            graph_feat_size=graph_feat_size,
-            dropout=dropout,
-            mode=mode,
-            number_atom_features=number_atom_features,
-            number_bond_features=number_bond_features,
-            n_classes=n_classes,
-            nfeat_name=nfeat_name,
-            efeat_name=efeat_name)
-        if mode == 'regression':
-            loss: Loss = L2Loss()
-            output_types = ['prediction']
-        else:
-            loss = SparseSoftmaxCrossEntropy()
-            output_types = ['prediction', 'loss']
-        super(AttentiveFPModel, self).__init__(
-            model, loss=loss, output_types=output_types, **kwargs)
+    model = AttentiveFP(
+        n_tasks=n_tasks,
+        num_layers=num_layers,
+        num_timesteps=num_timesteps,
+        graph_feat_size=graph_feat_size,
+        dropout=dropout,
+        mode=mode,
+        number_atom_features=number_atom_features,
+        number_bond_features=number_bond_features,
+        n_classes=n_classes,
+        nfeat_name=nfeat_name,
+        efeat_name=efeat_name)
+    if mode == 'regression':
+      loss: Loss = L2Loss()
+      output_types = ['prediction']
+    else:
+      loss = SparseSoftmaxCrossEntropy()
+      output_types = ['prediction', 'loss']
+    super(AttentiveFPModel, self).__init__(
+        model, loss=loss, output_types=output_types, **kwargs)
 
-        self._self_loop = self_loop
+    self._self_loop = self_loop
 
-    def _prepare_batch(self, batch):
-        """Create batch data for AttentiveFP.
+  def _prepare_batch(self, batch):
+    """Create batch data for AttentiveFP.
 
             Parameters
             ----------
@@ -306,16 +308,16 @@ class AttentiveFPModel(TorchModel):
             weights: list of torch.Tensor or None
                 The weights for each sample or sample/task pair converted to torch.Tensor.
             """
-        try:
-            import dgl
-        except:
-            raise ImportError('This class requires dgl.')
+    try:
+      import dgl
+    except:
+      raise ImportError('This class requires dgl.')
 
-        inputs, labels, weights = batch
-        dgl_graphs = [
-            graph.to_dgl_graph(self_loop=self._self_loop) for graph in inputs[0]
-        ]
-        inputs = dgl.batch(dgl_graphs).to(self.device)
-        _, labels, weights = super(AttentiveFPModel, self)._prepare_batch(([], labels,
-                                                                           weights))
-        return inputs, labels, weights
+    inputs, labels, weights = batch
+    dgl_graphs = [
+        graph.to_dgl_graph(self_loop=self._self_loop) for graph in inputs[0]
+    ]
+    inputs = dgl.batch(dgl_graphs).to(self.device)
+    _, labels, weights = super(AttentiveFPModel, self)._prepare_batch(
+        ([], labels, weights))
+    return inputs, labels, weights
