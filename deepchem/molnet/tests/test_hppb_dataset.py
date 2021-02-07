3ee5a52b27019679a0ec78658a67e8a3feb190d5
Minh Triet <minhtriet09@gmail.com>
2021-02-07 23:19:43 +0000

    add test

---
+++ deepchem/molnet/tests/test_hppb_dataset.py
@@ -0,0 +1,11 @@
+import deepchem as dc
+from deepchem.feat.molecule_featurizers import RawFeaturizer
+
+def test_multi_featurizer_hppb():
+    """
+    Expect this to exit with status 0.
+    """
+    feat = RawFeaturizer()
+    tasks, datasets, transformers = dc.molnet.load_hppb(featurizer=feat, reload=False)
+
+    tasks, datasets, transformers = dc.molnet.load_hppb(featurizer="ECFP", reload=False)
