713215d91260d88640306d1ed54e73713d3f4eab
yurievnamaria <yurievnamaria@gmail.com>
2021-02-25 13:06:58 +0100

    typo fix

--- deepchem/feat/graph_features.py
+++ deepchem/feat/graph_features.py
@@ -636,11 +636,10 @@ class ConvMolFeaturizer(MolecularFeaturizer):
   >>> smiles = ["C", "CCC"]
   >>> featurizer=dc.feat.ConvMolFeaturizer(per_atom_fragmentation=False)
   >>> f = featurizer.featurize(smiles)
-  Using ConvMolFeaturizer to create featurized fragments derived from molecules of interest.
-  This is used only in the context of performing interpretation of models using atomic
-  contributions (atom-based model interpretation)
-  >>> import deepchem as dc
-  >>> smiles = ["C", "CCC"]
+  >>> # Using ConvMolFeaturizer to create featurized fragments derived from molecules of interest.
+  ... # This is used only in the context of performing interpretation of models using atomic
+  ... # contributions (atom-based model interpretation)
+  ... smiles = ["C", "CCC"]
   >>> featurizer=dc.feat.ConvMolFeaturizer(per_atom_fragmentation=True)
   >>> f = featurizer.featurize(smiles)
   >>> len(f) # contains 2 lists with  featurized fragments from 2 mols
