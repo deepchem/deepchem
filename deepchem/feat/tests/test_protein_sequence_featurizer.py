d149bc5fef5663cafc9711ad301163a6c33099e3
alat-rights <alanamaemi@tutanota.com>
2021-06-08 09:52:43 +0900

    Add test case for protein sequence encoding and untransform

---
+++ deepchem/feat/tests/test_protein_sequence_featurizer.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from deepchem.feat.protein_featurizers import ProteinSequenceFeaturizer
+
+class TestProteinSequenceFeaturizer(unittest.TestCase):
+  """
+  Test ProteinSequenceFeaturizer
+  """
+
+  def test_protein_sequence_featurizer(self):
+    """
+    Test correct protein to integer conversion and untransform
+    """
+    ref_seq = "Met Ser Arg Gly Asp Glu Stop"
+    ref_int_seq = (4, 6, 20, 23, 16, 17, 12)
+    featurizer = ProteinSequenceFeaturizer()
+    int_seq = featurizer(ref_seq)
+    assert ref_int_seq == int_seq
+
+    # untransform
+    seq = featurizer.untransform(int_seq)
+    assert ref_seq == seq
