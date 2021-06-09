2edc0a6e22a3a608a94c99c19fc0a3ab60bfcdc6
alat-rights <alanamaemi@tutanota.com>
2021-06-10 08:28:42 +0900

    added test for RNA

--- deepchem/feat/tests/test_codon_featurizer.py
+++ deepchem/feat/tests/test_codon_featurizer.py
@@ -12,8 +12,8 @@ class TestCodonFeaturizer(unittest.TestCase):
     Test correct protein to integer conversion and untransform
     """
     ref_seq = "Met Ser Arg Gly Asp Glu Stop"
-    ref_int_seq = (4, 6, 20, 23, 16, 17, 12)
-    featurizer = ProteinSequenceFeaturizer()
+    ref_int_seq = (-1, 14, 18, 19, 24, 22, 23, 4) # First digit indicates sequence is protein
+    featurizer = CodonFeaturizer()
     int_seq = featurizer(ref_seq)
     assert ref_int_seq == int_seq
 
@@ -25,4 +25,13 @@ class TestCodonFeaturizer(unittest.TestCase):
     """
     Test correct RNA to integer conversion and untransform
     """
-    ref_seq = 
+    ref_seq = "AUGAGUAGGGGUGAUGAGUAG"
+    ref_int_seq = (-2, 35, 44, 47, 60, 56, 59, 11) # Last digit indicates sequence is RNA
+    featurizer = CodonFeaturizer()
+    int_seq = featurizer(ref_seq)
+    assert ref_int_seq == int_seq
+
+    # untransform
+    seq = featurizer.untransform(int_seq)
+    assert ref_seq == seq
+
