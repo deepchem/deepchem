b6c2c78d483b742dbf636b727c87ace9aa98e59c
alat-rights <alanamaemi@tutanota.com>
2021-06-10 20:14:19 +0900

    rename

--- deepchem/feat/tests/test_protein_tokenizer.py
+++ deepchem/feat/tests/test_protein_tokenizer.py
@@ -1,23 +1,22 @@
 import unittest
 
-from deepchem.feat import CodonFeaturizer
+from deepchem.feat import ProteinTokenizer
 
-class TestCodonFeaturizer(unittest.TestCase):
+class TestProteinTokenizer(unittest.TestCase):
   """
-  Test CodonFeaturizer
+  Test ProteinTokenizer
   """
 
-  def test_protein_sequence_featurization(self):
+  def test_protein_tokenizer(self):
     """
     Test correct protein to integer conversion and untransform
     """
     ref_seq = "Met Ser Arg Gly Asp Glu Stop"
     ref_int_seq = (-1, 14, 18, 19, 24, 22, 23, 4) # First digit indicates sequence is protein
-    featurizer = CodonFeaturizer()
-    int_seq = featurizer(ref_seq)
+    tokenizer = ProteinTokenizer()
+    int_seq = tokenizer(ref_seq)
     assert ref_int_seq == int_seq
 
     # untransform
-    seq = featurizer.untransform(int_seq)
+    seq = tokenizer.untransform(int_seq)
     assert ref_seq == seq
-
