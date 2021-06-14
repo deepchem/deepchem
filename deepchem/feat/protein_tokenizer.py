628125a787a4a09fc0f63b6b17008049ce21ccad
alat-rights <>
2021-06-14 16:42:14 +0900

    Debugging and documentation

--- deepchem/feat/protein_tokenizer.py
+++ deepchem/feat/protein_tokenizer.py
@@ -6,38 +6,79 @@ logger = logging.getLogger(__name__)
 from deepchem.feat.base_classes import Featurizer
 from typing import Iterable
 
+# Integer tokens for every amino acid code in the FASTA format.
 FASTA_tokens = {
   "A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8, "J":9, "K":10, "L":11, "M":12, "N":13, "O":14, "P":15, "Q":16, "R":17, "S":18, "T":19, "U":20, "V":21, "W":22, "Y":23, "Z":24, "X":25, "*":26, "-":27
 }
 
 class ProteinTokenizer(Featurizer):
-  def featurize(self, proteins: Iterable[str], log_every_n: int = 1000) -> np.ndarray:
+  """Tokenizes protein sequences in FASTA-format with start and end annotations
+  into integer arrays.
+
+  This tokenizer takes in an array of FASTA-format protein sequence strings 
+  that are terminated on both ends with [CLS] and [SEP] annotations, and
+  converts each string into a matching array of integer tokens.
+
+  The tokenizer is case-insensitive and safely ignores all non-FASTA characters 
+  (spaces, newlines, commas, etc.).
+
+  eg. ["EMV[CLS]ABCDE[SEP]X", "WMY[CLS]FGH[SEP]XAB"] -> [[0, 1, 2, 3, 4], [5, 6, 7]]
+
+  """
+
+  def featurize(self, proteins: Iterable[str],
+                log_every_n: int = 1000) -> np.ndarray:
+    """Tokenizes protein sequences.
+
+    Parameters
+    ----------
+    proteins: Iterable[str]
+      An iterable (list, array, etc.) of FASTA-format protein sequence strings
+      eg. ["ABCD[CLS]EFGHIJK[SEP]LMNOP", "[CLS]A[SEP]"]
+      Newlines, spaces, and commas in protein sequence strings are safely ignored.
+    log_every_n: int, optional (default 1000)
+      How many proteins are tokenized every time a tokenization is logged.
+    """
     # Return empty array if no proteins are provided as input
     if (len(proteins) <= 0): 
       return np.ndarray([])
     # Calls featurize() in parent class, which will call _featurize() for each protein in proteins
     return Featurizer.featurize(self, proteins, log_every_n)
 
-  def untransform(self, input_sequences: Iterable) -> str:
+  def untransform(self, input_sequences: Iterable[int]) -> tuple:
+    """Convert from tokenized arrays back into original string.
+
+    Parameters
+    ----------
+    input_sequences: Iterable[int]
+      Iterable of the token arrays to be untransformed.
+    
+    Returns
+    -------
+    tuple
+      Tuple of strings, where each element is a protein sequence.
+    """
     acid_codes = list(FASTA_tokens.keys()) # List of all keys in FASTA_tokens
     all_tokens = list(FASTA_tokens.values()) # List of all values in FASTA_tokens
-    output_acid_codes = "" # FASTA amino acid codes for values in input
+    output_acid_codes = tuple() # FASTA amino acid codes for values in input
     # Iterating through input_tokens
     for sequence in input_sequences:
+      token_codes = ""
       for token in sequence:
         idx = all_tokens.index(token) # Get index of token
         code = acid_codes[idx] # Get corresponding key (FASTA amino acid code)
-        output_acid_codes = output_acid_codes + code
+        token_codes = token_codes + code
+      output_acid_codes = output_acid_codes + (token_codes,)
     return output_acid_codes
 
   def _extract_relevant_sequence(self, protein: str) -> str:
-    first_idx = -1
+    first_idx = -1 # Will be set to first string index after [CLS]
     for i in range(len(protein)-4):
       if (first_idx == -1 and protein[i:i+5] == "[CLS]"):
         first_idx = i+5
         i = first_idx
       elif (first_idx != -1 and protein[i:i+5] == "[SEP]"):
-        return(protein[first_idx:i])
+        return(protein[first_idx:i]) # Note: i is index of "[" in "[SEP]"
     logger.info(f"[CLS] [SEP] pair not found for protein {protein}. Skipping...")
     return("")
 
@@ -45,11 +86,12 @@ class ProteinTokenizer(Featurizer):
     protein = protein.upper()
     protein = self._extract_relevant_sequence(protein)
     protein = protein.split()
-    tokens = np.array([])
-    for acid in protein:
-      if acid in FASTA_tokens:
+    tokens = np.array([], dtype=int) # Array of tokens for the protein sequence
+    for acid in protein: # Loops through amino acid codes in protein sequence
+      if acid in FASTA_tokens: # Tokenize amino acid
         token = FASTA_tokens.get(acid)
         tokens = np.append(tokens, token)
-      else:
+      else: # Ignore invalid FASTA code
         logger.info(f"Invalid FASTA amino acid code {acid}, skipping...")
+    print(tokens)
     return tokens
--- deepchem/feat/tests/test_protein_tokenizer.py
+++ deepchem/feat/tests/test_protein_tokenizer.py
@@ -12,29 +12,29 @@ class TestProteinTokenizer(unittest.TestCase):
     """
     Test correct protein to integer conversion and untransform
     """
-    ref_seq = np.array(["[CLS] A B C D E F G [SEP]"])
+    ref_seq = np.array(["AMC[CLS] A B C D E F G [SEP] E R"])
     tokenizer = ProteinTokenizer()
     int_seq = tokenizer(ref_seq)
     assert np.all(int_seq == [[0, 1, 2, 3, 4, 5, 6]])
 
     # untransform
     seq = tokenizer.untransform(int_seq)
-    assert (seq == "ABCDEFG")
+    assert (seq == ("ABCDEFG",))
 
   def test_protein_tokenizer_multiple_sequences(self):
     """
     Test correct protein to integer conversion and untransform for multiple
     FASTA strings.
     """
-    ref_seq = np.array(["[CLS] A B C D E F G [SEP]", "[CLS] H I J K L [SEP]"])
+    ref_seq = np.array(["[CLS] A B C D E F G [SEP] EH", "ABC[CLS] H I J K L [SEP]"])
     tokenizer = ProteinTokenizer()
     int_seq = tokenizer(ref_seq)
     ref_int_seq = []
-    ref_int_seq.append(np.array([0., 1., 2., 3., 4., 5., 6.]))
-    ref_int_seq.append(np.array([7., 8., 9., 10., 11.]))
+    ref_int_seq.append(np.array([0, 1, 2, 3, 4, 5, 6]))
+    ref_int_seq.append(np.array([7, 8, 9, 10, 11]))
     ref_int_seq_asarray = np.asarray(ref_int_seq)
     assert str(int_seq) == str(ref_int_seq_asarray)
 
     # untransform
     seq = tokenizer.untransform(int_seq)
-    assert (seq == "ABCDEFGHIJKL")
+    assert (seq == ("ABCDEFG","HIJKL"))
