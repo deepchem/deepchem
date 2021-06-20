cc493db066dfd572b313b551408e8ec0c34480bc
VIGNESHinZONE <mvenkataraman@ph.iitr.ac.in>
2021-06-21 01:48:56 +0530

    first commit

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ else:
   # Build a nightly package by default.
   IS_RELEASE = False
 
+# Environment-specific dependencies.
+extras = {'jax': ['dm-haiku==0.0.3', 'optax==0.0.8']}
+
 
 # get the version from deepchem/__init__.py
 def _get_version():
@@ -69,4 +72,5 @@ setup(
         'scikit-learn',
         'scipy',
     ],
+    extras_require=extras,
     python_requires='>=3.5')
