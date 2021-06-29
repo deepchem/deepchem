08080bc515ea946b9cfa9d8383e2fff2c46b29ce
Suzukazole <65379684+Suzukazole@users.noreply.github.com>
2021-06-30 00:41:05 +0530

    added example

--- deepchem/feat/base_classes.py
+++ deepchem/feat/base_classes.py
@@ -428,7 +428,23 @@ class DummyFeaturizer(Featurizer):
   This is useful when the raw dataset has to be used without
   featurizing the examples.
 
-
+  Examples
+  --------
+  >>> import deepchem as dc
+  >>> smi_map = [[
+         "N#C[S-].O=C(CBr)c1ccc(C(F)(F)F)cc1>CCO.[K+]",
+          "N#CSCC(=O)c1ccc(C(F)(F)F)cc1"
+      ], [
+          "C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O",
+          "FCC(c1cccc(Br)n1)N1CCOCC1"
+      ]]
+  >>> Featurizer = dc.feat.DummyFeaturizer()
+  >>> smi_feat = Featurizer.featurize(smi_map)
+  >>> smi_feat
+  array([['N#C[S-].O=C(CBr)c1ccc(C(F)(F)F)cc1>CCO.[K+]',
+          'N#CSCC(=O)c1ccc(C(F)(F)F)cc1'],
+         ['C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O',
+          'FCC(c1cccc(Br)n1)N1CCOCC1']], dtype='<U55')
   """
 
   def featurize(self, datapoints: Iterable[Any],
