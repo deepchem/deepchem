fa3537c1800ce710efa625d39f05c9ad55fd6828
Suzukazole <65379684+Suzukazole@users.noreply.github.com>
2021-06-30 01:05:53 +0530

    fix doctest error

--- deepchem/feat/base_classes.py
+++ deepchem/feat/base_classes.py
@@ -431,20 +431,17 @@ class DummyFeaturizer(Featurizer):
   Examples
   --------
   >>> import deepchem as dc
-  >>> smi_map = [[
-         "N#C[S-].O=C(CBr)c1ccc(C(F)(F)F)cc1>CCO.[K+]",
-          "N#CSCC(=O)c1ccc(C(F)(F)F)cc1"
-      ], [
-          "C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O",
-          "FCC(c1cccc(Br)n1)N1CCOCC1"
-      ]]
+  >>> smi_map = [["N#C[S-].O=C(CBr)c1ccc(C(F)(F)F)cc1>CCO.[K+]",
+  ...             "N#CSCC(=O)c1ccc(C(F)(F)F)cc1"],
+  ...           ["C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O",
+  ...             "FCC(c1cccc(Br)n1)N1CCOCC1"]]
   >>> Featurizer = dc.feat.DummyFeaturizer()
   >>> smi_feat = Featurizer.featurize(smi_map)
   >>> smi_feat
   array([['N#C[S-].O=C(CBr)c1ccc(C(F)(F)F)cc1>CCO.[K+]',
-          'N#CSCC(=O)c1ccc(C(F)(F)F)cc1'],
-         ['C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O',
-          'FCC(c1cccc(Br)n1)N1CCOCC1']], dtype='<U55')
+  ...     'N#CSCC(=O)c1ccc(C(F)(F)F)cc1'],
+  ...    ['C1COCCN1.FCC(Br)c1cccc(Br)n1>CCN(C(C)C)C(C)C.CN(C)C=O.O',
+  ...     'FCC(c1cccc(Br)n1)N1CCOCC1']], dtype='<U55')
   """
 
   def featurize(self, datapoints: Iterable[Any],
