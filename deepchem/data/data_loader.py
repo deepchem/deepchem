a578d08a830e9977943743288c6aceb2583b1843
alat-rights <>
2021-07-02 11:46:02 +0900

    yapf/flake8

--- deepchem/data/data_loader.py
+++ deepchem/data/data_loader.py
@@ -1002,7 +1002,7 @@ class FASTALoader(DataLoader):
             if line[-1:] == '\n':  # Check last character in string
               line = line[0:-1]  # Remove last character
             sequence = np.append(sequence, line)
-        sequences = _add_sequence(sequences, sequence) # Add last sequence
+        sequences = _add_sequence(sequences, sequence)  # Add last sequence
         return sequences
 
       def _add_sequence(sequences: np.array, sequence: np.array) -> np.array:
