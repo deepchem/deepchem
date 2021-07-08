42df36b34752752b7dd2b72e2217a1d1d668c8d7
alat-rights <>
2021-07-08 15:31:49 +0900

    yapf/flake

--- deepchem/data/tests/test_fasta_loader.py
+++ deepchem/data/tests/test_fasta_loader.py
@@ -40,12 +40,14 @@ class TestFASTALoader(unittest.TestCase):
     assert sequences.X.shape == (3, 58, 5)
 
   def test_fasta_one_hot_big(self):
-    protein = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-               '*', '-']
+    protein = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '*', '-'
+    ]
     input_file = os.path.join(self.current_dir,
                               "../../data/tests/uniprot_truncated.fasta")
-    loader = dc.data.FASTALoader(OneHotFeaturizer(charset=protein, max_length=1000), legacy=False)
+    loader = dc.data.FASTALoader(
+        OneHotFeaturizer(charset=protein, max_length=1000), legacy=False)
     sequences = loader.create_dataset(input_file)
 
     assert sequences.X.shape
@@ -59,5 +61,5 @@ class TestFASTALoader(unittest.TestCase):
     # Due to FASTALoader redesign, expected shape is now (3, 58, 5)
 
     assert sequences.X.shape == (3, 58, 5)
-   
+
   # TODO: test with full uniprot file once sharding support is added.
