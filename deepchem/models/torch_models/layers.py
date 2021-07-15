2865bec7430bcd97f0c1e6a13e98be39bbc82cd6
Atreya Majumdar <atreyamaj@gmail.com>
2021-07-15 14:52:12 +0530

    flake8

--- deepchem/models/torch_models/layers.py
+++ deepchem/models/torch_models/layers.py
@@ -1,27 +1,27 @@
 try:
-    import torch
-    import torch.nn as nn
-    from torch.nn.init import _calculate_fan_in_and_fan_out, _no_grad_normal_, _no_grad_uniform_
+  import torch
+  import torch.nn as nn
 except:
-    raise ImportError('These classes require Torch to be installed.')
+  raise ImportError('These classes require Torch to be installed.')
+
 
 class LayerNorm(nn.Module):
-    """Apply Layer Normalization to input.
-    
+  """Apply Layer Normalization to input.
+
     The layer takes input and applies layer Normalization to it.
-    
+
     References
     ----------
     .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
     """
 
-    def __init__(self, features, eps=1e-6):
-        super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
-        self.eps = eps
+  def __init__(self, features, eps=1e-6):
+    super(LayerNorm, self).__init__()
+    self.a_2 = nn.Parameter(torch.ones(features))
+    self.b_2 = nn.Parameter(torch.zeros(features))
+    self.eps = eps
 
-    def forward(self, x):
-        mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+  def forward(self, x):
+    mean = x.mean(-1, keepdim=True)
+    std = x.std(-1, keepdim=True)
+    return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
