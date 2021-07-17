41c8989fcf0c9103349701fe7b1ba465ddc9b0d1
Suzukazole <65379684+Suzukazole@users.noreply.github.com>
2021-07-17 20:45:14 +0530

    flake//yapf

--- deepchem/trans/tests/test_rxn_transform.py
+++ deepchem/trans/tests/test_rxn_transform.py
@@ -1,38 +1,41 @@
-import os 
 import unittest
 import numpy as np
 
 from deepchem.trans.transformers import RxnSplitTransformer
 
+reactions: np.ndarray = np.array(
+    [
+        "CC(C)C[Mg+].CON(C)C(=O)c1ccc(O)nc1>C1CCOC1.[Cl-]>CC(C)CC(=O)c1ccc(O)nc1",
+        "CN.O=C(O)c1ccc(Cl)c([N+](=O)[O-])c1>O>CNc1ccc(C(=O)O)cc1[N+](=O)[O-]",
+        "CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(N)cc3)cc21.O=CO>>CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(NC=O)cc3)cc21"
+    ],
+    dtype=object)
 
-reactions: np.ndarray = np.array(["CC(C)C[Mg+].CON(C)C(=O)c1ccc(O)nc1>C1CCOC1.[Cl-]>CC(C)CC(=O)c1ccc(O)nc1",
-"CN.O=C(O)c1ccc(Cl)c([N+](=O)[O-])c1>O>CNc1ccc(C(=O)O)cc1[N+](=O)[O-]",
-"CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(N)cc3)cc21.O=CO>>CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(NC=O)cc3)cc21"], dtype=object)
-
-sep_split = (np.array([['CC(C)C[Mg+].CON(C)C(=O)c1ccc(O)nc1>C1CCOC1.[Cl-]',
-        'CC(C)CC(=O)c1ccc(O)nc1'],
-       ['CN.O=C(O)c1ccc(Cl)c([N+](=O)[O-])c1>O',
-        'CNc1ccc(C(=O)O)cc1[N+](=O)[O-]'],
-       ['CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(N)cc3)cc21.O=CO>',
-        'CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(NC=O)cc3)cc21']], dtype='<U51'), [], [], [])
+# sep_split: Tuple[np.ndarray, ...] = (np.array([['CC(C)C[Mg+].CON(C)C(=O)c1ccc(O)nc1>C1CCOC1.[Cl-]',
+#        'CC(C)CC(=O)c1ccc(O)nc1'],
+#       ['CN.O=C(O)c1ccc(Cl)c([N+](=O)[O-])c1>O',
+#        'CNc1ccc(C(=O)O)cc1[N+](=O)[O-]'],
+#       ['CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(N)cc3)cc21.O=CO>',
+#        'CCn1cc(C(=O)O)c(=O)c2cc(F)c(-c3ccc(NC=O)cc3)cc21']], dtype='<U51'), [], [], [])
 
 
 class TestRxnSplitTransformer(unittest.TestCase):
-    """Tests the Reaction split transformer for the source/target splitting and
+  """Tests the Reaction split transformer for the source/target splitting and
     for the reagent mixing operation.
     """
 
-    def test_split(self):
-        """
+  def test_split(self):
+    """
         Tests the source/target split from an input reaction SMILES.
         """
-        trans = RxnSplitTransformer(sep_reagent=True)
-        split_reactions = trans.transform_array(X=reactions, y=[], w=[], ids=[])
-        assert split_reactions[0].shape == (3, 2)
+    trans = RxnSplitTransformer(sep_reagent=True)
+    split_reactions = trans.transform_array(X=reactions, y=[], w=[], ids=[])
+    assert split_reactions[0].shape == (3, 2)
+    # Should check for equality of split_reactions and sep_split!
 
-
-    #def test_mixing(self):
-    #    """
-    #    Tests the reagent - reactant mixing option.
-    #    """
-    #    pass 
+  # def test_mixing(self):
+  #    """
+  #    Tests the reagent - reactant mixing option.
+  #    """
+  # WIP
+  #    pass
