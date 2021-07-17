04634cad81fde12b1c57a261eccf9a5fbff1d977
Atreya Majumdar <atreyamaj@gmail.com>
2021-07-17 17:03:32 +0530

    Updated Docs

--- deepchem/models/torch_models/layers.py
+++ deepchem/models/torch_models/layers.py
@@ -9,23 +9,33 @@ except:
 class ScaleNorm(nn.Module):
   """Apply Scale Normalization to input.
 
-    All G values are initialized to sqrt(d).
+  The ScaleNorm layer first computes the square root of the scale, then computes the matrix/vector norm of the input tensor.
+  The norm value is calculated as `sqrt(scale) / matrix norm`. 
+  Finally, the result is returned as `input_tensor * norm value`.
 
-    References
-    ----------
-    .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
-    """
+  References
+  ----------
+  .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
+  
+  Examples
+  --------
+  >>> import deepchem as dc
+  >>> scale = 0.35
+  >>> layer = dc.models.torch_models.ScaleNorm(scale)
+  >>> input_tensor = torch.tensor([[1.269, 39.36], [0.00918, -9.12]])
+  >>> output_tensor = layer.forward(input_tensor)
+  """
 
   def __init__(self, scale, eps=1e-5):
     """Initialize a ScaleNorm layer.
 
-        Parameters
-        ----------
-        scale: Real number or single element tensor
-          Scale magnitude.
-        eps: float
-          Epsilon value.
-        """
+    Parameters
+    ----------
+    scale: Real number or single element tensor
+      Scale magnitude.
+    eps: float
+      Epsilon value.
+    """
 
     super(ScaleNorm, self).__init__()
     self.scale = nn.Parameter(torch.tensor(math.sqrt(scale)))
