0208abda95e3f6c8ff52f1e6fd9229345f7e2f49
alat-rights <>
2021-07-22 03:29:05 +0900

    stop relying on RobertaTokenizer

--- deepchem/feat/bert_tokenizer.py
+++ deepchem/feat/bert_tokenizer.py
@@ -5,21 +5,12 @@ import numpy as np
 import logging, warnings
 logger = logging.getLogger(__name__)
 try:
-  from transformers import BertTokenizerFast, RobertaTokenizerFast
+  from transformers import BertTokenizerFast
 except:
   raise ImportError("""This class requires the transformers package,
                     which was not found in your environment.""")
-# TODO discuss RDKitMol support (perhaps RobertaFeaturizer should not be a moleule featurizer?
-# Current approach to handling both molecules and strings is a bit messy.
-try:
-  from deepchem.utils.typing import RDKitMol
-except:
-  warnings.warn("""RDKitMol was not found in your environment, so this class
-                will only featurize strings and will not featurize RDKitMol
-                objects.""")
-                
 
-class BertFeaturizer(RobertaFeaturizer, BertTokenizerFast):
+class BertFeaturizer(Featurizer, BertTokenizerFast):
   """Bert Featurizer.
 
   The Bert Featurizer is a wrapper class for HuggingFace's BertTokenizerFast.
@@ -27,39 +18,42 @@ class BertFeaturizer(RobertaFeaturizer, BertTokenizerFast):
   This class intends to allow users to use the BertTokenizer API while
   remaining inside the DeepChem ecosystem.
 
-  Usage Example (based on HuggingFace and RostLab ProtBert docs)
-  --------------------------------------------------------------
+  Example
+  -------
   >>> from deepchem.feat import BertFeaturizer
   >>> featurizer = BertFeaturizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False)
   >>> inputs = featurizer.featurize('D L I P [MASK] L V T', return_tensors="pt")
 
   Notes
   -----
-  This class inherits first from RobertaFeaturizer and then from
-  BertTokenizerFast. Its logic relies on super() calls in RobertaFeaturizer
-  being delegated to BertTokenizerFast in accordance with Python's multiple
-  inheritance rules.
+  This class inherits from BertTokenizerFast.
 
-  Parts of this class, including its documentation and naming scheme, is based
-  on and/or bear similarities to RobertaFeaturizer for the sake of consistency.
+  This class may contain code and/or documentation taken from the
+  RobertaFeaturizer pull request (#2581), which have been moved here due to
+  code restructuring.
   """
 
-  def _featurize(self, sequence: Union[str, RDKitMol]) -> List[List[int]]:
+  def __init__(self, input_ids, attention_mask):
+    self.input_ids = input_ids
+    self.attention_mask = attention_mask
+    return
+
+  def _featurize(self, sequence: str) -> List[List[int]]:
     """Tokenizes a datapoint with BertTokenizerFast.
 
-      Parameters
-      ----------
-      sequence: Union[str, RDKitMol]
-        An arbitrary string or an RDKitMol object. The latter is only supported
-        if RDKitMol is imported.
+    Parameters
+    ----------
+    sequence: str
+    An arbitrary string. 
 
-      Returns
-      -------
-      encoding: list 
-        list containing two lists: `input_ids` and `attention_mask`
-      """
-      if not isinstance(sequence, str):
-        super()
-      obj = self(sequence, self.input_ids, self.attention_mask)
-      encoding = list(obj.values())  # TODO check if list() is extrenuous
-      return encoding
+    Returns
+    -------
+    encoding: list 
+    list containing two lists: `input_ids` and `attention_mask`
+    """
+    obj = self(sequence, self.input_ids, self.attention_mask)
+    encoding = list(obj.values())
+    return encoding
+
+  def __call__(self, *args) -> Dict[str, List[int]]:
+    return super().__call__(*args)
