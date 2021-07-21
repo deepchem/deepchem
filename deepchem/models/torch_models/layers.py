3057f5d9bba085736e91def4aef46ad22e961a59
Atreya Majumdar <atreyamaj@gmail.com>
2021-07-21 20:08:55 +0530

    Porting Layer code

--- deepchem/models/torch_models/layers.py
+++ deepchem/models/torch_models/layers.py
@@ -47,3 +47,119 @@ class ScaleNorm(nn.Module):
   def forward(self, x):
     norm = self.scale / torch.norm(x, dim=-1, keepdim=True).clamp(min=self.eps)
     return x * norm
+
+class Encoder(nn.Module):
+    """Encoder block for the Molecule Attention Transformer in [1]_.
+    
+    A stack of N layers which form the encoder block.
+    
+    References
+    ----------
+    .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
+    """
+    
+    def __init__(self, layer, N, scale_norm):
+        """Initialize an Encoder block.
+        Parameters
+        ----------
+        layer: dc.layers
+          Layer to be stacked in the encoder block.
+        N: int
+          Number of identical layers to be stacked.
+        scale_norm: Bool
+          If True, uses ScaleNorm, else uses LayerNorm.
+        """
+
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        if scale_norm:
+            self.norm = ScaleNorm(layer.size)
+        else:
+            self.norm = torch.LayerNorm(layer.size)
+    
+    def forward(self, x, mask, adj_matrix, distances_matrix, edges_att):
+        "Pass the input (and mask) through each layer in turn."
+        for layer in self.layers:
+            x = layer(x, mask, adj_matrix, distances_matrix, edges_att)
+        return self.norm(x)
+
+class SublayerConnection(nn.Module):
+    """SublayerConnection layer as used in [1]_.
+    
+    The SublayerConnection layer is a residual layer which is then passed through Layer Normalization.
+    
+    References
+    ----------
+    .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
+    """
+    
+    def __init__(self, size, dropout, scale_norm):
+        """Initialize a SublayerConnection Layer.
+        Parameters
+        ----------
+        size: int
+          Normalized shape of LayerNorm/ScaleNorm layer.
+        dropout: float
+          Dropout probability.
+        scale_norm: Bool
+          If True, uses ScaleNorm, else uses LayerNorm.
+        """
+
+        super(SublayerConnection, self).__init__()
+        self.norm = ScaleNorm(size) if scale_norm else torch.LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm(x)))
+
+class EncoderLayer(nn.Module):
+    """Encoder layer for use in the Molecular Attention Transformer.
+    
+    The Encoder layer is formed by adding self-attention and feed-forward to the encoder block.
+    
+    References
+    ----------
+    .. [1] Lukasz Maziarka et al. "Molecule Attention Transformer" Graph Representation Learning workshop and Machine Learning and the Physical Sciences workshop at NeurIPS 2019. 2020. https://arxiv.org/abs/2002.08264
+    """
+
+    def __init__(self, size, self_attn, feed_forward, dropout, scale_norm):
+        """Initialize an Encoder layer.
+        Parameters
+        ----------
+        size: int
+          Normalized shape/length of ScaleNorm/LayerNorm layer.
+        self_attn: Tensor
+          The p_attn attribute from the attention function for the Molecular Attention Transformer.
+        feed_forward: Bool
+          If True, uses ScaleNorm, else uses LayerNorm.
+        dropout:
+          asd
+        scale_norm: Bool
+          If True, uses ScaleNorm, else uses LayerNorm.
+        """
+
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout, scale_norm), 2)
+        self.size = size
+
+    def forward(self, x, mask, adj_matrix, distances_matrix, edges_att):
+        "Follow Figure 1 (left) for connections."
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, adj_matrix, distances_matrix, edges_att, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+class EdgeFeaturesLayer(nn.Module):
+    def __init__(self, d_model, d_edge, h, dropout):
+        super(EdgeFeaturesLayer, self).__init__()
+        assert d_model % h == 0
+        d_k = d_model // h
+        self.linear = nn.Linear(d_edge, 1, bias=False)
+        with torch.no_grad():
+            self.linear.weight.fill_(0.25)
+
+    def forward(self, x):
+        p_edge = x.permute(0, 2, 3, 1)
+        p_edge = self.linear(p_edge).permute(0, 3, 1, 2)
+        return torch.relu(p_edge)
