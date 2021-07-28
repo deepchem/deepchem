8755a27eb6940bbf2b798d5aabb1ab49e18c6b81
DavidRFB <dr.figueroa10@uniandes.edu.co>
2021-07-28 15:52:22 -0500

    fixedtest

--- deepchem/molnet/load_function/tests/test_swissprot_loader.py
+++ deepchem/molnet/load_function/tests/test_swissprot_loader.py
@@ -4,10 +4,10 @@ Tests for swissprot loader.
 
 import os
 import numpy as np
-
-#from deepchem.molnet import load_swissprot
+import deepchem as dc 
 from swissprot_datasets import load_swissprot
-@slow
+#from swissprot_datasets import load_swissprot
+
 def test_swissprot_loader():
   current_dir = os.path.dirname(os.path.abspath(__file__))
   codes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
@@ -15,7 +15,7 @@ def test_swissprot_loader():
   tasks, datasets = load_swissprot(
       reload=False,
       data_dir=current_dir,
-      featurizer=dc.featurizer.OneHoteFeatureizer(codes),
+      featurizer=dc.feat.OneHotFeaturizer(codes),
       splitter_kwargs={
           'seed': 42,
           'frac_train': 0.6,
@@ -24,3 +24,5 @@ def test_swissprot_loader():
       })
   assert len(tasks) == 0
   assert datasets.X.shape == (564638, 100, 35)
+
+test_swissprot_loader()
