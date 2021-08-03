d7e1d2a270df4406f00982f416a86b92bfdf8c3a
DavidRFB <dr.figueroa10@uniandes.edu.co>
2021-08-02 22:28:18 -0500

    SwsDB working Dummy task column

--- deepchem/molnet/load_function/swissprot_datasets.py
+++ deepchem/molnet/load_function/swissprot_datasets.py
@@ -2,13 +2,15 @@
 swissprot dataset loader.
 """
 import os
+import numpy as np
 import deepchem as dc
 from deepchem.molnet.load_function.molnet_loader import TransformerGenerator, _MolnetLoader
 from deepchem.data import Dataset
 from typing import List, Optional, Tuple, Union
 
 SWISSP_URL = "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/uniprot_swissprot_14_06_21.csv"
-SWISSP_TASK = []
+SWISSP_TASK = ["0"]
+
 
 class _SWISSPROTLoader(_MolnetLoader):
 
@@ -19,30 +21,31 @@ class _SWISSPROTLoader(_MolnetLoader):
       dc.utils.data_utils.download_url(url=SWISSP_URL, dest_dir=self.data_dir)
       dataset_file = os.path.join(self.data_dir,
                                   "uniprot_swissprot_14_06_21.csv")
-    print("loader")
     loader = dc.data.CSVLoader(
-        tasks=self.tasks, featurizer=self.featurizer,feature_field="SEQUENCE",id_field="ACCNUM")
-    return loader.create_dataset(dataset_file)#,shard_size=8192)
-
-
-#codes =
+        tasks=self.tasks,
+        featurizer=self.featurizer,
+        feature_field="SEQUENCE",
+    )
+    return loader.create_dataset(dataset_file)  #,shard_size=8192)
 
 
 def load_swissprot(
-  featurizer: Union[dc.feat.Featurizer, str] = dc.feat.OneHotFeaturizer([
-      'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q',
-      'R', 'S', 'T', 'V', 'W', 'Y'
-  ]),
-  splitter: Union[dc.splits.Splitter, str, None] = 'random',
-  transformers: List[Union[TransformerGenerator, str]] = [],
-  reload: bool = True,
-  data_dir: Optional[str] = None,
-  save_dir: Optional[str] = None,
-  **kwargs) -> Tuple[List[str], Tuple[Dataset, ...]]:
-  """Load Swissprot dataset.
+    featurizer: Union[dc.feat.Featurizer, str] = dc.feat.OneHotFeaturizer([
+        'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q',
+        'R', 'S', 'T', 'V', 'W', 'Y'
+    ]),
+    splitter: Union[dc.splits.Splitter, str, None] = 'random',
+    transformers: List[Union[TransformerGenerator, str]] = [],
+    reload: bool = True,
+    data_dir: Optional[str] = None,
+    save_dir: Optional[str] = None,
+    **kwargs) -> Tuple[List[str], Tuple[Dataset, ...]]:
+    
+  """
+    Load Swissprot dataset.
 
-    The Swiss-Prot Database is parto og the UniprotKnowledebase (UniprotKB) that contains a
-    collection of functiona information on protiens. Specially the Swiss-Prot Database 
+    The Swiss-Prot Database is part of the UniprotKnowledebase (UniprotKB) that contains a
+    collection of functionañ information on protiens. Specially the Swiss-Prot Database 
     contains records with information extracted from literature and curator-evaluated computational
     analysis. 
     https://www.uniprot.org/uniprot/?query=reviewed:yes
@@ -70,12 +73,16 @@ def load_swissprot(
     please visit : https://www.uniprot.org/downloads
 
 
+    This version of the swissprot DB contains 564638 curated sequences. 
+    
+
     References
     ----------
     .. [1] UniProt Consortium, T. UniProt: The Universal Protein Knowledgebase.
      Nucleic Acids Res 2018, 46 (5), 2699–2699. https://doi.org/10.1093/nar/gky092.
 
     """
+
   loader = _SWISSPROTLoader(featurizer, splitter, transformers, SWISSP_TASK,
                             data_dir, save_dir, **kwargs)
   return loader.load_dataset('swissprot', reload)
--- deepchem/molnet/load_function/tests/test_swissprot_loader.py
+++ deepchem/molnet/load_function/tests/test_swissprot_loader.py
@@ -4,25 +4,28 @@ Tests for swissprot loader.
 
 import os
 import numpy as np
-import deepchem as dc 
-from swissprot_datasets import load_swissprot
-#from swissprot_datasets import load_swissprot
+import deepchem as dc
+
+from deepchem.molnet import load_swissprot
+
 
 def test_swissprot_loader():
   current_dir = os.path.dirname(os.path.abspath(__file__))
-  codes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
-         'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-  tasks, datasets = load_swissprot(
-      reload=False,
+  codes = [
+      'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+      'S', 'T', 'V', 'W', 'Y'
+  ]
+  tasks, datasets, transformers = load_swissprot(
+      splitter='random',
       data_dir=current_dir,
-      featurizer=dc.feat.OneHotFeaturizer(codes),
-      splitter_kwargs={
-          'seed': 42,
-          'frac_train': 0.6,
-          'frac_valid': 0.2,
-          'frac_test': 0.2
-      })
-  assert len(tasks) == 0
-  assert datasets.X.shape == (564638, 100, 35)
+      save_dir=current_dir,
+      reload=False,
+      featurizer=dc.feat.OneHotFeaturizer(codes))
+  train, val, test = datasets
+  assert len(tasks) == 1
+  assert train.X.shape == (45796, 100, 21)
+  assert train.X[0].shape = (100,21)
+  #assert datasets.X.shape == (564638, 100, 35)
+
 
 test_swissprot_loader()
