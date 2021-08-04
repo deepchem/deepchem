8afa99e4ecdd0c1a8291f9cc7a3df4f3f8c6da3e
alat-rights <>
2021-08-05 02:47:25 +0900

    change test to be inline w roberta

--- deepchem/feat/tests/test_bert_tokenizer.py
+++ deepchem/feat/tests/test_bert_tokenizer.py
@@ -1,37 +1,42 @@
-import unittest
-from deepchem.feat import BertFeaturizer
-from transformers import BertTokenizerFast
+import pytest
 
 
-class TestBertFeaturizer(unittest.TestCase):
-  """Tests for BertFeaturizer, based on tests for RobertaFeaturizer and
-  usage examples in Rostlab prot_bert documentation hosted by HuggingFace."""
+@pytest.mark.torch
+def test_call():
+  """Test BertFeaturizer.__call__(), which is based on BertTokenizerFast."""
+  from deepchem.feat.bert_tokenizer import BertFeaturizer
+  sequence = [
+      '[CLS] D L I P T S S K L V [SEP]', '[CLS] V K K A F F A L V T [SEP]'
+  ]
+  sequence_long = ['[CLS] D L I P T S S K L V V K K A F F A L V T [SEP]']
+  featurizer = BertFeaturizer.from_pretrained(
+      "Rostlab/prot_bert", do_lower_case=False)
+  embedding = featurizer(sequence, return_tensors='pt')
+  embedding_long = featurizer(
+      sequence_long * 2, return_tensors='pt')
+  for emb in [embedding, embedding_long]:
+    assert 'input_ids' in emb.keys() and 'attention_mask' in emb.keys()
+    assert len(embedding['input_ids']) == 2 and len(
+        emb['attention_mask']) == 2
 
-  def setUp(self):
-    self.sequence = ['[CLS] D L I P T S S K L V [SEP]', '[CLS] V K K A F F A L V T [SEP]']
-    self.sequence_long = ['[CLS] D L I P T S S K L V V K K A F F A L V T [SEP]']
-    self.featurizer = BertFeaturizer.from_pretrained(
-        "Rostlab/prot_bert", do_lower_case=False)
-
-  def test_call(self):
-    """Test BertFeaturizer.__call__(), which is based on BertTokenizerFast."""
-    embedding = self.featurizer(
-        self.sequence, return_tensors='pt')
-    embedding_long = self.featurizer(
-      self.sequence_long * 2, return_tensors='pt')
-    for emb in [embedding, embedding_long]:
-      assert 'input_ids' in emb.keys() and 'attention_mask' in emb.keys()
-      assert len(embedding['input_ids']) == 2 and len(emb['attention_mask']) == 2
-
-  def test_featurize(self):
-    """Test that BertFeaturizer.featurize() correctly featurizes all sequences,
-    correctly outputs input_ids and attention_mask.
-    """
-    feats = self.featurizer.featurize(self.sequence)
-    long_feat = self.featurizer.featurize(
-        self.sequence_long)
-
-    assert (len(feats) == 2)
-    assert (all([len(f) == 3 for f in feats]))
-    assert (len(long_feat) == 1)
-    assert (len(long_feat[0] == 3))
+def test_featurize():
+  """Test that BertFeaturizer.featurize() correctly featurizes all sequences,
+  correctly outputs input_ids and attention_mask.
+  """
+  from deepchem.feat.bert_tokenizer import BertFeaturizer
+  sequence = [
+      '[CLS] D L I P T S S K L V [SEP]', '[CLS] V K K A F F A L V T [SEP]'
+  ]
+  sequence_long = ['[CLS] D L I P T S S K L V V K K A F F A L V T [SEP]']
+  featurizer = BertFeaturizer.from_pretrained(
+      "Rostlab/prot_bert", do_lower_case=False)
+  feats = featurizer.featurize(sequence)
+  long_feat = featurizer.featurize(sequence_long)
+  """
+  for f in feats:
+    print(f)
+  """
+  assert (len(feats) == 2)
+  assert (all([len(f) == 3 for f in feats]))
+  assert (len(long_feat) == 1)
+  assert (len(long_feat[0] == 2))
