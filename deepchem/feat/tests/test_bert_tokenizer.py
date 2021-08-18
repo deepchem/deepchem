a0c600fcee1e34e178edb762b224ccb87acde930
alat-rights <>
2021-08-18 19:12:36 +0900

    add datasets

--- deepchem/feat/tests/test_bert_tokenizer.py
+++ deepchem/feat/tests/test_bert_tokenizer.py
@@ -35,6 +35,6 @@ def test_loading():
 
   loader = FASTALoader(
       featurizer=featurizer, legacy=False, auto_add_annotations=True)
-  data = loader.create_dataset(input_files="../uniprot_truncated.fasta")
+  data = loader.create_dataset(input_files="./data/uniprot_truncated.fasta")
 
   assert data.X.shape == (61, 3, 5)
