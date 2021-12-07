21d4837b603ea368f1dca243e7c5b435c60641f9
DavidRFB <dr.figueroa10@uniandes.edu.co>
2021-12-07 13:41:47 -0500

    Flake 8 0 error

--- deepchem/feat/molecule_featurizers/sparse_matrix_one_hot_featurizer.py
+++ deepchem/feat/molecule_featurizers/sparse_matrix_one_hot_featurizer.py
@@ -2,21 +2,14 @@ import logging
 from typing import List
 
 import numpy as np
-import scipy 
-from deepchem.utils.typing import RDKitMol
+import scipy
 from deepchem.feat.base_classes import Featurizer
-from typing import Any, Iterable, Optional
+from typing import Any, Iterable
 
 logger = logging.getLogger(__name__)
-
-ZINC_CHARSET = [
-    '#', ')', '(', '+', '-', '/', '1', '3', '2', '5', '4', '7', '6', '8', '=',
-    '@', 'C', 'B', 'F', 'I', 'H', 'O', 'N', 'S', '[', ']', '\\', 'c', 'l', 'o',
-    'n', 'p', 's', 'r'
-]
 codes = [
-      'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
-      'S', 'T', 'V', 'W', 'Y','X','Z','B','U','O'
+    'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+    'S', 'T', 'V', 'W', 'Y', 'X', 'Z', 'B', 'U', 'O'
 ]
 
 class SparseMatrixOneHotFeaturizer(Featurizer):
@@ -46,34 +39,28 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
 
   """
 
-  def __init__(self,
-               charset: List[str] = codes,
-               ):
+  def __init__(
+      self,
+      charset: List[str] = codes,
+  ):
     """Initialize featurizer.
 
     Parameters
     ----------
-    charset: List[str] (default ZINC_CHARSET)
+    charset: List[str] (default code)
       A list of strings, where each string is length 1 and unique.
-    max_length: Optional[int], optional (default 100)
-      The max length for string. If the length of string is shorter than
-      max_length, the string is padded using space.
-
-      If max_length is None, no padding is performed and arbitrary length
-      strings are allowed.
     """
     if len(charset) != len(set(charset)):
       raise ValueError("All values in charset must be unique.")
     self.charset = charset
-    from sklearn.preprocessing import OneHotEncoder 
+    from sklearn.preprocessing import OneHotEncoder
     from sklearn.preprocessing import LabelEncoder
-    
+
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(self.charset)
     integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-    cat = np.array(self.charset).reshape(1,len(self.charset))
-    self.ohe = OneHotEncoder(categories = list(cat))
-    
+    cat = np.array(self.charset).reshape(1, len(self.charset))
+    self.ohe = OneHotEncoder(categories=list(cat))
 
   def featurize(self,
                 datapoints: Iterable[Any],
@@ -84,7 +71,7 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     Parameters
     ----------
     datapoints: list
-      A list of either strings (str or numpy.str_) 
+      A list of either strings (str or numpy.str_)
     log_every_n: int, optional (default 1000)
       How many elements are featurized every time a featurization is logged.
     """
@@ -98,7 +85,6 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     # Featurize str data
     if isinstance(datapoint, (str, np.str_)):
       return self._featurize_string(datapoint)
-
 
   def _featurize_string(self, string: str) -> scipy.sparse:
     """Compute one-hot featurization of string.
@@ -114,9 +100,8 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
 
     """
 
-    sparse_mat = self.ohe.fit_transform(np.array(list(string)).reshape(-1,1))
+    sparse_mat = self.ohe.fit_transform(np.array(list(string)).reshape(-1, 1))
     return sparse_mat
-
 
   def untransform(self, one_hot_vectors: scipy.sparse) -> str:
     """Convert from one hot representation back to original string
@@ -134,5 +119,5 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     string = ""
     invers_trans = self.ohe.inverse_transform(one_hot_vectors)
     for one_hot in invers_trans:
-      string += one_hot  
+      string += one_hot
     return string
