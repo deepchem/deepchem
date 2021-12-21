e42135b340c3ef865da88160f4b0a81103c05112
Tony Davis <acdavis629@gmail.com>
2021-12-20 19:51:31 -0500

    Update score_function.py
    Fixed yapf version

--- deepchem/metrics/score_function.py
+++ deepchem/metrics/score_function.py
@@ -23,7 +23,7 @@ kappa_score = cohen_kappa_score
 
 
 def pearsonr(y: np.ndarray, y_pred: np.ndarray) -> float:
-  """Computes Pearson correlation coefficient.
+    """Computes Pearson correlation coefficient.
 
   Parameters
   ----------
@@ -37,11 +37,11 @@ def pearsonr(y: np.ndarray, y_pred: np.ndarray) -> float:
   float
     The Pearson correlation coefficient.
   """
-  return scipy.stats.pearsonr(y, y_pred)[0]
+    return scipy.stats.pearsonr(y, y_pred)[0]
 
 
 def pearson_r2_score(y: np.ndarray, y_pred: np.ndarray) -> float:
-  """Computes Pearson R^2 (square of Pearson correlation).
+    """Computes Pearson R^2 (square of Pearson correlation).
 
   Parameters
   ----------
@@ -55,11 +55,11 @@ def pearson_r2_score(y: np.ndarray, y_pred: np.ndarray) -> float:
   float
     The Pearson-R^2 score.
   """
-  return scipy.stats.pearsonr(y, y_pred)[0]**2
+    return scipy.stats.pearsonr(y, y_pred)[0] ** 2
 
 
 def jaccard_index(y: np.ndarray, y_pred: np.ndarray) -> float:
-  """Computes Jaccard Index which is the Intersection Over Union metric
+    """Computes Jaccard Index which is the Intersection Over Union metric
   which is commonly used in image segmentation tasks.
 
   DEPRECATED: WILL BE REMOVED IN A FUTURE VERSION OF DEEEPCHEM. USE `jaccard_score` instead.
@@ -76,11 +76,11 @@ def jaccard_index(y: np.ndarray, y_pred: np.ndarray) -> float:
   score: float
     The jaccard index. A number between 0 and 1.
   """
-  return jaccard_score(y, y_pred)
+    return jaccard_score(y, y_pred)
 
 
 def pixel_error(y: np.ndarray, y_pred: np.ndarray) -> float:
-  """An error metric in case y, y_pred are images.
+    """An error metric in case y, y_pred are images.
 
   Defined as 1 - the maximal F-score of pixel similarity, or squared
   Euclidean distance between the original and the result labels.
@@ -97,11 +97,11 @@ def pixel_error(y: np.ndarray, y_pred: np.ndarray) -> float:
   score: float
     The pixel-error. A number between 0 and 1.
   """
-  return 1 - f1_score(y, y_pred)
+    return 1 - f1_score(y, y_pred)
 
 
 def prc_auc_score(y: np.ndarray, y_pred: np.ndarray) -> float:
-  """Compute area under precision-recall curve
+    """Compute area under precision-recall curve
 
   Parameters
   ----------
@@ -115,22 +115,22 @@ def prc_auc_score(y: np.ndarray, y_pred: np.ndarray) -> float:
   float
     The area under the precision-recall curve. A number between 0 and 1.
   """
-  precision, recall, _ = precision_recall_curve(y[:, 1], y_pred[:, 1])
-  return auc(recall, precision)
+    precision, recall, _ = precision_recall_curve(y[:, 1], y_pred[:, 1])
+    return auc(recall, precision)
 
 
 def rms_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-  """Computes RMS error."""
-  return np.sqrt(mean_squared_error(y_true, y_pred))
+    """Computes RMS error."""
+    return np.sqrt(mean_squared_error(y_true, y_pred))
 
 
 def mae_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-  """Computes MAE."""
-  return mean_absolute_error(y_true, y_pred)
+    """Computes MAE."""
+    return mean_absolute_error(y_true, y_pred)
 
 
-def bedroc_score(y_true: np.ndarray, y_pred: np.ndarray, alpha: float = 20.0):
-  """Compute BEDROC metric.
+def bedroc_score(y_true: np.ndarray, y_pred: np.ndarray, alpha: float=20.0):
+    """Compute BEDROC metric.
 
   BEDROC metric implemented according to Truchon and Bayley that modifies
   the ROC score by allowing for a factor of early recognition.
@@ -160,31 +160,31 @@ def bedroc_score(y_true: np.ndarray, y_pred: np.ndarray, alpha: float = 20.0):
      for the “early recognition” problem." Journal of chemical information and modeling
      47.2 (2007): 488-508.
   """
-  try:
-    from rdkit.ML.Scoring.Scoring import CalcBEDROC
-  except ModuleNotFoundError:
-    raise ImportError("This function requires RDKit to be installed.")
+    try:
+        from rdkit.ML.Scoring.Scoring import CalcBEDROC
+    except ModuleNotFoundError:
+        raise ImportError("This function requires RDKit to be installed.")
 
-  # validation
-  assert len(y_true) == len(y_pred), 'Number of examples do not match'
-  assert np.array_equal(np.unique(y_true).astype(int),
-                        [0, 1]), ('Class labels must be binary: %s' %
-                                  np.unique(y_true))
+# validation
+    assert len(y_true) == len(y_pred), 'Number of examples do not match'
+    assert np.array_equal(np.unique(y_true).astype(int),
+                          [0, 1]), ('Class labels must be binary: %s' %
+                                    np.unique(y_true))
 
-  yt = np.asarray(y_true)
-  yp = np.asarray(y_pred)
+    yt = np.asarray(y_true)
+    yp = np.asarray(y_pred)
 
-  yt = yt.flatten()
-  yp = yp[:, 1].flatten()  # Index 1 because one_hot predictions
+    yt = yt.flatten()
+    yp = yp[:, 1].flatten()  # Index 1 because one_hot predictions
 
-  scores = list(zip(yt, yp))
-  scores = sorted(scores, key=lambda pair: pair[1], reverse=True)
+    scores = list(zip(yt, yp))
+    scores = sorted(scores, key=lambda pair: pair[1], reverse=True)
 
-  return CalcBEDROC(scores, 0, alpha)
+    return CalcBEDROC(scores, 0, alpha)
 
 
 def concordance_index(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-  """Compute Concordance index.
+    """Compute Concordance index.
 
   Statistical metric indicates the quality of the predicted ranking.
   Please confirm details from [1]_.
@@ -207,29 +207,29 @@ def concordance_index(y_true: np.ndarray, y_pred: np.ndarray) -> float:
      Bounds on the concordance index." Advances in neural information processing systems (2008): 1209-1216.
   """
 
-  idx = np.argsort(y_true)
-  y_true = y_true[idx]
-  y_pred = y_pred[idx]
+    idx = np.argsort(y_true)
+    y_true = y_true[idx]
+    y_pred = y_pred[idx]
 
-  pairs = 0
-  correct_pairs = 0.0
+    pairs = 0
+    correct_pairs = 0.0
 
-  for i in range(len(y_true)):
-    true_a = y_true[i]
-    pred_a = y_pred[i]
+    for i in range(len(y_true)):
+        true_a = y_true[i]
+        pred_a = y_pred[i]
 
-    for j in range(i + 1, len(y_true)):
-      true_b = y_true[j]
-      pred_b = y_pred[j]
-      if true_a != true_b:
-        pairs += 1
-        if pred_a == pred_b:
-          correct_pairs += 0.5
-        elif pred_a < pred_b:
-          correct_pairs += true_a < true_b
-        else:
-          correct_pairs += true_a > true_b
+        for j in range(i + 1, len(y_true)):
+            true_b = y_true[j]
+            pred_b = y_pred[j]
+            if true_a != true_b:
+                pairs += 1
+                if pred_a == pred_b:
+                    correct_pairs += 0.5
+                elif pred_a < pred_b:
+                    correct_pairs += true_a < true_b
+                else:
+                    correct_pairs += true_a > true_b
 
-  assert pairs > 0, 'No pairs for comparision'
+    assert pairs > 0, 'No pairs for comparision'
 
-  return correct_pairs / pairs
+    return correct_pairs / pairs
