6d77d072660073931560e1bc0045dfc993808e06
DingQK <18210394728@163.com>
2022-01-04 20:59:38 +0800

    Update test_setshard.py

--- deepchem/data/tests/test_setshard.py
+++ deepchem/data/tests/test_setshard.py
@@ -3,17 +3,19 @@ import numpy as np
 
 
 def test_setshard_with_X_y():
-  """Test setharding on a simple example"""
+  """Test setsharding on a simple example"""
   X = np.random.rand(10, 3)
   y = np.random.rand(10,)
   dataset = dc.data.DiskDataset.from_numpy(X, y)
-  assert dataset.get_shape()[0][0] == 10
-  assert dataset.get_shape()[1][0] == 10
+  X_shape, y_shape, _, _ = dataset.get_shape()
+  assert X_shape[0] == 10
+  assert y_shape[0] == 10
   for i, (X, y, w, ids) in enumerate(dataset.itershards()):
     X = X[1:]
     y = y[1:]
     w = w[1:]
     ids = ids[1:]
     dataset.set_shard(i, X, y, w, ids)
-  assert dataset.get_shape()[0][0] == 9
-  assert dataset.get_shape()[1][0] == 9
+  X_shape, y_shape, _, _ = dataset.get_shape()
+  assert X_shape[0] == 9
+  assert y_shape[0] == 9
