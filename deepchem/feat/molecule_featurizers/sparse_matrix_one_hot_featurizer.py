7fc507f292d321a76b6222480e7ab7a55f08e446
davidRFB <dr.figueroa10@uniandes.edu.co>
2022-02-08 14:47:17 +0100

    Full Test OK

--- deepchem/feat/molecule_featurizers/sparse_matrix_one_hot_featurizer.py
+++ deepchem/feat/molecule_featurizers/sparse_matrix_one_hot_featurizer.py
@@ -26,23 +26,13 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
   >>> import deepchem as dc
   >>> featurizer = dc.feat.SparseMatrixOneHotFeaturizer()
   >>> sequence = "MMMQLA"
-  >>> encodings = featurizer.featurize(sequence)
-  >>> print(encodings)
-  (0, 10)	1.0
-  (1, 10)	1.0
-  (2, 10)	1.0
-  (3, 13)	1.0
-  (4, 9)	1.0
-  (5, 0)	1.0
+  >>> encodings = featurizer.featurize([sequence])
   >>> encodings[0].shape
   (6, 25)
-  >>> featurizer.untransform(encodings[0])
-  'MMMQLA'
 
   """
 
-  def __init__(self,
-               charset: List[str] = CHARSET):
+  def __init__(self, charset: List[str] = CHARSET):
     """Initialize featurizer.
 
     Parameters
@@ -57,7 +47,6 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     cat = np.array(self.charset).reshape(1, len(self.charset))
     self.ohe = OneHotEncoder(categories=list(cat), handle_unknown='ignore')
 
-
   def featurize(self,
                 datapoints: Iterable[Any],
                 log_every_n: int = 1000,
@@ -70,35 +59,32 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     log_every_n: int, optional (default 1000)
       How many elements are featurized every time a featurization is logged.
     """
-    #datapoints = datapoints)
     if (len(datapoints) < 1):
       return np.array([])
     # Featurize data using featurize() in parent class
     return Featurizer.featurize(self, datapoints, log_every_n)
 
   def _featurize(self, datapoint: Any, **kwargs):
-      """ Use parent method of base clase Featurizer.
-      
+    """ Use parent method of base clase Featurizer.
       Parameters
       ----------
-      datapoint : Any
-          DESCRIPTION.
-      **kwargs : TYPE
-          DESCRIPTION.
+      datapoint : list of string
+          string to be converted to a sparse one hot matrix.
+
 
       Returns
       -------
       TYPE
-          DESCRIPTION.
+          scipy sparse matrix
 
       """
     # Featurize str data
-      if isinstance(datapoint, (str, np.str_)):
-        sequence = np.array(list(datapoint)).reshape(-1,1)
-        sparse_mat = self.ohe.fit_transform(sequence)
-        return  sparse_mat    #self._featurize_string(datapoint)
-      else:
-        raise ValueError("Datapoint is not a string")
+    if isinstance(datapoint, (str, np.str_)):
+      sequence = np.array(list(datapoint)).reshape(-1, 1)
+      sparse_mat = self.ohe.fit_transform(sequence)
+      return sparse_mat
+    else:
+      raise ValueError("Datapoint is not a string")
 
   def untransform(self, one_hot_vectors: scipy.sparse) -> str:
     """Convert from one hot representation back to original string
@@ -116,5 +102,5 @@ class SparseMatrixOneHotFeaturizer(Featurizer):
     string = ""
     invers_trans = self.ohe.inverse_transform(one_hot_vectors)
     for one_hot in invers_trans:
-      string += one_hot
+      string += str(one_hot)
     return string
