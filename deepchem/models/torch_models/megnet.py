72e1d1e9c35b61355399415c6d307065e9d66f7d
arunppsg <arun.palaniappan1999@gmail.com>
2022-02-10 21:32:58 +0530

    megnet TorchModel initial commit

--- deepchem/models/torch_models/megnet.py
+++ deepchem/models/torch_models/megnet.py
@@ -1,12 +1,13 @@
 """
 Implementation of MEGNet class
 """
-from typing import Tuple
 import torch
 import torch.nn as nn
-from torch import Tensor
+import torch.nn.functional as F
 
+from deepchem.models.losses import Loss, L2Loss, SparseSoftmaxCrossEntropy
 from deepchem.models.torch_models.layers import GraphNetwork as GN
+from deepchem.models.torch_models import TorchModel
 
 
 class MEGNet(nn.Module):
@@ -92,9 +93,8 @@ class MEGNet(nn.Module):
     elif self.mode == 'classification':
       self.out = nn.Linear(in_features=16, out_features=n_tasks * n_classes)
 
-  def forward(self, node_features: Tensor, edge_index: Tensor,
-              edge_features: Tensor,
-              global_features: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+
+  def forward(self, pyg_batch: PyGBatch):
     """
     Parameters
     ----------
@@ -108,23 +108,97 @@ class MEGNet(nn.Module):
       Global features of the graph, shape: :math:`(F_g, 1)`
     where :math:`|\mathcal{V}|` and :math:`|\mathcal{E}|` denotes the number of nodes and edges in the graph,
       :math:F_n, :math:F_e, :math:F_g denotes the number of node features, edge features and global state features respectively.
+    node_batch_map: torch.LongTensor (optional)
+      A vector that maps each node to its respective graph identifier.
+    edge_batch_map: torch.LongTensor (optional)
+      A vector that maps each edge to its respective graph identifier.
 
     Returns
     -------
     torch.Tensor: Predictions for the graph
     """
+    if node_batch_map is None:
+      node_batch_map = node_features.new_zeros(node_features.size(0), dtype=torch.int64)
+    if edge_batch_map is None:
+      edge_batch_map = edge_features.new_zeros(edge_features.size(0), dtype=torch.int64)
+
     for i in range(self.n_blocks):
       node_features, edge_features, global_features = self.megnet_blocks[i](
-          node_features, edge_index, edge_features, global_features)
-    node_features = self.set2set_nodes(
-        node_features,
-        batch=node_features.new_zeros(node_features.size(0), dtype=torch.int64))
-    edge_features = self.set2set_edges(
-        edge_features,
-        batch=edge_features.new_zeros(edge_features.size(0), dtype=torch.int64))
-    out = torch.cat(
-        [node_features.squeeze(0),
-         edge_features.squeeze(0), global_features],
-        axis=0)
-    out = self.dense(out)
-    return self.out(out)
+          node_features, edge_index, edge_features, global_features, node_batch_map, edge_batch_map)
+
+    node_features = self.set2set_nodes(node_features, node_batch_map) 
+    edge_features = self.set2set_edges(edge_features, edge_batch_map) 
+    out = torch.cat([node_features, edge_features, global_features], axis=1)
+    out = self.out(self.dense(out))
+
+    if self.mode == 'classification':
+      if self.n_tasks == 1:
+        logits = out.view(-1, self.n_classes)
+        softmax_dim = 1
+      else:
+        logits = out.view(-1, self.n_tasks, self.n_classes)
+        softmax_dim = 2
+      proba = F.softmax(logits, dim=softmax_dim)
+      return proba, logits
+    elif self.mode == 'regression':
+      return out
+
+
+class MEGNetModel(TorchModel):
+  """MEGNet Model
+
+  """
+  def __init__(self,
+               n_node_features=32,
+               n_edge_features=32,
+               n_global_features=32,
+               n_blocks=1,
+               mode='regression',
+               n_classes=2,
+               n_tasks=1):
+
+    model = MEGNet(
+        n_node_features=n_node_features,
+        n_edge_features=n_edge_features,
+        n_global_features=n_global_features,
+        n_blocks=n_blocks,
+        mode=mode,
+        n_classes=n_classes,
+        n_tasks=n_tasks)
+    if mode == 'regression':
+      loss: Loss = L2Loss()
+      output_types = ['prediction']
+    elif mode == 'classification':
+      loss = SparseSoftmaxCrossEntropy()
+      output_types = ['prediction', 'loss']
+    super(MEGNetModel, self).__init__(
+        model, loss=loss, output_types=output_types, **kwargs)
+
+  def _prepare_batch(self, batch):
+    """Creates batch data for MEGNet model
+
+    Note
+    ----
+    Ideally, we should only override default_generator method. But the problem
+    here is that we _prepare_batch of TorchModel only supports non-graph
+    data types. Hence, we are overriding it here. This should be fixed
+    some time in the future.
+    """
+    try:
+      from torch_geometric.data import Batch
+    except ModuleNotFoundError:
+      raise ImportError("This module requires PyTorch Geometric")
+
+    # We convert deepchem.feat.GraphData to a PyG graph and then
+    # batch it.
+    graphs, labels, weights = batch
+    # The default_generator method returns an array of dc.feat.GraphData objects
+    # nested inside a list. To access the nested array of graphs, we are
+    # indexing by 0 here.
+    graph_list = [graph.to_pyg_graph() for graph in graphs[0]]
+    pyg_batch = Batch()
+    pyg_batch = pyg_batch.from_data_list(graph_list)
+
+    _, labels, weights = super(MEGNetModel, self)._prepare_batch(([], labels, weights))
+
+    return pyg_batch, labels, weights
