44f2ba47fdf2d76f1d7c53a71b371cca39d6c6a3
JoseAntonioSiguenza <80292371+JoseAntonioSiguenza@users.noreply.github.com>
2022-06-15 10:28:53 -0500

    normalizing flow documentation

--- deepchem/models/torch_models/normalizing_flows_pytorch.py
+++ deepchem/models/torch_models/normalizing_flows_pytorch.py
@@ -1,2 +1,32 @@
 """Normalizing flows for transforming probability distributions using PyTorch.
+
+class NormalizingFlow(nn.Module):
+  Normalizing flows are widley used to perform generative models.
+  Normalizing flow models gives advantages over variational autoencoders
+  (VAE) because of ease in sampling by applying invertible transformations
+  (Frey, Gadepally, & Ramsundar, 2022).
+
+  def __init__(self, transform, base_distribution, dim):
+      super().__init__()
+      self.dim = dim
+      self.tranforms = nn.ModuleList(transform)
+      self.base_distribution = base_distribution
+
+  def log_prob(self, inputs):
+      log_prob = torch.zeros(inputs.shape[0])
+      for biject in reversed(self.tranforms):
+          inputs, inverse_log_det_jacobian = biject.inverse(inputs)
+          log_prob += inverse_log_det_jacobian
+
+      return log_prob
+
+  def sample(self, n_samples):
+      outputs = self.base_distribution.sample((n_samples, ))
+      log_prob = self.base_distribution.log_prob(outputs)
+
+      for biject in self.transforms:
+          output, log_det_jacobian = biject.forward(output)
+          log_prob += log_det_jacobian
+
+      return output, log_prob
 """
