9a637746e2c229f13c0cd3caa4a528a14fe68415
JoseAntonioSiguenza <80292371+JoseAntonioSiguenza@users.noreply.github.com>
2022-06-23 15:30:18 -0500

    yapf corrections

--- deepchem/models/tests/test_normalizing_flow_pytorch.py
+++ deepchem/models/tests/test_normalizing_flow_pytorch.py
@@ -86,7 +86,7 @@ def test_normalizing_flow_pytorch():
   log_prob_ = log_prob_.detach().numpy()
   log_prob = log_prob.detach().numpy()
   zeros = np.zeros((samples,))
-  zeros_dim = np.zeros((samples,dim))
+  zeros_dim = np.zeros((samples, dim))
 
   # Assert errors for sample method
   assert np.not_equal(sampling, zeros_dim)
