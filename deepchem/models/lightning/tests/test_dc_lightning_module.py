f202b510cf994128c25e5e4402311c927350ea33
Princy Chahal <princychahal68@gmail.com>
2022-06-23 22:30:11 -0400

    Move helper classes inside test function

--- deepchem/models/lightning/tests/test_dc_lightning_module.py
+++ deepchem/models/lightning/tests/test_dc_lightning_module.py
@@ -13,42 +13,40 @@ except ImportError:
   PYTORCH_LIGHTNING_IMPORT_FAILED = True
 
 
-class TestDataset(torch.utils.data.Dataset):
-
-  def __init__(self, dataset):
-    self._samples = dataset
-
-  def __len__(self):
-    return len(self._samples)
-
-  def __getitem__(self, index):
-    y = np.zeros((1, 2))
-    y[0, int(self._samples.y[index][0])] = 1.0
-    return (
-        self._samples.X[index],
-        y,
-        self._samples.w[index],
-    )
-
-
-class TestDatasetBatch:
-
-  def __init__(self, batch):
-    X = [np.array([b[0] for b in batch])]
-    y = [np.array([b[1] for b in batch])]
-    w = [np.array([b[2] for b in batch])]
-    self.batch_list = [X, y, w]
-
-
-def collate_dataset_wrapper(batch):
-  return TestDatasetBatch(batch)
-
-
 class TestDCLightningModule(unittest.TestCase):
 
   @unittest.skipIf(PYTORCH_LIGHTNING_IMPORT_FAILED,
                    'PyTorch Lightning is not installed')
   def test_multitask_classifier(self):
+
+    class TestDatasetBatch:
+
+      def __init__(self, batch):
+        X = [np.array([b[0] for b in batch])]
+        y = [np.array([b[1] for b in batch])]
+        w = [np.array([b[2] for b in batch])]
+        self.batch_list = [X, y, w]
+
+    def collate_dataset_wrapper(batch):
+      return TestDatasetBatch(batch)
+
+    class TestDataset(torch.utils.data.Dataset):
+
+      def __init__(self, dataset):
+        self._samples = dataset
+
+      def __len__(self):
+        return len(self._samples)
+
+      def __getitem__(self, index):
+        y = np.zeros((1, 2))
+        y[0, int(self._samples.y[index][0])] = 1.0
+        return (
+            self._samples.X[index],
+            y,
+            self._samples.w[index],
+        )
+
     tasks, datasets, _ = dc.molnet.load_clintox()
     _, valid_dataset, _ = datasets
 
