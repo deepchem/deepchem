ff26395e62afb4018cf53400ec75130275f364b1
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2022-06-28 12:04:52 +0530

    updated old torch cnn test

--- deepchem/models/tests/test_cnn.py
+++ deepchem/models/tests/test_cnn.py
@@ -142,16 +142,22 @@ class TestCNN(unittest.TestCase):
 
 @pytest.mark.torch
 def test_cnn_torch():
-  """Test invoking ConvEncoderLayer"""
+    
+    from deepchem.models.torch_models.cnn import CNN
+    torch.manual_seed(0)
+    
+    n_tasks = 5
+    n_features = 8
+    n_classes = 7
+    batch_size = 2
+    mode = 'classification'
+    model = CNN(n_tasks=n_tasks, n_features=n_features, dims=2, dropouts=[0.5, 0.2, 0.4, 0.7],
+                layer_filters=[3, 8, 8, 16], kernel_size=3, n_classes=n_classes, mode=mode, uncertainty=False)
 
-  from deepchem.models.torch_models.cnn import CNN
+    x = torch.Tensor(batch_size, 8, 224, 224)
+    y = model(x)
 
-  torch.manual_seed(0)
-
-  encoder = CNN(layer_filters=[3, 16, 8, 32, 16],
-                             conv_dim=2,
-                             kernel_size=3)
-  x = torch.ones(1, 3, 224, 224)
-  y = encoder(x)
-
-  assert y.shape == (1, 16)
+    # output_type for classification = [output, logits]
+    assert len(y) == 2
+    assert y[0].shape == (batch_size, n_tasks, n_classes)
+    assert y[0].shape == y[1].shape
