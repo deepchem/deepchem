c24aa9e22439f7e71e47a58596b24bb7106692f2
ARY2260 <arb.aryan@gmail.com>
2022-06-29 16:45:34 +0530

    added _MapperDMPNN class and suitable tests (and updates to some type annotations)

--- deepchem/feat/molecule_featurizers/dmpnn_featurizer.py
+++ deepchem/feat/molecule_featurizers/dmpnn_featurizer.py
@@ -1,9 +1,9 @@
 # flake8: noqa
 
-from rdkit import Chem
+import numpy as np
 from typing import List, Tuple, Union, Dict, Set, Sequence
 import deepchem as dc
-from deepchem.utils.typing import RDKitAtom
+from deepchem.utils.typing import RDKitAtom, RDKitMol, RDKitBond
 
 from deepchem.utils.molecule_feature_utils import one_hot_encode
 from deepchem.utils.molecule_feature_utils import get_atom_total_degree_one_hot
@@ -29,7 +29,7 @@ class GraphConvConstants(object):
       'num_Hs': [0, 1, 2, 3, 4]
   }
   ATOM_FEATURES_HYBRIDIZATION: List[str] = ["SP", "SP2", "SP3", "SP3D", "SP3D2"]
-  """Dimension of atom feature vector"""
+  # Dimension of atom feature vector
   ATOM_FDIM = sum(len(choices) + 1 for choices in ATOM_FEATURES.values()) + len(
       ATOM_FEATURES_HYBRIDIZATION) + 1 + 2
   # len(choices) +1 and len(ATOM_FEATURES_HYBRIDIZATION) +1 to include room for unknown set
@@ -44,7 +44,7 @@ def get_atomic_num_one_hot(atom: RDKitAtom,
 
   Parameters
   ---------
-  atom: rdkit.Chem.rdchem.Atom
+  atom: RDKitAtom
     RDKit atom object
   allowable_set: List[int]
     The range of atomic numbers to consider.
@@ -70,7 +70,7 @@ def get_atom_chiral_tag_one_hot(
 
   Parameters
   ---------
-  atom: rdkit.Chem.rdchem.Atom
+  atom: RDKitAtom
     RDKit atom object
   allowable_set: List[int]
     The list of chirality tags to consider.
@@ -92,7 +92,7 @@ def get_atom_mass(atom: RDKitAtom) -> List[float]:
 
   Parameters
   ---------
-  atom: rdkit.Chem.rdchem.Atom
+  atom: RDKitAtom
     RDKit atom object
 
   Returns
@@ -104,7 +104,7 @@ def get_atom_mass(atom: RDKitAtom) -> List[float]:
 
 
 def atom_features(
-    atom: Chem.rdchem.Atom,
+    atom: RDKitAtom,
     functional_groups: List[int] = None,
     only_atom_num: bool = False) -> Sequence[Union[bool, int, float]]:
   """Helper method used to compute atom feature vector.
@@ -113,7 +113,7 @@ def atom_features(
 
   Parameters
   ----------
-  atom: RDKit.Chem.rdchem.Atom
+  atom: RDKitAtom
     Atom to compute features on.
   functional_groups: List[int]
     A k-hot vector indicating the functional groups the atom belongs to.
@@ -173,12 +173,12 @@ def atom_features(
   return features
 
 
-def bond_features(bond: Chem.rdchem.Bond) -> Sequence[Union[bool, int, float]]:
+def bond_features(bond: RDKitBond) -> Sequence[Union[bool, int, float]]:
   """wrapper function for bond_features() already available in deepchem, used to compute bond feature vector.
 
   Parameters
   ----------
-  bond: rdkit.Chem.rdchem.Bond
+  bond: RDKitBond
     Bond to compute features on.
 
   Returns
@@ -207,17 +207,17 @@ def bond_features(bond: Chem.rdchem.Bond) -> Sequence[Union[bool, int, float]]:
 
 
 def map_reac_to_prod(
-    mol_reac: Chem.Mol,
-    mol_prod: Chem.Mol) -> Tuple[Dict[int, int], List[int], List[int]]:
+    mol_reac: RDKitMol,
+    mol_prod: RDKitMol) -> Tuple[Dict[int, int], List[int], List[int]]:
   """
   Function to build a dictionary of mapping atom indices in the reactants to the products.
 
   Parameters
   ----------
-  mol_reac: Chem.Mol
+  mol_reac: RDKitMol
   An RDKit molecule of the reactants.
 
-  mol_prod: Chem.Mol
+  mol_prod: RDKitMol
   An RDKit molecule of the products.
 
   Returns
@@ -254,3 +254,170 @@ def map_reac_to_prod(
                   List[int]] = (reac_id_to_prod_id, only_prod_ids,
                                 only_reac_ids)
   return mappings
+
+
+class _MapperDMPNN:
+  """
+  This class is a helper class for DMPNN featurizer to generate concatenated feature vector and mapping.
+
+  `self.f_ini_atoms_bonds_zero_padded` is the concatenated feature vector which contains
+  concatenation of initial atom and bond features.
+
+  `self.mapping` is the mapping which maps bond index to 'array of indices of the bonds'
+  incoming at the initial atom of the bond (excluding the reverse bonds)
+  """
+
+  def __init__(self, datapoint: RDKitMol, concat_fdim: int,
+               f_atoms_zero_padded: np.ndarray):
+    """
+    Parameters
+    ----------
+    datapoint: RDKitMol
+      RDKit mol object.
+    concat_fdim: int
+      dimension of feature vector with concatenated atom (initial) and bond features
+    f_atoms_zero_padded: np.ndarray
+      mapping from atom index to atom features | initial input is a zero padding
+    """
+    self.datapoint = datapoint
+    self.concat_fdim = concat_fdim
+    self.f_atoms_zero_padded = f_atoms_zero_padded
+
+    # number of atoms
+    self.num_atoms: int = len(f_atoms_zero_padded) - 1
+
+    # number of bonds
+    self.num_bonds: int = 0
+
+    # mapping from bond index to concat(in_atom, bond) features | initial input is a zero padding
+    self.f_ini_atoms_bonds_zero_padded: np.ndarray = np.asarray(
+        [[0] * (self.concat_fdim)], dtype=float)
+
+    # mapping from atom index to list of indicies of incoming bonds
+    self.atom_to_incoming_bonds: List[List[int]] = [
+        [] for i in range(self.num_atoms + 1)
+    ]
+
+    # mapping from bond index to the index of the atom the bond is coming from
+    self.bond_to_ini_atom: List[int] = [0]
+
+    # mapping from bond index to the index of the reverse bond
+    self.b2revb: List[int] = [0]
+
+    self.mapping: np.ndarray = np.empty(0)
+
+    self._generate_mapping()
+
+  def _generate_mapping(self):
+    """
+    Generate mapping which maps bond index to 'array of indices of the bonds'
+    incoming at the initial atom of the bond (reverse bonds are not considered).
+
+    Steps:
+    - Iterate such that all bonds in the mol are considered.
+      For each iteration: (if bond exists)
+      - Update the `self.f_ini_atoms_bonds_zero_padded` concatenated feature vector.
+      - Update secondary mappings.
+    - Modify `self.atom_to_incoming_bonds` based on maximum number of bonds.
+    - Get mapping based on `self.atom_to_incoming_bonds` and `self.bond_to_ini_atom`.
+    - Replace reverse bond values with 0
+    """
+    for a1 in range(1, self.num_atoms + 1):
+      for a2 in range(a1 + 1, self.num_atoms + 1):
+        if not self._update_concat_vector(a1, a2):
+          continue
+        self._update_secondary_mappings(a1, a2)
+        self.num_bonds += 2
+    self._modify_based_on_max_bonds()
+
+    # get mapping which maps bond index to 'array of indices of the bonds' incoming at the initial atom of the bond
+    self.mapping = np.asarray(
+        self.atom_to_incoming_bonds)[self.bond_to_ini_atom]
+
+    self._replace_rev_bonds()
+
+  def _extend_concat_feature(self, a1: int, bond_feature: np.ndarray):
+    """
+    Helper method to concatenate initial atom and bond features and append them to `self.f_ini_atoms_bonds_zero_padded`.
+
+    Parameters
+    ----------
+    a1: int
+      index of the atom where the bond starts
+    bond_feature: np.ndarray
+      array of bond features
+    """
+    concat_input: np.ndarray = np.concatenate(
+        (self.f_atoms_zero_padded[a1], bond_feature),
+        axis=0).reshape([1, self.concat_fdim])
+    self.f_ini_atoms_bonds_zero_padded = np.concatenate(
+        (self.f_ini_atoms_bonds_zero_padded, concat_input), axis=0)
+
+  def _update_concat_vector(self, a1: int, a2: int):
+    """
+    Method to update `self.f_ini_atoms_bonds_zero_padded` with features of the bond between atoms `a1` and `a2`.
+
+    Parameters
+    ----------
+    a1: int
+      index of the atom 1
+    a2: int
+      index of the atom 2
+    """
+    bond: RDKitBond = self.datapoint.GetBondBetweenAtoms(a1 - 1, a2 - 1)
+    if bond is None:
+      return 0
+
+    # get bond features
+    f_bond: np.ndarray = np.asarray(bond_features(bond), dtype=float)
+
+    self._extend_concat_feature(a1, f_bond)
+    self._extend_concat_feature(a2, f_bond)
+    return 1
+
+  def _update_secondary_mappings(self, a1, a2):
+    """
+    Method to update `self.atom_to_incoming_bonds`, `self.bond_to_ini_atom` and `self.b2revb`
+    with respect to the bond between atoms `a1` and `a2`.
+
+    Parameters
+    ----------
+    a1: int
+      index of the atom 1
+    a2: int
+      index of the atom 2
+    """
+    b1: int = self.num_bonds + 1  # bond index
+    b2: int = self.num_bonds + 2  # reverse bond index
+
+    self.atom_to_incoming_bonds[a2].append(b1)  # b1 = a1 --> 'a2'
+    self.atom_to_incoming_bonds[a1].append(b2)  # b2 = a2 --> 'a1'
+
+    self.bond_to_ini_atom.append(a1)  # b1 starts at a1
+    self.bond_to_ini_atom.append(a2)  # b2 starts at a2 (remember, b2 =  b1+1)
+
+    self.b2revb.append(b2)  # reverse bond of b1 is b2
+    self.b2revb.append(b1)  # reverse bond of b2 is b1
+
+  def _modify_based_on_max_bonds(self):
+    """
+    Method to make number of incoming bonds equal to maximum number of bonds.
+    This is done by appending zeros to fill remaining space at each atom indicies.
+    """
+    max_num_bonds: int = max(
+        1,
+        max(
+            len(incoming_bonds)
+            for incoming_bonds in self.atom_to_incoming_bonds))
+    self.atom_to_incoming_bonds = [
+        self.atom_to_incoming_bonds[a] + [0] *
+        (max_num_bonds - len(self.atom_to_incoming_bonds[a]))
+        for a in range(self.num_atoms + 1)
+    ]
+
+  def _replace_rev_bonds(self):
+    """
+    Method to replace the reverse bond indicies with zeros.
+    """
+    for count, i in enumerate(self.b2revb):
+      self.mapping[count][np.where(self.mapping[count] == i)] = 0
