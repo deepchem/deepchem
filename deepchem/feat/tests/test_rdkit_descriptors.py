723dce5abaee0cc464c6f485bd69421bd03bccf3
ARY2260 <arb.aryan@gmail.com>
2022-07-07 20:52:01 +0530

    modify RDKitDescriptors class, add class for normalization parameters and unit tests

--- deepchem/feat/tests/test_rdkit_descriptors.py
+++ deepchem/feat/tests/test_rdkit_descriptors.py
@@ -28,10 +28,10 @@ class TestRDKitDescriptors(unittest.TestCase):
     featurizer = RDKitDescriptors()
     descriptors = featurizer([self.mol])
     assert descriptors.shape == (1, len(featurizer.descriptors))
-    assert np.allclose(
-        descriptors[0, featurizer.descriptors.index('ExactMolWt')],
-        180,
-        atol=0.1)
+    assert np.allclose(descriptors[0,
+                                   featurizer.descriptors.index('ExactMolWt')],
+                       180,
+                       atol=0.1)
 
   def test_rdkit_descriptors_on_smiles(self):
     """
@@ -40,10 +40,10 @@ class TestRDKitDescriptors(unittest.TestCase):
     featurizer = RDKitDescriptors()
     descriptors = featurizer('CC(=O)OC1=CC=CC=C1C(=O)O')
     assert descriptors.shape == (1, len(featurizer.descriptors))
-    assert np.allclose(
-        descriptors[0, featurizer.descriptors.index('ExactMolWt')],
-        180,
-        atol=0.1)
+    assert np.allclose(descriptors[0,
+                                   featurizer.descriptors.index('ExactMolWt')],
+                       180,
+                       atol=0.1)
 
   def test_rdkit_descriptors_with_use_fragment(self):
     """
@@ -55,7 +55,44 @@ class TestRDKitDescriptors(unittest.TestCase):
     assert descriptors.shape == (1, len(featurizer.descriptors))
     all_descriptors = Descriptors.descList
     assert len(featurizer.descriptors) < len(all_descriptors)
-    assert np.allclose(
-        descriptors[0, featurizer.descriptors.index('ExactMolWt')],
-        180,
-        atol=0.1)
+    assert np.allclose(descriptors[0,
+                                   featurizer.descriptors.index('ExactMolWt')],
+                       180,
+                       atol=0.1)
+
+  def test_rdkit_descriptors_with_use_bcut2d(self):
+    """
+    Test with use_bcut2d
+    """
+    from rdkit.Chem import Descriptors
+    featurizer = RDKitDescriptors(use_bcut2d=False)
+    descriptors = featurizer(self.mol)
+    assert descriptors.shape == (1, len(featurizer.descriptors))
+    all_descriptors = Descriptors.descList
+    assert len(featurizer.descriptors) < len(all_descriptors)
+
+    with self.assertRaises(ValueError):
+      featurizer.descriptors.index('BCUT2D_MWHI')
+
+    assert np.allclose(descriptors[0,
+                                   featurizer.descriptors.index('ExactMolWt')],
+                       180,
+                       atol=0.1)
+
+  def test_rdkit_descriptors_normalized(self):
+    """
+    Test with normalization
+    """
+    featurizer = RDKitDescriptors(is_normalized=True)
+    assert featurizer.normalized_desc != {}
+
+    descriptors = featurizer(self.mol)
+    assert descriptors.shape == (1, len(featurizer.descriptors))
+
+    # no normalized feature value should be greater than 1.0
+    assert len(np.where(descriptors > 1.0)[0]) == 0
+
+    assert np.allclose(descriptors[0,
+                                   featurizer.descriptors.index('ExactMolWt')],
+                       0.0098,
+                       atol=0.0001)
