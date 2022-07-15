d4d79525ef681f597478f7203a6bf304acb78ec4
Anshuman Mishra <shivanshuman021@gmail.com>
2022-07-15 10:35:31 +0530

    adding save-reload test

--- deepchem/models/tests/test_cnn.py
+++ deepchem/models/tests/test_cnn.py
@@ -1,4 +1,5 @@
 import pytest
+import tempfile
 from flaky import flaky
 
 try:
@@ -222,3 +223,61 @@ def test_cnn_regression_uncertainty():
   # Predict the output and uncertainty.
   pred, std = model.predict_uncertainty(dataset)
   assert np.mean(np.abs(y - pred)) < 0.2
+
+
+def test_1d_cnn_regression_reload():
+  """Test that a 2D CNN can overfit simple regression datasets."""
+
+  np.random.seed(1)
+
+  n_samples = 10
+  n_features = 3
+  n_tasks = 1
+
+  X = np.random.rand(n_samples, n_features, 10)
+  y = np.random.randint(2, size=(n_samples, n_tasks)).astype(np.float32)
+
+  dataset = dc.data.NumpyDataset(X, y)
+
+  regression_metric = dc.metrics.Metric(dc.metrics.mean_squared_error)
+  model_dir = tempfile.mkdtemp()
+
+  model = CNN(n_tasks,
+              n_features,
+              layer_filters=[100],
+              dims=1,
+              dropouts=0.,
+              kernel_size=3,
+              mode='regression',
+              learning_rate=0.003)
+
+  # Fit trained model
+  model.fit(dataset, nb_epoch=200)
+
+  # Eval model on train
+  scores = model.evaluate(dataset, [regression_metric])
+
+  assert scores[regression_metric.name] < 0.1
+
+  reloaded_model = CNN(n_tasks,
+                       n_features,
+                       layer_filters=[100],
+                       dims=1,
+                       dropouts=0.,
+                       kernel_size=3,
+                       mode='regression',
+                       learning_rate=0.003,
+                       model_dir=model_dir)
+
+  reloaded_model.restore()
+
+  # Check predictions match on random sample
+  Xpred = np.random.rand(n_samples, 10, n_features)
+  predset = dc.data.NumpyDataset(Xpred)
+  origpred = model.predict(predset)
+  reloadpred = reloaded_model.predict(predset)
+  assert np.all(origpred == reloadpred)
+
+  # Eval model on train
+  scores = reloaded_model.evaluate(dataset, [regression_metric])
+  assert scores[regression_metric.name] < 0.1
