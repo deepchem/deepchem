34fb1fa74f27d2d370dd4e2c985f73f8595ccb82
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2022-07-24 22:30:08 +0530

    input features complete

--- deepchem/models/torch_models/ferminet.py
+++ deepchem/models/torch_models/ferminet.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pyscf
-import rdkit.Chem.rdchem as rdchem
+from rdkit import Chem
 import numpy as np
 
 from deepchem.models.torch_models import TorchModel
@@ -15,7 +15,6 @@ import deepchem.models.optimizers as optim
 from deepchem.utils.electron_sampler import ElectronSampler
 
 # TODO look for the loss function(Hamiltonian)
-
 
 class Ferminet(nn.Module):
   """A deep-learning based Variational Monte Carlo method for calculating the ab-initio
@@ -33,11 +32,11 @@ class Ferminet(nn.Module):
 
   def __init__(self, nucleon_coordinates: List[str, List[int]]):
     """
-        Parameters:
-        -----------
-        nucleon_coordinates: Dict[str, List[int]]
-            A dictionary containing nucleon coordinates as the values with the keys as the element's symbol.
-        """
+    Parameters:
+    -----------
+    nucleon_coordinates:  List[str, List[int]]
+      A dictionary containing nucleon coordinates as the values with the keys as the element's symbol.
+    """
     super(Ferminet, self).__init__()
 
     self.nucleon_coordinates = nucleon_coordinates
@@ -49,24 +48,43 @@ class Ferminet(nn.Module):
 
   def prepare_input_stream(self):
     """Prepares the one-electron and two-electron input stream for the model.
-        """
+    
+    Returns:
+    --------
+    one_electron_vector: numpy.ndarray
+      The one-electron input stream containing the distance vector between the electron and nucleus coordinates.
+    one_electron_distance: numpy.ndarray
+      The one-electron input stream containing the distance between the electron and nucleus coordinates.
+    two_electron_vector: numpy.ndarray
+      The two-electron input stream containing the distance vector between all the electrons coordinates.
+    two_electron_distance: numpy.ndarray
+    """
 
     no_electrons = []
     nucleons = []
 
     for i in self.nucleon_coordinates:
-      no_electrons.append([rdchem.GetAtomicNum(i[0])])
+      mol=Chem.MolFromSmiles(i[0])
+      for j in mol.GetAtoms():
+        nucleons.append(j.GetAtomicNum()-j.GetFormalCharge())
       nucleons.append(i[1])
 
+    electron_no = np.array(no_electrons)
+    nuceleon_pos = np.array(nucleons)
+
     molecule = ElectronSampler(
-        central_value=np.array(nucleons), seed=0, f=self.test_f,
-        steps=1000)  # sample the electrons using the electron sampler
+        central_value=nucleons, seed=0, f=self.test_f,
+        steps=1000, seed=0)  # sample the electrons using the electron sampler
     molecule.gauss_initialize_position(
-        np.array(no_electrons))  # initialize the position of the electrons
+        no_electrons)  # initialize the position of the electrons
 
-    # TODO calculate the electron stream according to the shape of "molecule.x"
+    self.one_electron_vector=molecule.x-nucleons
+    self.one_electron_distance=np.linalg.norm(self.one_electron_vector,axis=-1)
 
-
+    shape=np.shape(nucleons)
+    self.two_electron_vector=nucleons-nucleons.reshape([shape[0], shape[2], 1, 3])
+    self.two_electron_distance=np.linalg.norm(self.two_electron_vector,axis=-1)
+    
 # TODO """
 # def loss():
 # """Calculate the loss."""
