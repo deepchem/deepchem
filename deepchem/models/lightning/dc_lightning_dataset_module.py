2fc8be60342187bcd7eb3c2d610a13b1f105f91f
Princy Chahal <princychahal68@gmail.com>
2022-07-25 22:07:46 -0400

    Add docstring

--- deepchem/models/lightning/dc_lightning_dataset_module.py
+++ deepchem/models/lightning/dc_lightning_dataset_module.py
@@ -16,8 +16,25 @@ def collate_dataset_wrapper(batch):
 
 
 class DCLightningDatasetModule(pl.LightningDataModule):
+  """DeepChem Lightning Dataset Module to be used with the DCLightningModule and a Lightning trainer.
+
+  This module wraps over the the deepchem pytorch dataset and dataloader providing a generic interface to run training.
+  https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
+
+  Notes
+  -----
+  This class requires PyTorch to be installed.
+  """
 
   def __init__(self, dataset, batch_size, collate_fn):
+    """Create a new DCLightningDatasetModule.
+
+    Parameters
+    ----------
+    dataset: A deepchem dataset.
+    batch_size: Batch size for the dataloader.
+    collate_fn: Method to collate instances across batch.
+    """
     super().__init__()
     self._batch_size = batch_size
     self._dataset = dataset.make_pytorch_dataset(batch_size=self._batch_size)
@@ -27,9 +44,16 @@ class DCLightningDatasetModule(pl.LightningDataModule):
     self.train_dataset = self._dataset
 
   def train_dataloader(self):
-    return torch.utils.data.DataLoader(
+    """Returns the train dataloader from train dataset.
+
+    Returns
+    -------
+    dataloader: train dataloader to be used with DCLightningModule.
+    """
+    dataloader = torch.utils.data.DataLoader(
         self.train_dataset,
         batch_size=None,
         collate_fn=self.collate_fn,
         shuffle=False,
     )
+    return dataloader
