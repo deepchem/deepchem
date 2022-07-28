af8dc61a2d0e3bd57de4740e631873d47dd1b682
ARY2260 <arb.aryan@gmail.com>
2022-07-28 12:48:06 +0530

    modify mapper class and unit tests

--- deepchem/models/tests/test_mapper_dmpnn.py
+++ deepchem/models/tests/test_mapper_dmpnn.py
@@ -17,9 +17,14 @@ smiles_list = ["C", "CC", "CCC", "C1=CC=CC=C1"]
 featurizer = DMPNNFeaturizer(use_original_atom_ranks=True,
                              features_generators=['morgan'])
 graphs = featurizer.featurize(smiles_list)
+benezene_atom_to_incoming_bonds: np.ndarray = np.asarray([[1, 10], [0,
+                                                                    3], [2, 5],
+                                                          [4, 7], [6, 9],
+                                                          [8, 11]])
 benezene_mapping: np.ndarray = np.asarray([[-1, 10], [-1, 3], [0, -1], [-1, 5],
                                            [2, -1], [-1, 7], [4, -1], [-1, 9],
-                                           [6, -1], [-1, 11], [8, -1], [1, -1]])
+                                           [6, -1], [-1, 11], [8, -1], [1, -1],
+                                           [-1, -1]])
 
 
 @pytest.mark.torch
@@ -37,8 +42,12 @@ def test_mapper_general_attributes():
     assert mapper.f_ini_atoms_bonds.shape == (graph.num_edges + 1,
                                               concat_feature_dim)
     assert (mapper.f_ini_atoms_bonds[-1] == np.zeros(concat_feature_dim)).all()
-    assert mapper.values == (mapper.f_ini_atoms_bonds, mapper.mapping,
-                             mapper.global_features)
+    assert len(mapper.f_ini_atoms_bonds) == len(mapper.mapping)
+    assert (mapper.values[0] == mapper.atom_features).all()
+    assert (mapper.values[1] == mapper.f_ini_atoms_bonds).all()
+    assert (mapper.values[2] == mapper.atom_to_incoming_bonds).all()
+    assert (mapper.values[3] == mapper.mapping).all()
+    assert (mapper.values[4] == mapper.global_features).all()
 
 
 @pytest.mark.torch
@@ -48,6 +57,7 @@ def test_mapper_no_bond():
   """
   mapper = _MapperDMPNN(graphs[0])
   assert (mapper.bond_to_ini_atom == np.empty(0)).all()
+  assert (mapper.atom_to_incoming_bonds == np.asarray([[-1]])).all()
   assert (mapper.mapping == np.asarray([[-1]])).all()
 
 
@@ -58,7 +68,8 @@ def test_mapper_two_directed_bonds_btw_two_atoms():
   """
   mapper = _MapperDMPNN(graphs[1])
   assert (mapper.bond_to_ini_atom == np.asarray([0, 1])).all()
-  assert (mapper.mapping == np.asarray([[-1], [-1]])).all()
+  assert (mapper.atom_to_incoming_bonds == np.asarray([[1], [0]])).all()
+  assert (mapper.mapping == np.asarray([[-1], [-1], [-1]])).all()
 
 
 @pytest.mark.torch
@@ -68,7 +79,10 @@ def test_mapper_two_adjacent_bonds():
   """
   mapper = _MapperDMPNN(graphs[2])
   assert (mapper.bond_to_ini_atom == np.asarray([0, 1, 1, 2])).all()
-  assert (mapper.mapping == np.asarray([[-1, -1], [-1, 3], [0, -1],
+  print(mapper.atom_to_incoming_bonds)
+  assert (mapper.atom_to_incoming_bonds == np.asarray([[1, -1], [0, 3],
+                                                       [2, -1]])).all()
+  assert (mapper.mapping == np.asarray([[-1, -1], [-1, 3], [0, -1], [-1, -1],
                                         [-1, -1]])).all()
 
 
@@ -80,4 +94,6 @@ def test_mapper_ring():
   mapper = _MapperDMPNN(graphs[3])
   assert (mapper.bond_to_ini_atom == np.asarray(
       [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 0])).all()
+  assert (
+      mapper.atom_to_incoming_bonds == benezene_atom_to_incoming_bonds).all()
   assert (mapper.mapping == benezene_mapping).all()
