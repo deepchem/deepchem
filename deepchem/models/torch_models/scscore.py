a679cf99d78dac75f90e2924625265eadc6d50ae
Balaji <balajivd01@gmail.com>
2022-10-03 18:25:55 +0700

    [WIP] Add scscore implemented using PyTorch
    I've implemented scscore architecture using PyTorch as referenced in https://github.com/deepchem/deepchem/issues/2863.
    I created the architecture using PyTorch nn module. I need to implement the TorchModel part (I've no idea on how to do it, need some more time to figure out).
    
    Also, the original keras implementation seems outdated. I didn't implement scscore based on keras implementation, but based on the original paper.
    So I'm just making this PR to get feedback on whether I've done something wrong.

---
+++ deepchem/models/torch_models/scscore.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+from deepchem.data import NumpyDataset
+from deepchem.feat import CircularFingerprint
+from deepchem.models.torch_models.torch_model import TorchModel
+from deepchem.models.losses import L1Loss
+
+class ScScore(nn.Module):
+    """
+    ScScore paper - https://pubs.acs.org/doi/abs/10.1021/acs.jcim.7b00622
+
+    ScScore is a FeedForward neural network with 5 hidden layers. Each hidden layer
+    contains 300 neurons. ReLU is applied for all the hidden layers. Sigmoid is applied
+    for the output layer. In the original paper, they didn't use Dropout. But in our
+    implementation, you can set the dropout for all the hidden layers at once.
+    """
+    
+    def __init__(self, n_features, layer_sizes, dropout, **kwargs):
+        """
+        Arguments
+        """
+        super().__init__()
+        self.dropout = dropout
+        self.input_layer = nn.Linear(n_features, layer_sizes[0])
+        self.hidden_layers = nn.ModuleList()
+        for layer_size in layer_sizes[1:]:
+            self.hidden_layers.append(nn.Linear(layer_size, layer_size))
+        self.output_layer = nn.Linear(layer_sizes[-1],1)
+        
+    def forward(self,x):
+        x = nn.ReLU()(self.input_layer(x))
+        for hidden_layer in self.hidden_layers:
+            x = hidden_layer(x)
+            if self.dropout > 0:
+                x = nn.Dropout(p=self.dropout)(x)
+            x = nn.ReLU()(x)
+        output = self.output_layer(x)
+        output = nn.Sigmoid()(output)
+        return output
+        
+class ScScoreModel(TorchModel):
+    
+    def __init__(self, n_features=1024, layer_sizes=[300,300,300,300,300], dropout = 0, **kwargs):
+        self.n_features = n_features
+        model = ScScore(n_features, layer_sizes, dropout)
+        output_types = ['prediction']
+        super(ScScoreModel, self).__init__(
+             model, L1Loss(), output_types=output_types, **kwargs)
