9a6d4cf65c709e577b9826f93a351ac84a1c1e02
Tony Davis <acdavis629@gmail.com>
2022-11-14 12:41:15 -0500

    yapf, todo flake8

--- deepchem/utils/test/test_jackhmmer.py
+++ deepchem/utils/test/test_jackhmmer.py
@@ -5,10 +5,11 @@ class TestJackhmmer(unittest.TestCase):
   """
   Test Jackhmmer  on a toy dataset
   """
+
   def test_jackhmmer(self):
     j = Jackhmmer(database_path='assets/test.fasta')
     result = j.query("assets/sequence.fasta")
     exp_val = '#=GC RF xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     for line in result[0]['sto'].split('\n'):
       if line.startswith('#=GC'):
-        self.assertEqual(line,exp_val)
+        self.assertEqual(line, exp_val)
