7cf0f290e24b2be6c1edd6431d04214b5a6c2242
Tony Davis <acdavis629@gmail.com>
2022-11-14 12:51:00 -0500

    reindent

--- deepchem/utils/test/test_jackhmmer.py
+++ deepchem/utils/test/test_jackhmmer.py
@@ -2,14 +2,14 @@ from deepchem.utils.jackhmmer import Jackhmmer
 import unittest
 
 class TestJackhmmer(unittest.TestCase):
-  """
-  Test Jackhmmer  on a toy dataset
-  """
+    """
+    Test Jackhmmer  on a toy dataset
+    """
 
-  def test_jackhmmer(self):
-    j = Jackhmmer(database_path='assets/test.fasta')
-    result = j.query("assets/sequence.fasta")
-    exp_val = '#=GC RF xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    for line in result[0]['sto'].split('\n'):
-      if line.startswith('#=GC'):
-        self.assertEqual(line, exp_val)
+    def test_jackhmmer(self):
+        j = Jackhmmer(database_path='assets/test.fasta')
+        result = j.query("assets/sequence.fasta")
+        exp_val = '#=GC RF xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+        for line in result[0]['sto'].split('\n'):
+            if line.startswith('#=GC'):
+                self.assertEqual(line, exp_val)
