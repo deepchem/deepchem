8bf0aed2c6aa258ad8b11302d78ed18f75692082
Balaji <balajivd01@gmail.com>
2022-12-09 00:02:24 +0530

    Update scscore.py

--- deepchem/models/torch_models/scscore.py
+++ deepchem/models/torch_models/scscore.py
@@ -3,17 +3,13 @@ import torch.nn as nn
 from deepchem.data import NumpyDataset
 from deepchem.feat import CircularFingerprint
 from deepchem.models.torch_models.torch_model import TorchModel
-from deepchem.models.losses import L1Loss
+from deepchem.models.losses import Loss
 
 
 class SCScore(nn.Module):
     """
-    SCScore paper - https://pubs.acs.org/doi/abs/10.1021/acs.jcim.7b00622
-
-    SCScore is a FeedForward neural network with 5 hidden layers. Each hidden layer
-    contains 300 neurons. ReLU is applied for all the hidden layers. Sigmoid is applied
-    for the output layer. In the original paper, they didn't use Dropout. But in our
-    implementation, you can set the dropout for all the hidden layers at once.
+    SCScore is a Feed-Forward Neural network with 5 layers.
+    Each layer consists of only 300 neurons.
     """
     
     def __init__(self, n_features: int, layer_sizes: list, dropout: int, **kwargs) -> None:
@@ -29,6 +25,8 @@ class SCScore(nn.Module):
         self.output_layer = nn.Linear(layer_sizes[-1],1)
         
     def forward(self,x: torch.Tensor) -> torch.Tensor:
+        #input tensor is of shape - (batch_size, 2, n_features)
+        x = torch.cat((x[:,0],x[:,1]),dim=0) 
         x = nn.ReLU()(self.input_layer(x))
         for hidden_layer in self.hidden_layers:
             x = hidden_layer(x)
@@ -36,18 +34,56 @@ class SCScore(nn.Module):
                 x = nn.Dropout(p=self.dropout)(x)
             x = nn.ReLU()(x)
         output = self.output_layer(x)
-        output = nn.Sigmoid()(output)
+        output = nn.Sigmoid()(output) * 4 + 1
+        
+        #output shape - (batch_size*2, 1)
+        selector = output.shape[0] // 2
+        output = (output[:selector], output[selector:])
+        #output[0] is reactant, output[1] is product
         return output
+
+
+class SCScoreLoss(Loss):
+    """
+    Defines the Loss for the SCScore Model.
+    The Loss for the SCScore is just a HingeLoss.
+    """
+    def __init__(self, offset):
+        self.offset = offset
+
+    def _create_pytorch_loss(self):
+
+        def loss(output, labels):
+            #output[0] is reactant, output[1] is product
+            loss_item = nn.ReLU() (self.offset + output[0] - output[1])
+            loss_item = torch.sum(loss_item)
+            return loss_item
+
+        return loss
         
-        
+
 class SCScoreModel(TorchModel):
     """
-    
+    SCScore paper link - https://pubs.acs.org/doi/abs/10.1021/acs.jcim.7b00622
     """
     
-    def __init__(self, n_features=1024, layer_sizes=[300,300,300,300,300], dropout = 0, **kwargs):
+    def __init__(self, n_features=1024, layer_sizes=[300,300,300,300,300], dropout = 0, offset=0.25, **kwargs):
         self.n_features = n_features
         model = SCScore(n_features, layer_sizes, dropout)
         output_types = ['prediction']
         super(SCScoreModel, self).__init__(
-             model, L1Loss(), output_types=output_types, **kwargs)
+             model, SCScoreLoss(offset), output_types=output_types, **kwargs)
+
+    def default_generator(self, dataset, epochs=1, mode='fit', deterministic=True, pad_batches=True):
+
+        for epoch in range(epochs):
+            for (X_b, y_b, w_b, ids_b) in dataset.iterbatches(
+                batch_size=self.batch_size,
+                deterministic=deterministic,
+                pad_batches=pad_batches):
+                #yield ([X_b[:, 0], X_b[:, 1]], [y_b], [w_b])
+                yield ([X_b], [y_b], [w_b])  
+    
+    def predict_mols(self, mols):
+        pass
+    
