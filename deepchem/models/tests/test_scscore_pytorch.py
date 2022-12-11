afb5791b452d7126af8c04c67bd3d1278b0d50f8
Balaji <balajivd01@gmail.com>
2022-12-11 14:45:10 +0530

    Update test_scscore_pytorch.py

--- deepchem/models/tests/test_scscore_pytorch.py
+++ deepchem/models/tests/test_scscore_pytorch.py
@@ -1,31 +1,45 @@
 import pytest
-import numpy as np
+import tempfile
 
-import unittest
+import deepchem as dc
+import numpy as np
 
 try:
   import torch
-  import torch.nn as nn
-  from deepchem.models.torch_models.scscore import ScScore
+  from deepchem.models.torch_models.scscore import SCScoreModel
   has_torch = True
 except:
   has_torch = False
-  
-  
-@unittest.skipIf(not has_torch, 'torch is not installed')
+
+
 @pytest.mark.torch
-def test_scscore_pytorch():
-    """
-    This test evaluates the ScScore.
-    """
-    
-    #Initializes the ScScore
-    n_features = 1024
-    layer_sizes = [300,300,300,300,300]
-    dropout_prob = 0
-    model = ScScore(n_features= n_features, layer_sizes= layer_sizes, dropout = dropout_prob)
-    
-    batch_size = 32
-    test_input = torch.randn((batch_size,n_features))
-    
-    assert model(test_input).shape == torch.Size([batch_size,1]) , "Model output doesn't match the expected shape"
+def test_scscore_reload():
+  """Tests the model reload capability"""
+
+  #create a random dataset
+  n_samples = 100
+  n_features = 256
+  X = np.random.rand(n_samples, 2, n_features) #2 indicates we need two molecular features for each sample
+  dataset = dc.data.NumpyDataset(X) #SCScore don't use labels
+
+  model_dir = tempfile.mkdtemp() #create an empty directory for saving the model
+
+  #initialize the model
+  model = SCScoreModel(n_features=n_features, model_dir=model_dir)
+  model.fit(dataset, nb_epoch=25)
+  model_preds = model.predict(dataset)
+
+  #reload the model
+  reloaded_model = SCScoreModel(n_features=n_features, model_dir=model_dir)
+  reloaded_model.restore()
+  reloaded_model_preds = reloaded_model.predict(dataset)
+
+  assert len(model_preds) == len(reloaded_model_preds)
+  for i,j in zip(model_preds, reloaded_model_preds):
+    assert np.all(i == j)
+  
+
+@pytest.mark.torch
+def test_scscore_overfit():
+  """Tests the model for overfit"""
+  pass
