59f4f670da8ca16acb640caae54b026b8949aedd
advikavs <advika.vs@gmail.com>
2023-01-05 20:08:46 +0530

    yapf fixes

--- deepchem/data/datasets.py
+++ deepchem/data/datasets.py
@@ -1089,9 +1089,9 @@ class NumpyDataset(Dataset):
           [ids, dataset.ids],
           axis=0,
       )
-    k = y.shape[0]
-    if y.shape == (k,):
-      y = y.reshape(k, 1)
+
+    if y.ndim == 1:
+      y = y.reshape(-1, 1)
     return NumpyDataset(X, y, w, ids, n_tasks=y.shape[1])
 
 
