a5da36c403fe25202ae3b463eae27ac930068c02
maithili232 <47156732+maithili232@users.noreply.github.com>
2023-01-17 16:59:07 +0400

    yapf fixes on trans/tests (#3178)
    performed yapf on 15 tests files in trans directory.

--- deepchem/trans/tests/test_coulomb.py
+++ deepchem/trans/tests/test_coulomb.py
@@ -4,15 +4,15 @@ import deepchem as dc
 
 
 def test_coulomb_fit_transformer():
-  """Test coulomb fit transformer on singletask dataset."""
-  n_samples = 10
-  n_features = 3
-  n_tasks = 1
-  ids = np.arange(n_samples)
-  X = np.random.rand(n_samples, n_features, n_features)
-  y = np.zeros((n_samples, n_tasks))
-  w = np.ones((n_samples, n_tasks))
-  dataset = dc.data.NumpyDataset(X, y, w, ids)
-  fit_transformer = dc.trans.CoulombFitTransformer(dataset)
-  X_t = fit_transformer.X_transform(dataset.X)
-  assert len(X_t.shape) == 2
+    """Test coulomb fit transformer on singletask dataset."""
+    n_samples = 10
+    n_features = 3
+    n_tasks = 1
+    ids = np.arange(n_samples)
+    X = np.random.rand(n_samples, n_features, n_features)
+    y = np.zeros((n_samples, n_tasks))
+    w = np.ones((n_samples, n_tasks))
+    dataset = dc.data.NumpyDataset(X, y, w, ids)
+    fit_transformer = dc.trans.CoulombFitTransformer(dataset)
+    X_t = fit_transformer.X_transform(dataset.X)
+    assert len(X_t.shape) == 2
