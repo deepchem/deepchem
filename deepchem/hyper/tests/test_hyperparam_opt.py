85f43f60f48956bcd0ba256e739f91660be59e5a
maithili232 <maithililohakare@gmail.com>
2023-01-18 16:12:19 +0400

    yapf fixes on hyper directory

--- deepchem/hyper/tests/test_hyperparam_opt.py
+++ deepchem/hyper/tests/test_hyperparam_opt.py
@@ -8,20 +8,21 @@ import deepchem as dc
 
 
 class TestHyperparamOpt(unittest.TestCase):
-  """
-  Test abstract superclass behavior.
-  """
+    """
+    Test abstract superclass behavior.
+    """
 
-  def test_cant_be_initialized(self):
-    """Test HyperparamOpt can't be initialized."""
-    initialized = True
+    def test_cant_be_initialized(self):
+        """Test HyperparamOpt can't be initialized."""
+        initialized = True
 
-    def rf_model_builder(model_params, model_dir):
-      sklearn_model = sklearn.ensemble.RandomForestRegressor(**model_params)
-      return dc.model.SklearnModel(sklearn_model, model_dir)
+        def rf_model_builder(model_params, model_dir):
+            sklearn_model = sklearn.ensemble.RandomForestRegressor(
+                **model_params)
+            return dc.model.SklearnModel(sklearn_model, model_dir)
 
-    try:
-      _ = dc.hyper.HyperparamOpt(rf_model_builder)
-    except ValueError:
-      initialized = False
-    assert not initialized
+        try:
+            _ = dc.hyper.HyperparamOpt(rf_model_builder)
+        except ValueError:
+            initialized = False
+        assert not initialized
