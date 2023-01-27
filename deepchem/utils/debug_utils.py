750452f08d3be2841d41310e6c0cd1a81e3c9357
maithili232 <47156732+maithili232@users.noreply.github.com>
2023-01-27 08:59:02 +0400

    yapf on deepchem/utils (#3203)

--- deepchem/utils/debug_utils.py
+++ deepchem/utils/debug_utils.py
@@ -3,32 +3,32 @@ _print_threshold = 10
 
 
 def get_print_threshold() -> int:
-  """Return the printing threshold for datasets.
+    """Return the printing threshold for datasets.
 
-  The print threshold is the number of elements from ids/tasks to
-  print when printing representations of `Dataset` objects.
+    The print threshold is the number of elements from ids/tasks to
+    print when printing representations of `Dataset` objects.
 
-  Returns
-  ----------
-  threshold: int
-    Number of elements that will be printed
-  """
-  return _print_threshold
+    Returns
+    ----------
+    threshold: int
+        Number of elements that will be printed
+    """
+    return _print_threshold
 
 
 def set_print_threshold(threshold: int):
-  """Set print threshold
+    """Set print threshold
 
-  The print threshold is the number of elements from ids/tasks to
-  print when printing representations of `Dataset` objects.
+    The print threshold is the number of elements from ids/tasks to
+    print when printing representations of `Dataset` objects.
 
-  Parameters
-  ----------
-  threshold: int
-    Number of elements to print.
-  """
-  global _print_threshold
-  _print_threshold = threshold
+    Parameters
+    ----------
+    threshold: int
+        Number of elements to print.
+    """
+    global _print_threshold
+    _print_threshold = threshold
 
 
 # If a dataset contains more than this number of elements, it won't
@@ -37,33 +37,33 @@ _max_print_size = 1000
 
 
 def get_max_print_size() -> int:
-  """Return the max print size for a dataset.
+    """Return the max print size for a dataset.
 
-  If a dataset is large, printing `self.ids` as part of a string
-  representation can be very slow. This field controls the maximum
-  size for a dataset before ids are no longer printed.
+    If a dataset is large, printing `self.ids` as part of a string
+    representation can be very slow. This field controls the maximum
+    size for a dataset before ids are no longer printed.
 
-  Returns
-  -------
-  max_print_size: int
-    Maximum length of a dataset for ids to be printed in string
-    representation.
-  """
-  return _max_print_size
+    Returns
+    -------
+    max_print_size: int
+        Maximum length of a dataset for ids to be printed in string
+        representation.
+    """
+    return _max_print_size
 
 
 def set_max_print_size(max_print_size: int):
-  """Set max_print_size
+    """Set max_print_size
 
-  If a dataset is large, printing `self.ids` as part of a string
-  representation can be very slow. This field controls the maximum
-  size for a dataset before ids are no longer printed.
+    If a dataset is large, printing `self.ids` as part of a string
+    representation can be very slow. This field controls the maximum
+    size for a dataset before ids are no longer printed.
 
-  Parameters
-  ----------
-  max_print_size: int
-    Maximum length of a dataset for ids to be printed in string
-    representation.
-  """
-  global _max_print_size
-  _max_print_size = max_print_size
+    Parameters
+    ----------
+    max_print_size: int
+        Maximum length of a dataset for ids to be printed in string
+        representation.
+    """
+    global _max_print_size
+    _max_print_size = max_print_size
