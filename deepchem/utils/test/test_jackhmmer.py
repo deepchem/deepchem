5aaee59ceddb635cea47550388ee5c72407698c3
Tony Davis <acdavis629@gmail.com>
2023-01-27 08:54:09 -0500

    pytest jackhammer

--- deepchem/utils/test/test_jackhmmer.py
+++ deepchem/utils/test/test_jackhmmer.py
@@ -1,16 +1,17 @@
 from deepchem.utils.jackhmmer import Jackhmmer
 import unittest
+import pytest
 
-
-class TestJackhmmer(unittest.TestCase):
-    """
-    Test Jackhmmer  on a toy dataset
-    """
-
-    def test_jackhmmer(self):
-        j = Jackhmmer(database_path='assets/test.fasta')
-        result = j.query("assets/sequence.fasta")
-        exp_val = '#=GC RF xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-        for line in result[0]['sto'].split('\n'):
-            if line.startswith('#=GC'):
-                self.assertEqual(line, exp_val)
+# class TestJackhmmer(unittest.TestCase):
+#     """
+#     Test Jackhmmer  on a toy dataset
+#     """
+@pytest
+def test_jackhmmer(self):
+    j = Jackhmmer(database_path='./assets/test.fasta')
+    result = j.query("./assets/sequence.fasta")
+    exp_val = '#=GC RF xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    for line in result[0]['sto'].split('\n'):
+        if line.startswith('#=GC'):
+            self.assertEqual(line, exp_val)
+test_jackhmmer()
