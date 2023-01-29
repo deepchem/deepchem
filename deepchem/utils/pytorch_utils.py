0611ac54e66589956435a7ea30a91f80b49d88d5
maithili232 <47156732+maithili232@users.noreply.github.com>
2023-01-29 22:45:55 +0530

    yapf fixes on deepchem/utils part three (#3205)
    * yapf fixes on deepchem/utils

--- deepchem/utils/pytorch_utils.py
+++ deepchem/utils/pytorch_utils.py
@@ -5,12 +5,12 @@ from typing import Callable, Union
 
 
 def get_activation(fn: Union[Callable, str]):
-  """Get a PyTorch activation function, specified either directly or as a string.
+    """Get a PyTorch activation function, specified either directly or as a string.
 
     This function simplifies allowing users to specify activation functions by name.
     If a function is provided, it is simply returned unchanged.  If a string is provided,
     the corresponding function in torch.nn.functional is returned.
     """
-  if isinstance(fn, str):
-    return getattr(torch.nn.functional, fn)
-  return fn
+    if isinstance(fn, str):
+        return getattr(torch.nn.functional, fn)
+    return fn
