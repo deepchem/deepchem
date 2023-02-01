fe6dd43cc952d47b2c93bad65bf0e02fb7804b59
gusty1g <arun.palaniappan1999@gmail.com>
2023-02-01 21:53:56 +0530

    Yapf fixes on test directories (#3212)
    * yapf fixes for models/tests, dock/tests, splits/tests, rl/tests, molnet/tests dir

--- deepchem/dock/tests/test_binding_pocket.py
+++ deepchem/dock/tests/test_binding_pocket.py
@@ -14,48 +14,46 @@ logger = logging.getLogger(__name__)
 
 
 class TestBindingPocket(unittest.TestCase):
-  """
-  Does sanity checks on binding pocket generation.
-  """
+    """Does sanity checks on binding pocket generation."""
 
-  def test_convex_init(self):
-    """Tests that ConvexHullPocketFinder can be initialized."""
-    dc.dock.ConvexHullPocketFinder()
+    def test_convex_init(self):
+        """Tests that ConvexHullPocketFinder can be initialized."""
+        dc.dock.ConvexHullPocketFinder()
 
-  def test_get_face_boxes_for_protein(self):
-    """Tests that binding pockets are detected."""
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    protein_file = os.path.join(current_dir, "1jld_protein.pdb")
-    coords = rdkit_utils.load_molecule(protein_file)[0]
+    def test_get_face_boxes_for_protein(self):
+        """Tests that binding pockets are detected."""
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        protein_file = os.path.join(current_dir, "1jld_protein.pdb")
+        coords = rdkit_utils.load_molecule(protein_file)[0]
 
-    boxes = box_utils.get_face_boxes(coords)
-    assert isinstance(boxes, list)
-    # Pocket is of form ((x_min, x_max), (y_min, y_max), (z_min, z_max))
-    for pocket in boxes:
-      assert isinstance(pocket, box_utils.CoordinateBox)
+        boxes = box_utils.get_face_boxes(coords)
+        assert isinstance(boxes, list)
+        # Pocket is of form ((x_min, x_max), (y_min, y_max), (z_min, z_max))
+        for pocket in boxes:
+            assert isinstance(pocket, box_utils.CoordinateBox)
 
-  def test_convex_find_pockets(self):
-    """Test that some pockets are filtered out."""
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    protein_file = os.path.join(current_dir, "1jld_protein.pdb")
+    def test_convex_find_pockets(self):
+        """Test that some pockets are filtered out."""
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        protein_file = os.path.join(current_dir, "1jld_protein.pdb")
 
-    finder = dc.dock.ConvexHullPocketFinder()
-    all_pockets = finder.find_all_pockets(protein_file)
-    pockets = finder.find_pockets(protein_file)
-    # Test that every atom in pocket maps exists
-    for pocket in pockets:
-      assert isinstance(pocket, box_utils.CoordinateBox)
+        finder = dc.dock.ConvexHullPocketFinder()
+        all_pockets = finder.find_all_pockets(protein_file)
+        pockets = finder.find_pockets(protein_file)
+        # Test that every atom in pocket maps exists
+        for pocket in pockets:
+            assert isinstance(pocket, box_utils.CoordinateBox)
 
-    assert len(pockets) < len(all_pockets)
+        assert len(pockets) < len(all_pockets)
 
-  def test_extract_active_site(self):
-    """Test that computed pockets have strong overlap with true binding pocket."""
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    protein_file = os.path.join(current_dir, "1jld_protein.pdb")
-    ligand_file = os.path.join(current_dir, "1jld_ligand.sdf")
+    def test_extract_active_site(self):
+        """Test that computed pockets have strong overlap with true binding pocket."""
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        protein_file = os.path.join(current_dir, "1jld_protein.pdb")
+        ligand_file = os.path.join(current_dir, "1jld_ligand.sdf")
 
-    active_site_box, active_site_coords = \
-      dc.dock.binding_pocket.extract_active_site(protein_file, ligand_file)
+        active_site_box, active_site_coords = \
+          dc.dock.binding_pocket.extract_active_site(protein_file, ligand_file)
 
-    assert isinstance(active_site_box, box_utils.CoordinateBox)
-    assert isinstance(active_site_coords, np.ndarray)
+        assert isinstance(active_site_box, box_utils.CoordinateBox)
+        assert isinstance(active_site_coords, np.ndarray)
--- deepchem/models/tests/test_robust.py
+++ deepchem/models/tests/test_robust.py
@@ -2,74 +2,73 @@ import numpy as np
 import deepchem as dc
 import pytest
 try:
-  import tensorflow as tf
-  has_tensorflow = True
+    import tensorflow as tf
+    has_tensorflow = True
 except:
-  has_tensorflow = False
+    has_tensorflow = False
 
 
 @pytest.mark.tensorflow
 def test_singletask_robust_multitask_classification():
-  """Test robust multitask singletask classification."""
-  n_tasks = 1
-  n_samples = 10
-  n_features = 3
-  n_classes = 2
+    """Test robust multitask singletask classification."""
+    n_tasks = 1
+    n_samples = 10
+    n_features = 3
+    n_classes = 2
 
-  # Generate dummy dataset
-  np.random.seed(123)
-  ids = np.arange(n_samples)
-  X = np.random.rand(n_samples, n_features)
-  y = np.zeros((n_samples, n_tasks))
-  w = np.ones((n_samples, n_tasks))
-  dataset = dc.data.NumpyDataset(X, y, w, ids)
+    # Generate dummy dataset
+    np.random.seed(123)
+    ids = np.arange(n_samples)
+    X = np.random.rand(n_samples, n_features)
+    y = np.zeros((n_samples, n_tasks))
+    w = np.ones((n_samples, n_tasks))
+    dataset = dc.data.NumpyDataset(X, y, w, ids)
 
-  classification_metric = dc.metrics.Metric(
-      dc.metrics.accuracy_score, task_averager=np.mean)
-  model = dc.models.RobustMultitaskClassifier(
-      n_tasks,
-      n_features,
-      layer_sizes=[50],
-      bypass_layer_sizes=[10],
-      dropouts=[0.],
-      learning_rate=0.003,
-      weight_init_stddevs=[.1],
-      batch_size=n_samples)
+    classification_metric = dc.metrics.Metric(dc.metrics.accuracy_score,
+                                              task_averager=np.mean)
+    model = dc.models.RobustMultitaskClassifier(n_tasks,
+                                                n_features,
+                                                layer_sizes=[50],
+                                                bypass_layer_sizes=[10],
+                                                dropouts=[0.],
+                                                learning_rate=0.003,
+                                                weight_init_stddevs=[.1],
+                                                batch_size=n_samples)
 
-  # Fit trained model
-  model.fit(dataset, nb_epoch=1)
+    # Fit trained model
+    model.fit(dataset, nb_epoch=1)
 
 
 @pytest.mark.tensorflow
 def test_singletask_robust_multitask_regression():
-  """Test singletask robust multitask regression."""
-  np.random.seed(123)
-  tf.random.set_seed(123)
-  n_tasks = 1
-  n_samples = 10
-  n_features = 3
-  n_classes = 2
+    """Test singletask robust multitask regression."""
+    np.random.seed(123)
+    tf.random.set_seed(123)
+    n_tasks = 1
+    n_samples = 10
+    n_features = 3
+    n_classes = 2
 
-  # Generate dummy dataset
-  np.random.seed(123)
-  ids = np.arange(n_samples)
-  X = np.random.rand(n_samples, n_features)
-  y = np.zeros((n_samples, n_tasks))
-  w = np.ones((n_samples, n_tasks))
+    # Generate dummy dataset
+    np.random.seed(123)
+    ids = np.arange(n_samples)
+    X = np.random.rand(n_samples, n_features)
+    y = np.zeros((n_samples, n_tasks))
+    w = np.ones((n_samples, n_tasks))
 
-  dataset = dc.data.NumpyDataset(X, y, w, ids)
+    dataset = dc.data.NumpyDataset(X, y, w, ids)
 
-  regression_metric = dc.metrics.Metric(
-      dc.metrics.mean_squared_error, task_averager=np.mean, mode="regression")
-  model = dc.models.RobustMultitaskRegressor(
-      n_tasks,
-      n_features,
-      layer_sizes=[50],
-      bypass_layer_sizes=[10],
-      dropouts=[0.],
-      learning_rate=0.003,
-      weight_init_stddevs=[.1],
-      batch_size=n_samples)
+    regression_metric = dc.metrics.Metric(dc.metrics.mean_squared_error,
+                                          task_averager=np.mean,
+                                          mode="regression")
+    model = dc.models.RobustMultitaskRegressor(n_tasks,
+                                               n_features,
+                                               layer_sizes=[50],
+                                               bypass_layer_sizes=[10],
+                                               dropouts=[0.],
+                                               learning_rate=0.003,
+                                               weight_init_stddevs=[.1],
+                                               batch_size=n_samples)
 
-  # Fit trained model
-  model.fit(dataset, nb_epoch=1)
+    # Fit trained model
+    model.fit(dataset, nb_epoch=1)
