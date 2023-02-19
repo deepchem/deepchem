bea74f4d68df4f6971c28153cd4124764f2f4033
brianpzaide <brianpzaide@gmail.com>
2023-02-19 06:22:12 +0000

    trimming dependency pyGPGO

--- deepchem/hyper/pyGPGO_classes.py
+++ deepchem/hyper/pyGPGO_classes.py
@@ -13,6 +13,7 @@ from scipy.optimize import minimize
 
 
 class GPGO:
+
     def __init__(self, surrogate, acquisition, f, parameter_dict, n_jobs=1):
         """
         Bayesian Optimization class.
@@ -64,11 +65,11 @@ class GPGO:
         d = OrderedDict()
         for index, param in enumerate(self.parameter_key):
             if self.parameter_type[index] == 'int':
-                d[param] = np.random.randint(
-                    self.parameter_range[index][0], self.parameter_range[index][1])
+                d[param] = np.random.randint(self.parameter_range[index][0],
+                                             self.parameter_range[index][1])
             elif self.parameter_type[index] == 'cont':
-                d[param] = np.random.uniform(
-                    self.parameter_range[index][0], self.parameter_range[index][1])
+                d[param] = np.random.uniform(self.parameter_range[index][0],
+                                             self.parameter_range[index][1])
             else:
                 raise ValueError('Unsupported variable type.')
         return d
@@ -119,21 +120,24 @@ class GPGO:
             Number of starting points for the optimization procedure. Default is 100.
         """
         start_points_dict = [self._sampleParam() for i in range(n_start)]
-        start_points_arr = np.array([list(s.values())
-                                     for s in start_points_dict])
+        start_points_arr = np.array(
+            [list(s.values()) for s in start_points_dict])
         x_best = np.empty((n_start, len(self.parameter_key)))
         f_best = np.empty((n_start,))
         if self.n_jobs == 1:
             for index, start_point in enumerate(start_points_arr):
-                res = minimize(self._acqWrapper, x0=start_point, method=method,
+                res = minimize(self._acqWrapper,
+                               x0=start_point,
+                               method=method,
                                bounds=self.parameter_range)
                 x_best[index], f_best[index] = res.x, np.atleast_1d(res.fun)[0]
         else:
-            opt = Parallel(n_jobs=self.n_jobs)(delayed(minimize)(self._acqWrapper,
-                                                                 x0=start_point,
-                                                                 method=method,
-                                                                 bounds=self.parameter_range) for start_point in
-                                               start_points_arr)
+            opt = Parallel(n_jobs=self.n_jobs)(
+                delayed(minimize)(self._acqWrapper,
+                                  x0=start_point,
+                                  method=method,
+                                  bounds=self.parameter_range)
+                for start_point in start_points_arr)
             x_best = np.array([res.x for res in opt])
             f_best = np.array([np.atleast_1d(res.fun)[0] for res in opt])
 
@@ -143,8 +147,7 @@ class GPGO:
         """
         Updates the internal model with the next acquired point and its evaluation.
         """
-        kw = {param: self.best[i]
-              for i, param in enumerate(self.parameter_key)}
+        kw = {param: self.best[i] for i, param in enumerate(self.parameter_key)}
         f_new = self.f(**kw)
         self.GP.update(np.atleast_2d(self.best), np.atleast_1d(f_new))
         self.tau = np.max(self.GP.y)
@@ -163,7 +166,8 @@ class GPGO:
         argtau = np.argmax(self.GP.y)
         opt_x = self.GP.X[argtau]
         res_d = OrderedDict()
-        for i, (key, param_type) in enumerate(zip(self.parameter_key, self.parameter_type)):
+        for i, (key, param_type) in enumerate(
+                zip(self.parameter_key, self.parameter_type)):
             if param_type == 'int':
                 res_d[key] = int(round(opt_x[i]))
             else:
@@ -192,9 +196,8 @@ class GPGO:
             self.logger._printCurrent(self)
 
 
-
-
 class GaussianProcess:
+
     def __init__(self, covfunc, optimize=False, usegrads=False, mprior=0):
         """
         Gaussian Process regressor class. Based on Rasmussen & Williams [1]_ algorithm 2.1.
@@ -257,13 +260,15 @@ class GaussianProcess:
             grads = None
             if self.usegrads:
                 grads = self._grad
-            self.optHyp(param_key=self.covfunc.parameters, param_bounds=self.covfunc.bounds, grads=grads)
+            self.optHyp(param_key=self.covfunc.parameters,
+                        param_bounds=self.covfunc.bounds,
+                        grads=grads)
 
         self.K = self.covfunc.K(self.X, self.X)
         self.L = cholesky(self.K).T
         self.alpha = solve(self.L.T, solve(self.L, y - self.mprior))
-        self.logp = -.5 * np.dot(self.y, self.alpha) - np.sum(np.log(np.diag(self.L))) - self.nsamples / 2 * np.log(
-            2 * np.pi)
+        self.logp = -.5 * np.dot(self.y, self.alpha) - np.sum(
+            np.log(np.diag(self.L))) - self.nsamples / 2 * np.log(2 * np.pi)
 
     def param_grad(self, k_param):
         """
@@ -282,7 +287,8 @@ class GaussianProcess:
         K = covfunc.K(self.X, self.X)
         L = cholesky(K).T
         alpha = solve(L.T, solve(L, self.y))
-        inner = np.dot(np.atleast_2d(alpha).T, np.atleast_2d(alpha)) - np.linalg.inv(K)
+        inner = np.dot(np.atleast_2d(alpha).T,
+                       np.atleast_2d(alpha)) - np.linalg.inv(K)
         grads = []
         for param in k_param_key:
             gradK = covfunc.gradK(self.X, self.X, param=param)
@@ -307,7 +313,8 @@ class GaussianProcess:
         k_param = OrderedDict()
         for k, v in zip(param_key, param_vector):
             k_param[k] = v
-        self.covfunc = self.covfunc.__class__(**k_param, bounds=self.covfunc.bounds)
+        self.covfunc = self.covfunc.__class__(**k_param,
+                                              bounds=self.covfunc.bounds)
 
         # This fixes recursion
         original_opt = self.optimize
@@ -319,7 +326,7 @@ class GaussianProcess:
 
         self.optimize = original_opt
         self.usegrads = original_grad
-        return (- self.logp)
+        return (-self.logp)
 
     def _grad(self, param_vector, param_key):
         """
@@ -338,7 +345,7 @@ class GaussianProcess:
         k_param = OrderedDict()
         for k, v in zip(param_key, param_vector):
             k_param[k] = v
-        return - self.param_grad(k_param)
+        return -self.param_grad(k_param)
 
     def optHyp(self, param_key, param_bounds, grads=None, n_trials=5):
         """
@@ -358,9 +365,18 @@ class GaussianProcess:
             for param, bound in zip(param_key, param_bounds):
                 x0.append(np.random.uniform(bound[0], bound[1], 1)[0])
             if grads is None:
-                res = minimize(self._lmlik, x0=x0, args=(param_key), method='L-BFGS-B', bounds=param_bounds)
+                res = minimize(self._lmlik,
+                               x0=x0,
+                               args=(param_key),
+                               method='L-BFGS-B',
+                               bounds=param_bounds)
             else:
-                res = minimize(self._lmlik, x0=x0, args=(param_key), method='L-BFGS-B', bounds=param_bounds, jac=grads)
+                res = minimize(self._lmlik,
+                               x0=x0,
+                               args=(param_key),
+                               method='L-BFGS-B',
+                               bounds=param_bounds,
+                               jac=grads)
             xs.append(res.x)
             fs.append(res.fun)
 
@@ -369,7 +385,8 @@ class GaussianProcess:
         k_param = OrderedDict()
         for k, x in zip(param_key, opt_param):
             k_param[k] = x
-        self.covfunc = self.covfunc.__class__(**k_param, bounds=self.covfunc.bounds)
+        self.covfunc = self.covfunc.__class__(**k_param,
+                                              bounds=self.covfunc.bounds)
 
     def predict(self, Xstar, return_std=False):
         """
@@ -411,6 +428,7 @@ class GaussianProcess:
         X = np.concatenate((self.X, xnew), axis=0)
         self.fit(X, y)
 
+
 default_bounds = {
     'l': [1e-4, 1],
     'sigmaf': [1e-4, 2],
@@ -420,6 +438,7 @@ default_bounds = {
     'alpha': [1e-3, 1e4],
     'period': [1e-3, 10]
 }
+
 
 def l2norm_(X, Xstar):
     """
@@ -454,8 +473,15 @@ def kronDelta(X, Xstar):
     """
     return cdist(X, Xstar) < np.finfo(np.float32).eps
 
+
 class matern32:
-    def __init__(self, l=1, sigmaf=1, sigman=1e-6, bounds=None, parameters=['l', 'sigmaf', 'sigman']):
+
+    def __init__(self,
+                 l=1,
+                 sigmaf=1,
+                 sigman=1e-6,
+                 bounds=None,
+                 parameters=['l', 'sigmaf', 'sigman']):
         """
         Matern v=3/2 kernel class.
         Parameters
@@ -499,8 +525,8 @@ class matern32:
             Computed covariance matrix.
         """
         r = l2norm_(X, Xstar)
-        one = (1 + np.sqrt(3 * (r / self.l) ** 2))
-        two = np.exp(- np.sqrt(3 * (r / self.l) ** 2))
+        one = (1 + np.sqrt(3 * (r / self.l)**2))
+        two = np.exp(-np.sqrt(3 * (r / self.l)**2))
         return self.sigmaf * one * two + self.sigman * kronDelta(X, Xstar)
 
     def gradK(self, X, Xstar, param):
@@ -521,19 +547,21 @@ class matern32:
         """
         if param == 'l':
             r = l2norm_(X, Xstar)
-            num = 3 * (r ** 2) * self.sigmaf * np.exp(-np.sqrt(3) * r / self.l)
-            return num / (self.l ** 3)
+            num = 3 * (r**2) * self.sigmaf * np.exp(-np.sqrt(3) * r / self.l)
+            return num / (self.l**3)
         elif param == 'sigmaf':
             r = l2norm_(X, Xstar)
-            one = (1 + np.sqrt(3 * (r / self.l) ** 2))
-            two = np.exp(- np.sqrt(3 * (r / self.l) ** 2))
+            one = (1 + np.sqrt(3 * (r / self.l)**2))
+            two = np.exp(-np.sqrt(3 * (r / self.l)**2))
             return one * two
         elif param == 'sigman':
             return kronDelta(X, Xstar)
         else:
             raise ValueError('Param not found')
 
+
 class Acquisition:
+
     def __init__(self, mode, eps=1e-06, **params):
         """
         Acquisition function class.
@@ -555,15 +583,24 @@ class Acquisition:
         self.eps = eps
 
         mode_dict = {
-            'ExpectedImprovement': self.ExpectedImprovement,
-            'IntegratedExpectedImprovement': self.IntegratedExpectedImprovement,
-            'ProbabilityImprovement': self.ProbabilityImprovement,
-            'IntegratedProbabilityImprovement': self.IntegratedProbabilityImprovement,
-            'UCB': self.UCB,
-            'IntegratedUCB': self.IntegratedUCB,
-            'Entropy': self.Entropy,
-            'tExpectedImprovement': self.tExpectedImprovement,
-            'tIntegratedExpectedImprovement': self.tIntegratedExpectedImprovement
+            'ExpectedImprovement':
+                self.ExpectedImprovement,
+            'IntegratedExpectedImprovement':
+                self.IntegratedExpectedImprovement,
+            'ProbabilityImprovement':
+                self.ProbabilityImprovement,
+            'IntegratedProbabilityImprovement':
+                self.IntegratedProbabilityImprovement,
+            'UCB':
+                self.UCB,
+            'IntegratedUCB':
+                self.IntegratedUCB,
+            'Entropy':
+                self.Entropy,
+            'tExpectedImprovement':
+                self.tExpectedImprovement,
+            'tIntegratedExpectedImprovement':
+                self.tIntegratedExpectedImprovement
         }
 
         self.f = mode_dict[mode]
@@ -644,7 +681,7 @@ class Acquisition:
         float:
             Predictive entropy.
         """
-        sp2 = std **2 + sigman
+        sp2 = std**2 + sigman
         return 0.5 * np.log(2 * np.pi * np.e * sp2)
 
     def IntegratedExpectedImprovement(self, tau, meanmcmc, stdmcmc):
@@ -665,7 +702,10 @@ class Acquisition:
         float:
             Integrated Expected Improvement
         """
-        acq = [self.ExpectedImprovement(tau, np.array([mean]), np.array([std])) for mean, std in zip(meanmcmc, stdmcmc)]
+        acq = [
+            self.ExpectedImprovement(tau, np.array([mean]), np.array([std]))
+            for mean, std in zip(meanmcmc, stdmcmc)
+        ]
         return np.average(acq)
 
     def IntegratedProbabilityImprovement(self, tau, meanmcmc, stdmcmc):
@@ -684,7 +724,10 @@ class Acquisition:
         float:
             Integrated Probability of Improvement
         """
-        acq = [self.ProbabilityImprovement(tau, np.array([mean]), np.array([std])) for mean, std in zip(meanmcmc, stdmcmc)]
+        acq = [
+            self.ProbabilityImprovement(tau, np.array([mean]), np.array([std]))
+            for mean, std in zip(meanmcmc, stdmcmc)
+        ]
         return np.average(acq)
 
     def IntegratedUCB(self, tau, meanmcmc, stdmcmc, beta=1.5):
@@ -707,7 +750,10 @@ class Acquisition:
         float:
             Integrated UCB.
         """
-        acq = [self.UCB(tau, np.array([mean]), np.array([std]), beta) for mean, std in zip(meanmcmc, stdmcmc)]
+        acq = [
+            self.UCB(tau, np.array([mean]), np.array([std]), beta)
+            for mean, std in zip(meanmcmc, stdmcmc)
+        ]
         return np.average(acq)
 
     def tExpectedImprovement(self, tau, mean, std, nu=3.0):
@@ -727,7 +773,9 @@ class Acquisition:
             Expected improvement.
         """
         gamma = (mean - tau - self.eps) / (std + self.eps)
-        return gamma * std * t.cdf(gamma, df=nu) + std * (1 + (gamma ** 2 - 1)/(nu - 1)) * t.pdf(gamma, df=nu)
+        return gamma * std * t.cdf(gamma, df=nu) + std * (1 + (gamma**2 - 1) /
+                                                          (nu - 1)) * t.pdf(
+                                                              gamma, df=nu)
 
     def tIntegratedExpectedImprovement(self, tau, meanmcmc, stdmcmc, nu=3.0):
         """
@@ -748,7 +796,13 @@ class Acquisition:
             Integrated Expected Improvement
         """
 
-        acq = [self.tExpectedImprovement(tau, np.array([mean]), np.array([std]), nu=nu) for mean, std in zip(meanmcmc, stdmcmc)]
+        acq = [
+            self.tExpectedImprovement(tau,
+                                      np.array([mean]),
+                                      np.array([std]),
+                                      nu=nu)
+            for mean, std in zip(meanmcmc, stdmcmc)
+        ]
         return np.average(acq)
 
     def eval(self, tau, mean, std):
@@ -769,6 +823,7 @@ class Acquisition:
         """
         return self.f(tau, mean, std, **self.params)
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -779,7 +834,9 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class EventLogger:
+
     def __init__(self, gpgo):
         self.gpgo = gpgo
         self.header = 'Evaluation \t Proposed point \t  Current eval. \t Best eval.'
@@ -797,4 +854,6 @@ class EventLogger:
 
     def _printInit(self, gpgo):
         for init_eval in range(gpgo.init_evals):
-            print(self.template.format('init', gpgo.GP.X[init_eval], gpgo.GP.y[init_eval], gpgo.tau))
+            print(
+                self.template.format('init', gpgo.GP.X[init_eval],
+                                     gpgo.GP.y[init_eval], gpgo.tau))
