dd7e38cb3b9c48a435cd43651a3600de3bc6845d
Tony Davis <acdavis629@gmail.com>
2023-03-02 12:53:42 -0500

    init test layers

---
+++ deepchem/models/torch_models/tests/test_mp_layers.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.torch
+def test_GINConv():
+    pass
+
+
+@pytest.mark.torch
+def test_GCNConv():
+    pass
+
+
+@pytest.mark.torch
+def test_GATConv():
+    pass
+
+
+@pytest.mark.torch
+def test_GraphSAGEConv():
+    pass
