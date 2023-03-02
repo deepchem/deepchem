0df86501d64a504a30990b066781a5109d8d0192
Tony Davis <acdavis629@gmail.com>
2023-03-02 12:56:26 -0500

    gen dataset

--- deepchem/models/torch_models/tests/test_mp_layers.py
+++ deepchem/models/torch_models/tests/test_mp_layers.py
@@ -2,20 +2,28 @@ import pytest
 
 
 @pytest.mark.torch
+def gen_dataset():
+    x, edge_index, edge_attr, y = gen_data()
+
+@pytest.mark.torch
 def test_GINConv():
-    pass
+    from deepchem.models.torch_models.mp_layers import GINConv
+    layer = GINConv(emb_dim=10)
 
 
 @pytest.mark.torch
 def test_GCNConv():
-    pass
+    from deepchem.models.torch_models.mp_layers import GCNConv
+    layer = GCNConv(emb_dim=10)
 
 
 @pytest.mark.torch
 def test_GATConv():
-    pass
+    from deepchem.models.torch_models.mp_layers import GATConv
+    layer = GATConv(emb_dim=10)
 
 
 @pytest.mark.torch
 def test_GraphSAGEConv():
-    pass
+    from deepchem.models.torch_models.mp_layers import GraphSAGEConv
+    layer = GraphSAGEConv(emb_dim=10)
