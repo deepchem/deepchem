36b3bf107acba2f561514dbd5bd619822d3440f1
Advika Vidhyadhiraja <advika.vs@gmail.com>
2023-03-07 13:28:15 +0530

    returning numpy dataset

--- deepchem/data/tests/test_dftyaml.py
+++ deepchem/data/tests/test_dftyaml.py
@@ -1,9 +1,10 @@
 from deepchem.data.data_loader import DFTYamlLoader
 import deepchem as dc
 
+
 def test_dftloader():
     input_files = 'dftdata.yaml'
     featurizer = dc.feat.DummyFeaturizer()
     k = DFTYamlLoader(featurizer)
     l = k.create_dataset(input_files, featurizer)
-    assert (l[0]).get_true_val() == 0.09194410469
+    assert ((l.X)[0]).get_true_val() == 0.09194410469
