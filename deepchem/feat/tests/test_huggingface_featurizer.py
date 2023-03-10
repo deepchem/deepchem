525b5479c4f337766fc3b03682eb949176a0c755
gusty1g <mcgusty1k@gmail.com>
2023-03-10 20:32:52 +0530

    test for hugging face tokenizer

---
+++ deepchem/feat/tests/test_huggingface_featurizer.py
@@ -0,0 +1,18 @@
+from deepchem.feat.huggingface_featurizer import HuggingFaceFeaturizer
+
+
+def testHuggingFaceFeaturizer():
+    # NOTE: The test depends on the sanity of the pretrained tokenizer,
+    # which is not guaranteed.
+    from transformers import RobertaTokenizerFast
+    hf_tokenizer = RobertaTokenizerFast.from_pretrained(
+        "seyonec/PubChem10M_SMILES_BPE_60k")
+
+    featurizer = HuggingFaceFeaturizer(tokenizer=hf_tokenizer)
+    output = featurizer.featurize(['CC(=O)C', 'CC'])
+    assert len(output) == 2
+    assert output[0]['input_ids'] == [0, 262, 263, 51, 13, 39, 2]
+    assert output[0]['attention_mask'] == [1, 1, 1, 1, 1, 1, 1]
+
+    assert output[1]['input_ids'] == [0, 262, 2]
+    assert output[1]['attention_mask'] == [1, 1, 1]
