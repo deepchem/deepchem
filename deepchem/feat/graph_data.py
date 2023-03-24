1e73760120d09d6c1a32794f5b244a4815279139
gusty1g <mcgusty1k@gmail.com>
2023-03-24 18:19:55 +0530

    grover batch graph under batchgraphdata

--- deepchem/feat/graph_data.py
+++ deepchem/feat/graph_data.py
@@ -1,4 +1,4 @@
-from typing import Optional, Sequence
+from typing import Optional, Sequence, List
 
 import numpy as np
 
@@ -380,3 +380,75 @@ class BatchGraphData(GraphData):
         graph_copy.graph_index = graph_index
 
         return graph_copy
+
+
+class GroverBatchGraphData(GraphData):
+    """Batch GraphData for Grover graph data"""
+
+    def __init__(self, graph_list: Sequence[GraphData]):
+        """
+        Parameters
+        ----------
+        graph_list: Sequence[GraphData]
+            List of GraphData
+        """
+        mol_graph_data = graph_list[0]
+        atom_features_dim = mol_graph_data.node_features.shape[1]
+        bond_features_dim = mol_graph_data.edge_features.shape[1]
+        # initial number of atoms, bonds (1 b/c of padding) and their features
+        n_atoms, n_bonds = 1, 1
+        f_atoms, f_bonds = [[0] * atom_features_dim], [[0] * bond_features_dim]
+
+        # list of tuples indicating (start_atom_index, num_atoms), (start_bond_index, num_bonds) for each molecule in batched molecular graph.
+        a_scope, b_scope = [], []
+        # mapping from atom index to incombing bond indices
+        a2b: List[List[int]] = [[]]
+        b2a = [
+            0
+        ]  # mapping from bond index to the index of the atom the bond is coming from
+        b2revb = [0]  # mapping from bond index to the index of the reverse bond
+        fg_labels = []  # to store functional group labels
+        additional_feats = []
+        smiles = []
+
+        for mol_graph in graph_list:
+            smiles.append(getattr(mol_graph, 'smiles'))
+            f_atoms.extend(mol_graph.node_features)
+            f_bonds.extend(mol_graph.edge_features)
+            fg_labels.append(getattr(mol_graph, 'fg_labels'))
+            additional_feats.append(getattr(mol_graph, 'additional_features'))
+
+            for a in range(mol_graph.num_nodes):
+                # for each atom in the molecule, append incoming bond indices
+                # by displacement with n_bonds
+                mg_a2b: List = getattr(mol_graph, 'a2b')
+                a2b.append([b + n_bonds for b in mg_a2b[a]])
+
+            for b in range(mol_graph.num_edges):
+                mg_b2a = getattr(mol_graph, 'b2a')
+                mg_b2revb = getattr(mol_graph, 'b2revb')
+                b2a.append(n_atoms + mg_b2a[b])
+                b2revb.append(n_bonds + mg_b2revb[b])
+
+            a_scope.append((n_atoms, mol_graph.num_nodes))
+            b_scope.append((n_bonds, mol_graph.num_edges))
+
+            n_atoms += mol_graph.num_nodes
+            n_bonds += mol_graph.num_edges
+
+        # max with 1 to fix a crash in rare case of all single-heavy-atom mols
+        max_num_bonds = max(1, max(len(in_bonds) for in_bonds in a2b))
+        # padding for a2b such that atoms with less than max_num_bonds have 0 padding
+        a2b = np.asarray([
+            a2b[a] + [0] * (max_num_bonds - len(a2b[a])) for a in range(n_atoms)
+        ])
+        a2a = b2a[a2b]  # type: ignore
+
+        super().__init__(node_features=f_atoms,
+                         edge_features=f_bonds,
+                         a2b=a2b,
+                         b2a=b2a,
+                         b2revb=b2revb,
+                         a2a=a2a,
+                         a_scope=a_scope,
+                         b_scope=b_scope)
