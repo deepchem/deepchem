3834030d7348403d48cdc0b26001e59065305cba
Tony Davis <acdavis629@gmail.com>
2023-03-29 08:17:26 -0400

    yapf

--- deepchem/models/torch_models/infograph.py
+++ deepchem/models/torch_models/infograph.py
@@ -563,56 +563,63 @@ class InfoGraphStarModel(ModularTorchModel):
         if self.task == 'supervised':
             return {
                 'encoder':
-                InfoGraphEncoder(self.num_features, self.edge_features,
-                                 self.embedding_dim),
+                    InfoGraphEncoder(self.num_features, self.edge_features,
+                                     self.embedding_dim),
                 'unsup_encoder':
-                InfoGraphEncoder(self.num_features, self.edge_features,
-                                 self.embedding_dim),
+                    InfoGraphEncoder(self.num_features, self.edge_features,
+                                     self.embedding_dim),
                 'ff1':
-                MultilayerPerceptron(2 * self.embedding_dim, self.embedding_dim,
-                                     (self.embedding_dim,)),
+                    MultilayerPerceptron(2 * self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,)),
                 'ff2':
-                MultilayerPerceptron(2 * self.embedding_dim, self.embedding_dim,
-                                     (self.embedding_dim,)),
+                    MultilayerPerceptron(2 * self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,)),
                 'fc1':
-                torch.nn.Linear(2 * self.embedding_dim, self.embedding_dim),
+                    torch.nn.Linear(2 * self.embedding_dim, self.embedding_dim),
                 'fc2':
-                torch.nn.Linear(self.embedding_dim, 1),
+                    torch.nn.Linear(self.embedding_dim, 1),
                 'local_d':
-                MultilayerPerceptron(self.embedding_dim,
-                                     self.embedding_dim, (self.embedding_dim,),
-                                     skip_connection=True),
+                    MultilayerPerceptron(self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,),
+                                         skip_connection=True),
                 'global_d':
-                MultilayerPerceptron(2 * self.embedding_dim,
-                                     self.embedding_dim, (self.embedding_dim,),
-                                     skip_connection=True)
+                    MultilayerPerceptron(2 * self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,),
+                                         skip_connection=True)
             }
         elif self.task == 'semisupervised':
             return {
                 'encoder':
-                InfoGraphEncoder(self.num_features, self.edge_features,
-                                 self.embedding_dim),
+                    InfoGraphEncoder(self.num_features, self.edge_features,
+                                     self.embedding_dim),
                 'unsup_encoder':
-                GINEncoder(self.num_features, self.embedding_dim,
-                           self.num_gc_layers),
+                    GINEncoder(self.num_features, self.embedding_dim,
+                               self.num_gc_layers),
                 'ff1':
-                MultilayerPerceptron(2 * self.embedding_dim, self.embedding_dim,
-                                     (self.embedding_dim,)),
+                    MultilayerPerceptron(2 * self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,)),
                 'ff2':
-                MultilayerPerceptron(self.embedding_dim, self.embedding_dim,
-                                     (self.embedding_dim,)),
+                    MultilayerPerceptron(self.embedding_dim, self.embedding_dim,
+                                         (self.embedding_dim,)),
                 'fc1':
-                torch.nn.Linear(2 * self.embedding_dim, self.embedding_dim),
+                    torch.nn.Linear(2 * self.embedding_dim, self.embedding_dim),
                 'fc2':
-                torch.nn.Linear(self.embedding_dim, 1),
+                    torch.nn.Linear(self.embedding_dim, 1),
                 'local_d':
-                MultilayerPerceptron(self.embedding_dim,
-                                     self.embedding_dim, (self.embedding_dim,),
-                                     skip_connection=True),
+                    MultilayerPerceptron(self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,),
+                                         skip_connection=True),
                 'global_d':
-                MultilayerPerceptron(self.embedding_dim,
-                                     self.embedding_dim, (self.embedding_dim,),
-                                     skip_connection=True)
+                    MultilayerPerceptron(self.embedding_dim,
+                                         self.embedding_dim,
+                                         (self.embedding_dim,),
+                                         skip_connection=True)
             }
 
     def build_model(self):
