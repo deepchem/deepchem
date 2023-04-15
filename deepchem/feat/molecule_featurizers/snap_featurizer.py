9ee5b9456ac0afc6ae54614a22bfb2c552504665
Tony Davis <acdavis629@gmail.com>
2023-04-15 12:26:54 -0400

    noqa: E122

--- deepchem/feat/molecule_featurizers/snap_featurizer.py
+++ deepchem/feat/molecule_featurizers/snap_featurizer.py
@@ -5,20 +5,20 @@ from deepchem.feat.graph_data import GraphData
 
 allowable_features = {
     'possible_atomic_num_list':
-    list(range(0, 119)),  # 0 represents a masked atom
+        list(range(0, 119)),  # 0 represents a masked atom
     'possible_formal_charge_list': [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
     'possible_chirality_list': [
         Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
         Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
         Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
         Chem.rdchem.ChiralType.CHI_OTHER
-    ],
+    ],  # noqa: E122
     'possible_hybridization_list': [
         Chem.rdchem.HybridizationType.S, Chem.rdchem.HybridizationType.SP,
         Chem.rdchem.HybridizationType.SP2, Chem.rdchem.HybridizationType.SP3,
         Chem.rdchem.HybridizationType.SP3D, Chem.rdchem.HybridizationType.SP3D2,
         Chem.rdchem.HybridizationType.UNSPECIFIED
-    ],
+    ],  # noqa: E122
     'possible_numH_list': [0, 1, 2, 3, 4, 5, 6, 7, 8],
     'possible_implicit_valence_list': [0, 1, 2, 3, 4, 5, 6],
     'possible_degree_list': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -26,11 +26,11 @@ allowable_features = {
     'possible_bonds': [
         0, Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE,
         Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.AROMATIC
-    ],
+    ],  # noqa: E122
     'possible_bond_dirs': [  # only for double bond stereo information
         Chem.rdchem.BondDir.NONE, Chem.rdchem.BondDir.ENDUPRIGHT,
         Chem.rdchem.BondDir.ENDDOWNRIGHT
-    ]
+    ]  # noqa: E122
 }
 
 
