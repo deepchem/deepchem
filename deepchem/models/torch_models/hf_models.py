6be9f342b59c6cfab32c546c019f33d8e974eb6f
gusty1g <mcgusty1k@gmail.com>
2023-04-28 20:16:24 +0530

    added predict method

--- deepchem/models/torch_models/hf_models.py
+++ deepchem/models/torch_models/hf_models.py
@@ -1,10 +1,12 @@
 import logging
 import time
+import numpy as np
 from collections.abc import Sequence as SequenceCollection
 from typing import (TYPE_CHECKING, Any, Callable, Iterable, List, Optional,
                     Tuple, Union)
 
 import torch
+from deepchem.trans import Transformer
 from deepchem.models.optimizers import LearningRateSchedule
 from deepchem.models.torch_models import TorchModel
 from deepchem.utils.typing import LossFn, OneOrMany
@@ -89,7 +91,8 @@ class HuggingFaceModel(TorchModel):
 
     def __init__(
             self, model: 'PreTrainedModel', task: str,
-            tokenizer: 'transformers.tokenization_utils.PreTrainedTokenizer'):
+            tokenizer: 'transformers.tokenization_utils.PreTrainedTokenizer',
+            **kwargs):
         self.task = task
         self.tokenizer = tokenizer
         if self.task == 'pretraining':
@@ -97,7 +100,7 @@ class HuggingFaceModel(TorchModel):
                 tokenizer=tokenizer)
         # For TorchModel, loss is a required argument but HuggingFace computes
         # loss during the forward iteration, removing the need for a loss function.
-        super(HuggingFaceModel, self).__init__(model=model, loss=None)
+        super(HuggingFaceModel, self).__init__(model=model, loss=None, **kwargs)
 
     def load_from_pretrained(self, path: str):
         if isinstance(str, path):
@@ -119,11 +122,14 @@ class HuggingFaceModel(TorchModel):
             tokens = self.tokenizer(smiles_batch[0].tolist(),
                                     padding=True,
                                     return_tensors="pt")
-            tensor_y = torch.from_numpy(y[0])
-            if self.model.config.problem_type == 'regression':
-                tensor_y = tensor_y.float()
-            inputs = {**tokens, 'labels': tensor_y}
-            return inputs, tensor_y, w
+            if y is not None:
+                # y is None during predict
+                y = torch.from_numpy(y[0])
+                if self.model.config.problem_type == 'regression':
+                    y = y.float()
+
+            inputs = {**tokens, 'labels': y}
+            return inputs, y, w
 
     def fit_generator(self,
                       generator: Iterable[Tuple[Any, Any, Any]],
@@ -261,3 +267,108 @@ class HuggingFaceModel(TorchModel):
         time2 = time.time()
         logger.info("TIMING: model fitting took %0.3f s" % (time2 - time1))
         return last_avg_loss
+
+    def _predict(self, generator: Iterable[Tuple[Any, Any, Any]],
+                 transformers: List[Transformer], uncertainty: bool,
+                 other_output_types: Optional[OneOrMany[str]]):
+        """Predicts output for data provided by generator.
+
+        This is the private implementation of prediction. Do not
+        call it directly. Instead call one of the public prediction methods.
+
+        Parameters
+        ----------
+        generator: generator
+            this should generate batches, each represented as a tuple of the form
+            (inputs, labels, weights).
+        transformers: list of dc.trans.Transformers
+            Transformers that the input data has been transformed by.  The output
+            is passed through these transformers to undo the transformations.
+        uncertainty: bool
+            specifies whether this is being called as part of estimating uncertainty.
+            If True, it sets the training flag so that dropout will be enabled, and
+            returns the values of the uncertainty outputs.
+        other_output_types: list, optional
+            Provides a list of other output_types (strings) to predict from model.
+
+        Returns
+        -------
+            a NumPy array of the model produces a single output, or a list of arrays
+            if it produces multiple outputs
+        """
+        results: Optional[List[List[np.ndarray]]] = None
+        variances: Optional[List[List[np.ndarray]]] = None
+        if uncertainty and (other_output_types is not None):
+            raise ValueError(
+                'This model cannot compute uncertainties and other output types simultaneously. Please invoke one at a time.'
+            )
+        if uncertainty:
+            if self._variance_outputs is None or len(
+                    self._variance_outputs) == 0:
+                raise ValueError('This model cannot compute uncertainties')
+            if len(self._variance_outputs) != len(self._prediction_outputs):
+                raise ValueError(
+                    'The number of variances must exactly match the number of outputs'
+                )
+        if other_output_types:
+            if self._other_outputs is None or len(self._other_outputs) == 0:
+                raise ValueError(
+                    'This model cannot compute other outputs since no other output_types were specified.'
+                )
+        self._ensure_built()
+        self.model.eval()
+        for batch in generator:
+            inputs, labels, weights = batch
+            inputs, _, _ = self._prepare_batch((inputs, None, None))
+
+            # Invoke the model.
+            if isinstance(inputs, list) and len(inputs) == 1:
+                inputs = inputs[0]
+            output_values = self.model(**inputs)
+            output_values = output_values.get('logits').detach().cpu().numpy()
+
+            # Apply tranformers and record results.
+            if uncertainty:
+                var = [output_values[i] for i in self._variance_outputs]
+                if variances is None:
+                    variances = [var]
+                else:
+                    for i, t in enumerate(var):
+                        variances[i].append(t)
+            access_values = []
+            if other_output_types:
+                access_values += self._other_outputs
+            elif self._prediction_outputs is not None:
+                access_values += self._prediction_outputs
+
+            if len(access_values) > 0:
+                output_values = [output_values[i] for i in access_values]
+
+            if len(transformers) > 0:
+                if len(output_values) > 1:
+                    raise ValueError(
+                        "predict() does not support Transformers for models with multiple outputs."
+                    )
+                elif len(output_values) == 1:
+                    output_values = [
+                        undo_transforms(output_values[0], transformers)
+                    ]
+            if results is None:
+                results = [[] for i in range(len(output_values))]
+            for i, t in enumerate(output_values):
+                results[i].append(t)
+
+        # Concatenate arrays to create the final results.
+        final_results = []
+        final_variances = []
+        if results is not None:
+            for r in results:
+                final_results.append(np.concatenate(r, axis=0))
+        if uncertainty and variances is not None:
+            for v in variances:
+                final_variances.append(np.concatenate(v, axis=0))
+            return zip(final_results, final_variances)
+        if len(final_results) == 1:
+            return final_results[0]
+        else:
+            return np.array(final_results)
