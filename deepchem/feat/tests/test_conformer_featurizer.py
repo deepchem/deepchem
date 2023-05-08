37f9791e05cf3260af35efe6574e830b9be91962
Tony Davis <acdavis629@gmail.com>
2023-05-08 09:16:15 -0400

    typo

--- deepchem/feat/tests/test_conformer_featurizer.py
+++ deepchem/feat/tests/test_conformer_featurizer.py
@@ -1,8 +1,8 @@
 def test_conformer_featurizer():
     from deepchem.feat.molecule_featurizers.conformer_featurizer import RDKitConformerFeaturizer
     smiles = ["C1=CC=NC=C1", "CC(=O)C", "C"]
-    featurizzer = RDKitConformerFeaturizer()
-    features = featurizzer.featurize(smiles)
+    featurizer = RDKitConformerFeaturizer()
+    features = featurizer.featurize(smiles)
     assert len(features) == 3  # 3 molecules
     assert features[0].node_features.shape[1] == 9  # 9 atom features
     # every edge index has a feature
