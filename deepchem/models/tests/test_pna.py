4edb84f72bf1ec5ef3c426bf9f63bd166159e2f1
Tony Davis <acdavis629@gmail.com>
2023-05-11 13:59:58 -0400

    atomencoder test

--- deepchem/models/tests/test_pna.py
+++ deepchem/models/tests/test_pna.py
@@ -1,15 +1,18 @@
 def test_AtomEncoder():
+    import numpy as np
     import torch
 
+    from deepchem.feat.graph_data import BatchGraphData
+    from deepchem.feat.molecule_featurizers.conformer_featurizer import (
+        RDKitConformerFeaturizer,)
     from deepchem.models.torch_models.pna_gnn import AtomEncoder
-    from deepchem.feat.molecule_featurizers.conformer_featurizer import RDKitConformerFeaturizer
-    
+
     atom_encoder = AtomEncoder(emb_dim=32)
-    smiles = ["C1=CC=NC=C1", "CC(=O)C", "C"]
-    featurizer = RDKitConformerFeaturizer(num_conformers=5, rmsd_cutoff=1)
-    features_list = featurizer.featurize(smiles)
-    features = BatchGraphData(np.concatenate(features_list).ravel())
-    atom_embeddings = atom_encoder(atom_features)
+    # simulate features from RDKitConformerFeaturizer
+    graph_features = torch.tensor([[5., 0., 3., 5., 0., 0., 1., 1., 1.],
+                                   [5., 0., 3., 5., 0., 0., 1., 1., 1.]])
+    atom_embeddings = atom_encoder(graph_features)
+
 
 def test_BondEncoder():
     import torch
@@ -19,6 +22,7 @@ def test_BondEncoder():
     bond_encoder = BondEncoder(emb_dim=32)
     bond_features = torch.randn(3, 3)
     bond_embeddings = bond_encoder(bond_features)
+
 
 def test_PNAlayer():
     pass
