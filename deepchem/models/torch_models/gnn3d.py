b968c6bf9d198191bf732f8a9e71da3148bfb47e
Tony Davis <acdavis629@gmail.com>
2023-05-24 10:22:04 -0400

    move loss to seperate PR

--- deepchem/models/torch_models/gnn3d.py
+++ deepchem/models/torch_models/gnn3d.py
@@ -7,7 +7,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from deepchem.feat.graph_data import GraphData
+from deepchem.models.losses import NTXentMultiplePositives
 from deepchem.models.torch_models import ModularTorchModel
 from deepchem.models.torch_models.layers import MultilayerPerceptron
 from deepchem.models.torch_models.pna_gnn import PNA, AtomEncoder
@@ -548,94 +548,3 @@ class InfoMax3DModular(ModularTorchModel):
             graph_data.to_dgl_graph().to(self.device) for graph_data in row
         ] for row in inputs]
         return graphs, labels, weights
-
-
-
-from torch.nn.modules.loss import _Loss
-from torch import Tensor
-
-
-def std_loss(x):
-    std = torch.sqrt(x.var(dim=0) + 1e-04)
-    return torch.mean(torch.relu(1 - std))
-
-
-def uniformity_loss(x1: Tensor, x2: Tensor, t=2) -> Tensor:
-    sq_pdist_x1 = torch.pdist(x1, p=2).pow(2)
-    uniformity_x1 = sq_pdist_x1.mul(-t).exp().mean().log()
-    sq_pdist_x2 = torch.pdist(x2, p=2).pow(2)
-    uniformity_x2 = sq_pdist_x2.mul(-t).exp().mean().log()
-    return (uniformity_x1 + uniformity_x2) / 2
-
-
-def cov_loss(x):
-    batch_size, metric_dim = x.size()
-    x = x - x.mean(dim=0)
-    cov = (x.T @ x) / (batch_size - 1)
-    off_diag_cov = cov.flatten()[:-1].view(metric_dim - 1,
-                                           metric_dim + 1)[:, 1:].flatten()
-    return off_diag_cov.pow_(2).sum() / metric_dim
-
-
-class NTXentMultiplePositives(_Loss):
-    '''
-        Normalized Temperature-scaled Cross Entropy Loss from SimCLR paper
-        Args:
-            z1, z2: Tensor of shape [batch_size, z_dim]
-            tau: Float. Usually in (0,1].
-            norm: Boolean. Whether to apply normlization.
-        '''
-
-    def __init__(self,
-                 norm: bool = True,
-                 tau: float = 0.5,
-                 uniformity_reg=0,
-                 variance_reg=0,
-                 covariance_reg=0,
-                 conformer_variance_reg=0) -> None:
-        super(NTXentMultiplePositives, self).__init__()
-        self.norm = norm
-        self.tau = tau
-        self.uniformity_reg = uniformity_reg
-        self.variance_reg = variance_reg
-        self.covariance_reg = covariance_reg
-        self.conformer_variance_reg = conformer_variance_reg
-
-    def forward(self, z1, z2, **kwargs) -> Tensor:
-        '''
-        :param z1: batchsize, metric dim
-        :param z2: batchsize*num_conformers, metric dim
-        '''
-        batch_size, metric_dim = z1.size()
-        z2 = z2.view(batch_size, -1,
-                     metric_dim)  # [batch_size, num_conformers, metric_dim]
-        z2 = z2.view(batch_size, -1,
-                     metric_dim)  # [batch_size, num_conformers, metric_dim]
-
-        sim_matrix = torch.einsum(
-            'ik,juk->iju', z1, z2)  # [batch_size, batch_size, num_conformers]
-
-        if self.norm:
-            z1_abs = z1.norm(dim=1)
-            z2_abs = z2.norm(dim=2)
-            sim_matrix = sim_matrix / torch.einsum('i,ju->iju', z1_abs, z2_abs)
-
-        sim_matrix = torch.exp(
-            sim_matrix / self.tau)  # [batch_size, batch_size, num_conformers]
-
-        sim_matrix = sim_matrix.sum(dim=2)  # [batch_size, batch_size]
-        pos_sim = torch.diagonal(sim_matrix)  # [batch_size]
-        loss = pos_sim / (sim_matrix.sum(dim=1) - pos_sim)
-        loss = -torch.log(loss).mean()
-
-        if self.variance_reg > 0:
-            loss += self.variance_reg * (std_loss(z1) + std_loss(z2))
-        if self.conformer_variance_reg > 0:
-            std = torch.sqrt(z2.var(dim=1) + 1e-04)
-            std_conf_loss = torch.mean(torch.relu(1 - std))
-            loss += self.conformer_variance_reg * std_conf_loss
-        if self.covariance_reg > 0:
-            loss += self.covariance_reg * (cov_loss(z1) + cov_loss(z2))
-        if self.uniformity_reg > 0:
-            loss += self.uniformity_reg * uniformity_loss(z1, z2)
-        return loss
