4508961d9d1181e47c3e00cd3958661893e50e68
riya-singh28 <riyasingh3909@gmail.com>
2023-06-14 19:58:35 +0530

    add unsorted_segment_sum and unittest

--- deepchem/utils/pytorch_utils.py
+++ deepchem/utils/pytorch_utils.py
@@ -14,3 +14,43 @@ def get_activation(fn: Union[Callable, str]):
     if isinstance(fn, str):
         return getattr(torch.nn.functional, fn)
     return fn
+
+
+def unsorted_segment_sum(data, segment_ids, num_segments):
+    """Computes the sum along segments of a tensor. Analogous to tf.unsorted_segment_sum.
+
+    Parameters
+    ----------
+    data: torch.Tensor
+        A tensor whose segments are to be summed.
+    segment_ids: torch.Tensor
+        The segment indices tensor.
+    num_segments: int
+        The number of segments.
+
+    Returns
+    -------
+    tensor: torch.Tensor
+
+    Examples
+    --------
+    >>> c = tf.constant([[1,2,3,4], [4, 3, 2, 1], [5,6,7,8]])
+    >>> tf.math.segment_sum(c, tf.constant([0, 0, 1])).numpy()
+    array([[5, 5, 5, 5],
+           [5, 6, 7, 8]], dtype=int32)
+
+    """
+    # segment_ids.shape should be a prefix of data.shape
+    assert all([i in data.shape for i in segment_ids.shape])
+
+    if len(segment_ids.shape) == 1:
+        s = torch.prod(torch.tensor(data.shape[1:])).long()
+        segment_ids = segment_ids.repeat_interleave(s).view(
+            segment_ids.shape[0], *data.shape[1:])
+
+    # data.shape and segment_ids.shape should be equal
+    assert data.shape == segment_ids.shape
+    shape = [num_segments] + list(data.shape[1:])
+    tensor = torch.zeros(*shape).scatter_add(0, segment_ids, data.float())
+    tensor = tensor.type(data.dtype)
+    return tensor
