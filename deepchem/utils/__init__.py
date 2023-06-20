0fbc190d6888b742ebb7f30ac839a5f9a096f75c
riya-singh28 <riyasingh3909@gmail.com>
2023-06-20 23:04:43 +0530

    make changes in deepchem.utils.__init__.py

--- deepchem/utils/__init__.py
+++ deepchem/utils/__init__.py
@@ -2,6 +2,10 @@
 Miscellaneous utility functions.
 """
 # flake8: noqa
+import logging
+
+logger = logging.getLogger(__name__)
+
 from deepchem.utils.conformers import ConformerGenerator
 from deepchem.utils.evaluate import relative_difference
 from deepchem.utils.evaluate import Evaluator
@@ -110,5 +114,9 @@ from deepchem.utils.voxel_utils import voxelize
 from deepchem.utils.sequence_utils import hhblits
 from deepchem.utils.sequence_utils import hhsearch
 
-from deepchem.utils.pytorch_utils import unsorted_segment_sum
-from deepchem.utils.pytorch_utils import segment_sum
+try:
+    from deepchem.utils.pytorch_utils import unsorted_segment_sum
+    from deepchem.utils.pytorch_utils import segment_sum
+except ModuleNotFoundError as e:
+    logger.warning(
+        f'Skipped loading some Pytorch utilities, missing a dependency. {e}')
