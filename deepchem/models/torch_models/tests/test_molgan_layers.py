27cf1ac7808c6ca9845dd9c4d4629994d9611c76
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2023-06-27 23:05:20 +0530

    made test_molgan_layers a pytest

--- deepchem/models/torch_models/tests/test_molgan_layers.py
+++ deepchem/models/torch_models/tests/test_molgan_layers.py
@@ -1,4 +1,3 @@
-import unittest
 import pytest
 
 try:
@@ -12,35 +11,25 @@ except ModuleNotFoundError:
     pass
 
 
-class TestMolganLayers(unittest.TestCase):
-    """
-  Unit testing for MolGAN basic layers
-  """
+@pytest.mark.torch
+def test_graph_convolution_layer():
+    from deepchem.models.torch_models.layers import MolGANConvolutionLayer
+    vertices = 9
+    nodes = 5
+    edges = 5
+    units = 128
 
-    @pytest.mark.torch
-    def test_graph_convolution_layer(self):
-        from deepchem.models.torch_models.layers import MolGANConvolutionLayer
-        vertices = 9
-        nodes = 5
-        edges = 5
-        units = 128
+    layer = MolGANConvolutionLayer(units=units, edges=edges, nodes=nodes)
+    adjacency_tensor = torch.randn((1, vertices, vertices, edges))
+    node_tensor = torch.randn((1, vertices, nodes))
+    output = layer([adjacency_tensor, node_tensor])
 
-        layer = MolGANConvolutionLayer(units=units, edges=edges, nodes=nodes)
-        adjacency_tensor = torch.randn((1, vertices, vertices, edges))
-        node_tensor = torch.randn((1, vertices, nodes))
-        output = layer([adjacency_tensor, node_tensor])
-
-        assert output[0].shape == torch.Size([1, vertices, vertices,
-                                              edges])  # adjacency_tensor
-        assert output[1].shape == torch.Size([1, vertices,
-                                              nodes])  # node_tensor
-        assert output[2].shape == torch.Size([1, vertices,
-                                              units])  # output of the layer
-        assert layer.units == units
-        assert layer.activation == F.tanh
-        assert layer.edges == 5
-        assert layer.dropout_rate == 0.0
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert output[0].shape == torch.Size([1, vertices, vertices,
+                                          edges])  # adjacency_tensor
+    assert output[1].shape == torch.Size([1, vertices, nodes])  # node_tensor
+    assert output[2].shape == torch.Size([1, vertices,
+                                          units])  # output of the layer
+    assert layer.units == units
+    assert layer.activation == F.tanh
+    assert layer.edges == 5
+    assert layer.dropout_rate == 0.0
