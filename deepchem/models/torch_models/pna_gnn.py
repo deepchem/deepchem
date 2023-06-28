fb8f333d6e3049e3ddc9cad82437c38c8d03e99d
arunppsg <arunppsg@gmail.com>
2023-06-28 17:33:56 +0530

    dgl graph clone instead of copy

--- deepchem/models/torch_models/pna_gnn.py
+++ deepchem/models/torch_models/pna_gnn.py
@@ -1,4 +1,3 @@
-import copy
 from functools import partial
 from math import sqrt
 from typing import Callable, Dict, List, Union
@@ -515,7 +514,7 @@ class PNAGNN(nn.Module):
         graph : dgl.DGLGraph
             Output graph with updated node features after applying the message passing layers.
         """
-        graph = copy.deepcopy(input_graph)
+        graph = input_graph.clone()
         graph.ndata['feat'] = self.atom_encoder(graph.ndata['x'])
         graph.edata['feat'] = self.bond_encoder(graph.edata['edge_attr'])
 
