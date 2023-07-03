61a0b7b20da75714bf3c079f74d0ea2b6bf77b1b
riya-singh28 <riyasingh3909@gmail.com>
2023-07-03 19:48:09 +0530

    Add test for node_pos_features

--- deepchem/feat/tests/test_mxmnet_featurizer.py
+++ deepchem/feat/tests/test_mxmnet_featurizer.py
@@ -22,6 +22,12 @@ node_features = {
     "C": np.asarray([[1.]])
 }
 
+node_pos = np.asarray([[-1.2700e-02, 1.0858e+00, 8.0000e-03],
+                       [2.2000e-03, -6.0000e-03, 2.0000e-03],
+                       [1.0117e+00, 1.4638e+00, 3.0000e-04],
+                       [-5.4080e-01, 1.4475e+00, -8.7660e-01],
+                       [-5.2380e-01, 1.4379e+00, 9.0640e-01]])
+
 
 class TestMXMNetFeaturizer(unittest.TestCase):
     """
@@ -102,3 +108,22 @@ class TestMXMNetFeaturizer(unittest.TestCase):
         assert len(graph_feat) == 4
 
         assert graph_feat[3].shape == (0,)
+
+    def test_node_pos_features(self):
+        """
+        Test for featurization of "C" using `MXMNetFeaturizer` class.
+        It checks whether node_pos_features are handled properly.
+        """
+        smile = ['C']
+        pos_x1 = [np.array([-0.0127, 0.0022, 1.0117, -0.5408, -0.5238])]
+        pos_y1 = [np.array([1.0858, -0.0060, 1.4638, 1.4475, 1.4379])]
+        pos_z1 = [np.array([0.0080, 0.0020, 0.0003, -0.8766, 0.9064])]
+
+        featurizer = dc.feat.molecule_featurizers.mxmnet_featurizer.MXMNetFeaturizer(
+            is_adding_hs=True)
+        graph_feat = featurizer.featurize(smile,
+                                          pos_x=pos_x1,
+                                          pos_y=pos_y1,
+                                          pos_z=pos_z1)
+        assert type(graph_feat[0].node_pos_features) == np.ndarray
+        assert (graph_feat[0].node_pos_features == node_pos).all
