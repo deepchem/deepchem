aa8c635c015f2be9e3ce1382498ab0f0c42f2f36
GreatRSingh <rakshitsingh421@gmail.com>
2023-07-13 22:44:41 +0530

    Added Test

--- deepchem/models/tests/test_dtnn.py
+++ deepchem/models/tests/test_dtnn.py
@@ -12,6 +12,8 @@ from deepchem.models.torch_models import DTNNModel
 def test_dtnn():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(current_dir, "assets/example_DTNN.mat")
+
+    # Load Datasets
     dataset = scipy_io.loadmat(input_file)
     X = dataset['X']
     y = dataset['T']
@@ -23,9 +25,14 @@ def test_dtnn():
                       n_distance=100,
                       learning_rate=1.0,
                       mode="regression")
-    # Fit trained model
+
+    # Check Train
     model.fit(dataset, nb_epoch=250)
+
     # Eval model on train
     pred = model.predict(dataset)
+
     mean_rel_error = np.mean(np.abs(1 - pred / y))
+
     assert mean_rel_error < 0.1
+    assert pred.shape == y.shape
