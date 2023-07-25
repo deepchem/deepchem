7539d2efb904150b32f27b96cc5ce2f956d90eee
Ashish <ashishvashist616@gmail.com>
2023-07-25 21:52:10 +0530

    Splitted test and modified class

--- deepchem/models/torch_models/tests/highway_test.py
+++ deepchem/models/torch_models/tests/highway_test.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
+from deepchem.models.torch_models.layers import Highway
+import numpy as np
+
 def test_highway():
   """Test invoking Highway."""
   width = 5
@@ -31,27 +34,43 @@ def test_highway_layer_shape():
     width = 5
     batch_size = 10
     torch.manual_seed(21)
+    inputs = torch.tensor([[0.4041, 0.3648, 0.5749, 0.0727, 0.7612],
+                                   [0.8641, 0.5746, 0.3337, 0.4103, 0.2618],
+                                   [0.0535, 0.5125, 0.2130, 0.2297, 0.2028],
+                                   [0.0251, 0.5023, 0.0383, 0.0829, 0.4311],
+                                   [0.0590, 0.3707, 0.7875, 0.5276, 0.3526],
+                                   [0.2961, 0.7429, 0.2964, 0.9543, 0.9686],
+                                   [0.8921, 0.7150, 0.8407, 0.8312, 0.9262],
+                                   [0.3936, 0.1754, 0.1488, 0.7005, 0.4626],
+                                   [0.9021, 0.3961, 0.2994, 0.1206, 0.4926],
+                                   [0.0156, 0.0346, 0.1109, 0.6938, 0.4572]])
 
-    layer = HighwayLayer(inputs=inputs)
+    layer = Highway(batch_size,width)
 
     tf_weights = np.load(
-         'deepchem/models/tests/assets/Highway_weights.npy',
+         'assets/highway_weights.npy',
          allow_pickle=True).item()
+         
     with torch.no_grad():
         layer.H.weight.data.weight.data = torch.from_numpy(
-         np.transpose(tf_weights['deepchem/models/tests/assets/TF_weights_H.npy']))
+         np.transpose(tf_weights['assets/TF_weights_H.npy']))
         layer.H.weight.data.bias.data = torch.from_numpy(
-         tf_weights['deepchem/models/tests/assets/TF_bias_T.npy'])
+         tf_weights['assets/TF_bias_T.npy'])
         layer.T.weight.data.weight.data = torch.from_numpy(
-         np.transpose(tf_weights['deepchem/models/tests/assets/TF_weights_T.npy']))
+         np.transpose(tf_weights['assets/TF_weights_T.npy']))
         layer.T.weight.data.bias.data = torch.from_numpy(
-         tf_weights['deepchem/models/tests/assets/TF_bias_T.npy'])   
+         tf_weights['assets/TF_bias_T.npy'])   
          
     output_tensor = torch.from_numpy(
-         np.load('deepchem/models/tests/assets/highway_output.npy').astype(
+         np.load('assets/highway_output.npy').astype(
              np.float32))
   
     assert torch.allclose(output, output_tensor, atol=1e-04)
+    print(output)
+    assert 1==2
+    
+
+
 
 
 
