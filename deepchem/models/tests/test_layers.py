4469d46320decd039e7414092652aacc26385c38
Riya Singh <riyasingh3909@gmail.com>
2023-07-28 15:11:16 +0530

    minor fix

--- deepchem/models/tests/test_layers.py
+++ deepchem/models/tests/test_layers.py
@@ -1248,8 +1248,6 @@ def test_local_MP():
     torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
 
     dim = 1
     n_layer = 2
