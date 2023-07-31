e2e95cfdd61a9102a28825420735941c473f1482
Riya Singh <riyasingh3909@gmail.com>
2023-07-31 07:34:09 +0530

    minor fix in test

--- deepchem/models/tests/test_layers.py
+++ deepchem/models/tests/test_layers.py
@@ -1218,7 +1218,7 @@ def test_mxmnet_envelope():
 
 
 @pytest.mark.torch
-def test_mxmnet_global_MP():
+def test_mxmnet_global_message_passing():
     """ Test for MXMNetGlobalMessagePassing Layer."""
     seed = 123
     torch.backends.cudnn.deterministic = True
@@ -1240,7 +1240,7 @@ def test_mxmnet_global_MP():
         [[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4],
          [1, 2, 3, 4, 0, 2, 3, 4, 0, 1, 3, 4, 0, 1, 2, 4, 0, 1, 2, 3]])
 
-    out = dc.models.torch_models.layers.GlobalMessagePassing(dim)
+    out = dc.models.torch_models.layers.MXMNetGlobalMessagePassing(dim)
     output = out(node_features, edge_attr, edge_indices)
     output = output.detach().numpy()
     result = np.array([[-0.27947044], [2.417905], [2.417905], [2.4178727],
