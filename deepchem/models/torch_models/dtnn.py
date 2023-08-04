ac36fdb393ccdb892d53767b0c7b96d5cd578530
GreatRSingh <rakshitsingh421@gmail.com>
2023-08-05 01:31:16 +0530

    DTNN module Example

--- deepchem/models/torch_models/dtnn.py
+++ deepchem/models/torch_models/dtnn.py
@@ -15,6 +15,31 @@ class DTNN(nn.Module):
 
     In this class, we establish a sequential model for the Deep Tensor Neural Network (DTNN) [1]_.
 
+    Examples
+    --------
+    >>> import os
+    >>> import torch
+    >>> from deepchem.models.torch_models import dtnn
+    >>> from deepchem.data import SDFLoader
+    >>> from deepchem.feat import CoulombMatrix
+    >>> from deepchem.models.torch_models import dtnn
+    >>> # Get Data
+    >>> model_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    >>> dataset_file = os.path.join(model_dir, 'tests/assets/qm9_mini.sdf')
+    >>> TASKS = ["alpha", "homo"]
+    >>> loader = SDFLoader(tasks=TASKS, featurizer=CoulombMatrix(29), sanitize=True)
+    >>> data = loader.create_dataset(dataset_file, shard_size=100)
+    >>> inputs = dtnn._compute_features_on_batch(data.X)
+    >>> atom_number, distance, atom_membership, distance_membership_i, distance_membership_j = inputs
+    >>> inputs = [torch.tensor(atom_number).to(torch.int64),
+    ...           torch.tensor(distance).to(torch.float32),
+    ...           torch.tensor(atom_membership).to(torch.int64),
+    ...           torch.tensor(distance_membership_i).to(torch.int64),
+    ...           torch.tensor(distance_membership_j).to(torch.int64)]
+    >>> n_tasks = data.y.shape[0]
+    >>> model = dtnn.DTNN(n_tasks)
+    >>> pred = model(inputs)
+
     References
     ----------
     .. [1] Schütt, Kristof T., et al. "Quantum-chemical insights from deep
@@ -132,7 +157,6 @@ class DTNNModel(TorchModel):
     Examples
     --------
     >>> import os
-    >>> import numpy as np
     >>> from deepchem.data import SDFLoader
     >>> from deepchem.feat import CoulombMatrix
     >>> from deepchem.models.torch_models import DTNNModel
@@ -143,10 +167,10 @@ class DTNNModel(TorchModel):
     >>> data = loader.create_dataset(dataset_file, shard_size=100)
     >>> n_tasks = data.y.shape[1]
     >>> model = DTNNModel(n_tasks,
-    ...               n_embedding=20,
-    ...               n_distance=100,
-    ...               learning_rate=1.0,
-    ...               mode="regression")
+    ...                   n_embedding=20,
+    ...                   n_distance=100,
+    ...                   learning_rate=1.0,
+    ...                   mode="regression")
     >>> loss = model.fit(data, nb_epoch=250)
     >>> pred = model.predict(data)
 
