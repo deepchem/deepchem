0fea8e1493bf5e2df09bde3e5c90124e5b9ed771
Ashish Vashist <ashishvashist616@gmail.com>
2023-08-18 13:04:50 +0530

    Fixed flake8 issues

--- deepchem/models/torch_models/tests/test_highway.py
+++ deepchem/models/torch_models/tests/test_highway.py
@@ -1,17 +1,17 @@
 import pytest
 
 try:
-     import torch
-     import numpy as np
-     has_torch = True
+    import torch
+    import numpy as np
+    has_torch = True
 except ModuleNotFoundError:
-     has_torch = False
-     pass
+    has_torch = False
+    pass
 from deepchem.models.torch_models.layers import Highway
+
 
 @pytest.mark.torch
 def test_highway_shape_no_params():
-    
     width = 5
     batch_size = 2
     torch.manual_seed(42)
@@ -25,9 +25,9 @@ def test_highway_shape_no_params():
     result2 = layer(input)
     assert torch.allclose(result, result2)
 
+
 @pytest.mark.torch
 def test_highway_layer_non_deterministic_output():
-
     width = 5
     batch_size = 2
     torch.manual_seed(42)
@@ -41,6 +41,7 @@ def test_highway_layer_non_deterministic_output():
 
     assert not torch.allclose(result, result2)
 
+
 @pytest.mark.torch
 def test_highway_layer_shape():
     from deepchem.models.torch_models.layers import Highway
@@ -50,11 +51,11 @@ def test_highway_layer_shape():
     layer_shape = [batch_size, width]
     inputs = torch.tensor(
         [[-0.13437884, 0.19134927, -0.42180598, 1.5959879, 0.04390939],
-        [1.1014341, 0.33973673, -0.34050095, 0.03709555, 0.5169554]])
-
+         [1.1014341, 0.33973673, -0.34050095, 0.03709555, 0.5169554]])
     layer = Highway(layer_shape=layer_shape)
 
-    tf_weights = np.load('deepchem/models/tests/assets/highway_weights_new.npy', allow_pickle=True).item()
+    tf_weights = np.load('deepchem/models/tests/assets/highway_weights_new.npy',
+                         allow_pickle=True).item()
 
     with torch.no_grad():
         layer.linear_H.weight.data = torch.from_numpy(
@@ -67,8 +68,6 @@ def test_highway_layer_shape():
 
     output = layer(inputs)
     output_tensor = torch.from_numpy(
-        np.load('deepchem/models/tests/assets/highway_output_new.npy').astype(np.float32))
+        np.load('deepchem/models/tests/assets/highway_output_new.npy').astype(
+            np.float32))
     assert torch.allclose(output, output_tensor, atol=1e-04)
-
-if __name__ == "__main__":
-    test_highway_shape_no_params()
