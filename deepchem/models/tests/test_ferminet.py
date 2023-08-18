042dc253732be0b04eed34adc655bb136738c832
GreatRSingh <rakshitsingh421@gmail.com>
2023-08-18 21:04:41 +0530

    Test - 1

--- deepchem/models/tests/test_ferminet.py
+++ deepchem/models/tests/test_ferminet.py
@@ -29,7 +29,7 @@ def test_FerminetModel():
     assert mol.up_spin == 2 and mol.down_spin == 1
 
 
-@pytest.mark.dqc
+@pytest.mark.torch
 def test_prepare_hf_solution():
     # Test for the prepare_hf_solution function of FerminetModel class
     H2_molecule = [['H', [0, 0, 0]], ['H', [0, 0, 0.748]]]
