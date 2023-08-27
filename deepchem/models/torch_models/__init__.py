7d4f6414b1915237ad9f3e70523d48793bdc66c8
shaipranesh2 <f202000731@pilani.bits-pilani.ac.in>
2023-08-27 19:21:44 +0530

    init file changes

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -13,6 +13,8 @@ try:
     from deepchem.models.torch_models.infograph import InfoGraphStar, InfoGraphStarModel, InfoGraphEncoder, GINEncoder, InfoGraph, InfoGraphModel, InfoGraphEncoder
     from deepchem.models.torch_models.megnet import MEGNetModel
     from deepchem.models.torch_models.layers import MultilayerPerceptron, CNNModule, CombineMeanStd, WeightedLinearCombo, AtomicConvolution, NeighborList, SetGather, EdgeNetwork, WeaveLayer, WeaveGather, MolGANConvolutionLayer, MolGANAggregationLayer, MolGANMultiConvolutionLayer, MolGANEncoderLayer, EncoderRNN, FerminetElectronFeature
+    from deepchem.models.torch_models.mat import MAT, MATModel
+    from deepchem.models.torch_models.grover import GroverModel, GroverPretrain, GroverFinetune
 except ModuleNotFoundError as e:
     logger.warning(
         f'Skipped loading modules with pytorch-geometric dependency, missing a dependency. {e}'
@@ -20,11 +22,9 @@ except ModuleNotFoundError as e:
 from deepchem.models.torch_models.mpnn import MPNN, MPNNModel
 from deepchem.models.torch_models.lcnn import LCNN, LCNNModel
 from deepchem.models.torch_models.pagtn import Pagtn, PagtnModel
-from deepchem.models.torch_models.mat import MAT, MATModel
 from deepchem.models.torch_models.normalizing_flows_pytorch import NormalizingFlow
 from deepchem.models.torch_models.cnn import CNN
 from deepchem.models.torch_models.attention import ScaledDotProductAttention, SelfAttention
-from deepchem.models.torch_models.grover import GroverModel, GroverPretrain, GroverFinetune
 from deepchem.models.torch_models.readout import GroverReadout
 from deepchem.models.torch_models.dtnn import DTNN, DTNNModel
 from deepchem.models.torch_models.ferminet import Ferminet, FerminetModel
