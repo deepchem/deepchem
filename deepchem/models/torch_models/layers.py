e08e5b3bbd01ce0276f569859d633fdb0a533bd1
GreatRSingh <rakshitsingh421@gmail.com>
2023-08-30 12:35:28 +0530

    Innitial Commit

--- deepchem/models/torch_models/layers.py
+++ deepchem/models/torch_models/layers.py
@@ -4794,6 +4794,45 @@ class MXMNetBesselBasisLayer(torch.nn.Module):
         return output
 
 
+class VariationalRandomizer(nn.Module):
+    """Add random noise to the embedding and include a corresponding loss."""
+    def __init__(self,
+                 embedding_dimension: int,
+                 annealing_start_step: int,
+                 annealing_final_step: int,
+                 **kwargs):
+        super(VariationalRandomizer, self).__init__(**kwargs)
+        self._embedding_dimension = embedding_dimension
+        self._annealing_final_step = annealing_final_step
+        self._annealing_start_step = annealing_start_step
+        self.dense_mean = nn.LazyLinear(embedding_dimension)
+        self.dense_stddev = nn.LazyLinear(embedding_dimension)
+        self.combine = CombineMeanStd(training_only=True)
+        self.loss_list = []
+
+    def forward(self, inputs, training=True):
+        input, global_step = inputs
+        embedding_mean = self.dense_mean(input)
+        embedding_stddev = self.dense_stddev(input)
+        embedding = self.combine([embedding_mean, embedding_stddev],
+                                 training=training)
+        mean_sq = embedding_mean * embedding_mean
+        stddev_sq = embedding_stddev * embedding_stddev
+        kl = mean_sq + stddev_sq - torch.log(stddev_sq + 1e-20) - 1
+        anneal_steps = self._annealing_final_step - self._annealing_start_step
+        if anneal_steps > 0:
+            current_step = global_step.to(torch.float32) - self._annealing_start_step
+            anneal_frac = torch.maximum(torch.tensor(0.0), current_step) / anneal_steps
+            kl_scale = torch.minimum(torch.tensor(1.0), anneal_frac * anneal_frac)
+        else:
+            kl_scale = 1.0
+        self.add_loss(0.5 * kl_scale * torch.mean(kl))
+        return embedding
+    
+    def add_loss(self, loss):
+        self.loss_list.append(loss)
+
+
 class EncoderRNN(nn.Module):
     """Encoder Layer for SeqToSeq Model.
 
