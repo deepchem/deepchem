bca0e4b619bd5cf3d5b7598a54a4d177cb2c0634
GreatRSingh <rakshitsingh421@gmail.com>
2023-09-03 20:59:51 +0530

    Torch Model Start

--- deepchem/models/torch_models/seqtoseq.py
+++ deepchem/models/torch_models/seqtoseq.py
@@ -1,6 +1,10 @@
 from typing import List
+
 import torch.nn as nn
+import torch
+
 from deepchem.models.torch_models.layers import EncoderRNN, DecoderRNN, VariationalRandomizer
+from deepchem.models.torch_models import TorchModel
 
 
 class SeqToSeq(nn.Module):
@@ -80,3 +84,117 @@ class SeqToSeq(nn.Module):
         output, _ = self.decoder(
             [embedding, embedding[:, -1].unsqueeze(0), None])
         return output
+
+
+class SeqToSeqModel(TorchModel):
+
+    sequence_end = object()
+
+    def __init__(self,
+                 input_tokens,
+                 output_tokens,
+                 max_output_length,
+                 embedding_dimension=512,
+                 dropout=0.0,
+                 reverse_input=True,
+                 variational=False,
+                 annealing_start_step=5000,
+                 annealing_final_step=10000,
+                 **kwargs):
+        """Construct a SeqToSeq model.
+        
+        Parameters
+        ----------
+        input_tokens: list
+            List of all tokens that may appear in input sequences.
+        output_tokens: list
+            List of all tokens that may appear in output sequences
+        max_output_length: int
+            Maximum length of output sequence that may be generated
+        embedding_dimension: int (default 512)
+            Width of the embedding vector. This also is the width of all recurrent
+            layers.
+        dropout: float (default 0.0)
+            Dropout probability to use during training.
+        reverse_input: bool (default True)
+            If True, reverse the order of input sequences before sending them into
+            the encoder. This can improve performance when working with long sequences.
+        variational: bool (default False)
+            If True, train the model as a variational autoencoder. This adds random
+            noise to the encoder, and also constrains the embedding to follow a unit
+            Gaussian distribution.
+        annealing_start_step: int (default 5000)
+            Step (that is, batch) at which to begin turning on the constraint term
+            for KL cost annealing
+        annealing_final_step: int (default 10000)
+            Step (that is, batch) at which to finish turning on the constraint term
+            for KL cost annealing
+
+        """
+        if SeqToSeq.sequence_end not in input_tokens:
+            input_tokens = input_tokens + [SeqToSeq.sequence_end]
+        if SeqToSeq.sequence_end not in output_tokens:
+            output_tokens = output_tokens + [SeqToSeq.sequence_end]
+        self._input_tokens = input_tokens
+        self._output_tokens = output_tokens
+        self._input_dict = dict((x, i) for i, x in enumerate(input_tokens))
+        self._output_dict = dict((x, i) for i, x in enumerate(output_tokens))
+        self._n_input_tokens = len(input_tokens)
+        self._n_output_tokens = len(output_tokens)
+        self._max_output_length = max_output_length
+        self._embedding_dimension = embedding_dimension
+        self._dropout = dropout
+        self._reverse_input = reverse_input
+        self._variational = variational
+        self._annealing_start_step = annealing_start_step
+        self._annealing_final_step = annealing_final_step
+
+        model: nn.Module = SeqToSeq(
+            n_input_tokens=self._n_input_tokens,
+            n_output_tokens=self._n_output_tokens,
+            max_output_length=self._max_output_length,
+            embedding_dimension=self._embedding_dimension,
+            dropout=self._dropout,
+            variational=self._variational,
+            annealing_start_step=self._annealing_start_step,
+            annealing_final_step=self._annealing_final_step)
+
+        super(SeqToSeq, self).__init__(model, self._create_loss(), **kwargs)
+
+    def _create_loss(self):
+        """Create loss function for model."""
+        if self._variational:
+            loss = sum(self.model.randomizer.loss_list)
+        else:
+            loss = torch.tensor(0.0)
+
+        def loss_fn(outputs, labels, weights):
+            loss_ = nn.NLLLoss(weights)(outputs, labels)
+            return loss + loss_
+
+        return loss_fn
+
+    def fit_sequences(self,
+                      sequences,
+                      max_checkpoints_to_keep=5,
+                      checkpoint_interval=1000,
+                      restore=False):
+        """Train this model on a set of sequences
+
+        Parameters
+        ----------
+        sequences: iterable
+            the training samples to fit to.  Each sample should be
+            represented as a tuple of the form (input_sequence, output_sequence).
+        max_checkpoints_to_keep: int
+            the maximum number of checkpoints to keep.  Older checkpoints are discarded.
+        checkpoint_interval: int
+            the frequency at which to write checkpoints, measured in training steps.
+        restore: bool
+            if True, restore the model from the most recent checkpoint and continue training
+            from there.  If False, retrain the model from scratch.
+        """
+        self.fit_generator(self._generate_batches(sequences),
+                           max_checkpoints_to_keep=max_checkpoints_to_keep,
+                           checkpoint_interval=checkpoint_interval,
+                           restore=restore)
