efe73afc2916bcbfa31e42765052a17864681234
GreatRSingh <rakshitsingh421@gmail.com>
2023-09-28 01:24:35 +0530

    config.py -> Complete and Documentation.

--- deepchem/utils/dft_utils/config.py
+++ deepchem/utils/dft_utils/config.py
@@ -2,13 +2,24 @@ from dataclasses import dataclass
 
 __all__ = ["config"]
 
+
 @dataclass
 class _Config(object):
-    # Threshold memory (matrix above this size should not be constructed)
-    THRESHOLD_MEMORY: int = 10 * 1024 ** 3  # in B
-    # The memory for splitting big tensors into chunks
-    CHUNK_MEMORY: int = 16 * 1024 ** 2  # in B
+    """Contains the configuration for the DFT module
 
-    VERBOSE: int = 0  # verbosity level
+    Attributes
+    ----------
+    THRESHOLD_MEMORY: int
+        Threshold memory (matrix above this size should not be constructed)
+    CHUNK_MEMORY: int
+        The memory for splitting big tensors into chunks
+    VERBOSE: int
+        Verbosity level
+
+    """
+    THRESHOLD_MEMORY: int = 10 * 1024**3  # in Bytes
+    CHUNK_MEMORY: int = 16 * 1024**2  # in Bytes
+    VERBOSE: int = 0
+
 
 config = _Config()
