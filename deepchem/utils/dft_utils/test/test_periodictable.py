c68187b73fdece5d8265cd80dddbe824eb5d2c40
GreatRSingh <rakshitsingh421@gmail.com>
2023-09-28 16:07:51 +0530

    periodictable -> test yapf fix

--- deepchem/utils/dft_utils/test/test_periodictable.py
+++ deepchem/utils/dft_utils/test/test_periodictable.py
@@ -5,17 +5,20 @@ Tests for DFT Periodic Table Utilities.
 from deepchem.utils.dft_utils.periodictable import get_atomz, get_atom_mass, get_period
 import numpy as np
 
+
 def test_get_atomz():
-    element_symbol = "Al" # Aluminium
-    atomic_number = get_atomz(element_symbol) 
-    assert atomic_number == 13 # Aluminium Atom Number
+    element_symbol = "Al"  # Aluminium
+    atomic_number = get_atomz(element_symbol)
+    assert atomic_number == 13  # Aluminium Atom Number
+
 
 def test_get_atom_mass():
     atomic_number = 41
     atomic_mass = get_atom_mass(atomic_number)
     assert np.allclose(atomic_mass, 92.9064 * 1822.888486209)
 
+
 def test_get_period():
     atomic_number = 13
     period = get_period(atomic_number)
-    assert period == 3 # Aluminium Period
+    assert period == 3  # Aluminium Period
