c000e3b552e3831e91b9ac19048563abb127a14b
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2023-09-29 12:39:48 +0530

    final changes

--- deepchem/models/tests/test_ferminet.py
+++ deepchem/models/tests/test_ferminet.py
@@ -56,6 +56,6 @@ def test_FerminetMode_pretrain():
     # Test for the init function of FerminetModel class
     H2_molecule = [['H', [0, 0, 0]], ['H', [0, 0, 0.748]]]
     # Testing ionic initialization
-    mol = FerminetModel(H2_molecule, spin=0, ion_charge=0, tasks='pretraining')
-    mol.train(nb_epoch=4)
+    mol = FerminetModel(H2_molecule, spin=0, ion_charge=0)
+    mol.train(nb_epoch=3)
     assert mol.loss_value <= torch.tensor(1.0)
