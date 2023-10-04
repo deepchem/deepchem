cbbc08c660da5db13f20ab88c4378587690232f9
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-04 10:44:33 +0530

    check removal

--- deepchem/utils/checks.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-with open(os.path.join(os.path.dirname(__file__), "assets", "periodic_table.json")) as read_file:
-    m = json.load(read_file)
-
-print(m["atom_masses"])
-
