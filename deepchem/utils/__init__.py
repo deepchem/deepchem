6b2518ae68c82cd052c89d5d4df1fa199352b3bb
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-06 16:39:21 +0530

    init

--- deepchem/utils/__init__.py
+++ deepchem/utils/__init__.py
@@ -117,6 +117,10 @@ from deepchem.utils.sequence_utils import hhblits
 from deepchem.utils.sequence_utils import hhsearch
 
 try:
+    from deepchem.utils.periodic_table_utils import get_atomz
+    from deepchem.utils.periodic_table_utils import get_atom_mass
+    from deepchem.utils.periodic_table_utils import get_period
+
     from deepchem.utils.pytorch_utils import unsorted_segment_sum
     from deepchem.utils.pytorch_utils import segment_sum
     from deepchem.utils.pytorch_utils import chunkify
