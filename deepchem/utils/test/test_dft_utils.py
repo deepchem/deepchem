5475b13edbacbf8690773def2f0849dcf9f4e05d
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-11 16:10:48 +0530

    tests

--- deepchem/utils/test/test_dft_utils.py
+++ deepchem/utils/test/test_dft_utils.py
@@ -1,5 +1,4 @@
 """Test DFT utils."""
-
 import pytest
 try:
     import torch
@@ -7,12 +6,37 @@ try:
 except:
     has_torch = False
 
-from deepchem.utils.dft_utils.xc import BaseXC
-from deepchem.utils.dft_utils.datastruct import ValGrad
+
+@pytest.mark.torch
+def test_val_grad():
+    """Test ValGrad data structure"""
+    from deepchem.utils.dft_utils.datastruct import ValGrad
+
+    vg1 = ValGrad(torch.tensor([1, 2, 3]))
+    assert torch.allclose(vg1.value, torch.tensor([1, 2, 3]))
+    vg1.grad = torch.tensor([4, 5, 6])
+    assert torch.allclose(vg1.grad, torch.tensor([4, 5, 6]))
+    vg2 = ValGrad(torch.tensor([1, 2, 3]), torch.tensor([4, 5, 6]))
+    vg3 = vg1 + vg2
+    assert torch.allclose(vg3.value, torch.tensor([2, 4, 6]))
+    assert torch.allclose(vg3.grad, torch.tensor([8, 10, 12]))
+
+
+@pytest.mark.torch
+def test_spin_param():
+    """Test SpinParam data structure"""
+    from deepchem.utils.dft_utils.datastruct import SpinParam
+
+    sp = SpinParam(1, 2)
+    assert sp.sum() == 3
+    assert sp.reduce(lambda x, y: x * y + 2) == 4
 
 
 @pytest.mark.torch
 def test_base_xc():
+    """Test BaseXC."""
+    from deepchem.utils.dft_utils.datastruct import ValGrad
+    from deepchem.utils.dft_utils.xc import BaseXC
 
     class MyXC(BaseXC):
 
