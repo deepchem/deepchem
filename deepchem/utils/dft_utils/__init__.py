b32de1750a89871c3fd3921849a99c590f57520e
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-29 01:18:59 +0530

    All

--- deepchem/utils/dft_utils/__init__.py
+++ deepchem/utils/dft_utils/__init__.py
@@ -18,6 +18,8 @@ try:
     from deepchem.utils.dft_utils.xc.base_xc import BaseXC
     from deepchem.utils.dft_utils.xc.base_xc import AddBaseXC
     from deepchem.utils.dft_utils.xc.base_xc import MulBaseXC
+
+    from deepchem.utils.dft_utils.api.parser import parse_moldesc
 except ModuleNotFoundError as e:
     logger_.warning(
         f'Skipped loading some Pytorch utilities, missing a dependency. {e}')
--- deepchem/utils/test/test_dft_utils.py
+++ deepchem/utils/test/test_dft_utils.py
@@ -8,6 +8,34 @@ from typing import Union
 
 
 @pytest.mark.torch
+def test_parse_moldesc():
+    """Tests Moldesc Parser."""
+    from deepchem.utils.dft_utils import parse_moldesc
+    system = {
+        'type': 'mol',
+        'kwargs': {
+            'moldesc': 'H 0.86625 0 0; F -0.86625 0 0',
+            'basis': '6-311++G(3df,3pd)'
+        }
+    }
+    atomzs, atomposs = parse_moldesc(system["kwargs"]["moldesc"])
+    assert torch.allclose(atomzs, torch.tensor([1., 9.], dtype=torch.float64))
+    assert torch.allclose(
+        atomposs,
+        torch.tensor(
+            [[0.86625, 0.00000, 0.00000], [-0.86625, 0.00000, 0.00000]],
+            dtype=torch.float64))
+    system2 = (['H', 'F'], torch.tensor([[0.86625, 0, 0], [-0.86625, 0, 0]]))
+    atomzs2, atomposs2 = parse_moldesc(system2)
+    assert torch.allclose(atomzs2, torch.tensor([1., 9.], dtype=torch.float64))
+    assert torch.allclose(
+        atomposs2,
+        torch.tensor(
+            [[0.86625, 0.00000, 0.00000], [-0.86625, 0.00000, 0.00000]],
+            dtype=torch.float64))
+
+
+@pytest.mark.torch
 def test_lattice():
     """Test lattice object.
     Comparing it's output with it's original implementation in dqc.
