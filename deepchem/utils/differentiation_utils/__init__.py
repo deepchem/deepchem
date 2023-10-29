c7437c3de0c5e50b8e5f709bbd23f1fcefd09048
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-29 11:35:21 +0530

    broadcast

--- deepchem/utils/differentiation_utils/__init__.py
+++ deepchem/utils/differentiation_utils/__init__.py
@@ -1,4 +1,8 @@
 try:
     from deepchem.utils.differentiation_utils.editable_module import EditableModule
+
+    from deepchem.utils.differentiation_utils.utilities import normalize_bcast_dims
+    from deepchem.utils.differentiation_utils.utilities import get_bcasted_dims
+    from deepchem.utils.differentiation_utils.utilities import match_dim
 except:
     pass
