598fc33b1146d56dab89e5216f9431932939e253
GreatRSingh <rakshitsingh421@gmail.com>
2023-10-29 17:38:58 +0530

    main class

--- deepchem/utils/dft_utils/__init__.py
+++ deepchem/utils/dft_utils/__init__.py
@@ -14,6 +14,10 @@ try:
     from deepchem.utils.dft_utils.data.datastruct import ZType
     from deepchem.utils.dft_utils.data.datastruct import SpinParam
     from deepchem.utils.dft_utils.data.datastruct import ValGrad
+    
+    from deepchem.utils.dft_utils.xc.base_xc import BaseXC
+    from deepchem.utils.dft_utils.xc.base_xc import AddBaseXC
+    from deepchem.utils.dft_utils.xc.base_xc import MulBaseXC
 except ModuleNotFoundError as e:
     logger_.warning(
         f'Skipped loading some Pytorch utilities, missing a dependency. {e}')
