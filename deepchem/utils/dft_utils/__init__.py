a0bdd7abdb9da4552a3140a4da3eef1ea5727935
GreatRSingh <rakshitsingh421@gmail.com>
2023-11-02 18:25:19 +0530

    main class

--- deepchem/utils/dft_utils/__init__.py
+++ deepchem/utils/dft_utils/__init__.py
@@ -22,6 +22,8 @@ try:
     from deepchem.utils.dft_utils.hamilton.orbparams import MatExpOrbParams
 
     from deepchem.utils.dft_utils.api.parser import parse_moldesc
+
+    from deepchem.utils.dft_utils.xc.base_xc import BaseXC
 except ModuleNotFoundError as e:
     logger_.warning(
         f'Skipped loading some Pytorch utilities, missing a dependency. {e}')
