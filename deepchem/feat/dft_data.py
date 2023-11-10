d6f5c6f03104c096d2c4a009561ac835e58a01ce
GreatRSingh <rakshitsingh421@gmail.com>
2023-11-10 21:51:06 +0530

    BaseGrid [Update and init]

--- deepchem/feat/dft_data.py
+++ deepchem/feat/dft_data.py
@@ -10,8 +10,8 @@ import numpy as np
 # dqc dependencies
 from dqc.system.mol import Mol
 from dqc.system.base_system import BaseSystem
-from deepchem.utils.dftutils import KSCalc, BaseGrid
-from deepchem.utils.dft_utils import parse_moldesc
+from deepchem.utils.dftutils import KSCalc
+from deepchem.utils.dft_utils import parse_moldesc, BaseGrid
 
 
 class DFTSystem():
