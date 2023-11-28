d4ed58bf9a95481b578bb16994ece2e3db9a3a88
Nimisha <nimisha.jsw@gmail.com>
2023-11-28 21:42:09 +0530

    Corrections in init

--- deepchem/metalearning/__init__.py
+++ deepchem/metalearning/__init__.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 try:
     from deepchem.metalearning.maml import MAML, MetaLearner
-    from deepchem.metalearning import MAML, MetaLearner
+    from deepchem.metalearning.torch_maml import MAML, MetaLearner
 except ModuleNotFoundError as e:
     logger.warning(
         f'Skipped loading some PyTorch models, missing a dependency. {e}')
