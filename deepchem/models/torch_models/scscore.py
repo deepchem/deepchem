cfe0c761c7ac80dd46cc579e1ed8f56253382db3
aaronrockmenezes <85219711+aaronrockmenezes@users.noreply.github.com>
2023-12-01 02:24:28 +0530

    Added TorchModel Wrapper and documentation

--- deepchem/models/torch_models/scscore.py
+++ deepchem/models/torch_models/scscore.py
@@ -1,7 +1,7 @@
-from typing import Callable, List, Optional, Union
+from typing import Callable, Iterable, List, Optional, Tuple, Union
 import numpy as np
 import torch
-from deepchem.data import NumpyDataset
+from deepchem.data import Dataset, NumpyDataset
 from deepchem.feat import CircularFingerprint
 from deepchem.models import TorchModel
 from deepchem.models.losses import HingeLoss, Loss
@@ -13,17 +13,49 @@ class ScScore(nn.Module):
     """
     Builder class for ScScore Model.
 
-    The SCScore model is a feed forward neural network model based on the work of Coley et al. [1]_ that predicts the synthetic complexity score (SCScore) of molecules and correlates it with the expected number of reaction steps required to produce the given target molecule.
+    The SCScore model is a neural network model based on the work of Coley et al. [1]_ that predicts the synthetic complexity score (SCScore) of molecules and correlates it with the expected number of reaction steps required to produce the given target molecule.
     It is trained on a dataset of over 12 million reactions from the Reaxys database to impose a pairwise inequality constraint enforcing that on average the products of published chemical reactions should be more synthetically complex than their corresponding reactants.
     The learned metric (SCScore) exhibits highly desirable nonlinear behavior, particularly in recognizing increases in synthetic complexity throughout a number of linear synthetic routes.
+    The SCScore model can accurately predict the synthetic complexity of a variety of molecules, including both drug-like and natural product molecules.
+    SCScore has the potential to be a valuable tool for chemists who are working on drug discovery and other areas of chemistry.
+
+    The learned metric (SCScore) exhibits highly desirable nonlinear behavior, particularly in recognizing increases in synthetic complexity throughout a number of linear synthetic routes.
+
+    Our model uses hingeloss instead of the shifted relu loss as in the supplementary material [2]_ provided by the author.
+    This could cause differentiation issues with compounds that are "close" to each other in "complexity".
+
+    The default values for the model are the same as the ones used in the original paper [1]_.
+
+    References
+    ----------
+    .. [1] Coley, C. W., Rogers, L., Green, W., & Jensen, K. F. (2018). "SCScore: Synthetic Complexity Learned from a Reaction Corpus". Journal of Chemical Information and Modeling, 58(2), 252-261. https://doi.org/10.1021/acs.jcim.7b00622
+
+    .. [2] Coley, C. W., Rogers, L., Green, W., & Jensen, K. F. (2018). Supplementary material to "SCScore: Synthetic Complexity Learned from a Reaction Corpus". Journal of Chemical Information and Modeling, 58(2), 252-261. https://github.com/connorcoley/scscore
     """
 
-    def __init__(self, n_features=1024, layer_sizes=[300, 300, 300, 300, 300], dropout=0.0, score_scale=5):
+    def __init__(self, 
+                 n_features: int=1024, 
+                 layer_sizes: list=[300, 300, 300, 300, 300], 
+                 dropout: float=0.0, 
+                 score_scale: int=5):
+        """
+        Parameters
+        ----------
+        n_features: int (default 1024)
+            Number of features per molecule.
+        layer_sizes: list of int (default [300, 300, 300, 300, 300])
+            Size of each hidden layer, length of the list is the number of hidden layers.
+        dropouts: float (default 0.0)
+            Droupout to apply to each hidden layer.
+        score_scale: int (default 5)
+            Scale of the output score.
+        
+        """
         super(ScScore, self).__init__()
+        self.n_features = n_features
+        self.layer_sizes = layer_sizes
         self.dropout = dropout
         self.score_scale = score_scale
-        self.layer_sizes = layer_sizes
-        self.n_features = n_features
 
         input_size = self.layer_sizes[0]
         self.input_layer = nn.Linear(self.n_features, input_size)
@@ -35,7 +67,20 @@ class ScScore(nn.Module):
         
         self.output_layer = nn.Linear(self.layer_sizes[-1], 1)
 
-    def forward(self, x):
+    def forward(self, input):
+        """
+        Parameters
+        ----------
+        inputs: list of Tensor
+            data inputs for the model
+
+        Returns
+        -------
+        output: Tensor
+            Synthetic complexity score of the input molecule.
+
+        """
+
         x = F.relu(self.input_layer(x))
         if self.dropout > 0.0:
             x = F.dropout(x, p=self.dropout)
@@ -64,6 +109,8 @@ class ScScoreModel(TorchModel):
     Our model uses hingeloss instead of the shifted relu loss as in the supplementary material [2]_ provided by the author.
     This could cause differentiation issues with compounds that are "close" to each other in "complexity".
 
+    The default values for the model are the same as the ones used in the original paper [1]_.
+
     References
     ----------
     .. [1] Coley, C. W., Rogers, L., Green, W., & Jensen, K. F. (2018). "SCScore: Synthetic Complexity Learned from a Reaction Corpus". Journal of Chemical Information and Modeling, 58(2), 252-261. https://doi.org/10.1021/acs.jcim.7b00622
@@ -72,27 +119,47 @@ class ScScoreModel(TorchModel):
     """
 
     def __init__(self, 
-                 n_features=1024, 
-                 layer_sizes=[300,300,300,300], 
-                 dropout=0.0, 
-                 score_scale=5, 
+                 n_features: int=1024, 
+                 layer_sizes: list=[300,300,300,300], 
+                 dropout: float=0.0, 
+                 score_scale: int=5, 
                  **kwargs):
         """
         Parameters
         ----------
-        n_features: int
-            number of features per molecule
-        layer_sizes: list of int
-            size of each hidden layer
-        dropouts: float
-            droupout to apply to each hidden layer
-        kwargs
-            This takes all kwargs as TensorGraph
-        """
+        n_features: int (default 1024)
+            Number of features per molecule.
+        layer_sizes: list of int (default [300, 300, 300, 300, 300])
+            Size of each hidden layer, length of the list is the number of hidden layers.
+        dropouts: float (default 0.0)
+            Droupout to apply to each hidden layer.
+        score_scale: int (default 5)
+            Max output score, range of output = (1, score_scale).
         
-        model = ScScore(n_features, layer_sizes, dropout, score_scale)
+        """
+        if dropout < 0.0 or dropout > 1.0:
+            raise ValueError(f"Dropout must be between 0.0 and 1.0, but got {dropout}")
+        
+        if score_scale < 1:
+            raise ValueError(f"Score scale must be greater than 1, but got {score_scale}")
+        
+        model = ScScore(n_features=n_features, 
+                        layer_sizes=layer_sizes, 
+                        dropout=dropout, 
+                        score_scale=score_scale)
+        
         loss = HingeLoss()
+        
         super(ScScoreModel, self).__init__(model, 
                                            loss=loss, 
                                            **kwargs)
     
+    def default_generator(self, 
+                          dataset: Dataset, 
+                          epochs: int = 1, 
+                          mode: str = 'fit', 
+                          deterministic: bool = True, 
+                          pad_batches: bool = True):
+        
+        # Idk what to do here, I wanna use the def_gen function from TorchModel
+        pass
