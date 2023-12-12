ae01b1830b6810b198d903575880a8c232f4754a
aaronrockmenezes <85219711+aaronrockmenezes@users.noreply.github.com>
2023-12-12 14:00:09 +0530

    Added BBBC004 dataset

--- deepchem/molnet/load_function/bbbc_datasets.py
+++ deepchem/molnet/load_function/bbbc_datasets.py
@@ -19,6 +19,10 @@ BBBC2_IMAGE_URL = 'https://data.broadinstitute.org/bbbc/BBBC002/BBBC002_v1_image
 BBBC2_LABEL_URL = 'https://data.broadinstitute.org/bbbc/BBBC002/BBBC002_v1_counts.txt'
 BBBC2_TASKS = ["cell-count"]
 
+BBBC4_IMAGE_URL = "https://data.broadinstitute.org/bbbc/BBBC004/BBBC004_v1_000_images.zip"
+BBBC4_FOREGROUND_URL = "https://data.broadinstitute.org/bbbc/BBBC004/BBBC004_v1_000_foreground.zip"
+BBBC4_TASKS = ["cell-count"]
+
 
 class _BBBC001Loader(_MolnetLoader):
 
@@ -146,3 +150,83 @@ def load_bbbc002(
     loader = _BBBC002Loader(featurizer, splitter, transformers, BBBC2_TASKS,
                             data_dir, save_dir, **kwargs)
     return loader.load_dataset('bbbc002', reload)
+
+class _BBBC004_Segmentation_Loader(_MolnetLoader):
+
+    def create_dataset(self) -> Dataset:
+        dataset_file = os.path.join(self.data_dir, "BBBC004_v1_000_images.zip")
+        foreground_file = os.path.join(self.data_dir, "BBBC004_v1_000_foreground.zip")
+        if not os.path.exists(dataset_file):
+            dc.utils.data_utils.download_url(url=BBBC4_IMAGE_URL,
+                                             dest_dir=self.data_dir)
+        if not os.path.exists(foreground_file):     
+            dc.utils.data_utils.download_url(url=BBBC4_FOREGROUND_URL,
+                                             dest_dir=self.data_dir)
+        labels = np.full(len(dataset_file), 300, dtype=int)
+
+        loader = dc.data.ImageLoader(sorting=False)
+        return loader.create_dataset(inputs=(dataset_file, foreground_file),
+                                     in_memory=False)
+
+class _BBBC004_Loader(_MolnetLoader):
+    
+    def create_dataset(self) -> Dataset:
+        dataset_file = os.path.join(self.data_dir, "BBBC004_v1_000_images.zip")
+        if not os.path.exists(dataset_file):
+            dc.utils.data_utils.download_url(url=BBBC4_IMAGE_URL,
+                                             dest_dir=self.data_dir)
+        labels = np.full(20, 300, dtype=int)
+        
+        loader = dc.data.ImageLoader(sorting=False)
+        return loader.create_dataset(inputs=(dataset_file, labels),
+                                     in_memory=False)
+
+
+def load_bbbc004(
+    load_segmentation_mask: bool = False,
+    splitter: Union[dc.splits.Splitter, str, None] = 'index',
+    transformers: List[Union[TransformerGenerator, str]] = [],
+    reload: bool = True,
+    data_dir: Optional[str] = None,
+    save_dir: Optional[str] = None,
+    **kwargs
+) -> Tuple[List[str], Tuple[Dataset, ...], List[dc.trans.Transformer]]:
+    """Load BBBC004 dataset
+
+    This dataset contains data corresponding to 20 samples of synthetically generated 
+    fluorescent cell population images. There are 300 cells in each sample, each an image 
+    of size 950x950. Ground truth labels contain cell counts and segmentation masks for 
+    this dataset. Full details about this dataset are present at
+    https://data.broadinstitute.org/bbbc/BBBC004/.
+
+    Parameters
+    ----------
+    load_segmentation_mask: bool
+        if True, the dataset will contain segmentation masks as labels. Otherwise,
+        the dataset will contain cell counts as labels.
+    splitter: Splitter or str
+        the splitter to use for splitting the data into training, validation, and
+        test sets.  Alternatively you can pass one of the names from
+        dc.molnet.splitters as a shortcut.  If this is None, all the data
+        will be included in a single dataset.
+    transformers: list of TransformerGenerators or strings
+        the Transformers to apply to the data.  Each one is specified by a
+        TransformerGenerator or, as a shortcut, one of the names from
+        dc.molnet.transformers.
+    reload: bool
+        if True, the first call for a particular featurizer and splitter will cache
+        the datasets to disk, and subsequent calls will reload the cached datasets.
+    data_dir: str
+        a directory to save the raw data in
+    save_dir: str
+        a directory to save the dataset in
+    """
+    featurizer = dc.feat.UserDefinedFeaturizer([])  # Not actually used
+    if load_segmentation_mask==True:
+        loader = _BBBC004_Segmentation_Loader(featurizer, splitter, transformers, BBBC4_TASKS,
+                                              data_dir, save_dir, **kwargs)
+    else:
+        loader = _BBBC004_Loader(featurizer, splitter, transformers, BBBC4_TASKS,
+                                 data_dir, save_dir, **kwargs)
+        
+    return loader.load_dataset('bbbc004', reload)
