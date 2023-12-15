3cc2f875aad5ae493ed1e7c3c53ee55074d5a7d6
aaronrockmenezes <85219711+aaronrockmenezes@users.noreply.github.com>
2023-12-15 18:07:03 +0530

    Tests file linting and imports fix

--- deepchem/models/torch_models/tests/test_scscore.py
+++ deepchem/models/torch_models/tests/test_scscore.py
@@ -1,10 +1,7 @@
-import unittest
+import pytest
 import tempfile
 import numpy as np
-from deepchem.models.torch_models import ScScoreModel
-from deepchem.data import NumpyDataset
-import pytest
-import torch
+import deepchem as dc
 
 
 @pytest.mark.torch
@@ -14,14 +11,22 @@ def test_restore_scscore():
 
     X = np.random.rand(100, n_features).astype(np.float32)
     y = np.random.uniform(1, 5, size=(100)).astype(np.float32)
-    np_dataset = NumpyDataset(X, y)
+    np_dataset = dc.data.NumpyDataset(X, y)
 
     model_dir = tempfile.mkdtemp()
-    model = ScScoreModel(n_features, layer_sizes, dropout=0.0, score_scale=5, model_dir=model_dir)
+    model = dc.models.torch_models.ScScoreModel(n_features,
+                                                layer_sizes,
+                                                dropout=0.0,
+                                                score_scale=5,
+                                                model_dir=model_dir)
     model.fit(np_dataset, nb_epoch=50)
     pred = model.predict(np_dataset)
 
-    reloaded_model = ScScoreModel(n_features, layer_sizes, dropout=0.0, score_scale=5, model_dir=model_dir)
+    reloaded_model = dc.models.torch_models.ScScoreModel(n_features,
+                                                         layer_sizes,
+                                                         dropout=0.0,
+                                                         score_scale=5,
+                                                         model_dir=model_dir)
     reloaded_model.restore()
 
     pred = model.predict(np_dataset)
