0f5d4f9cacb160b398ea9a86e5f02abfc360a525
GreatRSingh <rakshitsingh421@gmail.com>
2023-12-28 10:41:04 +0530

    test and add to init [skip ci]

--- deepchem/utils/differentiation_utils/__init__.py
+++ deepchem/utils/differentiation_utils/__init__.py
@@ -18,5 +18,7 @@ try:
     from deepchem.utils.differentiation_utils.linop import AdjointLinearOperator
     from deepchem.utils.differentiation_utils.linop import MatmulLinearOperator
     from deepchem.utils.differentiation_utils.linop import MatrixLinearOperator
+
+    from deepchem.utils.differentiation_utils.pure_function import PureFunction
 except:
     pass
