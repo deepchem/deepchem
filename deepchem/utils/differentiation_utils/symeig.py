926d326499e0927b254187649c39763bc22291da
GreatRSingh <rakshitsingh421@gmail.com>
2023-12-30 08:47:26 +0530

    added examples

--- deepchem/utils/differentiation_utils/symeig.py
+++ deepchem/utils/differentiation_utils/symeig.py
@@ -1,4 +1,105 @@
 import torch
+from typing import Optional, Sequence
+from deepchem.utils.differentiation_utils import LinearOperator
+import functools
+from deepchem.utils.pytorch_utils import tallqr
+
+
+def _set_initial_v(vinit_type: str,
+                   dtype: torch.dtype,
+                   device: torch.device,
+                   batch_dims: Sequence,
+                   na: int,
+                   nguess: int,
+                   M: Optional[LinearOperator] = None) -> torch.Tensor:
+    """Set the initial guess for the eigenvectors.
+
+    Examples
+    --------
+    >>> import torch
+    >>> vinit_type = "eye"
+    >>> dtype = torch.float64
+    >>> device = torch.device("cpu")
+    >>> batch_dims = (2, 3)
+    >>> na = 4
+    >>> nguess = 2
+    >>> M = None
+    >>> V = _set_initial_v(vinit_type, dtype, device, batch_dims, na, nguess, M)
+    >>> V
+    tensor([[[[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]],
+    <BLANKLINE>
+             [[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]],
+    <BLANKLINE>
+             [[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]]],
+    <BLANKLINE>
+    <BLANKLINE>
+            [[[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]],
+    <BLANKLINE>
+             [[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]],
+    <BLANKLINE>
+             [[1., 0.],
+              [0., 1.],
+              [0., 0.],
+              [0., 0.]]]], dtype=torch.float64)
+
+    Parameters
+    ----------
+    vinit_type: str
+        Mode of the initial guess (``"randn"``, ``"rand"``, ``"eye"``)
+    dtype: torch.dtype
+        Data type of the initial guess.
+    device: torch.device
+        Device of the initial guess.
+    batch_dims: Sequence
+        Batch dimensions of the initial guess.
+    na: int
+        Number of basis functions.
+    nguess: int
+        Number of initial guesses.
+    M: Optional[LinearOperator] (default None)
+        The overlap matrix. If None, identity matrix is used.
+
+    Returns
+    -------
+    V: torch.Tensor
+        Initial guess for the eigenvectors.
+
+    """
+
+    torch.manual_seed(12421)
+    if vinit_type == "eye":
+        nbatch = functools.reduce(lambda x, y: x * y, batch_dims, 1)
+        V = torch.eye(na, nguess, dtype=dtype,
+                      device=device).unsqueeze(0).repeat(nbatch, 1, 1).reshape(
+                          *batch_dims, na, nguess)
+    elif vinit_type == "randn":
+        V = torch.randn((*batch_dims, na, nguess), dtype=dtype, device=device)
+    elif vinit_type == "random" or vinit_type == "rand":
+        V = torch.rand((*batch_dims, na, nguess), dtype=dtype, device=device)
+    else:
+        raise ValueError("Unknown v_init type: %s" % vinit_type)
+
+    # orthogonalize V
+    if isinstance(M, LinearOperator):
+        V, R = tallqr(V, MV=M.mm(V))
+    else:
+        V, R = tallqr(V)
+    return V
 
 
 def _take_eigpairs(eival: torch.Tensor, eivec: torch.Tensor, neig: int,
