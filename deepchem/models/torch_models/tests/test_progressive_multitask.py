df024e2da8b2d1c363fc237efc5b9470d0a792a0
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2024-02-07 18:46:27 +0530

    add: unit tests for torch ProgressiveMultitask

--- deepchem/models/torch_models/tests/test_progressive_multitask.py
+++ deepchem/models/torch_models/tests/test_progressive_multitask.py
@@ -1,5 +1,6 @@
 import numpy as np
 import deepchem as dc
+import tempfile
 
 import pytest
 
@@ -151,3 +152,124 @@ def move_weights(torch_model, weights):
     torch_model.adapters[0][1][0].weight = torch_weights["adapter-0-1-0-w"]
     torch_model.adapters[0][1][0].bias = torch_weights["adapter-0-1-0-b"]
     torch_model.adapters[0][1][1].weight = torch_weights["adapter-0-1-1-w"]
+
+
+@pytest.mark.torch
+def test_progressivemultitask_regression_overfit():
+    """Test that the model can overfit simple regression datasets."""
+    n_samples = 10
+    n_features = 5
+    n_tasks = 3
+
+    np.random.seed(123)
+    torch.manual_seed(123)
+    X = np.random.rand(n_samples, n_features)
+    y = np.random.rand(n_samples, n_tasks).astype(np.float32)
+    dataset = dc.data.NumpyDataset(X, y)
+
+    regression_metric = dc.metrics.Metric(dc.metrics.mean_squared_error,
+                                          task_averager=np.mean,
+                                          mode="regression")
+
+    model = dc.models.torch_models.ProgressiveMultitaskModel(
+        n_tasks,
+        n_features,
+        layer_sizes=[128, 256],
+        dropouts=0.2,
+        alpha_init_stddevs=0.02,
+        weight_init_stddevs=0.02,
+        bias_init_consts=0.0,
+        mode='regression',
+    )
+
+    model.fit(dataset, nb_epoch=200)
+
+    scores = model.evaluate(dataset, [regression_metric])
+    assert scores[regression_metric.name] < 0.05, "Failed to overfit"
+
+
+@pytest.mark.torch
+def test_progressivemultitask_classification_overfit():
+    """Test that the model can overfit simple classification datasets."""
+    n_samples = 20
+    n_features = 5
+    n_tasks = 3
+
+    # Generate dummy dataset
+    np.random.seed(123)
+    torch.manual_seed(123)
+    ids = np.arange(n_samples)
+    X = np.random.rand(n_samples, n_features)
+    y = np.random.randint(2, size=(n_samples, n_tasks))
+    w = np.ones((n_samples, n_tasks))
+    dataset = dc.data.NumpyDataset(X, y, w, ids)
+
+    classification_metric = dc.metrics.Metric(dc.metrics.accuracy_score)
+
+    model = dc.models.torch_models.ProgressiveMultitaskModel(
+        n_tasks,
+        n_features,
+        layer_sizes=[128, 256],
+        dropouts=0.2,
+        alpha_init_stddevs=0.02,
+        weight_init_stddevs=0.02,
+        bias_init_consts=0.0,
+        mode='classification',
+    )
+
+    model.fit(dataset, nb_epoch=200)
+
+    scores = model.evaluate(dataset, [classification_metric])
+
+    assert scores[classification_metric.name] > 0.9, "Failed to overfit"
+
+
+def test_progressivemultitask_reload():
+    """Test that the model can be reloaded from disk."""
+
+    n_samples = 20
+    n_features = 5
+    n_tasks = 3
+
+    # Generate dummy dataset
+    np.random.seed(123)
+    torch.manual_seed(123)
+    ids = np.arange(n_samples)
+    X = np.random.rand(n_samples, n_features)
+    y = np.random.randint(2, size=(n_samples, n_tasks))
+    w = np.ones((n_samples, n_tasks))
+    dataset = dc.data.NumpyDataset(X, y, w, ids)
+
+    model_dir = tempfile.mkdtemp()
+
+    orig_model = dc.models.torch_models.ProgressiveMultitaskModel(
+        n_tasks,
+        n_features,
+        layer_sizes=[128, 256],
+        dropouts=0.2,
+        alpha_init_stddevs=0.02,
+        weight_init_stddevs=0.02,
+        bias_init_consts=0.0,
+        mode='classification',
+        model_dir=model_dir)
+
+    orig_model.fit(dataset, nb_epoch=200)
+
+    reloaded_model = dc.models.torch_models.ProgressiveMultitaskModel(
+        n_tasks,
+        n_features,
+        layer_sizes=[128, 256],
+        dropouts=0.2,
+        alpha_init_stddevs=0.02,
+        weight_init_stddevs=0.02,
+        bias_init_consts=0.0,
+        mode='classification',
+        model_dir=model_dir)
+
+    reloaded_model.restore()
+
+    X_new = np.random.rand(n_samples, n_features)
+    orig_preds = orig_model.predict_on_batch(X_new)
+    reloaded_preds = reloaded_model.predict_on_batch(X_new)
+
+    assert np.all(orig_preds == reloaded_preds), "Predictions are not the same"
