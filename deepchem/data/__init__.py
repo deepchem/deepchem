a559198d6b5a08942f2b17a3820fe39c7d7ae4ea
Shreyas Vinaya <vinaya.shreyas@gmail.com>
2024-03-12 20:28:58 +0530

    add missing fastqloader to loaders

--- deepchem/data/__init__.py
+++ deepchem/data/__init__.py
@@ -19,6 +19,7 @@ from deepchem.data.data_loader import UserCSVLoader
 from deepchem.data.data_loader import JsonLoader
 from deepchem.data.data_loader import SDFLoader
 from deepchem.data.data_loader import FASTALoader
+from deepchem.data.data_loader import FASTQLoader
 from deepchem.data.data_loader import ImageLoader
 from deepchem.data.data_loader import InMemoryLoader
 try:
