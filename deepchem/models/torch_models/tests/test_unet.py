9dfd471db49876d955a7af521e512fdbf3dd2d66
aaronrockmenezes <85219711+aaronrockmenezes@users.noreply.github.com>
2024-04-03 17:52:32 +0530

    Modified test input size for restore

--- deepchem/models/torch_models/tests/test_unet.py
+++ deepchem/models/torch_models/tests/test_unet.py
@@ -27,10 +27,10 @@ def test_UNetModel():
 def test_restore_UNetModel():
     from deepchem.models.torch_models import UNetModel
 
-    input_samples = np.random.randn(5, 3, 10, 10).astype(
-        np.float32)  # 5 RGB 10x10 pixel input images
-    output_samples = np.random.randn(5, 1, 10, 10).astype(
-        np.float32)  # 5 grey scale 10x10 pixel output segmentation masks
+    input_samples = np.random.randn(5, 3, 32, 32).astype(
+        np.float32)  # 5 RGB 32x32 pixel input images
+    output_samples = np.random.randn(5, 1, 32, 32).astype(
+        np.float32)  # 5 grey scale 32x32 pixel output segmentation masks
 
     np_dataset = dc.data.NumpyDataset(input_samples, output_samples)
 
