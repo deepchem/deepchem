0bcb12b0c17bafe8424f9405e2413cd60e140d24
Aaron Rock Menezes <85219711+aaronrockmenezes@users.noreply.github.com>
2024-04-08 21:44:04 +0530

    ScScore Porting (#3692)
    * Basic model implemented
    
    * Added TorchModel Wrapper and documentation
    
    * Added tests
    
    * Tests file
    
    * Tests file linting and imports fix
    
    * Edited tests
    
    * Added usage example, added model to init, removed tf scscore from init
    
    * Converted Model to ModularTorchModel
    
    * Reverted to TorchModel
    
    * Reduced fit epochs from 50 to 5 in restore test case
    
    * Added test for loading pretrained model
    
    * Linting fix
    
    * Doctest fixes
    
    * Made minor change to init file
    
    * Fixed init file and import exmaple

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -20,6 +20,7 @@ from deepchem.models.torch_models.megnet import MEGNetModel
 from deepchem.models.torch_models.normalizing_flows_pytorch import NormalizingFlow
 from deepchem.models.torch_models.layers import MultilayerPerceptron, CNNModule, CombineMeanStd, WeightedLinearCombo, AtomicConvolution, NeighborList, SetGather, EdgeNetwork, WeaveLayer, WeaveGather, MolGANConvolutionLayer, MolGANAggregationLayer, MolGANMultiConvolutionLayer, MolGANEncoderLayer, VariationalRandomizer, EncoderRNN, DecoderRNN, AtomicConv
 from deepchem.models.torch_models.cnn import CNN
+from deepchem.models.torch_models.scscore import ScScore, ScScoreModel
 from deepchem.models.torch_models.weavemodel_pytorch import Weave, WeaveModel
 from deepchem.models.torch_models.attention import ScaledDotProductAttention, SelfAttention
 from deepchem.models.torch_models.grover import GroverModel, GroverPretrain, GroverFinetune
