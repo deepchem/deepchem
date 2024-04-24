8d3a6fa28f0d7d2a25a51779fc8c6a81b10981d9
Nimisha Dey <75609169+NimishaDey@users.noreply.github.com>
2024-04-24 21:54:05 +0530

    Adding PPO class (#3954)

--- deepchem/rl/tests/test_rl_reload_torch.py
+++ deepchem/rl/tests/test_rl_reload_torch.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 from deepchem.models.optimizers import Adam
 try:
-    from deepchem.rl.torch_rl import A2C
+    from deepchem.rl.torch_rl import A2C, PPO
     import torch
     import torch.nn as nn
     import torch.nn.functional as F
@@ -76,6 +76,26 @@ def test_a2c_reload():
     new_a2c = A2C(env, policy, model_dir=a2c._model.model_dir)
     new_a2c.restore()
     action_prob2, value2 = new_a2c.predict([[0]])
+
+    assert np.all(action_prob == action_prob2)
+    assert value == value2
+
+
+@pytest.mark.torch
+def test_ppo_reload():
+    env = RouletteEnvironment()
+    policy = TestPolicy(env)
+    ppo = PPO(env,
+              policy,
+              max_rollout_length=20,
+              optimization_epochs=8,
+              optimizer=Adam(learning_rate=0.003))
+    ppo.fit(1000)
+    action_prob, value = ppo.predict([[0]])
+
+    new_ppo = PPO(env, policy, model_dir=ppo._model.model_dir)
+    new_ppo.restore()
+    action_prob2, value2 = new_ppo.predict([[0]])
 
     assert np.all(action_prob == action_prob2)
     assert value == value2
