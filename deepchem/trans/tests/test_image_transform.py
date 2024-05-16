6f7e4c3058096ae99e7df37cbad8fadaed20f01a
aaronrockmenezes <85219711+aaronrockmenezes@users.noreply.github.com>
2024-05-16 23:57:46 +0530

    Fixed size mismatch error

--- deepchem/trans/tests/test_image_transform.py
+++ deepchem/trans/tests/test_image_transform.py
@@ -21,7 +21,7 @@ def test_x_image_transform():
                                                 transform_y=False)
     dataset = dataset.transform(img_transformer)
 
-    assert dataset.X.shape == (n_samples, new_size[0], new_size[1], channels)
+    assert dataset.X.shape == (n_samples, new_size[1], new_size[0], channels)
     assert dataset.y.shape == (n_samples, 1)
     assert dataset.w.shape == (n_samples, 1)
     assert dataset.ids.shape == (n_samples, 1)
@@ -47,7 +47,7 @@ def test_y_image_transform():
     dataset = dataset.transform(img_transformer)
 
     assert dataset.X.shape == (n_samples, width, height, channels)
-    assert dataset.y.shape == (n_samples, new_size[0], new_size[1], channels)
+    assert dataset.y.shape == (n_samples, new_size[1], new_size[0], channels)
     assert dataset.w.shape == (n_samples, 1)
     assert dataset.ids.shape == (n_samples, 1)
 
@@ -71,8 +71,8 @@ def test_xy_image_transform():
                                                 transform_y=True)
     dataset = dataset.transform(img_transformer)
 
-    assert dataset.X.shape == (n_samples, new_size[0], new_size[1], channels)
-    assert dataset.y.shape == (n_samples, new_size[0], new_size[1], channels)
+    assert dataset.X.shape == (n_samples, new_size[1], new_size[0], channels)
+    assert dataset.y.shape == (n_samples, new_size[1], new_size[0], channels)
     assert dataset.w.shape == (n_samples, 1)
     assert dataset.ids.shape == (n_samples, 1)
 
@@ -95,7 +95,7 @@ def test_image_transform_array():
                                               transform_y=True)
     data = img_transform.transform_array(X, y, w, ids)
 
-    assert data[0].shape == (n_samples, new_size[0], new_size[1], channels)
-    assert data[1].shape == (n_samples, new_size[0], new_size[1], channels)
+    assert data[0].shape == (n_samples, new_size[1], new_size[0], channels)
+    assert data[1].shape == (n_samples, new_size[1], new_size[0], channels)
     assert data[2].shape == (n_samples, 1)
     assert data[3].shape == (n_samples, 1)
