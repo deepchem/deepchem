5901de7d78eca0eb6074c622a03d53b927d85c03
Shiva-sankaran <sankaran110601@gmail.com>
2024-06-27 10:29:25 -0400

    Added all features of ProtBERT

--- deepchem/models/torch_models/prot_bert.py
+++ deepchem/models/torch_models/prot_bert.py
@@ -8,16 +8,16 @@ class ProtBERT(HuggingFaceModel):
     """
     ProtBERT model[1].
 
-    ProtBERT model is based on BERT architecture and the current implementation
+    ProtBERT model is based on the BERT architecture and the current implementation
     supports only MLM pretraining and classification mode, as described by the
-    authors in HuggingFace[2]. For classfication we currently only support
-    Logistic regression and a simple Feed forward neural network.
+    authors in HuggingFace[2]. The classification mode supports three tasks:
+    membrane protein prediction, subcellular localization prediction, and custom
+    classification tasks with a user-provided classifier head.
 
     The model converts the input protein sequence into a vector through a trained BERT tokenizer, which is then
     processed by the corresponding model based on the task. `BertForMaskedLM` is used to facilitate the MLM
     pretraining task. For the sequence classification task, we follow `BertForSequenceClassification` but change
-    the classifier to either a logistic regression (LogReg) or a feed-forward neural network (FFN), depending on
-    the specified `cls_name`. The FFN is a simple 2-layer network with 512 as the hidden dimension.
+    the classifier to a custom `nn.Module` if provided.
 
 
     Examples
@@ -41,12 +41,13 @@ class ProtBERT(HuggingFaceModel):
     >>> from deepchem.models.torch_models.prot_bert import ProtBERT
     >>> pretrain_model_dir = os.path.join(tempdir, 'pretrain-model')
     >>> model_path = 'Rostlab/prot_bert'
-    >>> pretrain_model = ProtBERT(task='mlm', HG_model_path=model_path, n_tasks=1, model_dir=pretrain_model_dir)  # mlm pretraining
+    >>> pretrain_model = ProtBERT(task='mlm', model_pretrain_dataset='uniref100', n_tasks=1, model_dir=pretrain_model_dir)  # mlm pretraining
     >>> pretraining_loss = pretrain_model.fit(dataset, nb_epoch=1)
     >>> del pretrain_model
 
     >>> finetune_model_dir = os.path.join(tempdir, 'finetune-model')
-    >>> finetune_model = ProtBERT(task='classification', HG_model_path=model_path, n_tasks=1, model_dir=finetune_model_dir)
+    >>> custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512), nn.ReLU(),nn.Linear(512, 2))
+    >>> finetune_model = ProtBERT(task='classification', model_pretrain_dataset='uniref100', n_tasks=1,cls_task="custom",cls_head=custom_torch_cls_seq_network, model_dir=finetune_model_dir)
     >>> finetune_model.load_from_pretrained(pretrain_model_dir)
     >>> finetuning_loss = finetune_model.fit(dataset, nb_epoch=1)
 
@@ -63,66 +64,99 @@ class ProtBERT(HuggingFaceModel):
 
     def __init__(self,
                  task: str,
-                 model_pretrain_dataset = "uniref100",
+                 model_pretrain_dataset: str = "uniref100",
                  n_tasks: int = 1,
-                 cls_name: str = "LogReg",
+                 cls_task: str = "custom",
+                 cls_head: Union[nn.Module, None] = None,
+                 n_classes: int = 2,
                  **kwargs) -> None:
         """
         Parameters
         ----------
         task: str
-            The task defines the type of learning task in the model. The supported tasks are
+            The task defines the type of learning task in the model. The supported tasks are:
             - `mlm` - masked language modeling commonly used in pretraining
             - `classification` - use it for classification tasks
-        model_path: str
-            Path to the HuggingFace model
+        model_pretrain_dataset: str
+            The dataset used for pretraining the model. Options are 'uniref100' and 'bfd'.
         n_tasks: int, default 1
             Number of prediction targets for a multitask learning model
-        cls_name: str
-            The classifier head to use for classification mode. Currently only supports "FFN" and "LogReg"
+        cls_task: str, default "custom"
+            The specific classification task. Options are "membrane", "subcellular location", and "custom".
+        cls_head: nn.Module, optional
+            A custom classifier head to use for classification mode.
+        n_classes: int, default 2
+            Number of classes for classification.
         """
 
-        
-
         self.n_tasks: int = n_tasks
-        tokenizer: BertTokenizer 
-        model: Union[BertForMaskedLM, BertForSequenceClassification]
 
-        if(model_pretrain_dataset == "uniref100"):
+        if model_pretrain_dataset == "uniref100":
             model_path = 'Rostlab/prot_bert'
-        elif(model_pretrain_dataset == "bfd"):
+        elif model_pretrain_dataset == "bfd":
             model_path = 'Rostlab/prot_bert_bfd'
         else:
-            raise ValueError('Invalid pretraining data: {}.'.format(model_pretrain_dataset))
+            raise ValueError('Invalid pretraining dataset: {}.'.format(
+                model_pretrain_dataset))
 
-        tokenizer =  BertTokenizer.from_pretrained(
-            model_path, do_lower_case=False)
+        tokenizer = BertTokenizer.from_pretrained(model_path,
+                                                  do_lower_case=False)
 
         if task == "mlm":
             model = BertForMaskedLM.from_pretrained(model_path)
         elif task == "classification":
-            cls_head: Union[nn.Linear, nn.Sequential]
-            protbert_config: BertConfig = BertConfig.from_pretrained(
-            pretrained_model_name_or_path=model_path,
-            vocab_size=tokenizer.vocab_size)
-            if n_tasks == 1:
-                protbert_config.problem_type = 'single_label_classification'
+            if model_pretrain_dataset == "uniref100" and cls_task in [
+                    "membrane", "subcellular location"
+            ]:
+                raise ValueError(
+                    "Classification model for '{}' task is only available with BFD pretraining dataset."
+                    .format(cls_task))
+
+            if cls_task == "membrane":
+                if cls_head is not None:
+                    raise ValueError(
+                        "Custom classifier head is not supported for 'membrane' task."
+                    )
+                model = BertForSequenceClassification.from_pretrained(
+                    'Rostlab/prot_bert_bfd_membrane')
+
+            elif cls_task == "subcellular location":
+                if cls_head is not None:
+                    raise ValueError(
+                        "Custom classifier head is not supported for 'subcellular location' task."
+                    )
+                model = BertForSequenceClassification.from_pretrained(
+                    'Rostlab/prot_bert_bfd_localization')
+
+            elif cls_task == "custom":
+                if isinstance(cls_head, nn.Module):
+                    cls_net = cls_head
+                else:
+                    raise ValueError(
+                        'Invalid classifier head type. Expected nn.Module but got {}.'
+                        .format(type(cls_head)))
+
+                protbert_config = BertConfig.from_pretrained(
+                    pretrained_model_name_or_path=model_path,
+                    vocab_size=tokenizer.vocab_size)
+                protbert_config.num_labels = n_classes
+
+                if n_tasks == 1:
+                    protbert_config.problem_type = 'single_label_classification'
+                else:
+                    protbert_config.problem_type = 'multi_label_classification'
+
+                model = BertForSequenceClassification.from_pretrained(
+                    model_path, config=protbert_config)
+                model.classifier = cls_net
+
             else:
-                protbert_config.problem_type = 'multi_label_classification'
-
-            if (cls_name == "LogReg"):
-                cls_head = nn.Linear(1024, n_tasks + 1)
-            elif (cls_name == "FFN"):
-                cls_head = nn.Sequential(nn.Linear(1024, 512), nn.ReLU(),
-                                         nn.Linear(512, n_tasks + 1))
-
-            else:
-                raise ValueError('Invalid classifier: {}.'.format(cls_name))
-
-            model = BertForSequenceClassification.from_pretrained(
-                model_path, config=protbert_config)
-            model.classifier = cls_head
-
+                raise ValueError(
+                    'Invalid classification task: {}. Expected "membrane", "subcellular location", or "custom".'
+                    .format(cls_task))
         else:
-            raise ValueError('Invalid task specification')
+            raise ValueError(
+                'Invalid task specification: {}. Expected "mlm" or "classification".'
+                .format(task))
+
         super().__init__(model=model, task=task, tokenizer=tokenizer, **kwargs)
--- deepchem/models/torch_models/tests/test_prot_bert.py
+++ deepchem/models/torch_models/tests/test_prot_bert.py
@@ -6,8 +6,66 @@ import numpy as np
 try:
     import torch
     from deepchem.models.torch_models.prot_bert import ProtBERT
+    import torch.nn as nn
+    import torch.nn.functional as F
 except ModuleNotFoundError:
     pass
+
+
+class SimpleCNN(nn.Module):
+
+    def __init__(self, input_dim=1024, num_classes=2):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=1,
+                               out_channels=32,
+                               kernel_size=3,
+                               padding=1)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32 * (input_dim // 2),
+                             num_classes)  # Adjusting for the pooling layer
+
+    def forward(self, x):
+        x = x.unsqueeze(1)  # Adding channel dimension
+        x = self.pool(F.relu(self.conv1(x)))
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+
+
+@pytest.mark.torch
+def test_prot_bert_load():
+    # Test to ensure all variants of the model are supported
+    model = ProtBERT(task='mlm', model_pretrain_dataset="uniref100", n_tasks=1)
+    model = ProtBERT(task='mlm', model_pretrain_dataset="bfd", n_tasks=1)
+
+    custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512),
+                                                 nn.ReLU(), nn.Linear(512, 256),
+                                                 nn.ReLU(), nn.Linear(256, 15))
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="uniref100",
+                     cls_task="custom",
+                     cls_head=custom_torch_cls_seq_network,
+                     n_tasks=1,
+                     n_classes=15)
+
+    custom_torch_CNN_network = SimpleCNN()
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="bfd",
+                     cls_task="custom",
+                     cls_head=custom_torch_CNN_network,
+                     n_tasks=1,
+                     n_classes=2)
+
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="bfd",
+                     cls_task="membrane",
+                     n_tasks=1)
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="bfd",
+                     cls_task="subcellular location",
+                     n_tasks=1)
+
+    assert model
 
 
 @pytest.mark.torch
@@ -24,12 +82,13 @@ def test_prot_bert_pretraining_mlm(protein_classification_dataset):
 @pytest.mark.torch
 def test_prot_bert_finetuning(protein_classification_dataset):
 
-    pretrain_data_type = "uniref100"
-
+    custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512),
+                                                 nn.ReLU(), nn.Linear(512, 2))
     model = ProtBERT(task='classification',
-                     model_pretrain_dataset=pretrain_data_type,
-                     n_tasks=1,
-                     cls_name="LogReg")
+                     model_pretrain_dataset="bfd",
+                     cls_task="custom",
+                     cls_head=custom_torch_cls_seq_network,
+                     n_tasks=1)
     loss = model.fit(protein_classification_dataset, nb_epoch=1)
     eval_score = model.evaluate(protein_classification_dataset,
                                 metrics=dc.metrics.Metric(
@@ -38,11 +97,12 @@ def test_prot_bert_finetuning(protein_classification_dataset):
     prediction = model.predict(protein_classification_dataset)
     assert prediction.shape == (protein_classification_dataset.y.shape[0], 2)
 
-    pretrain_data_type = "bfd"
+    custom_torch_CNN_network = SimpleCNN()
     model = ProtBERT(task='classification',
-                     model_pretrain_dataset=pretrain_data_type,
-                     n_tasks=1,
-                     cls_name="FFN")
+                     model_pretrain_dataset="bfd",
+                     cls_task="custom",
+                     cls_head=custom_torch_CNN_network,
+                     n_tasks=1)
     loss = model.fit(protein_classification_dataset, nb_epoch=1)
     eval_score = model.evaluate(protein_classification_dataset,
                                 metrics=dc.metrics.Metric(
@@ -50,24 +110,54 @@ def test_prot_bert_finetuning(protein_classification_dataset):
     assert eval_score, loss
     prediction = model.predict(protein_classification_dataset)
     assert prediction.shape == (protein_classification_dataset.y.shape[0], 2)
+
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="bfd",
+                     cls_task="membrane",
+                     n_tasks=1)
+    loss = model.fit(protein_classification_dataset, nb_epoch=1)
+    eval_score = model.evaluate(protein_classification_dataset,
+                                metrics=dc.metrics.Metric(
+                                    dc.metrics.accuracy_score),
+                                n_classes=2)
+    assert eval_score, loss
+    prediction = model.predict(protein_classification_dataset)
+    assert prediction.shape == (protein_classification_dataset.y.shape[0], 2)
+
+    model = ProtBERT(task='classification',
+                     model_pretrain_dataset="bfd",
+                     cls_task="subcellular location",
+                     n_tasks=1)
+    loss = model.fit(protein_classification_dataset, nb_epoch=1)
+    eval_score = model.evaluate(protein_classification_dataset,
+                                metrics=dc.metrics.Metric(
+                                    dc.metrics.accuracy_score),
+                                n_classes=10)
+    assert eval_score, loss
+    prediction = model.predict(protein_classification_dataset)
+    assert prediction.shape == (protein_classification_dataset.y.shape[0], 10)
 
 
 @pytest.mark.torch
 def test_protbert_load_from_pretrained(tmpdir):
-    
+
     pretrain_data_type = "uniref100"
-    pretrain_model_dir = os.path.join(tmpdir, 'pretrain_{}'.format(pretrain_data_type))
-    finetune_model_dir = os.path.join(tmpdir, 'finetune_{}'.format(pretrain_data_type))
+    pretrain_model_dir = os.path.join(tmpdir,
+                                      'pretrain_{}'.format(pretrain_data_type))
+    finetune_model_dir = os.path.join(tmpdir,
+                                      'finetune_{}'.format(pretrain_data_type))
     pretrain_model = ProtBERT(task='mlm',
                               model_pretrain_dataset=pretrain_data_type,
                               n_tasks=1,
                               model_dir=pretrain_model_dir)
     pretrain_model.save_checkpoint()
-
+    custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512),
+                                                 nn.ReLU(), nn.Linear(512, 2))
     finetune_model = ProtBERT(task='classification',
                               model_pretrain_dataset=pretrain_data_type,
+                              cls_task="custom",
+                              cls_head=custom_torch_cls_seq_network,
                               n_tasks=1,
-                              cls_name="LogReg",
                               model_dir=finetune_model_dir)
     finetune_model.load_from_pretrained(pretrain_model_dir)
 
@@ -86,10 +176,11 @@ def test_protbert_load_from_pretrained(tmpdir):
 
     assert all(matches)
 
-
     pretrain_data_type = "bfd"
-    pretrain_model_dir = os.path.join(tmpdir, 'pretrain_{}'.format(pretrain_data_type))
-    finetune_model_dir = os.path.join(tmpdir, 'finetune_{}'.format(pretrain_data_type))
+    pretrain_model_dir = os.path.join(tmpdir,
+                                      'pretrain_{}'.format(pretrain_data_type))
+    finetune_model_dir = os.path.join(tmpdir,
+                                      'finetune_{}'.format(pretrain_data_type))
     pretrain_model = ProtBERT(task='mlm',
                               model_pretrain_dataset=pretrain_data_type,
                               n_tasks=1,
@@ -97,9 +188,9 @@ def test_protbert_load_from_pretrained(tmpdir):
     pretrain_model.save_checkpoint()
 
     finetune_model = ProtBERT(task='classification',
-                              model_pretrain_dataset=pretrain_data_type,
+                              model_pretrain_dataset="bfd",
+                              cls_task="membrane",
                               n_tasks=1,
-                              cls_name="LogReg",
                               model_dir=finetune_model_dir)
     finetune_model.load_from_pretrained(pretrain_model_dir)
 
@@ -121,19 +212,22 @@ def test_protbert_load_from_pretrained(tmpdir):
 
 @pytest.mark.torch
 def test_protbert_save_reload(tmpdir):
-    pretrain_data_type = "bfd"
+    custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512),
+                                                 nn.ReLU(), nn.Linear(512, 2))
     model = ProtBERT(task='classification',
-                     model_pretrain_dataset=pretrain_data_type,
+                     model_pretrain_dataset="bfd",
+                     cls_task="custom",
+                     cls_head=custom_torch_cls_seq_network,
                      n_tasks=1,
-                     cls_name="FFN",
                      model_dir=tmpdir)
     model._ensure_built()
     model.save_checkpoint()
 
     model_new = ProtBERT(task='classification',
-                         model_pretrain_dataset=pretrain_data_type,
+                         model_pretrain_dataset="bfd",
+                         cls_task="custom",
+                         cls_head=custom_torch_cls_seq_network,
                          n_tasks=1,
-                         cls_name="FFN",
                          model_dir=tmpdir)
     model_new.restore()
 
@@ -148,6 +242,7 @@ def test_protbert_save_reload(tmpdir):
     assert all(matches)
 
 
+####
 @pytest.mark.torch
 def test_protbert_overfit():
     np.random.seed(32)
@@ -162,30 +257,34 @@ def test_protbert_overfit():
     dataset = loader.create_dataset(
         os.path.join(current_dir,
                      "../../tests/assets/example_protein_classification.csv"))
-    
+
     pretrain_data_type = "bfd"
+    custom_torch_cls_seq_network = nn.Sequential(nn.Linear(1024, 512),
+                                                 nn.ReLU(), nn.Linear(512, 2))
     finetune_model_bfd = ProtBERT(task='classification',
-                              model_pretrain_dataset=pretrain_data_type,
-                              n_tasks=1,
-                              cls_name="FFN",
-                              batch_size=5,
-                              learning_rate=1e-5,log_frequency=1)
+                                  model_pretrain_dataset=pretrain_data_type,
+                                  cls_task="custom",
+                                  cls_head=custom_torch_cls_seq_network,
+                                  n_tasks=1,
+                                  batch_size=5,
+                                  learning_rate=1e-5)
     classification_metric = dc.metrics.Metric(dc.metrics.accuracy_score)
     finetune_model_bfd.fit(dataset, nb_epoch=100)
-    eval_score_bfd = finetune_model_bfd.evaluate(dataset, [classification_metric])
+    eval_score_bfd = finetune_model_bfd.evaluate(dataset,
+                                                 [classification_metric])
     assert eval_score_bfd[classification_metric.name] > 0.9
-    
+
     pretrain_data_type = "uniref100"
+
     finetune_model_uniref = ProtBERT(task='classification',
-                              model_pretrain_dataset=pretrain_data_type,
-                              n_tasks=1,
-                              cls_name="FFN",
-                              batch_size=1,
-                              learning_rate=1e-5)
+                                     model_pretrain_dataset=pretrain_data_type,
+                                     cls_task="custom",
+                                     cls_head=custom_torch_cls_seq_network,
+                                     n_tasks=1,
+                                     batch_size=1,
+                                     learning_rate=1e-5)
     classification_metric = dc.metrics.Metric(dc.metrics.accuracy_score)
     finetune_model_uniref.fit(dataset, nb_epoch=10)
-    eval_score_uniref = finetune_model_uniref.evaluate(dataset, [classification_metric])
+    eval_score_uniref = finetune_model_uniref.evaluate(dataset,
+                                                       [classification_metric])
     assert eval_score_uniref[classification_metric.name] > 0.9
-
-
-    
