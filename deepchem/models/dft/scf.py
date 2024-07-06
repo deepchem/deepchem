98f1d306b64fa4a92dc2302d54cff462ba404d92
GreatRSingh <rakshitsingh421@gmail.com>
2024-07-06 18:34:30 +0530

    catch

--- deepchem/models/dft/scf.py
+++ deepchem/models/dft/scf.py
@@ -3,11 +3,11 @@ from abc import abstractmethod
 from typing import Union
 import torch
 import numpy as np
-from deepchem.feat.dft_data import DFTEntry, DFTSystem
-from deepchem.utils.dftutils import KSCalc, hashstr, SpinParam
 try:
     from dqc.qccalc.ks import KS
     from deepchem.models.dft.nnxc import BaseNNXC, HybridXC
+    from deepchem.utils.dftutils import KSCalc, hashstr, SpinParam
+    from deepchem.feat.dft_data import DFTEntry, DFTSystem
 except:
     pass
 
