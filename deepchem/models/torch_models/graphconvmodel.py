4cdf54461dba271ecca24cf4cfbe6528bb89dd9a
Nimisha Dey <75609169+NimishaDey@users.noreply.github.com>
2024-07-26 21:45:22 +0530

    Adding Graph Conv Model class (#4063)

--- deepchem/models/torch_models/graphconvmodel.py
+++ deepchem/models/torch_models/graphconvmodel.py
@@ -1,14 +1,21 @@
 from collections.abc import Sequence as SequenceCollection
+import deepchem as dc
+import numpy as np
 try:
     import torch
     import torch.nn as nn
     import torch.nn.functional as F
 except ModuleNotFoundError:
     raise ImportError('These classes require PyTorch to be installed.')
-from typing import List
+from typing import List, Union, Callable, Any
+from deepchem.data import Dataset
+from deepchem.feat.mol_graphs import ConvMol
+from deepchem.metrics import to_one_hot
 from deepchem.utils.typing import OneOrMany
 import deepchem.models.torch_models.layers as torch_layers
 from deepchem.utils.pytorch_utils import get_activation
+from deepchem.models.torch_models.torch_model import TorchModel
+from deepchem.models.losses import L2Loss, SoftmaxCrossEntropy
 
 
 class TrimGraphOutput(nn.Module):
@@ -150,7 +157,7 @@ class _GraphConvTorchModel(nn.Module):
             for _ in range(len(graph_conv_layers))
         ])
         self.batch_norms.append(
-            nn.BatchNorm1d(num_features=128,
+            nn.BatchNorm1d(num_features=dense_layer_size,
                            eps=1e-3,
                            momentum=0.99,
                            affine=True,
@@ -174,7 +181,8 @@ class _GraphConvTorchModel(nn.Module):
             self.regression_dense: nn.Linear = nn.Linear(
                 dense_layer_size * 2, n_tasks)
             if self.uncertainty:
-                self.uncertainty_dense: nn.Linear = nn.Linear(n_tasks, n_tasks)
+                self.uncertainty_dense: nn.Linear = nn.Linear(
+                    dense_layer_size * 2, n_tasks)
                 self.uncertainty_trim = TrimGraphOutput()
 
     def forward(self,
@@ -214,9 +222,9 @@ class _GraphConvTorchModel(nn.Module):
         dense: torch.Tensor = self.dense(in_layer)
         denseact: torch.Tensor = self.dense_act(dense)
         if self.batch_norms[-1] is not None:
-            dense = self.batch_norms[-1](denseact)
+            denseact = self.batch_norms[-1](denseact)
         if training and self.dropouts[-1] is not None:
-            dense = self.dropouts[-1](denseact)
+            denseact = self.dropouts[-1](denseact)
         neural_fingerprint: torch.Tensor = self.graph_gather(
             [denseact, degree_slice, membership] + deg_adjs)
         if self.mode == 'classification':
@@ -239,3 +247,176 @@ class _GraphConvTorchModel(nn.Module):
                 outputs = [output, neural_fingerprint]
 
         return outputs
+
+
+class GraphConvModel(TorchModel):
+    """Graph Convolutional Models.
+
+    This class implements the graph convolutional model from the
+    following paper [1]_. These graph convolutions start with a per-atom set of
+    descriptors for each atom in a molecule, then combine and recombine these
+    descriptors over convolutional layers.
+    following [1]_.
+
+    Example
+    --------
+    >>> import deepchem as dc
+    >>> import numpy as np
+    >>> from deepchem.models.torch_models import GraphConvModel
+    >>> featurizer = dc.feat.ConvMolFeaturizer()
+    >>> tasks = ["outcome"]
+    >>> mols = ["C", "CO", "CC"]
+    >>> X = featurizer(mols)
+    >>> y = np.array([0, 1, 0])
+    >>> dataset = dc.data.NumpyDataset(X, y)
+    >>> classification_metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean, mode="classification")
+    >>> batch_size = 10
+    >>> model = GraphConvModel(len(tasks), number_input_features=[75, 64], batch_size=batch_size, batch_normalize=False, mode='classification')
+    >>> loss = model.fit(dataset, nb_epoch=20)
+
+    References
+    ----------
+    .. [1] Duvenaud, David K., et al. "Convolutional networks on graphs for
+        learning molecular fingerprints." Advances in neural information processing
+        systems. 2015.
+    """
+
+    def __init__(self,
+                 n_tasks: int,
+                 number_input_features: List[int],
+                 graph_conv_layers: List[int] = [64, 64],
+                 dense_layer_size: int = 128,
+                 dropout: float = 0.0,
+                 mode: str = "classification",
+                 number_atom_features: int = 75,
+                 n_classes: int = 2,
+                 batch_size: int = 100,
+                 batch_normalize: bool = True,
+                 uncertainty: bool = False,
+                 **kwargs):
+        """The wrapper class for graph convolutions.
+
+        Note that since the underlying _GraphConvKerasModel class is
+        specified using imperative subclassing style, this model
+        cannout make predictions for arbitrary outputs.
+
+        Parameters
+        ----------
+        n_tasks: int
+            Number of tasks
+        number_input_features: list of int
+            Number of input features to each of the Graph Conv Layer
+        graph_conv_layers: list of int
+            Width of channels for the Graph Convolution Layers
+        dense_layer_size: int
+            Width of channels for Atom Level Dense Layer after GraphPool
+        dropout: list or float
+            the dropout probablity to use for each layer.  The length of this list
+            should equal len(graph_conv_layers)+1 (one value for each convolution
+            layer, and one for the dense layer).  Alternatively this may be a single
+            value instead of a list, in which case the same value is used for every
+            layer.
+        mode: str
+            Either "classification" or "regression"
+        number_atom_features: int
+            75 is the default number of atom features created, but
+            this can vary if various options are passed to the
+            function atom_features in graph_features
+        n_classes: int
+            the number of classes to predict (only used in classification mode)
+        batch_normalize: True
+            if True, apply batch normalization to model
+        uncertainty: bool
+            if True, include extra outputs and loss terms to enable the uncertainty
+            in outputs to be predicted
+        """
+        self.mode: str = mode
+        self.n_tasks: int = n_tasks
+        self.n_classes: int = n_classes
+        self.batch_size: int = batch_size
+        self.uncertainty: bool = uncertainty
+        model = _GraphConvTorchModel(
+            n_tasks,
+            graph_conv_layers=graph_conv_layers,
+            number_input_features=number_input_features,
+            dense_layer_size=dense_layer_size,
+            dropout=dropout,
+            mode=mode,
+            number_atom_features=number_atom_features,
+            n_classes=n_classes,
+            batch_normalize=batch_normalize,
+            uncertainty=uncertainty,
+            batch_size=batch_size)
+        loss: Union[SoftmaxCrossEntropy, L2Loss, Callable[[Any, Any, Any], Any]]
+        if mode == "classification":
+            output_types = ['prediction', 'loss', 'embedding']
+            loss = SoftmaxCrossEntropy()
+        else:
+            if self.uncertainty:
+                output_types = [
+                    'prediction', 'variance', 'loss', 'loss', 'embedding'
+                ]
+
+                def loss(outputs, labels, weights):
+                    output, labels = dc.models.losses._make_pytorch_shapes_consistent(
+                        outputs[0], labels[0])
+                    losses = torch.square(output - labels) / torch.exp(
+                        outputs[1]) + outputs[1]
+                    w = weights[0]
+                    if len(w.shape) < len(losses.shape):
+                        shape = tuple(w.shape)
+                        shape = tuple(-1 if x is None else x for x in shape)
+                        w = torch.reshape(
+                            w,
+                            shape + (1,) * (len(losses.shape) - len(w.shape)))
+                    return torch.mean(losses * w)
+            else:
+                output_types = ['prediction', 'embedding']
+                loss = L2Loss()
+        super(GraphConvModel, self).__init__(model,
+                                             loss,
+                                             output_types=output_types,
+                                             batch_size=batch_size,
+                                             **kwargs)
+
+    def default_generator(self,
+                          dataset: Dataset,
+                          epochs: int = 1,
+                          mode: str = 'fit',
+                          deterministic: bool = True,
+                          pad_batches: bool = True):
+        """Convert a dataset into the tensors needed for learning.
+        Parameters
+        ----------
+        dataset: `dc.data.Dataset`
+            Dataset to convert
+        epochs: int, optional (Default 1)
+            Number of times to walk over `dataset`
+        mode: str, optional (Default 'fit')
+            Ignored in this implementation.
+        deterministic: bool, optional (Default True)
+            Whether the dataset should be walked in a deterministic fashion
+        pad_batches: bool, optional (Default True)
+            If true, each returned batch will have size `self.batch_size`.
+        Returns
+        -------
+        Iterator which walks over the batches
+        """
+        for epoch in range(epochs):
+            for (X_b, y_b, w_b,
+                 ids_b) in dataset.iterbatches(batch_size=self.batch_size,
+                                               deterministic=deterministic,
+                                               pad_batches=pad_batches):
+                if y_b is not None and self.mode == 'classification' and not (
+                        mode == 'predict'):
+                    y_b = to_one_hot(y_b.flatten(), self.n_classes).reshape(
+                        -1, self.n_tasks, self.n_classes)
+                multiConvMol = ConvMol.agglomerate_mols(X_b)
+                n_samples = np.array(X_b.shape[0])
+                inputs = [
+                    multiConvMol.get_atom_features(), multiConvMol.deg_slice,
+                    np.array(multiConvMol.membership), n_samples
+                ]
+                for i in range(1, len(multiConvMol.get_deg_adjacency_lists())):
+                    inputs.append(multiConvMol.get_deg_adjacency_lists()[i])
+                yield (inputs, [y_b], [w_b])
