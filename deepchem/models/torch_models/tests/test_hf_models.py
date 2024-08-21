cd4b3705c7cfbc5d1437fcefd5e360926d64c830
Dhuvi Karthikeyan <dkarthikeyan@berkeley.edu>
2024-08-21 12:51:22 -0400

    Fill Mask Pipeline to HuggingFace Model (#4092)
    * Fill Mask Pipeline to HuggingFace Model
    
    * Fixed Dict Typing Import
    
    * Linted Tests File
    
    * Updated Documentation to NumpyDoc Format + Intermediate Comments
    
    * Split Fill Maks Test into 2 Tests
    
    * Yapf 0.32.0 formatted
    
    * Typo fixed

--- deepchem/models/torch_models/tests/test_hf_models.py
+++ deepchem/models/torch_models/tests/test_hf_models.py
@@ -201,3 +201,68 @@ def test_load_from_hf_checkpoint():
 
     # keys should not match
     assert all(not_matches)
+
+
+@pytest.mark.torch
+def test_fill_mask_IO(tmpdir, hf_tokenizer):
+    from transformers import (RobertaConfig, RobertaForMaskedLM)
+
+    config = RobertaConfig(vocab_size=hf_tokenizer.vocab_size)
+    model = RobertaForMaskedLM(config)
+    hf_model = HuggingFaceModel(model=model,
+                                tokenizer=hf_tokenizer,
+                                task='mlm',
+                                model_dir=tmpdir,
+                                device=torch.device('cpu'))
+    hf_model._ensure_built()
+
+    test_string = "CN(c1ccccc1)c1ccccc1C(=O)NCC1(O)CCOCC1"
+    tokenized_test_string = hf_tokenizer(test_string)
+    tokenized_test_string.input_ids[1] = hf_tokenizer.mask_token_id
+    masked_test_string = hf_tokenizer.decode(tokenized_test_string.input_ids)
+
+    results = hf_model.fill_mask([masked_test_string, masked_test_string])
+
+    # Test 1. Ensure that the correct number of filled items comes out.
+    assert len(results) == 2
+
+    # Test 2. Ensure the types are the expected types
+    assert isinstance(results, list)
+    assert isinstance(results[0], list)
+    assert isinstance(results[0][0], dict)
+
+
+@pytest.mark.torch
+def test_fill_mask_fidelity(tmpdir, hf_tokenizer):
+    from transformers import (RobertaConfig, RobertaForMaskedLM)
+
+    config = RobertaConfig(vocab_size=hf_tokenizer.vocab_size)
+    model = RobertaForMaskedLM(config)
+    hf_model = HuggingFaceModel(model=model,
+                                tokenizer=hf_tokenizer,
+                                task='mlm',
+                                model_dir=tmpdir,
+                                device=torch.device('cpu'))
+    hf_model._ensure_built()
+
+    test_string = "CN(c1ccccc1)c1ccccc1C(=O)NCC1(O)CCOCC1"
+    tokenized_test_string = hf_tokenizer(test_string)
+    tokenized_test_string.input_ids[1] = hf_tokenizer.mask_token_id
+    masked_test_string = hf_tokenizer.decode(tokenized_test_string.input_ids)
+
+    results = hf_model.fill_mask([masked_test_string, masked_test_string])
+
+    for result in results:
+        for filled in result:
+            # Test 1. Check that the right keys exist
+            assert 'sequence' in filled
+            assert 'score' in filled
+            assert 'token' in filled
+            assert 'token_str' in filled
+
+            # Test 2. Check that the scores are probabilities
+            assert filled['score'] < 1
+            assert filled['score'] >= 0
+
+            # Test 3. Check that the infilling went to the right spot
+            assert filled['sequence'].startswith(f'<s>{filled["token_str"]}')
