9ddde3d3eb509d6889e07c16aafe90f866ee4b5d
spellsharp <svcodin22@gmail.com>
2024-11-01 17:33:29 +0530

    Made regressor and classifier classes inheriting the existing class to replicate tf model

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -28,13 +28,13 @@ from deepchem.models.torch_models.readout import GroverReadout
 from deepchem.models.torch_models.dtnn import DTNN, DTNNModel
 from deepchem.models.torch_models.seqtoseq import SeqToSeq, SeqToSeqModel
 from deepchem.models.torch_models.acnn import AtomConvModel
-from deepchem.models.torch_models.progressive_multitask import ProgressiveMultitask, ProgressiveMultitaskModel
+from deepchem.models.torch_models.progressive_multitask import ProgressiveMultitask, ProgressiveMultitaskModel, ProgressiveMultitaskClassifier, ProgressiveMultitaskRegressor
 from deepchem.models.torch_models.text_cnn import TextCNNModel
 from deepchem.models.torch_models.flows import Flow, Affine, MaskedAffineFlow, ActNorm, ClampExp, ConstScaleLayer, MLPFlow, NormalizingFlow, NormalizingFlowModel
 from deepchem.models.torch_models.unet import UNet, UNetModel
 from deepchem.models.torch_models.graphconvmodel import _GraphConvTorchModel, GraphConvModel
 from deepchem.models.torch_models.smiles2vec import Smiles2Vec, Smiles2VecModel
-from deepchem.models.torch_models.robust_multitask import RobustMultitask
+from deepchem.models.torch_models.robust_multitask import RobustMultitask, RobustMultitaskRegressor
 try:
     from deepchem.models.torch_models.dmpnn import DMPNN, DMPNNModel
     from deepchem.models.torch_models.gnn import GNN, GNNHead, GNNModular
