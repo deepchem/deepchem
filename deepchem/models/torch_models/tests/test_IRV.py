4b1e8b04b99701b9caf96139ea3051932250ba09
Harindhar10 <harindhar99@gmail.com>
2024-11-11 18:43:28 +0530

    irv changes
    flake8,doctest fixes

--- deepchem/models/torch_models/tests/test_IRV.py
+++ deepchem/models/torch_models/tests/test_IRV.py
@@ -6,18 +6,19 @@ import pickle
 import os
 import pytest
 
+
 @pytest.mark.torch
 def test_IRV_multitask_classification_overfit():
     """Test IRV classifier overfits tiny data."""
     n_tasks = 5
     n_samples = 10
     n_features = 128
-    K=5
-    
+    K = 5
+
     # Generate dummy dataset
     np.random.seed(123)
     ids = np.arange(n_samples)
-    X = np.random.randint(2, size=(n_samples, n_features))  
+    X = np.random.randint(2, size=(n_samples, n_features))
     y = np.ones((n_samples, n_tasks))
     w = np.ones((n_samples, n_tasks))
     dataset = dc.data.NumpyDataset(X, y, w, ids)
@@ -25,16 +26,16 @@ def test_IRV_multitask_classification_overfit():
     dataset_trans = IRV_transformer.transform(dataset)
     classification_metric = dc.metrics.Metric(dc.metrics.accuracy_score,
                                               task_averager=np.mean)
-    print(dataset_trans)
     model = dc.models.torch_models.MultitaskIRVClassifier(n_tasks,
-                                             K,
-                                             learning_rate=0.01,
-                                             batch_size=n_samples)
+                                                          K,
+                                                          learning_rate=0.01,
+                                                          batch_size=n_samples)
     model.fit(dataset_trans)
 
     # Eval model on train
     scores = model.evaluate(dataset_trans, [classification_metric])
     assert scores[classification_metric.name] > .9
+
 
 @pytest.mark.torch
 def test_IRV_multitask_classification_reload():
@@ -42,8 +43,8 @@ def test_IRV_multitask_classification_reload():
     n_tasks = 5
     n_samples = 20
     n_features = 128
-    K=5
-    
+    K = 5
+
     # Generate dummy dataset
     np.random.seed(123)
     ids = np.arange(n_samples)
@@ -58,10 +59,10 @@ def test_IRV_multitask_classification_reload():
                                               task_averager=np.mean)
     model_dir = tempfile.mkdtemp()
     model = dc.models.torch_models.MultitaskIRVClassifier(n_tasks,
-                                             K=5,
-                                             learning_rate=0.01,
-                                             batch_size=n_samples,
-                                             model_dir=model_dir)
+                                                          K=5,
+                                                          learning_rate=0.01,
+                                                          batch_size=n_samples,
+                                                          model_dir=model_dir)
 
     # Fit trained model
     model.fit(dataset_trans)
@@ -71,11 +72,12 @@ def test_IRV_multitask_classification_reload():
     assert scores[classification_metric.name] > .9
 
     # Reload Trained Model
-    reloaded_model = dc.models.torch_models.MultitaskIRVClassifier(n_tasks,
-                                                      K=5,
-                                                      learning_rate=0.01,
-                                                      batch_size=n_samples,
-                                                      model_dir=model_dir)
+    reloaded_model = dc.models.torch_models.MultitaskIRVClassifier(
+        n_tasks,
+        K=5,
+        learning_rate=0.01,
+        batch_size=n_samples,
+        model_dir=model_dir)
     reloaded_model.restore()
 
     # Check predictions match on random sample
@@ -89,14 +91,15 @@ def test_IRV_multitask_classification_reload():
     scores = reloaded_model.evaluate(dataset_trans, [classification_metric])
     assert scores[classification_metric.name] > .9
 
+
 @pytest.mark.torch
-def compare_tf_and_torch_output():
-    """Test IRV classifier overfits tiny data."""
+def test_IRV_multitask_classification_compare_with_tf_impl():
+    """Compare the ouputs of tensorflow and pytorch models"""
     n_tasks = 5
     n_samples = 10
     n_features = 128
-    K=5
-    
+    K = 5
+
     # Generate dummy dataset
     np.random.seed(123)
     ids = np.arange(n_samples)
@@ -106,19 +109,17 @@ def compare_tf_and_torch_output():
     dataset = dc.data.NumpyDataset(X, y, w, ids)
     IRV_transformer = dc.trans.IRVTransformer(K, n_tasks, dataset)
     dataset_trans = IRV_transformer.transform(dataset)
-    
+
     # Instantiate pytorch model
-    model_torch=dc.models.torch_models.MultitaskIRVClassifier(n_tasks,
-                                             K=5,
-                                             learning_rate=0.01,
-                                             batch_size=n_samples)    
-     
+    model_torch = dc.models.torch_models.MultitaskIRVClassifier(
+        n_tasks, K=5, learning_rate=0.01, batch_size=n_samples)
+
     tf_weights_dir = os.path.join(os.path.dirname(__file__), "assets",
-                                "IRV_tf_weights.pickle")
+                                  "IRV_tf_weights.pickle")
     # Load tensorflow weights
-    with open(tf_weights_dir,'rb') as f:
-        data=pickle.load(f)
-    
+    with open(tf_weights_dir, 'rb') as f:
+        data = pickle.load(f)
+
     # Copy tensorflow weights to pytorch model
     for name, param in model_torch.model.named_parameters():
         if "V" == name:
@@ -131,15 +132,17 @@ def compare_tf_and_torch_output():
             param.data.fill_(data[2][0])
         elif "b2" == name:
             param.data.fill_(data[3][0])
-    
+
     tf_output_dir = os.path.join(os.path.dirname(__file__), "assets",
-                                "IRV_tf_output.pickle")
+                                 "IRV_tf_output.pickle")
     # Load tensorflow output for comparison
-    with open(tf_output_dir,'rb') as f:
-        tf_output=pickle.load(f)
-    
+    with open(tf_output_dir, 'rb') as f:
+        tf_output = pickle.load(f)
+
     # Predict pytorch outputs
-    torch_output=model_torch.predict(dataset= dataset_trans,transformers=[IRV_transformer])
-    
+    torch_output = model_torch.predict(dataset=dataset_trans,
+                                       transformers=[IRV_transformer])
+
     # Compare pytorch and tensorflow outputs
-    assert np.allclose(tf_output, torch_output,rtol=1e-5, atol=1e-6), "Outputs do not match!"
+    assert np.allclose(tf_output, torch_output, rtol=1e-5,
+                       atol=1e-6), "Outputs do not match!"
