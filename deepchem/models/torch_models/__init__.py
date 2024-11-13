7cecb1516926e7d8116b01cb7d6e36275d98f6de
spellsharp <svcodin22@gmail.com>
2024-11-13 13:16:31 +0530

    Robust regressor pytorch class:

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -34,7 +34,7 @@ from deepchem.models.torch_models.flows import Flow, Affine, MaskedAffineFlow, A
 from deepchem.models.torch_models.unet import UNet, UNetModel
 from deepchem.models.torch_models.graphconvmodel import _GraphConvTorchModel, GraphConvModel
 from deepchem.models.torch_models.smiles2vec import Smiles2Vec, Smiles2VecModel
-from deepchem.models.torch_models.robust_multitask import RobustMultitask
+from deepchem.models.torch_models.robust_multitask import RobustMultitask, RobustMultitaskRegressor
 try:
     from deepchem.models.torch_models.dmpnn import DMPNN, DMPNNModel
     from deepchem.models.torch_models.gnn import GNN, GNNHead, GNNModular
