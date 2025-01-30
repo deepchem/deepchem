6d36b20a79162caf2e490968b8c036a2a4f078f5
try-er <avyarth00@gmail.com>
2025-01-30 12:14:21 +0530

    fixed formatting check errors

--- deepchem/models/torch_models/tests/test_lstm_generator_models.py
+++ deepchem/models/torch_models/tests/test_lstm_generator_models.py
@@ -1,6 +1,4 @@
-import deepchem as dc
 import pytest
-import numpy as np
 
 
 @pytest.mark.torch
@@ -17,6 +15,7 @@ def test_load_lstmneuralnet():
 
     _ = model(x)
 
+
 @pytest.mark.torch
 def test_default_generator():
     from deepchem.models.torch_models.lstm_generator_models import LSTMGenerator
@@ -27,39 +26,39 @@ def test_default_generator():
     dataset = NumpyDataset(["CCC"])
     gen_iter = generator.default_generator(dataset)
     value = list(gen_iter)[0]
-    assert torch.equal(value[0], torch.Tensor([[101, 21362, 1658]]))
-    assert torch.equal(value[1], torch.Tensor([[21362, 1658, 102]]))
+    assert torch.equal(value[0][0], torch.Tensor([[101, 21362, 1658]]))
+    assert torch.equal(value[1][0], torch.Tensor([[21362, 1658, 102]]))
+
 
 @pytest.mark.torch
 def test_fit_model():
     from deepchem.models.torch_models.lstm_generator_models import LSTMGenerator
     from deepchem.data import NumpyDataset
-    import torch
 
     generator = LSTMGenerator(model_dir="./assets/lstm_generator")
     dataset = NumpyDataset(["CCC"])
-    loss1, loss2 = generator.fit(dataset, checkpoint_interval=1, max_checkpoints_to_keep=1)
-    assert type(loss1) == float 
-    assert type(loss2) == float 
+    loss = generator.fit(dataset,
+                         checkpoint_interval=1,
+                         max_checkpoints_to_keep=1)
+    assert type(loss) is float
+
 
 @pytest.mark.torch
 def test_load_pretrained():
     from deepchem.models.torch_models.lstm_generator_models import LSTMGenerator
-    import torch
 
     generator = LSTMGenerator()
 
-    generator.load_from_pretrained("./assets/lstm_generator")
+    generator.load_from_pretrained(model_dir="./assets/lstm_generator")
     assert generator._built
+
 
 @pytest.mark.torch
 def test_sampling():
     from deepchem.models.torch_models.lstm_generator_models import LSTMGenerator
-    import torch
 
     generator = LSTMGenerator()
 
-    generator.load_from_pretrained("./assets/lstm_generator")
+    generator.load_from_pretrained(model_dir="./assets/lstm_generator")
     random_gens = generator.sample(3, max_len=10)
-    assert len(random_gens) == 3 
-
+    assert len(random_gens) == 3
