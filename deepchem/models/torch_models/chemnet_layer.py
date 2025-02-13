fdbcbd5bbf4020e8893c0d47d8b73c4d7f8f23b2
Dragonwagon18 <126950672+Dragonwagon18@users.noreply.github.com>
2025-02-14 01:43:47 +0530

    linting fixes

--- deepchem/models/torch_models/chemnet_layer.py
+++ deepchem/models/torch_models/chemnet_layer.py
@@ -1,40 +1,49 @@
 """
-Contains implementations of layers used in ChemCeption .
+Contains implementations of layers used in ChemCeption.
 """
+
 import torch
 import torch.nn as nn
 
 
 class Stem(nn.Module):
     """
-    Stem Layer as defined in https://arxiv.org/abs/1710.02238.
-    This layer downsamples the image to reduce computational complexity before passing it to deeper layers.
+    Implements the Stem layer from ChemCeption as described in the paper:
+    https://arxiv.org/abs/1710.02238.
+
+    This layer downsamples the input image to reduce computational complexity
+    before passing it to deeper layers.
     """
 
-    def __init__(self: "Stem", num_filters: int, input_shape: tuple,
-                 **kwargs) -> None:
+    def __init__(self, num_filters: int, input_shape: tuple) -> None:
         """
-        Parameters
-        ----------
-        num_filters: int
-            Number of convolutional filters.
-        input_shape: tuple
-            Shape of the input image in (H, W, C) format.
-            - For `img_spec="std"`, use input_shape=(H, W, 1)  (Grayscale image)
-            - For `img_spec="engd"`, use input_shape=(H, W, 4)  (Multi-channel image)
+        Initializes the Stem layer.
+
+        Args:
+            num_filters (int): Number of output convolutional filters.
+            input_shape (tuple): Shape of the input image as (C, H, W).
+                - For `img_spec="std"`, use (1, H, W)  (Grayscale image).
+                - For `img_spec="engd"`, use (4, H, W)  (Multi-channel image).
         """
-        super(Stem, self).__init__(**kwargs)
+        super().__init__()
+
         in_channels = input_shape[0]  # Extracts channels from (C, H, W)
 
         self.conv_layer = nn.Conv2d(in_channels=in_channels,
                                     out_channels=num_filters,
                                     kernel_size=4,
-                                    stride=2)  # No padding
+                                    stride=2)
         self.activation_layer = nn.ReLU()
 
-    def forward(self: "Stem", inputs: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the Stem layer.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (N, C, H, W).
+
+        Returns:
+            torch.Tensor: Output tensor after convolution and activation.
         """
-        conv1 = self.conv_layer(inputs)
-        return self.activation_layer(conv1)
+        x = self.conv_layer(x)
+        return self.activation_layer(x)
