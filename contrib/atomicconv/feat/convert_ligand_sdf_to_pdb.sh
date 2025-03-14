fa0e640d52e0eff100021b092bc99c2af7b2f466
Pragati5-DEBUG <pragativ005@gmail.com>
2025-03-14 12:13:34 +0530

    Add NormalizingFlow model to general_purpose_models.csv cheatsheet (fixes #3161)

--- contrib/atomicconv/feat/convert_ligand_sdf_to_pdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd v2015
-for x in */
-do
-  cd $x
-  babel -isdf ${x%/}_ligand.sdf -opdb ${x%/}_ligand.pdb
-  cd ../
-done
-cd ../
--- examples/datasets/pretty_print.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import deepchem as dc
-
-dataset = dc.data.NumpyDataset(np.random.rand(500, 5))
-print(dataset)
