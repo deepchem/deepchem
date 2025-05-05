57a0c40ab3bbb3a7699443d2ce1016348129e6d6
Debasish Mohanty <75255871+TRY-ER@users.noreply.github.com>
2025-05-06 00:07:21 +0530

    LSTM Sampler integration [ paper code @Debasish ] (#4246)
    * added code for lstm sampler
    
    * added test
    
    * added lstm model file
    
    * done porting: working on tests
    
    * added tests and done cleanup
    
    * added documentation for class and it's methods
    
    * added the documentation in .rst file and crosschecked types
    
    * rebased and test resources reduction
    
    * fixed doctest
    
    * added example for LSTMNeuralNet class
    
    * included dataset iteration replacing list input
    
    * updated the documentation
    
    * fixed formatting check errors
    
    * potential mypy fix
    
    * reapplying for the mypy fix
    
    * fixing unittests and raising to rerun the linting workflow
    
    * fixing unittests and raising to rerun the linting workflow
    
    * fixing mypy __
    
    * fixing signature issue in mypy with torchmodel
    
    * trying to fix mypy issue ___
    
    * mypy fix ____
    
    * fixing torch error for python3.8
    
    * fixing pytest error for python3.8 by explicitly managing input and outputshapes of inference
    
    * fixed doctest
    
    * removed the pre defined checkpoint in assets generated during pytest
    
    * highlighted our paper reference

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -35,6 +35,7 @@ from deepchem.models.torch_models.unet import UNet, UNetModel
 from deepchem.models.torch_models.graphconvmodel import _GraphConvTorchModel, GraphConvModel
 from deepchem.models.torch_models.smiles2vec import Smiles2Vec, Smiles2VecModel
 from deepchem.models.torch_models.robust_multitask import RobustMultitask
+from deepchem.models.torch_models.lstm_generator_models import LSTMNeuralNet, LSTMGenerator
 from deepchem.models.torch_models.hf_models import HuggingFaceModel
 from deepchem.models.torch_models.inceptionv3 import InceptionV3Model, InceptionA, InceptionB, InceptionC, InceptionD, InceptionE, InceptionAux, BasicConv2d
 from deepchem.models.torch_models.mobilenetv2 import MobileNetV2Model, InvertedResidual
