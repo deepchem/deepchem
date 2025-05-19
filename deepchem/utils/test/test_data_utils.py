df13f8e1a003b1526ffa941113cccc5afade1d85
Riya Singh <riyasingh3909@gmail.com>
2025-05-19 22:42:52 +0530

    add test

--- deepchem/utils/test/test_data_utils.py
+++ deepchem/utils/test/test_data_utils.py
@@ -1,6 +1,8 @@
 import unittest
 import os
-from deepchem.utils.data_utils import load_sdf_files
+import tempfile
+from openbabel import pybel
+from deepchem.utils.data_utils import load_sdf_files, convert_xyz_files_to_sdf
 
 
 class TestFileLoading(unittest.TestCase):
@@ -18,3 +20,28 @@ class TestFileLoading(unittest.TestCase):
         self.assertEqual(len(eval(df['pos_x'][0])), n_atoms_mol1)
         self.assertEqual(len(eval(df['pos_y'][0])), n_atoms_mol1)
         self.assertEqual(len(eval(df['pos_y'][0])), n_atoms_mol1)
+
+
+def test_no_absurd_nitrogen_charges():
+    # Create a temporary output SDF file
+    xyz_folder_path = './assets/xyz_test'
+    with tempfile.NamedTemporaryFile(suffix=".sdf", delete=False) as tmp_sdf:
+        sdf_output_file_path = tmp_sdf.name
+
+    try:
+        # Convert the XYZ files to SDF
+        convert_xyz_files_to_sdf(xyz_folder_path, sdf_output_file_path)
+
+        # Read the SDF file and validate nitrogen charges
+        for mol in pybel.readfile("sdf", sdf_output_file_path):
+            print()
+            for atom in mol.atoms:
+                print(atom, atom.formalcharge)
+                assert atom.formalcharge == 0
+                if atom.atomicnum == 7:
+                    charge = atom.formalcharge
+                    assert charge < 4, f"Nitrogen atom has absurd charge {charge} in molecule: {mol.title}"
+
+    finally:
+        # Clean up the temporary file
+        os.remove(sdf_output_file_path)
