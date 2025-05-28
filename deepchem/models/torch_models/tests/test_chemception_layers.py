548ab57ebb703d752e90d63d243755fccdae89bb
Dragonwagon18 <126950672+Dragonwagon18@users.noreply.github.com>
2025-05-28 06:40:01 +0530

    few changes in docstring

--- deepchem/models/torch_models/tests/test_chemception_layers.py
+++ deepchem/models/torch_models/tests/test_chemception_layers.py
@@ -10,8 +10,10 @@ except:
 
 @pytest.mark.torch
 def test_Stem():
-    """Test invoking stem layer."""
-
+    """
+    Test Stem layer against manually verified TensorFlow output
+    using fixed weights and bias.
+    """
     input_np = np.array([[[[1.0, 0.5, 0.2, 0.8], [0.3, 0.4, 0.7, 0.2],
                            [0.6, 0.2, 0.9, 0.5], [0.4, 0.1, 0.5, 0.7]],
                           [[0.8, 0.3, 0.6, 0.9], [0.2, 0.9, 0.6, 0.5],
