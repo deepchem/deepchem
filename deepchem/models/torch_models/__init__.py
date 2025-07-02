f3af352210e354bb76a46ff9bc170bf50fdc3503
Dragonwagon18 <126950672+Dragonwagon18@users.noreply.github.com>
2025-07-02 17:03:32 +0530

    Porting InceptionResnetC, ReductionA and ReductionB layer

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -50,6 +50,9 @@ from deepchem.models.torch_models.chemnet_layers import InceptionResnetC
 from deepchem.models.torch_models.chemnet_layers import ReductionA
 from deepchem.models.torch_models.chemnet_layers import ReductionB
 from deepchem.models.torch_models.hnn import HNN
+from deepchem.models.torch_models.chemnet_layers import InceptionResnetC
+from deepchem.models.torch_models.chemnet_layers import ReductionA
+from deepchem.models.torch_models.chemnet_layers import ReductionB
 try:
     from deepchem.models.torch_models.dmpnn import DMPNN, DMPNNModel
     from deepchem.models.torch_models.gnn import GNN, GNNHead, GNNModular
