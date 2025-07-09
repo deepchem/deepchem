77fdc3305798419d432b968e0deab949c58164ec
Harindhar10 <harindhar99@gmail.com>
2025-07-09 12:36:17 +0530

    formatting fix

--- deepchem/feat/tests/test_actinn_featurizer.py
+++ deepchem/feat/tests/test_actinn_featurizer.py
@@ -4,15 +4,14 @@ import pandas as pd
 import numpy as np
 
 
-
 class TestACTINNFeaturizer(unittest.TestCase):
+
     def test_actinn_compare_with_existing_implementation(self):
-        
+
         ref_array = np.load('actinn_feat_ref_compressed.npz')['arr_0']
         train_set = pd.read_hdf('actinn_sample_data.h5')
 
         featurizer = ACTINNFeaturizer()
         train_dataset = featurizer.featurize(train_set)
-        
-        assert np.allclose(train_dataset.X, ref_array, rtol=1e-5,
-                       atol=1e-6)
+
+        assert np.allclose(train_dataset.X, ref_array, rtol=1e-5, atol=1e-6)
