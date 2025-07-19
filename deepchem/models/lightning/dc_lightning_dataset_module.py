a7b13b7430bd9a87ddc14d3a1fc0827a5747fe62
Bhuvan M <bhuvanmdev@gmail.com>
2025-07-19 16:04:19 +0530

    init

--- deepchem/models/lightning/dc_lightning_dataset_module.py
+++ deepchem/models/lightning/dc_lightning_dataset_module.py
@@ -1,38 +1,29 @@
-from typing import Callable
+from typing import Callable, Optional
 import deepchem as dc
 import lightning as L
 import torch
-
-
-class DCLightningDatasetBatch:
-
-    def __init__(self, batch):
-        X = [batch[0]]
-        y = [batch[1]]
-        w = [batch[2]]
-        self.batch_list = [X, y, w]
-
-
-def collate_dataset_wrapper(batch):
-    return DCLightningDatasetBatch(batch)
+from deepchem.utils import collate_dataset_fn
+from deepchem.models.torch_models import TorchModel
 
 
 class DCLightningDatasetModule(L.LightningDataModule):
     """DeepChem Lightning Dataset Module to be used with the DCLightningModule and a Lightning trainer.
 
     This module wraps over the the deepchem pytorch dataset and dataloader providing a generic interface to run training.
-    https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
 
     Notes
     -----
-    This class requires PyTorch to be installed.
+    This class requires PyTorch and lightning to be installed.
+    For more information, see:
+      - PyTorch Lightning DataModule Documentation: https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
     """
 
     def __init__(self,
                  dataset: dc.data.Dataset,
                  batch_size: int,
-                 collate_fn: Callable = collate_dataset_wrapper,
-                 num_workers: int = 0):
+                 collate_fn: Optional[Callable] = None,
+                 num_workers: int = 0,
+                 model: Optional[TorchModel] = None):
         """Create a new DCLightningDatasetModule.
 
         Parameters
@@ -41,33 +32,67 @@ class DCLightningDatasetModule(L.LightningDataModule):
             A deepchem dataset.
         batch_size: int
             Batch size for the dataloader.
-        collate_fn: Callable
-            Method to collate instances across batch.
+        collate_fn: Optional[Callable], default None
+            Custom collate function. If None and model is provided, defaults to collate_dataset_fn.
         num_workers: int
             Number of workers to load data
+        model: Optional[TorchModel], default None
+            DeepChem model for collate function.
         """
         super().__init__()
         self._batch_size = batch_size
-        self._dataset = dataset.make_pytorch_dataset(
-            batch_size=self._batch_size)
-        self.collate_fn = collate_fn
+        self._dataset = dc.data._TorchIndexDiskDataset(dataset)
+        if collate_fn is None and model is not None:
+            self.collate_fn = lambda batch: collate_dataset_fn(batch_data=batch,
+                                                               model=model)
+        else:
+            self.collate_fn = collate_fn
         self.num_workers = num_workers
 
     def setup(self, stage):
-        self.train_dataset = self._dataset
+        """Set up datasets for each stage.
+
+        Parameters
+        ----------
+        stage: str
+            The stage to set up datasets for ('fit' or 'predict').
+        """
+        if stage == "fit":
+            self.train_dataset = self._dataset
+        elif stage == "predict":
+            self.predict_dataset = self._dataset
 
     def train_dataloader(self):
         """Returns the train dataloader from train dataset.
 
         Returns
         -------
-        dataloader: train dataloader to be used with DCLightningModule.
+        DataLoader
+            Pytorch DataLoader for train data.
         """
         dataloader = torch.utils.data.DataLoader(
             self.train_dataset,
-            batch_size=None,
+            batch_size=self._batch_size,
             collate_fn=self.collate_fn,
-            shuffle=False,
+            shuffle=True,
             num_workers=self.num_workers,
         )
+        return dataloader
+
+    def predict_dataloader(self):
+        """Return the prediction dataloader.
+
+        Returns
+        -------
+        DataLoader
+            PyTorch DataLoader for prediction data.
+        """
+        dataloader = torch.utils.data.DataLoader(
+            self.predict_dataset,
+            batch_size=self._batch_size,
+            collate_fn=self.collate_fn,
+            shuffle=False,  # Critical: never shuffle during prediction
+            num_workers=self.num_workers,
+            drop_last=True)
+
         return dataloader
