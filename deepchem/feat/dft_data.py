fix backpropagation

deepchem/feat/dft_data.py
@@ -242,7 +242,7 @@ class _EntryDM(DFTEntry):
 
     def get_val(self, qcs: List[KSCalc]) -> np.ndarray:
         val = qcs[0].aodmtot()
-        return np.array([val.tolist()])
+        return val.unsqueeze(0)
 
 
 class _EntryDens(DFTEntry):
@@ -291,7 +291,7 @@ class _EntryDens(DFTEntry):
         grid = self.get_integration_grid()
         rgrid = grid.get_rgrid()
         val = qc.dens(rgrid)
-        return np.array(val.tolist())
+        return val
 
     def get_integration_grid(self) -> BaseGrid:
         """
@@ -359,7 +359,7 @@ class _EntryIE(DFTEntry):
         e_1 = [m.energy() for m in qcs]
         e = [item1 * item2 for item1, item2 in zip(systems, e_1)]
         val = sum(e) - 2 * e[0]
-        return np.array([val.tolist()])
+        return val.unsqueeze(0)
 
 
 class _EntryAE(_EntryIE):

deepchem/models/dft/dftxc.py
@@ -71,18 +71,19 @@ class DFTXC(torch.nn.Module):
             Calculated value of the data point after running the Kohn Sham iterations
             using the neural network XC functional.
         """
-        hybridxc = HybridXC(self.xcstr, self.nnmodel, aweight0=0.0)
+        hybridxc = HybridXC(self.xcstr, self.nnmodel, aweight0=0.5)
         output = []
         for entry in inputs:
             evl = XCNNSCF(hybridxc, entry)
             qcs = []
             for system in entry.get_systems():
                 qcs.append(evl.run(system))
+            # get_val now returns torch tensors with gradients attached
+            val = entry.get_val(qcs)
             if entry.entry_type == 'dm':
-                output.append((torch.as_tensor(entry.get_val(qcs)[0])))
+                output.append(val[0])
             else:
-                output.append(
-                    torch.tensor(entry.get_val(qcs), requires_grad=True))
+                output.append(val)
         return output
 
 