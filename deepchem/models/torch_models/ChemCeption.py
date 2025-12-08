d6f0999fcf23a8c751038a0786dea53b82e4069a
José Antonio Sigüenza <80292371+JoseAntonioSiguenza@users.noreply.github.com>
2025-12-08 12:42:16 -0700

    Fixes for CI `unit-tests` (#4582)
    * unit-tests CI fixes
    
    * revert deepvariant changes, fix weave_torch docstests
    
    ---------
    
    Co-authored-by: Jose Siguenza <jose-siguenza@berkeley.edu>

--- deepchem/models/torch_models/ChemCeption.py
+++ deepchem/models/torch_models/ChemCeption.py
@@ -38,6 +38,7 @@ class ChemCeption(nn.Module):
     -------
     >>> import numpy as np
     >>> import torch
+    >>> import deepchem as dc
     >>> from deepchem.models.torch_models.ChemCeption import ChemCeption
     >>> base_filters = 16
     >>> img_size = 80
@@ -45,7 +46,7 @@ class ChemCeption(nn.Module):
     >>> n_classes = 2
     >>> model = ChemCeption(img_spec="std", img_size=img_size, base_filters=base_filters, inception_blocks={"A": 3, "B": 3, "C": 3}, n_tasks=n_tasks, n_classes=n_classes, augment=False, mode="classification")
     >>> smiles = ['CC(=O)OC1=CC=CC=C1C(=O)O']
-    >>> featurizer = deepchem.feat.SmilesToImage(img_size=80, img_spec='std')
+    >>> featurizer = dc.feat.SmilesToImage(img_size=80, img_spec='std')
     >>> images = featurizer.featurize(smiles)
     >>> image = torch.tensor(images, dtype=torch.float32)
     >>> image = image.permute(0, 3, 1, 2)  # Convert to NCHW format
