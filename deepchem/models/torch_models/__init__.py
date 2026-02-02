a6b61063e484db791b09d71919fc7c8fedb6db10
marq <classicvector63@gmail.com>
2026-02-02 20:46:22 +0530

    Implement Differentiable Kalman Filter in PyTorch

--- deepchem/models/torch_models/__init__.py
+++ deepchem/models/torch_models/__init__.py
@@ -52,7 +52,7 @@ from deepchem.models.torch_models.hnn import HNN, HNNModel
 from deepchem.models.torch_models.ChemCeption import ChemCeption
 from deepchem.models.torch_models.fno import FNO, FNOModel
 from deepchem.models.torch_models.lnn import LNN, LNNModel
-
+from deepchem.models.torch_models.kalman_filter import KalmanFilter
 try:
     from deepchem.models.torch_models.dmpnn import DMPNN, DMPNNModel
     from deepchem.models.torch_models.gnn import GNN, GNNHead, GNNModular
