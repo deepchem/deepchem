d9a228616723e86295c6acf54320a0af6e25a739
Zrahay <shubhangsinha44@gmail.com>
2026-07-07 19:03:09 +0530

    Adding Genomic tests to conftest

--- deepchem/models/torch_models/tests/conftest.py
+++ deepchem/models/torch_models/tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import numpy as np
 import deepchem as dc
 import pandas as pd
 
@@ -63,4 +64,44 @@ def protein_classification_dataset(tmpdir):
                                feature_field="protein",
                                featurizer=dc.feat.DummyFeaturizer())
     dataset = loader.create_dataset(filepath)
+    return dataset
+
+
+@pytest.fixture
+def genomic_regression_dataset():
+    sequences = [
+        "ATGCGTACGTTAGCTAGCATGCGTACG",
+        "GGCTAACCGTATCGGATCAAGTCCTAG",
+        "TTAAGCCGTACGATCGATCGATCGATCG",
+        "CCGATCGATCGATCGATCGATCGATCGA",
+        "ATCGATCGATCGATCGATCGATCGATCG",
+        "GCTAGCTAGCTAGCTAGCTAGCTAGCTA",
+        "AATTCCGGAATTCCGGAATTCCGGAATT",
+        "CCGGTTAACCGGTTAACCGGTTAACCGG",
+        "TTAGGCCAATTAGGCCAATTAGGCCAAT",
+        "GGCCAATTGGCCAATTGGCCAATTGGCC",
+    ]
+    np.random.seed(42)
+    y = np.random.rand(10, 1)
+    dataset = dc.data.NumpyDataset(X=np.array(sequences), y=y)
+    return dataset
+
+
+@pytest.fixture
+def genomic_multitask_regression_dataset():
+    sequences = [
+        "ATGCGTACGTTAGCTAGCATGCGTACG",
+        "GGCTAACCGTATCGGATCAAGTCCTAG",
+        "TTAAGCCGTACGATCGATCGATCGATCG",
+        "CCGATCGATCGATCGATCGATCGATCGA",
+        "ATCGATCGATCGATCGATCGATCGATCG",
+        "GCTAGCTAGCTAGCTAGCTAGCTAGCTA",
+        "AATTCCGGAATTCCGGAATTCCGGAATT",
+        "CCGGTTAACCGGTTAACCGGTTAACCGG",
+        "TTAGGCCAATTAGGCCAATTAGGCCAAT",
+        "GGCCAATTGGCCAATTGGCCAATTGGCC",
+    ]
+    np.random.seed(42)
+    y = np.random.rand(10, 2)
+    dataset = dc.data.NumpyDataset(X=np.array(sequences), y=y)
     return dataset
